f46b20775b564bb3065e23d1b5781ae4e10d2523
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2018-06-13 13:32:01 +0200

    🐳(build) dockerize marsha
    We get Ubuntu and Alpine docker images for production
    and development, docker compose files and make targets
    for easy use.
    
    This work is mostly a copy of @jmaupetit's work on
    github.com/openfun/richie

---
+++ docker/files/usr/local/etc/gunicorn/marsha.py
@@ -0,0 +1,18 @@
+"""Gunicorn configuration file for marsha."""
+
+# Gunicorn-django settings
+bind = ["0.0.0.0:8000"]
+name = "marsha"
+python_path = "/app"
+
+# Run
+graceful_timeout = 90
+timeout = 90
+workers = 3
+
+# Logging
+# Using '-' for the access log file makes gunicorn log accesses to stdout
+accesslog = "-"
+# Using '-' for the error log file makes gunicorn log errors to stderr
+errorlog = "-"
+loglevel = "info"
