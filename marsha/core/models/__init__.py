06d157e8e7c2b0e61d58dacaa15d1d01e5a7682b
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2018-07-05 18:40:15 +0200

    ♻️(models) reorganize models inside a directory
    This avoids having an akward "base_models.py" file
    at the root of the app. We take this opportunity to
    split models in 2 groups: account and video.

---
+++ marsha/core/models/__init__.py
@@ -0,0 +1,4 @@
+"""Make all models available from marsha.core.models."""
+# pylint: disable=wildcard-import,unused-wildcard-import
+from .account import *  # noqa isort:skip
+from .video import *  # noqa isort:skip
