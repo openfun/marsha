5734521cd039b7f2a533fdab4585045b2896ac1c
ProCAN Group <wajdi.louati@procan-group.com>
2018-07-16 16:31:01 +0100

    Update urls.py

--- marsha/lti_provider/urls.py
+++ marsha/lti_provider/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 
-from marsha.lti_provider.views import LTIConfigView, LTILandingPage, LTIFailAuthorization,LTIRoutingView,AddVideo
+from marsha.lti_provider.views import LTILandingPage, LTIFailAuthorization,LTIRoutingView,AddVideo
 
 
 urlpatterns = [
-    url(r'^config.xml$', LTIConfigView.as_view(), {}, 'lti-config'),
     url(r'^auth$', LTIFailAuthorization.as_view(), {}, 'lti-fail-auth'),
     url(r'^landing/$', LTILandingPage.as_view(), {}, 'lti-landing-page'),
     url(r'^$', LTIRoutingView.as_view(), {}, 'lti-login'),
