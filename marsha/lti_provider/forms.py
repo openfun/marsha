1cb42cc2b355052146d3d012bb7bb7aaa69643ac
ProCAN Group <wajdi.louati@procan-group.com>
2018-07-23 19:06:12 +0100

    Update forms.py

--- marsha/lti_provider/forms.py
+++ marsha/lti_provider/forms.py
@@ -4,10 +4,7 @@ from marsha.core.models.video import Video
 
 
 class VideoForm(ModelForm):
-
-    """
-    A video form that should be filled by the instructor in order to create or modify a video.
-    """
+    """A video form that should be filled by the instructor in order to create or modify a video."""
 
     class Meta:
 
