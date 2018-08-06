a37171b9eec4751930bd6ead407fef51b65ffd32
Mehdi Benadda <me@mbenadda.com>
2018-08-06 17:14:30 +0200

    🔧(front) set up build environment for frontend payload
    Set up our frontend build pipeline, revolving around typescript +
    babel + webpack to build our react app into a bundle.
    This is mostly reusing config from openfun/richie, to get this up
    and running faster and to make it easy to work accross our project
    by sharing the general infrastructure.

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+
+module.exports = {
+  // Disable production-specific optimizations by default
+  // They can be re-enabled by running the cli with `--mode=production` or making a separate
+  // webpack config for production.
+  mode: 'development',
+
+  // Include whatwg fetch as an entry point (and not an import) as it's replacing (when necessary)
+  // a globally available browser-provided function
+  entry: ['./front/index.tsx'],
+  output: {
+    filename: 'index.js',
+    path: __dirname + '/data/static/js',
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: true,
+            },
+          },
+          { loader: 'ts-loader' },
+        ],
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
+};
