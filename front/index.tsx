2474e16f72dd162bf51b76e83a815d8a9fea060e
Mehdi Benadda <me@mbenadda.com>
2018-08-10 00:49:47 +0200

    ✨(front) set up a context to manage user access rights
    By using a react context to pass around the current user's
    role (and therefore access rights) we enable components, however
    deep in the tree they may be, to easily access it and self-manage
    what they should and should not display and what actions they
    should allow the user to perform.

--- front/index.tsx
+++ front/index.tsx
@@ -4,11 +4,15 @@ import { IntlProvider } from 'react-intl';
 
 import { RootComponent } from './components/RootComponent/RootComponent';
 
+export const UserContext = React.createContext('user');
+
 // Wait for the DOM to load before we scour it for an element that requires React to render
 document.addEventListener('DOMContentLoaded', event => {
   ReactDOM.render(
     <IntlProvider>
-      <RootComponent />
+      <UserContext.Provider value="student">
+        <RootComponent />
+      </UserContext.Provider>
     </IntlProvider>,
     document.querySelector('#marsha-frontend-root'),
   );
