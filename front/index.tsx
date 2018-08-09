af8f3b99a537d3ab80fc1faf09852519a52e4b45
Mehdi Benadda <me@mbenadda.com>
2018-08-10 00:27:54 +0200

    ✨(front) wrap RootComponent into the react-intl Provider
    Wrapping our whole component tree into The IntlProvider from
    react-intl will enable each of our components to use intl's
    translation facilities and receive the actual translations
    once we have set them up.

--- front/index.tsx
+++ front/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
+
 import { RootComponent } from './components/RootComponent/RootComponent';
 
 // Wait for the DOM to load before we scour it for an element that requires React to render
 document.addEventListener('DOMContentLoaded', event => {
   ReactDOM.render(
-    <RootComponent />,
+    <IntlProvider>
+      <RootComponent />
+    </IntlProvider>,
     document.querySelector('#marsha-frontend-root'),
   );
 });
