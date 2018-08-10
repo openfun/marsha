e4ba89a3adff1a3e93a4d7f8a90a064614d1252e
Mehdi Benadda <me@mbenadda.com>
2018-08-10 10:45:00 +0200

    ✨(front) add a video-js player component
    Make a simple React Wrapper for video-js to easily set it up
    and use it in our component tree.
    Note: it uses a hardcoded video name for now, we'll be updating
    that later on to come from a prop and handle dynamic changes.

---
+++ front/components/VideoJsPlayer/VideoJsPlayer.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import videojs from 'video.js';
+
+import { Nullable } from '../../utils/types';
+
+// tslint:disable-next-line:no-empty-interface
+export interface VideoJsPlayerProps extends videojs.PlayerOptions {}
+
+interface VideoJsPlayerState {
+  player: videojs.Player;
+  videoNode: Nullable<HTMLVideoElement>;
+}
+
+export class VideoJsPlayer extends React.Component<
+  VideoJsPlayerProps,
+  VideoJsPlayerState
+> {
+  videoNodeRef: Nullable<HTMLVideoElement> = null;
+
+  componentDidMount() {
+    // Instantiate Video.js and keep the instance in state
+    this.setState({
+      player: videojs(this.videoNodeRef, this.props),
+    });
+  }
+
+  componentWillUnmount() {
+    // Destroy player on unmount
+    if (this.state.player) {
+      this.state.player.dispose();
+    }
+  }
+
+  render() {
+    // Wrap the player in a div with a `data-vjs-player` attribute so videojs won't create an additional
+    // wrapper in the DOM; see https://github.com/videojs/video.js/pull/3856
+    return (
+      <div data-vjs-player>
+        <video
+          ref={node => (this.videoNodeRef = node)}
+          className="video-js"
+          controls={true}
+        >
+          <source src="/static/vid/video.mp4" type="video/mp4" />
+        </video>
+      </div>
+    );
+  }
+}
