6f1d67f619e16de49a2cbaf33febf0c513affece
Mehdi Benadda <me@mbenadda.com>
2018-08-14 15:50:34 +0200

    ♻️(terraform) make sure lambda js code passes prettier
    All the lambda-related JS code we're adding to the project was built
    in another project that did not enforce prettier formatting.
    
    Run prettier over all the invalid files & take this opportunity to
    use arrow functions and drop some unecessary punctuation.

--- terraform/source/configure/media-convert.js
+++ terraform/source/configure/media-convert.js
@@ -1,79 +1,74 @@
-"use strict";
-const fs = require("fs");
-const AWS = require("aws-sdk");
+'use strict';
+
+const fs = require('fs');
+const AWS = require('aws-sdk');
 
 const presets = [
-  "./presets/jpeg_144p.json",
-  "./presets/jpeg_240p.json",
-  "./presets/jpeg_480p.json",
-  "./presets/jpeg_720p.json",
-  "./presets/jpeg_1080p.json",
-  "./presets/mp4_h264_144p_30fps_300kbps.json",
-  "./presets/mp4_h264_240p_30fps_600kbps.json",
-  "./presets/mp4_h264_480p_30fps_1200kbps.json",
-  "./presets/mp4_h264_720p_30fps_2400kbps.json",
-  "./presets/mp4_h264_1080p_30fps_5400kbps.json"
+  './presets/jpeg_144p.json',
+  './presets/jpeg_240p.json',
+  './presets/jpeg_480p.json',
+  './presets/jpeg_720p.json',
+  './presets/jpeg_1080p.json',
+  './presets/mp4_h264_144p_30fps_300kbps.json',
+  './presets/mp4_h264_240p_30fps_600kbps.json',
+  './presets/mp4_h264_480p_30fps_1200kbps.json',
+  './presets/mp4_h264_720p_30fps_2400kbps.json',
+  './presets/mp4_h264_1080p_30fps_5400kbps.json',
 ];
 
 let response;
 
 // Return MediaConvert regional account Endpoint
-let createEndPoint = function() {
+let createEndPoint = () => {
   const mediaconvert = new AWS.MediaConvert();
-  response = new Promise((res, reject) => {
-    mediaconvert.describeEndpoints(function(error, data) {
+  return new Promise((resolve, reject) => {
+    mediaconvert.describeEndpoints((error, data) => {
       if (error) reject(error);
-      else res({ EndpointUrl: data.Endpoints[0].Url });
+      else resolve({ EndpointUrl: data.Endpoints[0].Url });
     });
   });
-  return response;
 };
 
 // Create Custom MediaConvert presets
-let createPreset = function(preset, url) {
+let createPreset = (preset, url) => {
   const mediaconvert = new AWS.MediaConvert({
     endpoint: url,
-    region: process.env.AWS_REGION
+    region: process.env.AWS_REGION,
   });
-  return new Promise(function(res, reject) {
-    let params = JSON.parse(fs.readFileSync(preset, "utf8"));
+  return new Promise((resolve, reject) => {
+    let params = JSON.parse(fs.readFileSync(preset, 'utf8'));
     // TODO: update does not work
-    mediaconvert.createPreset(params, function(error, data) {
+    mediaconvert.createPreset(params, (error, data) => {
       if (error) reject(error);
       else {
-        console.log("created preset: ", data.Preset.Name);
-        res(data);
+        console.log('created preset: ', data.Preset.Name);
+        resolve(data);
       }
     });
   });
 };
 
-let createPresets = function(event) {
-  response = new Promise((res, reject) => {
+let createPresets = event => {
+  return new Promise((resolve, reject) => {
     let promises = [];
     let url = event.EndPoint;
 
-    presets.forEach(function(preset) {
-      promises.push(createPreset(preset, url));
-    });
+    presets.forEach(preset => promises.push(createPreset(preset, url)));
     Promise.all(promises)
       .then(data => {
         // Return a list of the names of the presets that we created
-        res({
+        resolve({
           Presets: data.reduce((list, item) => {
             list.push(item.Preset.Name);
             return list;
-          }, [])
+          }, []),
         });
       })
-      .catch(error => {
-        reject(error);
-      });
+      .catch(error => reject(error));
   });
-  return response;
 };
 
 module.exports = {
   MediaConvertEndPoint: createEndPoint,
-  MediaConvertPresets: createPresets
+  MediaConvertPresets: createPresets,
 };
--- terraform/source/encode/index.js
+++ terraform/source/encode/index.js
@@ -1,14 +1,15 @@
-"use strict";
-const AWS = require("aws-sdk");
+'use strict';
+
+const AWS = require('aws-sdk');
 
 exports.handler = (event, context, callback) => {
-  console.log("Received event:", JSON.stringify(event, null, 2));
+  console.log('Received event:', JSON.stringify(event, null, 2));
 
-  let objectKey = event.Records[0].s3.object.key;
+  const objectKey = event.Records[0].s3.object.key;
 
-  if (objectKey.split("/").length != 2) {
+  if (objectKey.split('/').length != 2) {
     let error =
-      "Source videos should be uploaded in the folder of their playlist.";
+      'Source videos should be uploaded in the folder of their playlist.';
     callback(error);
     return;
   }
@@ -17,108 +18,109 @@ exports.handler = (event, context, callback) => {
     Role: process.env.MEDIA_CONVERT_ROLE,
     UserMetadata: {
       Bucket: event.Records[0].s3.bucket.name,
-      ObjectKey: objectKey
+      ObjectKey: objectKey,
     },
     Settings: {
       AdAvailOffset: 0,
       Inputs: [
         {
-          FilterEnable: "AUTO",
-          PsiControl: "USE_PSI",
+          FilterEnable: 'AUTO',
+          PsiControl: 'USE_PSI',
           FilterStrength: 0,
-          DeblockFilter: "DISABLED",
-          DenoiseFilter: "DISABLED",
-          TimecodeSource: "EMBEDDED",
+          DeblockFilter: 'DISABLED',
+          DenoiseFilter: 'DISABLED',
+          TimecodeSource: 'EMBEDDED',
           VideoSelector: {
-            ColorSpace: "FOLLOW"
+            ColorSpace: 'FOLLOW',
           },
           AudioSelectors: {
-            "Audio Selector 1": {
+            'Audio Selector 1': {
               Offset: 0,
-              DefaultSelection: "DEFAULT",
-              ProgramSelection: 1
-            }
+              DefaultSelection: 'DEFAULT',
+              ProgramSelection: 1,
+            },
           },
-          FileInput: "s3://" + event.Records[0].s3.bucket.name + "/" + objectKey
-        }
+          FileInput:
+            's3://' + event.Records[0].s3.bucket.name + '/' + objectKey,
+        },
       ],
       OutputGroups: [
         {
-          CustomName: "Video MP4 outputs",
-          Name: "File Group",
+          CustomName: 'Video MP4 outputs',
+          Name: 'File Group',
           OutputGroupSettings: {
-            Type: "FILE_GROUP_SETTINGS",
+            Type: 'FILE_GROUP_SETTINGS',
             FileGroupSettings: {
               Destination:
-                "s3://" +
+                's3://' +
                 process.env.S3_DESTINATION_BUCKET +
-                "/" +
-                objectKey.split("/")[0] +
-                "/mp4/"
-            }
+                '/' +
+                objectKey.split('/')[0] +
+                '/mp4/',
+            },
           },
           Outputs: [
             {
-              Preset: "marsha_mp4_144",
-              NameModifier: "_144"
+              Preset: 'marsha_mp4_144',
+              NameModifier: '_144',
             },
             {
-              Preset: "marsha_mp4_240",
-              NameModifier: "_240"
+              Preset: 'marsha_mp4_240',
+              NameModifier: '_240',
             },
             {
-              Preset: "marsha_mp4_480",
-              NameModifier: "_480"
+              Preset: 'marsha_mp4_480',
+              NameModifier: '_480',
             },
             {
-              Preset: "marsha_mp4_720",
-              NameModifier: "_720"
+              Preset: 'marsha_mp4_720',
+              NameModifier: '_720',
             },
             {
-              Preset: "marsha_mp4_1080",
-              NameModifier: "_1080"
-            }
-          ]
+              Preset: 'marsha_mp4_1080',
+              NameModifier: '_1080',
+            },
+          ],
         },
         {
-          CustomName: "Frame Capture outputs",
-          Name: "File Group",
+          CustomName: 'Frame Capture outputs',
+          Name: 'File Group',
           OutputGroupSettings: {
-            Type: "FILE_GROUP_SETTINGS",
+            Type: 'FILE_GROUP_SETTINGS',
             FileGroupSettings: {
               Destination:
-                "s3://" +
+                's3://' +
                 process.env.S3_DESTINATION_BUCKET +
-                "/" +
-                objectKey.split("/")[0] +
-                "/jpeg/"
-            }
+                '/' +
+                objectKey.split('/')[0] +
+                '/jpeg/',
+            },
           },
           Outputs: [
             {
-              Preset: "marsha_jpeg_144",
-              NameModifier: "_144"
+              Preset: 'marsha_jpeg_144',
+              NameModifier: '_144',
             },
             {
-              Preset: "marsha_jpeg_240",
-              NameModifier: "_240"
+              Preset: 'marsha_jpeg_240',
+              NameModifier: '_240',
             },
             {
-              Preset: "marsha_jpeg_480",
-              NameModifier: "_480"
+              Preset: 'marsha_jpeg_480',
+              NameModifier: '_480',
             },
             {
-              Preset: "marsha_jpeg_720",
-              NameModifier: "_720"
+              Preset: 'marsha_jpeg_720',
+              NameModifier: '_720',
             },
             {
-              Preset: "marsha_jpeg_1080",
-              NameModifier: "_1080"
-            }
-          ]
-        }
-      ]
-    }
+              Preset: 'marsha_jpeg_1080',
+              NameModifier: '_1080',
+            },
+          ],
+        },
+      ],
+    },
   };
 
   new AWS.MediaConvert({ endpoint: process.env.MEDIA_CONVERT_END_POINT })
