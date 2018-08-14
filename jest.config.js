a4cc245a92681510e42d5f2ca4b09f9735811739
Mehdi Benadda <me@mbenadda.com>
2018-08-14 15:57:06 +0200

    🔧(front) don't run lambda tests using Jest
    Lambda JS tests have been written for a node/mocha testing
    environment. Don't run them using our frontend test runner.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js'],
-  testMatch: ['**/*.spec.+(ts|tsx|js)'],
+  testMatch: [__dirname + '/front/**/*.spec.+(ts|tsx|js)'],
   testURL: 'https://localhost',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
