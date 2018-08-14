7c31ab37a411825536b838f954e1eee3e8e1381c
Mehdi Benadda <me@mbenadda.com>
2018-08-14 15:50:34 +0200

    ♻️(terraform) make sure lambda js code passes prettier
    All the lambda-related JS code we're adding to the project was built
    in another project that did not enforce prettier formatting.
    
    Run prettier over all the invalid files & take this opportunity to
    use arrow functions and drop some unecessary punctuation.

--- terraform/source/configure/media-convert.spec.js
+++ terraform/source/configure/media-convert.spec.js
@@ -1,44 +1,43 @@
-"use strict";
-let expect = require("chai").expect;
-var path = require("path");
-let AWS = require("aws-sdk-mock");
-AWS.setSDK(path.resolve("./node_modules/aws-sdk"));
+'use strict';
 
-let lambda = require("./media-convert.js");
+const expect = require('chai').expect;
+const path = require('path');
+const AWS = require('aws-sdk-mock');
+AWS.setSDK(path.resolve('./node_modules/aws-sdk'));
 
-describe("Media Convert", function() {
-  afterEach(function() {
-    AWS.restore("MediaConvert");
-  });
+const lambda = require('./media-convert.js');
 
-  it("should return data when getting endpoint", function(done) {
-    let data = {
-      Endpoints: [{ Url: "https://test.mediaconvert.eu-west-1.amazonaws.com" }]
+describe('Media Convert', () => {
+  afterEach(() => AWS.restore('MediaConvert'));
+
+  it('should return data when getting endpoint', done => {
+    const data = {
+      Endpoints: [{ Url: 'https://test.mediaconvert.eu-west-1.amazonaws.com' }],
     };
 
-    AWS.mock("MediaConvert", "describeEndpoints", function(callback) {
-      callback(null, data);
-    });
+    AWS.mock('MediaConvert', 'describeEndpoints', callback =>
+      callback(null, data),
+    );
 
     lambda.MediaConvertEndPoint().then(data => {
       expect(data.EndpointUrl).to.equal(
-        "https://test.mediaconvert.eu-west-1.amazonaws.com"
+        'https://test.mediaconvert.eu-west-1.amazonaws.com',
       );
       done();
     });
   });
 
-  it("should return data when creating presets", function(done) {
-    let event = {
-      EndPoint: "https://test.mediaconvert.eu-west-1.amazonaws.com"
+  it('should return data when creating presets', done => {
+    const event = {
+      EndPoint: 'https://test.mediaconvert.eu-west-1.amazonaws.com',
     };
 
     // Mock the function that create presets and return a different name
     // for the preset time it is called
     let callCount = 0;
-    AWS.mock("MediaConvert", "createPreset", function(params, callback) {
-      callback(null, { Preset: { Name: callCount++ } });
-    });
+    AWS.mock('MediaConvert', 'createPreset', (params, callback) =>
+      callback(null, { Preset: { Name: callCount++ } }),
+    );
 
     lambda
       .MediaConvertPresets(event)
@@ -46,8 +45,6 @@ describe("Media Convert", function() {
         expect(data).to.deep.equal({ Presets: Array.from(Array(10).keys()) });
         done();
       })
-      .catch(err => {
-        done(err);
-      });
+      .catch(err => done(err));
   });
 });
