7a676d449aa86afed074e20bc7085e4584780af2
Mehdi Benadda <me@mbenadda.com>
2018-08-30 16:32:59 +0200

    🔧(front) enable testing of css-importing modules
    Some of our modules do or will import css. This is handled by
    webpack through extensions for product code but causes issues when
    Jest attemps to parse it as Javascript.
    
    The simple solution is to provide a stub through config so a
    side-effect-free empty module is loaded instead.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,10 @@ module.exports = {
       useBabelrc: true,
     },
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'css'],
+  moduleNameMapper: {
+    '\\.(css)$': '<rootDir>/front/__mocks__/styleMock.js',
+  },
   testMatch: [__dirname + '/front/**/*.spec.+(ts|tsx|js)'],
   testURL: 'https://localhost',
   transform: {
