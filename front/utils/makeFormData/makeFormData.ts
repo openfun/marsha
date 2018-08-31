41774c5ef620070706810b252f6128ea5a4abe01
Mehdi Benadda <me@mbenadda.com>
2018-08-31 18:30:10 +0200

    ✨(front) make a simple wrapper to create FormData instances
    We need to make a FormData factory to enable us to test functions
    that use/return FormData instances. They are complex to evaluate
    and compare. Using this factory, we can just mock it, check its input
    and stub its output.
    Make a cleaner interface for our factory than the mutating one FormData
    has by default.

---
+++ front/utils/makeFormData/makeFormData.ts
@@ -0,0 +1,11 @@
+// This formData wrapper has 2 purposes:
+//   - provide a level of indirection that lets us write unit tests
+//   - use a cleaner API than calling formData.append N times manually
+export const makeFormData = (
+  // NB: order of keys is important here, which is why we do not iterate over an object
+  ...keyValuePairs: Array<[string, string | File]>
+) => {
+  const formData = new FormData();
+  keyValuePairs.forEach(([key, value]) => formData.append(key, value));
+  return formData;
+};
