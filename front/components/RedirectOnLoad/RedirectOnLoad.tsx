e85fd6483df44dfa4673799217cfa7ea8fc9bc23
Mehdi Benadda <me@mbenadda.com>
2018-09-05 16:45:35 +0200

    ✨(front) use frontend URL fragments from variables
    Using strings directly wherever we define a route or link/redirect to
    them can cause issues when some of the URL fragments change or when
    we decide to translate them.
    Using functions returning strings instead gives us a simple API that
    manages these issues and will enable us to add a helper to enable
    URL localization later on if we need to.

--- front/components/RedirectOnLoad/RedirectOnLoad.tsx
+++ front/components/RedirectOnLoad/RedirectOnLoad.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import { Redirect } from 'react-router-dom';
 
 import { AppDataContext } from '../..';
+import { ROUTE as ERROR_ROUTE } from '../ErrorComponent/ErrorComponent';
+import { ROUTE as FORM_ROUTE } from '../VideoForm/VideoForm';
+import { ROUTE as PLAYER_ROUTE } from '../VideoJsPlayer/VideoJsPlayer';
+
+export const ROUTE = () => '/';
 
 // RedirectOnLoad assesses the initial state of the application using appData and determines the proper
 // route to load in the Router
@@ -13,20 +18,20 @@ export const RedirectOnLoad = () => {
 
         // Get LTI errors out of the way
         if (state === 'error') {
-          return <Redirect push to="/errors/lti" />;
+          return <Redirect push to={ERROR_ROUTE('lti')} />;
         }
         // Everyone gets the video when it exists (so that instructors see the iframes like a student would by default)
         else if (video.status === 'ready') {
-          return <Redirect push to="/player" />;
+          return <Redirect push to={PLAYER_ROUTE()} />;
         }
         // Only instructors are allowed to interact with a non-ready video
         else if (state === 'instructor') {
-          return <Redirect push to="/form" />;
+          return <Redirect push to={FORM_ROUTE()} />;
         }
         // For safety default to the 404 view: this is for students, and any other role we add later on and don't add
         // a special clause for, when the video is not ready.
         else {
-          return <Redirect push to="/errors/notFound" />;
+          return <Redirect push to={ERROR_ROUTE('notFound')} />;
         }
       }}
     </AppDataContext.Consumer>
