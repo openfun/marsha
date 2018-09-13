1613163c042bb8e3d15042c4e81c98162fa23bf3
Mehdi Benadda <me@mbenadda.com>
2018-09-13 19:00:33 +0200

    💄(front) add a styled compatible CSS reboot
    We need to use a CSS reboot to start from a clean slate and minimize unwanted
    differences between browsers.
    
    We picked Bootstrap's as it's pretty vanilla and achieves the look and feel we
    want. styled-reboot simply ports it to styled-components so we don't have to try
    and load it separately.

--- front/index.tsx
+++ front/index.tsx
@@ -4,6 +4,7 @@ import { IntlProvider } from 'react-intl';
 
 import { AppRoutes } from './components/AppRoutes/AppRoutes';
 import { parseDataElements } from './utils/parseDataElements/parseDataElements';
+import { baseStyles } from './utils/theme/baseStyles';
 
 const appData = parseDataElements(
   // Spread to pass an array instead of a NodeList
@@ -15,6 +16,9 @@ export const AppDataContext = React.createContext(appData);
 
 // Wait for the DOM to load before we scour it for an element that requires React to render
 document.addEventListener('DOMContentLoaded', event => {
+  // Load our style reboot into the DOM
+  baseStyles();
+  // Render our actual component tree
   ReactDOM.render(
     <IntlProvider>
       <AppDataContext.Provider value={appData}>
