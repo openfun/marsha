4ff3de2e9f2d6c5988f446cffa33dc20bdbb6447
Mehdi Benadda <me@mbenadda.com>
2018-09-14 10:57:50 +0200

    💄(front) add some basic styling to error pages
    For now we use the same ErrorComponent with the same layout
    to display all our errors. Add some basic styling to center the
    content & use proper typography.
    
    As we're using a specific H2, write similar styled components for
    other heading levels.

---
+++ front/components/Headings/Headings.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+import { colors } from '../../utils/theme/theme';
+
+const headingsStyle = `
+  margin-bottom: 0.5rem;
+  font-family: inherit;
+  font-weight: 500;
+  line-height: 1.2;
+  color: inherit;
+`;
+
+export const H1 = styled.h1`
+  font-size: 2.5rem;
+  ${headingsStyle};
+`;
+
+export const H2 = styled.h2`
+  font-size: 2rem;
+  ${headingsStyle};
+`;
+
+export const H3 = styled.h3`
+  font-size: 1.75rem;
+  ${headingsStyle};
+`;
+
+export const H4 = styled.h4`
+  font-size: 1.5rem;
+  ${headingsStyle};
+`;
+
+export const H5 = styled.h5`
+  font-size: 1.25rem;
+  ${headingsStyle};
+`;
+
+export const H6 = styled.h6`
+  font-size: 1rem;
+  ${headingsStyle};
+`;
+
+export const IframeHeading = styled(H2)`
+  padding: 0.5rem 0;
+`;
