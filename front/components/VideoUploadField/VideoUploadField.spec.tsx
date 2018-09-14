98ba8e318a2ffdb3167add1ffde895eb35268d84
Mehdi Benadda <me@mbenadda.com>
2018-09-14 14:06:47 +0200

    ✨(front) improve the UI for the VideoUploadField
    We want our VideoUpload field to emphasize that it works
    as a feature and users can just drag-and-drop files onto it,
    and still keep a real button for the file input for accessibility.
    
    VideoUploadField is mostly a functional container, DropzonePlaceholder
    is the visual aspect of the dropzone including the button, and
    DropzoneIcon is an SVG icon for the dropzone.
    
    We added reusable styles for a Button component as we needed a styled
    button for the DropzonePlaceholder.

--- front/components/VideoUploadField/VideoUploadField.spec.tsx
+++ front/components/VideoUploadField/VideoUploadField.spec.tsx
@@ -2,40 +2,18 @@ import '../../testSetup';
 
 import { mount } from 'enzyme';
 import * as React from 'react';
-import { IntlProvider } from 'react-intl';
 
 import { VideoUploadField } from './VideoUploadField';
 
 test('VideoUploadField renders a Dropzone with the relevant messages', () => {
   const wrapper = mount(<VideoUploadField onContentUpdated={jest.fn()} />);
-
-  expect(wrapper.html()).toContain('dropzone');
-  expect(wrapper.text()).toContain('Pick a video to upload');
-  expect(wrapper.text()).not.toContain('Clear selected file');
+  expect(wrapper.text()).toContain('Select a video to upload');
 });
 
-test('VideoUploadField.onDrop() passes the file to the callback and updates the UI', () => {
+test('VideoUploadField.onDrop() passes the file to the callback', () => {
   const callback = jest.fn();
   const wrapper = mount(<VideoUploadField onContentUpdated={callback} />);
   const componentInstance = wrapper.instance() as VideoUploadField;
   componentInstance.onDrop(['file_1']);
-
   expect(callback).toHaveBeenCalledWith('file_1');
-  expect(wrapper.text()).not.toContain('Pick a video to upload');
-  expect(wrapper.text()).toContain('Clear selected file');
-  expect(wrapper.html()).toContain('aria-disabled="true"');
-});
-
-test('VideoUploadField.clearFile() calls the callback with undefined and resets the UI', () => {
-  const callback = jest.fn();
-  const wrapper = mount(<VideoUploadField onContentUpdated={callback} />);
-  const componentInstance = wrapper.instance() as VideoUploadField;
-  componentInstance.onDrop(['file_1']);
-  callback.mockReset();
-  componentInstance.clearFile();
-
-  expect(callback).toHaveBeenCalledWith(undefined);
-  expect(wrapper.text()).toContain('Pick a video to upload');
-  expect(wrapper.text()).not.toContain('Clear selected file');
-  expect(wrapper.html()).toContain('aria-disabled="false"');
 });
