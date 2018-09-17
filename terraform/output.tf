97be78aaca9c44ec10b7dd9b7689ff737db589e9
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2018-09-17 17:43:05 +0200

    ✨(terraform) add necessary outputs to Terraform
    We need to output the data that Marsha will need:
    access key id and secret key for the AWS account
    used by Marsha to upload videos. The Terraform
    module that creates the state bucket also needs to
    output the KMS key id that the main Terraform module
    uses to encrypt its state.

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,11 @@ output "endpoint" {
 output "presets" {
   value = "${data.aws_lambda_invocation.configure_lambda_presets.result}"
 }
+
+output "iam_access_key_id" {
+  value = "${aws_iam_access_key.marsha_access_key.id}"
+}
+
+output "iam_secret_access_key" {
+  value = "${aws_iam_access_key.marsha_access_key.secret}"
+}
