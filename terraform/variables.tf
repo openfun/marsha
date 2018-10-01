55d6e5152fa0a1ad2fbdd182722fa6d9581227be
Mehdi Benadda <me@mbenadda.com>
2018-10-01 18:08:40 +0200

    ✨(terraform) add environment variables for the confirm lambda
    In order to implement the confirm lambda (that calls a Django
    route when processing of a video is complete), we need it to
    have access to the Django endpoint URL, and a shared secret to
    sign messages.
    
    We can just pass the endpoint URL as an environment variable. As
    for the shared secret, we're generating it from a deployment
    stamp (using terraform's random provider) and adding it to our
    outputs so it is not lost to us. It's the deployer's responsibility
    to pass it to Django as an environment variable.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,11 @@ variable "aws_region" {
   type    = "string"
   default = "eu-west-1"
 }
+
+variable "upload_confirm_endpoint" {
+  type    = "string"
+}
+
+variable "deployment_stamp" {
+  type    = "string"
+}
