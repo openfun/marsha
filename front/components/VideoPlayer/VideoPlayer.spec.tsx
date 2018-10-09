199e15ea1d8abd0fed6689cd66eee23a82ef242d
Mehdi Benadda <me@mbenadda.com>
2018-10-09 17:43:28 +0200

    ✨(front) enable adaptive bitrate in VideoPlayer using shaka
    Now that we generate all the file we need for adaptive bitrate
    streaming, we can add HLS to our sources (which is automatically
    picked up by the iPhone native player) and use shaka to stream
    using DASH ISO when available.

--- front/components/VideoPlayer/VideoPlayer.spec.tsx
+++ front/components/VideoPlayer/VideoPlayer.spec.tsx
@@ -3,9 +3,15 @@ import '../../testSetup';
 import { mount, shallow } from 'enzyme';
 import Plyr from 'plyr';
 import * as React from 'react';
+import shaka from 'shaka-player';
 
 import { Video } from '../../types/Video';
 import { VideoPlayer } from './VideoPlayer';
+
+const mockShakaPlayer: jest.Mocked<typeof shaka.Player> = shaka.Player as any;
+const mockShakaPolyfill: jest.Mocked<
+  typeof shaka.polyfill
+> = shaka.polyfill as any;
 
 describe('VideoPlayer', () => {
   const video = {
@@ -14,6 +20,10 @@ describe('VideoPlayer', () => {
     state: 'ready',
     title: 'Some title',
     urls: {
+      manifests: {
+        dash: 'https://example.com/dash.mpd',
+        hls: 'https://example.com/hls.m3u8',
+      },
       mp4: {
         144: 'https://example.com/144p.mp4',
         1080: 'https://example.com/1080p.mp4',
@@ -21,9 +31,14 @@ describe('VideoPlayer', () => {
     },
   } as Video;
 
+  beforeEach(() => jest.clearAllMocks());
+
   it('renders the video element with all the relevant sources', () => {
     const wrapper = shallow(<VideoPlayer video={video} />);
 
+    expect(wrapper.html()).toContain(
+      '<source src="https://example.com/hls.m3u8" type="application/vnd.apple.mpegURL"/>',
+    );
     expect(wrapper.html()).toContain(
       '<source size="144" src="https://example.com/144p.mp4" type="video/mp4"/>',
     );
@@ -43,5 +58,31 @@ describe('VideoPlayer', () => {
     ).instance() as VideoPlayer;
     instance.componentWillUnmount();
     expect(instance.state.player!.destroy).toHaveBeenCalled();
+  });
+
+  describe('in an EME enabled environment', () => {
+    beforeEach(() => mockShakaPlayer.isBrowserSupported.mockReturnValue(true));
+
+    it('instantiates shaka Player and loads our dash manifest', () => {
+      mount(<VideoPlayer video={video} />);
+
+      expect(mockShakaPlayer.prototype.constructor).toHaveBeenCalledWith(
+        expect.any(HTMLMediaElement),
+      );
+      expect(mockShakaPolyfill.installAll).toHaveBeenCalled();
+      expect(mockShakaPlayer.prototype.configure).toHaveBeenCalled();
+      expect(mockShakaPlayer.prototype.load).toHaveBeenCalledWith(
+        'https://example.com/dash.mpd',
+      );
+    });
+  });
+
+  describe('in an environment without EME', () => {
+    beforeEach(() => mockShakaPlayer.isBrowserSupported.mockReturnValue(true));
+
+    it('never instantiates shaka', () => {
+      expect(mockShakaPlayer.prototype.constructor).not.toHaveBeenCalled();
+      expect(mockShakaPlayer.prototype.load).not.toHaveBeenCalled();
+    });
   });
 });
--- front/components/VideoPlayer/VideoPlayer.tsx
+++ front/components/VideoPlayer/VideoPlayer.tsx
@@ -1,6 +1,7 @@
 import Plyr from 'plyr';
 import 'plyr/dist/plyr.css';
 import * as React from 'react';
+import shaka from 'shaka-player';
 
 import { Video, videoSize } from '../../types/Video';
 import { Maybe, Nullable } from '../../utils/types';
@@ -29,8 +30,28 @@ export class VideoPlayer extends React.Component<
   }
 
   componentDidMount() {
+    const { video } = this.props;
+
     // Instantiate Plyr and keep the instance in state
     this.setState({ player: new Plyr(this.videoNodeRef!) });
+
+    // Use Shaka Player to stream using DASH ISO if the browser supports it
+    // Otherwise we'll fall back to HLS/MP4 as described in the <sources> of the <video> element
+    if (shaka.Player.isBrowserSupported()) {
+      // Install any necessary polyfills from the shaka package
+      shaka.polyfill.installAll();
+      // Instantiate the player, passing our <video> element
+      const shakaInstance = new shaka.Player(this.videoNodeRef!);
+      shakaInstance.configure({
+        abr: {
+          // Set a more sensible default bandwidth
+          // This lets us start with 480p instead of 144p with the default value
+          defaultBandwidthEstimate: 1600000,
+        },
+      });
+      // Pass our DASH manifest to Shaka
+      shakaInstance.load(video.urls.manifests.dash);
+    }
   }
 
   componentWillUnmount() {
@@ -42,8 +63,14 @@ export class VideoPlayer extends React.Component<
 
   render() {
     const { video } = this.props;
+
     return (
       <video ref={node => (this.videoNodeRef = node)} crossOrigin="anonymous">
+        <source
+          src={video.urls.manifests.hls}
+          size="auto"
+          type="application/vnd.apple.mpegURL"
+        />
         {(Object.keys(video.urls.mp4) as videoSize[]).map(size => (
           <source
             key={video.urls.mp4[size]}
