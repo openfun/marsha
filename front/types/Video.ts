44120371700c76fe2fe0714189207c8db396d959
Mehdi Benadda <me@mbenadda.com>
2018-10-09 17:46:15 +0200

    ♻️(front) update Video type to conform to actual values
    The Video type needs to undergo two changes to conform to what is
    actually implemented on the backend & API:
    - add a manifests property for DASH ISO & HLS playback
    - narrow down state from "string" to just the possible values

--- front/types/Video.ts
+++ front/types/Video.ts
@@ -1,11 +1,21 @@
 export type videoSize = '144' | '240' | '480' | '720' | '1080';
 
+export enum videoState {
+  ERROR = 'error',
+  PENDING = 'pending',
+  READY = 'ready',
+}
+
 export interface Video {
   description: string;
   id: string;
-  state: string;
+  state: videoState;
   title: string;
   urls: {
+    manifests: {
+      dash: string;
+      hls: string;
+    };
     mp4: { [key in videoSize]: string };
     thumbnails: { [key in videoSize]: string };
   };
