ddac287d207449bc7d10721a74217ad9b58006f2
Mehdi Benadda <me@mbenadda.com>
2018-10-10 14:41:12 +0200

    ♻️(lambda) make updateState lambda code easier to reuse
    We're currently lacking an acceptable way to reuse code accross
    lambdas (apart from having them call each other, which incurs
    infrastructure costs)
    
    As a result, we've decide to just duplicate the code we need
    until we have some time to implement a better solution. Here,
    we're separating the part of the code we want to duplicate so
    it's easier to copy it and to refactor once we figure out how to
    reuse code.

--- terraform/source/update_state/index.spec.js
+++ terraform/source/update_state/index.spec.js
@@ -1,30 +1,20 @@
 'use strict';
 
-const endpoint = 'https://example.com/update-state-endpoint/';
-process.env.ENDPOINT = endpoint;
-process.env.SHARED_SECRET = 'some secret';
-
-// Use a stub to mock calls to the network
-const requestStub = jest.fn();
-jest.mock('request-promise-native', () => requestStub);
+const mockUpdateState = jest.fn();
+jest.doMock('./src/updateState', () => mockUpdateState);
 
 // Don't pollute tests with logs intended for CloudWatch
-jest.spyOn(console, 'log');
+jest.spyOn(console, 'log').mockReset();
 
 const lambda = require('./index.js').handler;
 
-describe('Confirm', () => {
-  beforeEach(() => {
-    requestStub.mockReset();
-    console.log.mockReset();
-  });
-
-  it('calculates the signature and posts the new state to the server', async () => {
+describe('lambda', () => {
+  it('calls updateState with "ready" when the task was "COMPLETE" and triggers the callback', async () => {
     const callback = jest.fn();
     const event = {
       detail: {
         userMetadata: {
-          ObjectKey: 'successful object key',
+          ObjectKey: 'some object key',
         },
         status: 'COMPLETE',
       },
@@ -32,21 +22,11 @@ describe('Confirm', () => {
 
     await lambda(event, null, callback);
 
-    expect(requestStub).toHaveBeenCalledWith({
-      body: {
-        key: 'successful object key',
-        signature:
-          'c39021355124558ee9300e4874c357c68dad40374b299eab747d5449b7572308',
-        state: 'ready',
-      },
-      json: true,
-      method: 'POST',
-      uri: endpoint,
-    });
-    expect(callback).toHaveBeenCalled();
+    expect(mockUpdateState).toHaveBeenCalledWith('some object key', 'ready');
+    expect(callback).toHaveBeenCalledWith();
   });
 
-  it('posts an error state when transcoding has failed', async () => {
+  it('calls updateState with "error" when the task was not "COMPLETE and triggers the callback', async () => {
     const callback = jest.fn();
     const event = {
       detail: {
@@ -59,21 +39,11 @@ describe('Confirm', () => {
 
     await lambda(event, null, callback);
 
-    expect(requestStub).toHaveBeenCalledWith({
-      body: {
-        key: 'failed object key',
-        signature:
-          '59e04fa0b3b72bb529d88d40d96bd6c306d8d7a6963d9ae9b912da5fdba6f25f',
-        state: 'error',
-      },
-      json: true,
-      method: 'POST',
-      uri: endpoint,
-    });
-    expect(callback).toHaveBeenCalled();
+    expect(mockUpdateState).toHaveBeenCalledWith('failed object key', 'error');
+    expect(callback).toHaveBeenCalledWith();
   });
 
-  it('triggers the callback with the error when the POST fails', async () => {
+  it('triggers the callback with an error when updateState failed', async () => {
     const callback = jest.fn();
     const event = {
       detail: {
@@ -83,12 +53,17 @@ describe('Confirm', () => {
         status: 'COMPLETE',
       },
     };
-    requestStub.mockImplementation(
-      () => new Promise((resolve, reject) => reject('Failed!')),
+
+    mockUpdateState.mockImplementation(
+      () => new Promise((resolve, reject) => reject('Failed to updateState!')),
     );
 
     await lambda(event, null, callback);
 
-    expect(callback).toHaveBeenCalledWith('Failed!');
+    expect(mockUpdateState).toHaveBeenCalledWith(
+      'object key that will fail to update',
+      'ready',
+    );
+    expect(callback).toHaveBeenCalledWith('Failed to updateState!');
   });
 });
---
+++ terraform/source/update_state/src/updateState.spec.js
@@ -0,0 +1,40 @@
+const endpoint = 'https://example.com/update-state-endpoint/';
+process.env.ENDPOINT = endpoint;
+process.env.SHARED_SECRET = 'some secret';
+
+// Use a stub to mock calls to the network
+const requestStub = jest.fn();
+jest.doMock('request-promise-native', () => requestStub);
+
+// Don't pollute tests with logs intended for CloudWatch
+jest.spyOn(console, 'log').mockReset();
+
+const updateState = require('./updateState');
+
+describe('src/updateState()', () => {
+  it('calculates the signature and posts the new state to the server', async () => {
+    await updateState('successful object key', 'ready');
+
+    expect(requestStub).toHaveBeenCalledWith({
+      body: {
+        key: 'successful object key',
+        signature:
+          'c39021355124558ee9300e4874c357c68dad40374b299eab747d5449b7572308',
+        state: 'ready',
+      },
+      json: true,
+      method: 'POST',
+      uri: endpoint,
+    });
+  });
+
+  it('rejects when the request fails', async () => {
+    requestStub.mockImplementation(
+      () => new Promise((resolve, reject) => reject('Failed!')),
+    );
+
+    await expect(
+      updateState('successful object key', 'COMPLETE'),
+    ).rejects.toEqual('Failed!');
+  });
+});
