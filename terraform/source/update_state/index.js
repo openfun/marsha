53729845ef84a3891d7af30d625df4ce98e8b7eb
Mehdi Benadda <me@mbenadda.com>
2018-10-10 14:41:12 +0200

    ♻️(lambda) make updateState lambda code easier to reuse
    We're currently lacking an acceptable way to reuse code accross
    lambdas (apart from having them call each other, which incurs
    infrastructure costs)
    
    As a result, we've decide to just duplicate the code we need
    until we have some time to implement a better solution. Here,
    we're separating the part of the code we want to duplicate so
    it's easier to copy it and to refactor once we figure out how to
    reuse code.

--- terraform/source/update_state/index.js
+++ terraform/source/update_state/index.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const request = require('request-promise-native');
-const crypto = require('crypto');
-
-const { ENDPOINT, SHARED_SECRET } = process.env;
+const updateState = require('./src/updateState');
 
 exports.handler = async (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
@@ -11,26 +8,10 @@ exports.handler = async (event, context, callback) => {
   const key = event.detail.userMetadata.ObjectKey;
   const state = event.detail.status === 'COMPLETE' ? 'ready' : 'error';
 
-  const hmac = crypto.createHmac('sha256', SHARED_SECRET);
-  hmac.update(key);
-  const signature = hmac.digest('hex');
-
   try {
-    await request({
-      body: {
-        key,
-        signature,
-        state,
-      },
-      json: true,
-      method: 'POST',
-      uri: ENDPOINT,
-    });
-
-    console.log(`Confirmed processing for ${key}. New state is (${state}).`);
+    await updateState(key, state);
     callback();
   } catch (error) {
-    console.log('into error');
     callback(error);
   }
 };
