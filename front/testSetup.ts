98d93a28d30496bed12db24d78446ea27f374b21
Mehdi Benadda <me@mbenadda.com>
2018-10-19 17:34:19 +0200

    ➕(front) add styled-components helpers for Jest
    "jest-styled-components" includes helpers for Jest testing. Some of
    them are about snapshots (which we're not using right now) but the
    most interesting one for us is "toHaveStyleRule".
    
    This lets us check variable CSS props which change depending on
    input/context. We can also use that to ensure some of the most
    important CSS rules are applied later on.

--- front/testSetup.ts
+++ front/testSetup.ts
@@ -3,6 +3,9 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+// Jest helpers for styled-components
+import 'jest-styled-components';
+
 // Utility function to allow flushing of all outstanding (completed) promises
 // Use it like this :
 //    const asyncValue = someFunction();
