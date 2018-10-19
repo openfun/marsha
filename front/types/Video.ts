b90cdbf5dfcaf8821dee0acd4a14c2a8f0ebfc64
Mehdi Benadda <me@mbenadda.com>
2018-10-19 17:38:09 +0200

    ✨(front) expand video state list to include UPLOADING & PROCESSING
    PROCESSING is a new state that exists on the server side along with
    PENDING, READY and ERROR. We need to add it here to have the complete
    list.
    
    As for UPLOADING, it does not exist on the backend side, as during
    upload the model value for the state is still PENDING. However, for
    us on the frontend, they are two different states of the app & the
    interface.
    
    We can therefore add it to simplify our code and make it easier to
    work with those states. This state will not actually be applied on
    any Video but will be used as a stand-in wherever we're passing
    video state without the full video and need to represent this state.

--- front/types/Video.ts
+++ front/types/Video.ts
@@ -1,11 +1,28 @@
+/** Possible sizes for a video file or stream. Used as keys in lists of files. */
 export type videoSize = '144' | '240' | '480' | '720' | '1080';
 
+/** Possible states for a video.
+ *
+ * NB: PENDING, PROCESSING, READY and ERROR are the actual possible values
+ * for the state field on a video record.
+ *
+ * UPLOADING does not exist as a video state on the backend side, as during
+ * upload the model value for the state is still PENDING. However, here
+ * on the frontend, they are two different states as far as the user is concerned.
+ *
+ * We add it in to make it easier to work with those states. It should not actually be
+ * applied on any Video but will be used as a stand-in wherever we're passing
+ * video state without the full video and need to represent this state.
+ */
 export enum videoState {
   ERROR = 'error',
   PENDING = 'pending',
+  PROCESSING = 'processing',
   READY = 'ready',
+  UPLOADING = 'uploading',
 }
 
+/** A Video record as it exists on the backend. */
 export interface Video {
   description: string;
   id: string;
