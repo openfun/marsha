e97ed90d94dca5d028636e935b6a919a4c0df6d5
Manuel Raynaud <manu@raynaud.io>
2018-11-09 15:51:41 +0100

    ♻️(front) use an Enum for app state name
    to reflect changes made in our code base, an enum is used to represent
    all the state the app can have. This will avoid typo and mistake.

--- src/frontend/types/AppData.ts
+++ src/frontend/types/AppData.ts
@@ -2,11 +2,17 @@ import { Nullable } from '../utils/types';
 import { AWSPolicy } from './AWSPolicy';
 import { Video } from './Video';
 
+export enum appState {
+  ERROR = 'error',
+  INSTRUCTOR = 'instructor',
+  STUDENT = 'student',
+}
+
 export interface AppData {
   jwt: string;
   policy?: AWSPolicy;
   resourceLinkid: string;
-  state: 'error' | 'instructor' | 'student';
+  state: appState;
   video: Nullable<Video>;
 
   updateVideo?: (video: Video) => void;
