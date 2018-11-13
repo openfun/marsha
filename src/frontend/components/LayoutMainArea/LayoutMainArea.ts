2ad64805af2ca15459c0cced7e889bc0ffc4355c
Mehdi Benadda <me@mbenadda.com>
2018-11-13 19:10:57 +0100

    ✨(front) rework frontend layout to work with iframe-resizer
    iframe-resizer works by measuring the contents of the iframe and
    adapting the iframe to its measurements. Obviously, this cannot
    bring us any results when our layours are built to occupy 100%
    of the iframe height all the time.
    
    We can instead base them on an aspect ratio. As we're working with
    video, we can take 16/9 as a starting point, which we can easily
    achieve through the use of the `vw` unit for both width and height.
    
    This gives our layouts are fixed height, which iframe-resizer can
    then use as a target to adapt the iframe itself.

---
+++ src/frontend/components/LayoutMainArea/LayoutMainArea.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const LayoutMainArea = styled.div`
+  /**
+   * 100/56.25 == 16/9
+   * We use this ratio as we base marsha's layout on video display
+   */
+  width: 100vw;
+  height: 56.25vw;
+`;
