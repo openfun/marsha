07d3e7c5c17d6fce07d326277da9676894fda9bf
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2018-11-13 11:17:43 +0100

    🐛(aws) fix selecting region for S3 buckets
    The S3 buckets were not taking into account the region passed in
    the environment. We should declare it explicitly in the bucket
    definition.

--- src/aws/create_state_bucket/s3.tf
+++ src/aws/create_state_bucket/s3.tf
@@ -5,6 +5,7 @@ resource "aws_kms_key" "state_key" {
 resource "aws_s3_bucket" "state_bucket" {
   bucket = "marsha-terraform"
   acl    = "private"
+  region = "${var.aws_region}"
 
   versioning {
     enabled = true
