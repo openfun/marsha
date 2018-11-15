a55bf5d265a2de663b0d5a81e95aaac4c7659f27
Mehdi Benadda <me@mbenadda.com>
2018-11-15 18:47:27 +0100

    🔊(lambda) add logs before update-state requests are fired
    The `encode` and `update-state` lambdas fire requests to the Django
    backend, using a shared secret to authenticate the requests. Those
    requests are *not* supposed to fail unless the Djago server is down
    or having issues.
    
    Add logs of the outgoing requests' method, URL and body to help
    with debugging when they do fail.

--- src/aws/lambda-update-state/src/updateState.spec.js
+++ src/aws/lambda-update-state/src/updateState.spec.js
@@ -7,13 +7,28 @@ const requestStub = jest.fn();
 jest.doMock('request-promise-native', () => requestStub);
 
 // Don't pollute tests with logs intended for CloudWatch
-jest.spyOn(console, 'log').mockReset();
+const mockConsoleLog = jest.spyOn(console, 'log');
 
 const updateState = require('./updateState');
 
-describe('src/updateState()', () => {
+describe('lambda/update_state/src/updateState()', () => {
+  beforeEach(mockConsoleLog.mockReset);
+
   it('calculates the signature and posts the new state to the server', async () => {
     await updateState('successful object key', 'ready');
+
+    const anteLogArgument = mockConsoleLog.mock.calls[0][0];
+    expect(anteLogArgument).toContain(`Updating state: POST ${endpoint}`);
+    expect(anteLogArgument).toContain('successful object key');
+    expect(anteLogArgument).toContain(
+      'c39021355124558ee9300e4874c357c68dad40374b299eab747d5449b7572308',
+    );
+    expect(anteLogArgument).toContain('ready');
+
+    const postLogArgument = mockConsoleLog.mock.calls[1][0];
+    expect(postLogArgument).toEqual(
+      `Updated successful object key. New state is (ready).`,
+    );
 
     expect(requestStub).toHaveBeenCalledWith({
       body: {
@@ -33,8 +48,16 @@ describe('src/updateState()', () => {
       () => new Promise((resolve, reject) => reject('Failed!')),
     );
 
-    await expect(
-      updateState('successful object key', 'COMPLETE'),
-    ).rejects.toEqual('Failed!');
+    await expect(updateState('failed object key', 'ready')).rejects.toEqual(
+      'Failed!',
+    );
+
+    const anteLogArgument = mockConsoleLog.mock.calls[0][0];
+    expect(anteLogArgument).toContain(`Updating state: POST ${endpoint}`);
+    expect(anteLogArgument).toContain('failed object key');
+    expect(anteLogArgument).toContain(
+      '59e04fa0b3b72bb529d88d40d96bd6c306d8d7a6963d9ae9b912da5fdba6f25f',
+    );
+    expect(anteLogArgument).toContain('ready');
   });
 });
