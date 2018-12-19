d952f84d9b36ad60c820d8fd3d15bfba0a058425
Mehdi Benadda <me@mbenadda.com>
2018-12-19 18:06:32 +0100

    ✨(api) add a schema view
    We're currently missing a schema route on the API. It's useful to
    get access to choices for some fields that can be dynamically changed
    on the backend side.
    
    Use Django-Rest-Framework's builtin tool to autogenerate a schema and
    expose it on an API endpoint.

---
+++ src/backend/marsha/core/tests/test_api_schema.py
@@ -0,0 +1,14 @@
+"""Tests for the Schema endpoint on the API of the Marsha project."""
+import json
+
+from django.test import TestCase
+
+
+class SchemaAPITest(TestCase):
+    """Test the API route for the schema."""
+
+    def test_api_schema(self):
+        """The API has a schema route that answers."""
+        response = self.client.get("/api/schema")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content)["_meta"]["title"], "Marsha API")
