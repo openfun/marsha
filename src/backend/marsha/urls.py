2db061c0d1a1e93b1c9ff2645e63f9898da53646
Mehdi Benadda <me@mbenadda.com>
2018-12-19 18:06:32 +0100

    ✨(api) add a schema view
    We're currently missing a schema route on the API. It's useful to
    get access to choices for some fields that can be dynamically changed
    on the backend side.
    
    Use Django-Rest-Framework's builtin tool to autogenerate a schema and
    expose it on an API endpoint.

--- src/backend/marsha/urls.py
+++ src/backend/marsha/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from marsha.core.admin import admin_site
 from marsha.core.api import TimedTextTrackViewSet, VideoViewSet, update_state
@@ -20,6 +21,7 @@ urlpatterns = [
     path(f"{admin_site.name}/", admin_site.urls),
     path("lti-video/", VideoLTIView.as_view(), name="lti_video"),
     path("api/update-state", update_state, name="update_state"),
+    path("api/schema", get_schema_view(title="Marsha API"), name="schema"),
     path("api/", include(router.urls)),
 ]
 
