aecf1486a5a3fa7c627249806d196e13bb66e91a
Manuel Raynaud <manu@raynaud.io>
2018-12-31 16:32:54 +0100

    ♻️(xapi) use a class to manage xAPI statements
    A class is the right way to manage xAPi statements instead of having
    several function repeating some code.

---
+++ src/frontend/types/XAPI.ts
@@ -0,0 +1,57 @@
+export type contextExtensionsKey =
+  | 'length'
+  | 'ccSubtitleEnabled'
+  | 'ccSubtitleLanguage'
+  | 'completionTreshold'
+  | 'frameRate'
+  | 'fullScreen'
+  | 'quality'
+  | 'screenSize'
+  | 'videoPlaybackSize'
+  | 'speed'
+  | 'track'
+  | 'userAgent'
+  | 'volume';
+
+/************* Played event  *************/
+export interface PlayedResultExtensions {
+  time: number;
+}
+
+/*************** Initialized event ************/
+export interface InitializedContextExtensions {
+  length: number;
+  ccSubtitleEnabled?: boolean;
+  ccSubtitleLanguage?: string;
+  completionTreshold?: number;
+  frameRate?: number;
+  fullScreen?: boolean;
+  quality?: string;
+  screenSize?: string;
+  videoPlaybackSize?: string;
+  speed?: string;
+  track?: string;
+  userAgent?: string;
+  volume?: number;
+}
+
+export interface DataPayload {
+  context?: {
+    extensions: {
+      [key: string]: string | boolean | number | undefined;
+    };
+  };
+  id?: string;
+  timestamp?: string;
+  result?: {
+    extensions: {
+      [key: string]: string | boolean | number | undefined;
+    };
+  };
+  verb: {
+    id: string;
+    display: {
+      [key: string]: string;
+    };
+  };
+}
