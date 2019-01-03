eba673e597fee450a6da5fb6a05503f37dbb9f3b
Manuel Raynaud <manu@raynaud.io>
2019-01-03 11:30:02 +0100

    ✨(plyr) bind event on xapi interacted statement
    Multiple event event can be bind to the interacted statement.

--- src/frontend/Player/createPlyrPlayer.spec.ts
+++ src/frontend/Player/createPlyrPlayer.spec.ts
@@ -28,5 +28,62 @@ describe('createPlyrPlayer', () => {
       'playing',
       expect.any(Function),
     );
+    expect(player.on).toHaveBeenNthCalledWith(3, 'pause', expect.any(Function));
+    expect(player.on).toHaveBeenNthCalledWith(
+      4,
+      'timeupdate',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      5,
+      'seeking',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      6,
+      'seeked',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(7, 'ended', expect.any(Function));
+    expect(player.on).toHaveBeenNthCalledWith(
+      8,
+      'captionsdisabled',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      9,
+      'captionsenabled',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      10,
+      'enterfullscreen',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      11,
+      'exitfullscreen',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      12,
+      'languagechange',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      13,
+      'qualitychange',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      14,
+      'ratechange',
+      expect.any(Function),
+    );
+    expect(player.on).toHaveBeenNthCalledWith(
+      15,
+      'volumechange',
+      expect.any(Function),
+    );
   });
 });
