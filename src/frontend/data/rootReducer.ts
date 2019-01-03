113dbb28eb5a7ec2828eec1b8e12a541feaf1317
Mehdi Benadda <me@mbenadda.com>
2019-01-03 15:37:30 +0100

    ✨(frontend) prepare the app to handle timedtexttracks
    Until now marsha only supported one kind of Resource, Video. As
    we're adding timed text tracks, we need to add in some relevant
    types and adapt our data (RootState & reducer) to handle them.
    
    We also took this opportunity to add typings for schemas &
    route OPTIONS as we are going to need them.

--- src/frontend/data/rootReducer.ts
+++ src/frontend/data/rootReducer.ts
@@ -4,6 +4,10 @@ import { appState } from '../types/AppData';
 import { modelName } from '../types/models';
 import { Video } from '../types/tracks';
 import { Nullable } from '../utils/types';
+import {
+  timedtexttracks,
+  TimedTextTracksState,
+} from './timedtexttracks/reducer';
 import { videos, VideosState } from './videos/reducer';
 
 export interface RootState {
@@ -13,6 +17,7 @@ export interface RootState {
     ltiState: appState;
   };
   resources: {
+    [modelName.TIMEDTEXTTRACKS]: TimedTextTracksState;
     [modelName.VIDEOS]: VideosState;
   };
 }
@@ -20,6 +25,10 @@ export interface RootState {
 export const rootReducer: Reducer<RootState> = (state, action) => ({
   context: state!.context,
   resources: {
+    [modelName.TIMEDTEXTTRACKS]: timedtexttracks(
+      (state && state.resources[modelName.TIMEDTEXTTRACKS]) || undefined,
+      action,
+    ),
     [modelName.VIDEOS]: videos(
       (state && state.resources[modelName.VIDEOS]) || undefined,
       action,
---
+++ src/frontend/data/timedtexttracks/reducer.ts
@@ -0,0 +1,36 @@
+import { AnyAction, Reducer } from 'redux';
+
+import { modelName } from '../../types/models';
+import { ResourceByIdState } from '../../types/Resource';
+import { TimedText } from '../../types/tracks';
+import { Maybe } from '../../utils/types';
+import {
+  ResourceAdd,
+  ResourceMultipleAdd,
+} from '../genericReducers/resourceById/actions';
+import {
+  byId,
+  initialState as resourceByIdInit,
+} from '../genericReducers/resourceById/resourceById';
+
+const initialState = { ...resourceByIdInit };
+
+export type TimedTextTracksState = Maybe<ResourceByIdState<TimedText>>;
+
+export const timedtexttracks: Reducer<TimedTextTracksState> = (
+  state: TimedTextTracksState = initialState,
+  action?: ResourceAdd<TimedText> | ResourceMultipleAdd<TimedText> | AnyAction,
+) => {
+  if (!action) {
+    return state;
+  }
+
+  if (
+    action.resourceName &&
+    action.resourceName !== modelName.TIMEDTEXTTRACKS
+  ) {
+    return state;
+  }
+
+  return byId(state, action);
+};
