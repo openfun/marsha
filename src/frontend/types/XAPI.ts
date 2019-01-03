9629598483e7a8ddf71508eb60a29ac2c3c0f381
Manuel Raynaud <manu@raynaud.io>
2019-01-03 16:55:34 +0100

    ♻️(XAPI) transform context, result and verb definition in enum
    ContextExtensionsDefintion, ResultExtensionsDefinition and
    VerbDefinition should be enum instead of object. This will avoid
    modification.

--- src/frontend/types/XAPI.ts
+++ src/frontend/types/XAPI.ts
@@ -29,6 +29,41 @@ export interface CompletedDataPlayload extends DataPayload {
   };
 }
 
+export enum ContextExtensionsDefintion {
+  ccSubtitleEnabled = 'https://w3id.org/xapi/video/extensions/cc-subtitle-enabled',
+  ccSubtitleLanguage = 'https://w3id.org/xapi/video/extensions/cc-subtitle-lang',
+  completionTreshold = 'https://w3id.org/xapi/video/extensions/completion-threshold',
+  frameRate = 'https://w3id.org/xapi/video/extensions/frame-rate',
+  fullScreen = 'https://w3id.org/xapi/video/extensions/full-screen',
+  length = 'https://w3id.org/xapi/video/extensions/length',
+  quality = 'https://w3id.org/xapi/video/extensions/quality',
+  screenSize = 'https://w3id.org/xapi/video/extensions/screen-size',
+  sessionId = 'https://w3id.org/xapi/video/extensions/session-id',
+  speed = 'https://w3id.org/xapi/video/extensions/speed',
+  track = 'https://w3id.org/xapi/video/extensions/track',
+  userAgent = 'https://w3id.org/xapi/video/extensions/user-agent',
+  videoPlaybackSize = 'https://w3id.org/xapi/video/extensions/video-playback-size',
+  volume = 'https://w3id.org/xapi/video/extensions/volume',
+}
+
+export enum ResultExtensionsDefinition {
+  playedSegment = 'https://w3id.org/xapi/video/extensions/played-segments',
+  progress = 'https://w3id.org/xapi/video/extensions/progress',
+  time = 'https://w3id.org/xapi/video/extensions/time',
+  timeFrom = 'https://w3id.org/xapi/video/extensions/time-from',
+  timeTo = 'https://w3id.org/xapi/video/extensions/time-to',
+}
+
+export enum VerbDefinition {
+  completed = 'http://adlnet.gov/expapi/verbs/completed',
+  initialized = 'http://adlnet.gov/expapi/verbs/initialized',
+  interacted = 'http://adlnet.gov/expapi/verbs/interacted',
+  paused = 'https://w3id.org/xapi/video/verbs/paused',
+  played = 'https://w3id.org/xapi/video/verbs/played',
+  seeked = 'https://w3id.org/xapi/video/verbs/seeked',
+  terminated = 'http://adlnet.gov/expapi/verbs/terminated',
+}
+
 /************* Played event  *************/
 export interface PlayedResultExtensions {
   time: number;
