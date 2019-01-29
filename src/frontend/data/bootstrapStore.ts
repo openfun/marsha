df8ceec5911eaa79b5f2566bdb6e6018fa01bd0b
Mehdi Benadda <me@mbenadda.com>
2019-01-29 11:17:16 +0100

    ✨(frontend) add a saga to handle resource list GETs
    One of our most simple needs when it comes to side effects is to
    GET list of resources, and store them both as a list and as a series
    of unique resources.
    
    We can implement a simple saga using the `resourceList` reducer's
    actions. We also need to set up the base saga plumbing with relation
    to the redux store as it is our first saga.

--- src/frontend/data/bootstrapStore.ts
+++ src/frontend/data/bootstrapStore.ts
@@ -1,19 +1,32 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import { AppData } from '../types/AppData';
 import { modelName } from '../types/models';
 import { initialState } from './context/reducer';
 import { rootReducer } from './rootReducer';
+import { rootSaga } from './rootSaga';
 
-export const bootstrapStore = (appData: AppData) =>
-  createStore(rootReducer, {
-    context: initialState,
-    resources: {
-      [modelName.TIMEDTEXTTRACKS]: {},
-      [modelName.VIDEOS]: {
-        ...(appData.video
-          ? { byId: { [appData.video.id]: appData.video } }
-          : {}),
+const sagaMiddleware = createSagaMiddleware();
+
+export const bootstrapStore = (appData: AppData) => {
+  const store = createStore(
+    rootReducer,
+    {
+      context: initialState,
+      resources: {
+        [modelName.TIMEDTEXTTRACKS]: {},
+        [modelName.VIDEOS]: {
+          ...(appData.video
+            ? { byId: { [appData.video.id]: appData.video } }
+            : {}),
+        },
       },
     },
-  });
+    applyMiddleware(sagaMiddleware),
+  );
+
+  sagaMiddleware.run(rootSaga);
+
+  return store;
+};
