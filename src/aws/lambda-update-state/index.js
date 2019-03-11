df95d73cb560b64d589fff3d78f2a941362bbb92
Manuel Raynaud <manu@raynaud.io>
2019-03-11 14:51:37 +0100

    ✨(aws) complete update-state lambda to update videos content-type
    When a user would like to download a video, we don't want to play the
    video in a new tab browser but to force the download. To do that we must
    change the video content-type to binary/octet-stream.

--- src/aws/lambda-update-state/index.js
+++ src/aws/lambda-update-state/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const updateState = require('./src/updateState');
+const updateVideosContentType = require('./src/updateVideosContentType');
 
 exports.handler = async (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
@@ -10,6 +11,16 @@ exports.handler = async (event, context, callback) => {
 
   try {
     await updateState(key, state);
+    if (event.detail.status === 'COMPLETE') {
+      const videos = event.detail.outputGroupDetails.find(el => {
+        const regex = RegExp('^s3://.*.mp4$');
+        return (
+          el.type === 'FILE_GROUP' &&
+          regex.test(el.outputDetails[0].outputFilePaths[0])
+        );
+      }).outputDetails;
+      await updateVideosContentType(videos);
+    }
     callback();
   } catch (error) {
     callback(error);
