54bf21c0d5cf4c39bde4d5566adef4437ed329f7
Manuel Raynaud <manu@raynaud.io>
2019-03-13 17:23:47 +0100

    ✨(front) load translations with react-intl
    React-intl was installed but no translations were used. Now, based on
    the JWT locale property we are able to load translations or fallback on
    default messages if translation is missing.

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,25 +1,40 @@
 import 'iframe-resizer/js/iframeResizer.contentWindow';
 
 import { Grommet } from 'grommet';
+import jwtDecode from 'jwt-decode';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IntlProvider } from 'react-intl';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
+import fr from 'react-intl/locale-data/fr';
 import { Provider } from 'react-redux';
 
 import { AppRoutesConnected } from './components/AppRoutesConnected/AppRoutesConnected';
 import { appData } from './data/appData';
 import { bootstrapStore } from './data/bootstrapStore';
+import translatedMessages from './translations/translation.json';
+import { DecodedJwt } from './types/jwt';
+import { localesMapping, TranslatedMessages } from './types/translation';
 // Load our style reboot into the DOM
 import { GlobalStyles } from './utils/theme/baseStyles';
 import { theme } from './utils/theme/theme';
 
 const store = bootstrapStore(appData);
+addLocaleData([...en, ...es, ...fr]);
+
+const decodedToken: DecodedJwt = jwtDecode(appData.jwt);
+
+const messages =
+  (translatedMessages as TranslatedMessages)[
+    localesMapping[decodedToken.locale]
+  ] || null;
 
 // Wait for the DOM to load before we scour it for an element that requires React to render
 document.addEventListener('DOMContentLoaded', event => {
   // Render our actual component tree
   ReactDOM.render(
-    <IntlProvider>
+    <IntlProvider locale={decodedToken.locale} messages={messages}>
       <Grommet theme={theme}>
         <Provider store={store}>
           <AppRoutesConnected />
