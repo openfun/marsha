1ba72eb68fd723f915afccf98ff692017109a219
Manuel Raynaud <manu@raynaud.io>
2019-03-21 17:05:02 +0100

    ✨(aws) complete lambda-encode to process video's thumbnails
    Once a user has upload a new thumbnail we will resize it in differents
    sizes. This job is made by lambda-encode.

--- src/aws/lambda-encode/index.js
+++ src/aws/lambda-encode/index.js
@@ -3,6 +3,7 @@
 const encodeTimedTextTrack = require('./src/encodeTimedTextTrack');
 const encodeVideo = require('./src/encodeVideo');
 const updateState = require('./src/updateState');
+const resizeThumbnails = require('./src/resizeThumbnails');
 
 exports.handler = async (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
@@ -12,17 +13,33 @@ exports.handler = async (event, context, callback) => {
 
   const parts = objectKey.split('/');
   const [resourceId, kind, recordId, extendedStamp] = parts;
-  if (parts.length != 4 || !['timedtexttrack', 'video'].includes(kind)) {
-    let error =
-      kind === 'video'
-        ? 'Source videos should be uploaded in a folder of the form ' +
-          '"{playlist_id}/videos/{video_id}/{stamp}".'
-        : kind === 'timedtexttrack'
-        ? 'Source timed text files should be uploaded to a folder of the form ' +
-          '"{playlist_id}/timedtexttrack/{timedtext_id}/{stamp}_{language}[_{has_closed_caption}]".'
-        : kind
-        ? `Unrecognized kind ${kind} in key "${objectKey}".`
-        : `Unrecognized key format "${objectKey}"`;
+  if (
+    parts.length != 4 ||
+    !['timedtexttrack', 'video', 'thumbnail'].includes(kind)
+  ) {
+    let error;
+    switch (kind) {
+      case 'video':
+        error =
+          'Source videos should be uploaded in a folder of the form ' +
+          '"{playlist_id}/videos/{video_id}/{stamp}".';
+        break;
+      case 'timedtexttrack':
+        error =
+          'Source timed text files should be uploaded to a folder of the form ' +
+          '"{playlist_id}/timedtexttrack/{timedtext_id}/{stamp}_{language}[_{has_closed_caption}]".';
+        break;
+      case 'thumbnail':
+        error =
+          'Source thumbnails should be uploaded in a folder of the form ' +
+          '"{playlist_id}/thumbnail/{thumbnail_id}/{stamp}".';
+        break;
+      default:
+        error = kind
+          ? `Unrecognized kind ${kind} in key "${objectKey}".`
+          : `Unrecognized key format "${objectKey}"`;
+        break;
+    }
     callback(error);
     return;
   }
@@ -50,6 +67,18 @@ exports.handler = async (event, context, callback) => {
       }
       console.log(JSON.stringify(jobData, null, 2));
       callback(null, { Job: jobData.Job.Id });
+      break;
+
+    case 'thumbnail':
+      try {
+        await resizeThumbnails(objectKey, sourceBucket);
+        await updateState(objectKey, 'ready');
+      } catch (error) {
+        return callback(error);
+      }
+      console.log(
+        `Successfully received and resized thumbnail ${objectKey} from ${sourceBucket}.`,
+      );
       break;
   }
 };
---
+++ src/aws/lambda-encode/src/resizeThumbnails.spec.js
@@ -0,0 +1,54 @@
+process.env.S3_DESTINATION_BUCKET = 'destination bucket';
+
+// Don't pollute tests with logs intended for CloudWatch
+jest.spyOn(console, 'log');
+
+// Mock the AWS SDK calls used in encodeTimedTextTrack
+const mockGetObject = jest.fn();
+const mockPutObject = jest.fn();
+jest.mock('aws-sdk', () => ({
+  S3: function() {
+    this.getObject = mockGetObject;
+    this.putObject = mockPutObject;
+  },
+}));
+
+const mockGetBufferAsync = jest.fn();
+const mockResize = jest.fn();
+const mockRead = jest.fn();
+
+jest.mock('jimp', () => ({
+  read: mockRead,
+}));
+
+const resizeThumbnails = require('./resizeThumbnails');
+
+describe('lambda-encode/src/resizeThumbnails', () => {
+  beforeEach(() => {
+    console.log.mockReset();
+    jest.resetAllMocks();
+  });
+  it('resizes uploaded image', async () => {
+    mockGetObject.mockReturnValue({
+      promise: () =>
+        new Promise(resolve => resolve({ Body: 'input timed text' })),
+    });
+    mockPutObject.mockReturnValue({
+      promise: () => new Promise(resolve => resolve()),
+    });
+    mockRead.mockReturnValue(
+      Promise.resolve({
+        resize: mockResize,
+      }),
+    );
+    mockGetBufferAsync.mockReturnValue(Promise.resolve('resized_image'));
+    mockResize.mockReturnValue({
+      getBufferAsync: mockGetBufferAsync,
+    });
+
+    await resizeThumbnails('foo/thumbnail/bar/baz', 'source bucket');
+
+    expect(mockResize).toHaveBeenCalledTimes(5);
+    expect(mockGetBufferAsync).toHaveBeenCalledTimes(5);
+  });
+});
