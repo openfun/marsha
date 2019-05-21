7b1973a047eb8c6ad3205426522ef6ff7dae35b7
Manuel Raynaud <manu@raynaud.io>
2019-05-21 17:10:56 +0200

    ♻️(backend) extract LTI.get_or_create_video to a standalone function
    LTI.get_or_create_video method can be extracted as a standaline
    function. Doing this will allow us to create in the future other
    standlaone function to create other resources like files.

--- src/backend/marsha/core/lti/__init__.py
+++ src/backend/marsha/core/lti/__init__.py
@@ -4,14 +4,13 @@ from urllib.parse import urlparse
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.db.models import Q
 from django.utils.datastructures import MultiValueDictKeyError
 
 from pylti.common import LTIException, verify_request_common
 
-from .defaults import PENDING, READY
-from .models import ConsumerSite, Playlist, Video
-from .models.account import INSTRUCTOR, LTI_ROLES, STUDENT, LTIPassport
+from ..defaults import READY
+from ..models import ConsumerSite, Video
+from ..models.account import INSTRUCTOR, LTI_ROLES, STUDENT, LTIPassport
 
 
 class LTI:
@@ -273,86 +272,6 @@ class LTI:
 
         """
         return self.request.POST.get("launch_presentation_locale", "en")
-
-    def get_or_create_video(self):
-        """Get or create the video targeted by the LTI launch request.
-
-        Create the playlist if it does not pre-exist (it can only happen with consumer site scope
-        passports).
-
-        Raises
-        ------
-        LTIException
-            Exception raised if the request does not contain a context id (playlist).
-
-        Returns
-        -------
-        core.models.video.Video
-            The video instance targeted by the launch url or None.
-
-        """
-        # Make sure LTI verification has run successfully. It raises an LTIException otherwise.
-        consumer_site = self.verify()
-
-        try:
-            assert self.context_id
-        except AssertionError:
-            raise LTIException("A context ID is required.")
-
-        # If the video already exists, retrieve it from database
-        filter_kwargs = {"upload_state": READY} if not self.is_instructor else {}
-        try:
-            return Video.objects.get(
-                Q(playlist__lti_id=self.context_id)
-                | Q(playlist__is_portable_to_playlist=True, upload_state=READY),
-                Q(playlist__consumer_site=consumer_site)
-                | Q(playlist__is_portable_to_consumer_site=True, upload_state=READY)
-                | Q(
-                    playlist__consumer_site__in=consumer_site.reachable_from.all(),
-                    upload_state=READY,
-                ),
-                pk=self.resource_id,
-                **filter_kwargs,
-            )
-        except Video.DoesNotExist:
-            pass
-
-        # If we didn't find any existing video, we will only create a new video if the
-        # request comes from an instructor
-        if not self.is_instructor:
-            return None
-
-        # Check that the video does not already exist (possible in another playlist and/or
-        # on another consumer site if it is not portable or not ready).
-        try:
-            video = Video.objects.get(pk=self.resource_id)
-        except Video.DoesNotExist:
-            pass
-        else:
-            raise LTIException(
-                "The video ID {!s} already exists but is not portable to your playlist ({!s}) "
-                "and/or consumer site ({!s}).".format(
-                    video.id, self.context_id, consumer_site.domain
-                )
-            )
-
-        # Creating the video...
-        # - Get the playlist if it exists or create it
-        playlist, _ = Playlist.objects.get_or_create(
-            lti_id=self.context_id,
-            consumer_site=consumer_site,
-            defaults={"title": self.context_title},
-        )
-
-        # Create the video, pointing to the file from the origin video if any
-        return Video.objects.create(
-            pk=self.resource_id,
-            lti_id=self.resource_link_id,
-            playlist=playlist,
-            upload_state=PENDING,
-            title=self.resource_link_title,
-            show_download=consumer_site.video_show_download_default,
-        )
 
 
 class OpenEdxLTI(LTI):
