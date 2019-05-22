bd6d03da82653c05b213f42d2ff53f8c328fb461
Manuel Raynaud <manu@raynaud.io>
2019-05-22 20:41:04 +0200

    ✨(backend) create get_or_create_file
    Like for video we need a function able to get or create a file on a LTI
    request. First we refactor the existing get_or_create_video to extract
    generic part and then reuse them in get_or_create_file.

--- src/backend/marsha/core/lti/utils.py
+++ src/backend/marsha/core/lti/utils.py
@@ -4,7 +4,91 @@ from django.db.models import Q
 from pylti.common import LTIException
 
 from ..defaults import PENDING, READY
-from ..models import Playlist, Video
+from ..models import File, Playlist, Video
+
+
+def _get_or_create_resource(model, lti):
+    """Get or Create a resource targeted by a LTI request.
+
+    This function is generic and will use the `model` argument to create
+    the wanted resource.
+
+    Create the playlist if it does not pre-exist (it can only happen with consumer site scope
+    passports).
+
+    Parameters
+    ----------
+    lti : Type[LTI]
+
+    model:
+        The model we want to get or create.
+
+    Raises
+    ------
+    LTIException
+        Exception raised if the request does not contain a context id (playlist).
+
+    Returns
+    -------
+    An instance of the model targeted by the LTI request or None
+
+    """
+    lti.verify()
+    try:
+        assert lti.context_id
+    except AssertionError:
+        raise LTIException("A context ID is required.")
+
+    # If the video already exists, retrieve it from database
+    filter_kwargs = {"upload_state": READY} if not lti.is_instructor else {}
+    try:
+        return model.objects.get(
+            Q(playlist__lti_id=lti.context_id)
+            | Q(playlist__is_portable_to_playlist=True, upload_state=READY),
+            Q(playlist__consumer_site=lti.get_consumer_site())
+            | Q(playlist__is_portable_to_consumer_site=True, upload_state=READY)
+            | Q(
+                playlist__consumer_site__in=lti.get_consumer_site().reachable_from.all(),
+                upload_state=READY,
+            ),
+            pk=lti.resource_id,
+            **filter_kwargs,
+        )
+    except model.DoesNotExist:
+        pass
+
+    if not lti.is_instructor:
+        return None
+
+    try:
+        resource = model.objects.get(pk=lti.resource_id)
+    except model.DoesNotExist:
+        pass
+    else:
+        raise LTIException(
+            "The {!s} ID {!s} already exists but is not portable to your playlist ({!s}) "
+            "and/or consumer site ({!s}).".format(
+                model.__name__,
+                resource.id,
+                lti.context_id,
+                lti.get_consumer_site().domain,
+            )
+        )
+
+    playlist, _ = Playlist.objects.get_or_create(
+        lti_id=lti.context_id,
+        consumer_site=lti.get_consumer_site(),
+        defaults={"title": lti.context_title},
+    )
+
+    return model.objects.create(
+        pk=lti.resource_id,
+        lti_id=lti.resource_link_id,
+        playlist=playlist,
+        upload_state=PENDING,
+        title=lti.resource_link_title,
+        show_download=lti.get_consumer_site().video_show_download_default,
+    )
 
 
 def get_or_create_video(lti):
@@ -28,65 +112,28 @@ def get_or_create_video(lti):
         The video instance targeted by the launch url or None.
 
     """
-    # Make sure LTI verification has run successfully. It raises an LTIException otherwise.
-    consumer_site = lti.verify()
+    return _get_or_create_resource(Video, lti)
 
-    try:
-        assert lti.context_id
-    except AssertionError:
-        raise LTIException("A context ID is required.")
 
-    # If the video already exists, retrieve it from database
-    filter_kwargs = {"upload_state": READY} if not lti.is_instructor else {}
-    try:
-        return Video.objects.get(
-            Q(playlist__lti_id=lti.context_id)
-            | Q(playlist__is_portable_to_playlist=True, upload_state=READY),
-            Q(playlist__consumer_site=consumer_site)
-            | Q(playlist__is_portable_to_consumer_site=True, upload_state=READY)
-            | Q(
-                playlist__consumer_site__in=consumer_site.reachable_from.all(),
-                upload_state=READY,
-            ),
-            pk=lti.resource_id,
-            **filter_kwargs,
-        )
-    except Video.DoesNotExist:
-        pass
+def get_or_create_file(lti):
+    """Get or create the file targeted by the LTI launch request.
 
-    # If we didn't find any existing video, we will only create a new video if the
-    # request comes from an instructor
-    if not lti.is_instructor:
-        return None
+    Create the playlist if it does not pre-exist (it can only happen with consumer site scope
+    passports).
 
-    # Check that the video does not already exist (possible in another playlist and/or
-    # on another consumer site if it is not portable or not ready).
-    try:
-        video = Video.objects.get(pk=lti.resource_id)
-    except Video.DoesNotExist:
-        pass
-    else:
-        raise LTIException(
-            "The video ID {!s} already exists but is not portable to your playlist ({!s}) "
-            "and/or consumer site ({!s}).".format(
-                video.id, lti.context_id, consumer_site.domain
-            )
-        )
+    Parameters
+    ----------
+    lti : Type[LTI]
 
-    # Creating the video...
-    # - Get the playlist if it exists or create it
-    playlist, _ = Playlist.objects.get_or_create(
-        lti_id=lti.context_id,
-        consumer_site=consumer_site,
-        defaults={"title": lti.context_title},
-    )
+    Raises
+    ------
+    LTIException
+        Exception raised if the request does not contain a context id (playlist).
 
-    # Create the video, pointing to the file from the origin video if any
-    return Video.objects.create(
-        pk=lti.resource_id,
-        lti_id=lti.resource_link_id,
-        playlist=playlist,
-        upload_state=PENDING,
-        title=lti.resource_link_title,
-        show_download=consumer_site.video_show_download_default,
-    )
+    Returns
+    -------
+    core.models.video.Video
+        The video instance targeted by the launch url or None.
+
+    """
+    return _get_or_create_resource(File, lti)
