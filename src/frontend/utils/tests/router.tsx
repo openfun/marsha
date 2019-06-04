ab2c4d7f7caeeea098ef9dc2f1505a259d9ec869
Mehdi Benadda <me@mbenadda.com>
2019-06-04 17:16:00 +0200

    ✅(router) add helper to easily use routes in tests
    As we use testing-library's complete rendering in our tests, we
    need to wrap our tested components in a `react-router-dom` router
    if they use any of its utilities or need to render <Redirect />
    or <Link /> components.
    
    As such, this does not warrant adding a utility. However, we sometimes
    need to manage and test the redirects, which is not easy to achieve
    through mocking. We can instead have our utility mock our redirect targets
    and replace them with trivial elements to test them.

---
+++ src/frontend/utils/tests/router.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MemoryRouter, Route, Switch } from 'react-router-dom';
+
+export const wrapInRouter = (
+  Component: JSX.Element,
+  routes?: Array<{ path: string; render: ({ match }: any) => JSX.Element }>,
+) => (
+  <MemoryRouter>
+    <Switch>
+      {!routes
+        ? null
+        : routes.map(routeProps => (
+            <Route exact key={routeProps.path} {...routeProps} />
+          ))}
+      <Route path="/" render={() => Component} />
+    </Switch>
+  </MemoryRouter>
+);
