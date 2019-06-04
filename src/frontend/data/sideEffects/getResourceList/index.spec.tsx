46a5f1c0d456caa5fd88fea95b6856c03cf67fc5
Mehdi Benadda <me@mbenadda.com>
2019-06-04 17:31:09 +0200

    ♻️(data) replace the getResourceList saga with a simple side effect
    We're in the process of distancing ourselves from heavy-duty state
    management tools such as saga & redux. The first step is to refactor
    the two sagas we had into simpler side effects that interact with a
    dispatch function.
    
    This will allow us to plug them with other state management solutions,
    whether builtin hooks or a simpler state management lib, way more
    easily.

---
+++ src/frontend/data/sideEffects/getResourceList/index.spec.tsx
@@ -0,0 +1,113 @@
+import fetchMock from 'fetch-mock';
+
+import { requestStatus } from '../../../types/api';
+import { modelName } from '../../../types/models';
+import { uploadState, Video } from '../../../types/tracks';
+import { getResourceList } from './';
+
+// We'll be testing with a course-like Resource as the saga needs some specifics to operate: we want
+// something simple but we don't want to rely on the specific implementation of a resource
+describe('sideEffects/getResourceList', () => {
+  const dispatch = jest.fn();
+
+  const video42 = {
+    id: '42',
+    is_ready_to_play: false,
+    upload_state: uploadState.PENDING,
+  } as Video;
+
+  const video43 = {
+    id: '43',
+    is_ready_to_play: true,
+    upload_state: uploadState.READY,
+  } as Video;
+
+  afterEach(fetchMock.restore);
+  afterEach(jest.resetAllMocks);
+
+  it('requests the resource list, handles the response and resolves with a success', async () => {
+    fetchMock.mock(
+      '/api/videos/?limit=2&offset=43',
+      JSON.stringify([video42, video43]),
+    );
+
+    const status = await getResourceList(dispatch, 'some token')(
+      modelName.VIDEOS,
+      {
+        limit: 2,
+        offset: 43,
+      },
+    );
+
+    expect(status).toEqual(requestStatus.SUCCESS);
+    expect(dispatch).toHaveBeenCalledWith({
+      params: { limit: 2, offset: 43 },
+      resourceName: modelName.VIDEOS,
+      type: 'RESOURCE_LIST_GET',
+    });
+    expect(dispatch).toHaveBeenCalledWith({
+      apiResponse: { objects: [video42, video43] },
+      params: { limit: 2, offset: 43 },
+      resourceName: modelName.VIDEOS,
+      type: 'RESOURCE_LIST_GET_SUCCESS',
+    });
+    expect(dispatch).toHaveBeenCalledWith({
+      resourceName: modelName.VIDEOS,
+      resources: [video42, video43],
+      type: 'RESOURCE_MULTIPLE_ADD',
+    });
+  });
+
+  it('resolves with a failure and handles it when it fails to get the resource list (local)', async () => {
+    fetchMock.mock(
+      '/api/videos/?limit=2&offset=43',
+      Promise.reject(new Error('Failed to perform the request')),
+    );
+
+    const status = await getResourceList(dispatch, 'some token')(
+      modelName.VIDEOS,
+      {
+        limit: 2,
+        offset: 43,
+      },
+    );
+
+    expect(status).toEqual(requestStatus.FAILURE);
+    expect(dispatch).toHaveBeenCalledWith({
+      params: { limit: 2, offset: 43 },
+      resourceName: modelName.VIDEOS,
+      type: 'RESOURCE_LIST_GET',
+    });
+    expect(dispatch).toHaveBeenCalledWith({
+      error: new Error('Failed to perform the request'),
+      resourceName: modelName.VIDEOS,
+      type: 'RESOURCE_LIST_GET_FAILURE',
+    });
+  });
+
+  it('returns an error response when it fails to get the resource list (api)', async () => {
+    fetchMock.mock('/api/videos/?limit=2&offset=43', 404);
+
+    const status = await getResourceList(dispatch, 'some token')(
+      modelName.VIDEOS,
+      {
+        limit: 2,
+        offset: 43,
+      },
+    );
+
+    expect(status).toEqual(requestStatus.FAILURE);
+    expect(dispatch).toHaveBeenCalledWith({
+      params: { limit: 2, offset: 43 },
+      resourceName: modelName.VIDEOS,
+      type: 'RESOURCE_LIST_GET',
+    });
+    expect(dispatch).toHaveBeenCalledWith({
+      error: new Error(
+        'Failed to get list for /api/videos/ and {"limit":2,"offset":43} : 404.',
+      ),
+      resourceName: modelName.VIDEOS,
+      type: 'RESOURCE_LIST_GET_FAILURE',
+    });
+  });
+});
