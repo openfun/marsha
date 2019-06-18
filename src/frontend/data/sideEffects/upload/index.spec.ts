d22f3c4c286894c0ab702fb80e6c6ca887e4eae7
Manuel Raynaud <manu@raynaud.io>
2019-06-18 16:35:57 +0200

    ♻️(front) refactor UploadForm component
    This commit is part of our ongoing refactoring of marsha's frontend. The
    UploadForm component is now a functional component and the connection to
    redux is made in one module.
    Also, is order to easily and fully test the `upload` function we choosed
    to extract it out of the component, as a standalone function and fully
    test it.

---
+++ src/frontend/data/sideEffects/upload/index.spec.ts
@@ -0,0 +1,256 @@
+import fetchMock from 'fetch-mock';
+import xhrMock from 'xhr-mock';
+
+import { modelName } from '../../../types/models';
+import { timedTextMode, uploadState, Video } from '../../../types/tracks';
+import { upload } from './';
+
+describe('upload', () => {
+  const object = {
+    description: 'Some description',
+    id: 'video-id',
+    is_ready_to_play: true,
+    show_download: false,
+    thumbnail: null,
+    timed_text_tracks: [
+      {
+        active_stamp: 1549385921,
+        id: 'ttt-1',
+        is_ready_to_play: true,
+        language: 'fr',
+        mode: timedTextMode.SUBTITLE,
+        upload_state: uploadState.READY,
+        url: 'https://example.com/timedtext/ttt-1.vtt',
+      },
+      {
+        active_stamp: 1549385922,
+        id: 'ttt-2',
+        is_ready_to_play: false,
+        language: 'fr',
+        mode: timedTextMode.SUBTITLE,
+        upload_state: uploadState.READY,
+        url: 'https://example.com/timedtext/ttt-2.vtt',
+      },
+      {
+        active_stamp: 1549385923,
+        id: 'ttt-3',
+        is_ready_to_play: true,
+        language: 'en',
+        mode: timedTextMode.CLOSED_CAPTIONING,
+        upload_state: uploadState.READY,
+        url: 'https://example.com/timedtext/ttt-3.vtt',
+      },
+      {
+        active_stamp: 1549385924,
+        id: 'ttt-4',
+        is_ready_to_play: true,
+        language: 'fr',
+        mode: timedTextMode.TRANSCRIPT,
+        upload_state: uploadState.READY,
+        url: 'https://example.com/timedtext/ttt-4.vtt',
+      },
+    ],
+    title: 'Some title',
+    upload_state: 'pending',
+    urls: {
+      manifests: {
+        dash: 'https://example.com/dash.mpd',
+        hls: 'https://example.com/hls.m3u8',
+      },
+      mp4: {
+        144: 'https://example.com/144p.mp4',
+        1080: 'https://example.com/1080p.mp4',
+      },
+      thumbnails: {
+        720: 'https://example.com/144p.jpg',
+      },
+    },
+  } as Video;
+
+  const updateObject = jest.fn();
+  const setStatus = jest.fn();
+  const notifyObjectUploadProgress = jest.fn();
+  const jwt = 'foo';
+  const objectType = modelName.VIDEOS;
+
+  beforeEach(jest.resetAllMocks);
+  beforeEach(() => xhrMock.setup());
+
+  afterEach(fetchMock.restore);
+  afterEach(() => xhrMock.teardown());
+
+  it('gets the policy from the API and uses it to upload the file', async () => {
+    const file = new File(['(⌐□_□)'], 'course.mp4', { type: 'video/mp4' });
+
+    fetchMock.mock(
+      '/api/videos/video-id/initiate-upload/',
+      {
+        bucket: 'dev',
+        s3_endpoint: 's3.aws.example.com',
+      },
+      { method: 'POST' },
+    );
+
+    xhrMock.post('https://s3.aws.example.com/dev', {
+      body: 'form data body',
+      status: 204,
+    });
+
+    await upload(
+      updateObject,
+      setStatus,
+      notifyObjectUploadProgress,
+      jwt,
+      objectType,
+      object,
+    )(file);
+
+    expect(
+      fetchMock.calls('/api/videos/video-id/initiate-upload/', {
+        method: 'POST',
+      }),
+    ).toHaveLength(1);
+    expect(setStatus).toHaveBeenCalledWith('uploading');
+    expect(updateObject).toHaveBeenCalledWith({
+      ...object,
+      upload_state: uploadState.UPLOADING,
+    });
+    expect(updateObject).toHaveBeenCalledWith({
+      ...object,
+      upload_state: uploadState.PROCESSING,
+    });
+  });
+
+  it('reports the error and does not upload to AWS when it fails to create an initiate upload', async () => {
+    const file = new File(['(⌐□_□)'], 'course.mp4', { type: 'video/mp4' });
+
+    fetchMock.mock('/api/videos/video-id/initiate-upload/', 400, {
+      method: 'POST',
+    });
+
+    xhrMock.post('https://s3.aws.example.com/dev', () => {
+      throw new Error('upload file should not be called');
+    });
+
+    await upload(
+      updateObject,
+      setStatus,
+      notifyObjectUploadProgress,
+      jwt,
+      objectType,
+      object,
+    )(file);
+
+    expect(
+      fetchMock.calls('/api/videos/video-id/initiate-upload/', {
+        method: 'POST',
+      }),
+    ).toHaveLength(1);
+    expect(setStatus).toHaveBeenCalledWith('policy_error');
+  });
+
+  it('marks the object with an error state when it fails to upload the file', async () => {
+    const file = new File(['(⌐□_□)'], 'course.mp4', { type: 'video/mp4' });
+
+    fetchMock.mock(
+      '/api/videos/video-id/initiate-upload/',
+      {
+        bucket: 'dev',
+        s3_endpoint: 's3.aws.example.com',
+      },
+      { method: 'POST' },
+    );
+
+    xhrMock.post('https://s3.aws.example.com/dev', {
+      status: 400,
+    });
+
+    await upload(
+      updateObject,
+      setStatus,
+      notifyObjectUploadProgress,
+      jwt,
+      objectType,
+      object,
+    )(file);
+
+    expect(
+      fetchMock.calls(
+        `/api/${modelName.VIDEOS}/${object.id}/initiate-upload/`,
+        {
+          method: 'POST',
+        },
+      ),
+    ).toHaveLength(1);
+    expect(setStatus).toHaveBeenCalledWith('uploading');
+    expect(updateObject).toHaveBeenCalledWith({
+      ...object,
+      upload_state: uploadState.UPLOADING,
+    });
+    expect(updateObject).toHaveBeenCalledWith({
+      ...object,
+      upload_state: uploadState.ERROR,
+    });
+  });
+
+  it('does not execute the upload function if there is no file', async () => {
+    fetchMock.mock(
+      '/api/videos/video-id/initiate-upload/',
+      {
+        bucket: 'dev',
+        s3_endpoint: 's3.aws.example.com',
+      },
+      { method: 'POST' },
+    );
+
+    await upload(
+      updateObject,
+      setStatus,
+      notifyObjectUploadProgress,
+      jwt,
+      objectType,
+      object,
+    )(undefined);
+
+    expect(
+      fetchMock.calls(
+        `/api/${modelName.VIDEOS}/${object.id}/initiate-upload/`,
+        {
+          method: 'POST',
+        },
+      ),
+    ).toHaveLength(0);
+    expect(setStatus).not.toHaveBeenCalled();
+    expect(updateObject).not.toHaveBeenCalled();
+  });
+
+  it('sets the status to not found and stops the execution if there is no object', async () => {
+    fetchMock.mock(
+      '/api/videos/video-id/initiate-upload/',
+      {
+        bucket: 'dev',
+        s3_endpoint: 's3.aws.example.com',
+      },
+      { method: 'POST' },
+    );
+
+    const file = new File(['(⌐□_□)'], 'course.mp4', { type: 'video/mp4' });
+
+    await upload(
+      updateObject,
+      setStatus,
+      notifyObjectUploadProgress,
+      jwt,
+      objectType,
+      undefined,
+    )(file);
+
+    expect(
+      fetchMock.calls('/api/videos/video-id/initiate-upload/', {
+        method: 'POST',
+      }),
+    ).toHaveLength(0);
+    expect(setStatus).toHaveBeenCalledWith('not_found_error');
+    expect(updateObject).not.toHaveBeenCalled();
+  });
+});
---
+++ src/frontend/data/sideEffects/upload/index.ts
@@ -0,0 +1,80 @@
+import { Status } from '../../../components/UploadForm';
+import { AWSPolicy } from '../../../types/AWSPolicy';
+import { modelName } from '../../../types/models';
+import { UploadableObject, uploadState } from '../../../types/tracks';
+import { makeFormData } from '../../../utils/makeFormData/makeFormData';
+import { Maybe } from '../../../utils/types';
+import { UploadProgressNotification } from '../../context/actions';
+import { initiateUpload } from '../initiateUpload/initiateUpload';
+import { uploadFile } from '../uploadFile/uploadFile';
+
+export const upload = (
+  updateObject: (object: UploadableObject) => void,
+  setStatus: (status: Status) => void,
+  notifyObjectUploadProgress: (
+    progress: UploadProgressNotification<UploadableObject>['progress'],
+  ) => void,
+  jwt: string,
+  objectType: modelName,
+  object: Maybe<UploadableObject>,
+) => async (file: Maybe<File>) => {
+  // Do not trigger an upload if we did not receive a file object.
+  if (!file) {
+    return;
+  }
+
+  if (!object) {
+    return setStatus('not_found_error');
+  }
+
+  let policy: AWSPolicy;
+  try {
+    policy = await initiateUpload(jwt, objectType, object.id);
+  } catch (error) {
+    return setStatus('policy_error');
+  }
+
+  setStatus('uploading');
+
+  // Use FormData to meet the requirement of a multi-part POST request for s3
+  // NB: order of keys is important here, which is why we do not iterate over an object
+  const formData = makeFormData.apply(null, [
+    ['key', policy.key],
+    ['acl', policy.acl],
+    ...(([modelName.VIDEOS, modelName.THUMBNAIL].includes(objectType)
+      ? [['Content-Type', file!.type]]
+      : []) as any),
+    ['X-Amz-Credential', policy.x_amz_credential],
+    ['X-Amz-Algorithm', policy.x_amz_algorithm],
+    ['X-Amz-Date', policy.x_amz_date],
+    ['Policy', policy.policy],
+    ['X-Amz-Signature', policy.x_amz_signature],
+    // Add the file after all of the text fields
+    ['file', file!],
+  ]);
+
+  try {
+    // Update the state to reflect the in-progress upload (for the dashboard)
+    // Useful for the Dashboard loader and help text.
+    updateObject({
+      ...object,
+      upload_state: uploadState.UPLOADING,
+    });
+
+    await uploadFile(
+      `https://${policy.s3_endpoint}/${policy.bucket}`,
+      formData,
+      notifyObjectUploadProgress,
+    );
+
+    updateObject({
+      ...object,
+      upload_state: uploadState.PROCESSING,
+    });
+  } catch (error) {
+    updateObject({
+      ...object,
+      upload_state: uploadState.ERROR,
+    });
+  }
+};
