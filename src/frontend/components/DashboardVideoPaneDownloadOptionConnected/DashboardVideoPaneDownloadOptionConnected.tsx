e84f3deae0dbe42093afb1d52b6fa6bcc55fa811
Mehdi Benadda <me@mbenadda.com>
2019-06-20 11:40:31 +0200

    ♻️(frontend) use appData directly in updateResource
    It is unnecessary to pass any part of the context reducer, except for
    `uploads_progress` through state management and connect shenanigans
    as almost all of it is entirely static.
    
    We can simply import `appData` directly and get rid of the boilerplate.

--- src/frontend/components/DashboardVideoPaneDownloadOptionConnected/DashboardVideoPaneDownloadOptionConnected.tsx
+++ src/frontend/components/DashboardVideoPaneDownloadOptionConnected/DashboardVideoPaneDownloadOptionConnected.tsx
@@ -4,20 +4,14 @@ import { Dispatch } from 'redux';
 import { DashboardVideoPaneDownloadOption } from '../DashboardVideoPaneDownloadOption/DashboardVideoPaneDownloadOption';
 
 import { addResource } from '../../data/genericReducers/resourceById/actions';
-import { RootState } from '../../data/rootReducer';
-import { appStateSuccess } from '../../types/AppData';
 import { modelName } from '../../types/models';
 import { Video } from '../../types/tracks';
-
-const mapStateToProps = (state: RootState<appStateSuccess>) => ({
-  jwt: state.context.jwt,
-});
 
 const mapDistachToProps = (dispatch: Dispatch) => ({
   addResource: (video: Video) => dispatch(addResource(modelName.VIDEOS, video)),
 });
 
 export const DashboardVideoPaneDownloadOptionConnected = connect(
-  mapStateToProps,
+  null,
   mapDistachToProps,
 )(DashboardVideoPaneDownloadOption);
