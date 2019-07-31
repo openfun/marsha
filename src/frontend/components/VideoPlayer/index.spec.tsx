42b7bc94323611a31b1fcb4e1c0fdbf8ba34693c
Manuel Raynaud <manu@raynaud.io>
2019-07-31 15:44:55 +0200

    ♻️(front) remove redux connect on VideoPlayer component
    VideoPlqayer component only read data from our zustand stores, so we can
    immediately remove the redux connect on this component.

--- src/frontend/components/VideoPlayer/index.spec.tsx
+++ src/frontend/components/VideoPlayer/index.spec.tsx
@@ -1,11 +1,8 @@
 import { render, wait } from '@testing-library/react';
 import fetchMock from 'fetch-mock';
 import React from 'react';
-import { Provider } from 'react-redux';
 
-import { bootstrapStore } from '../../data/bootstrapStore';
-import { appState } from '../../types/AppData';
-import { timedTextMode, uploadState, Video } from '../../types/tracks';
+import { appData } from '../../data/appData';
 import { isHlsSupported, isMSESupported } from '../../utils/isAbrSupported';
 import { jestMockOf } from '../../utils/types';
 import { VideoPlayer } from './index';
@@ -31,6 +28,71 @@ const mockIsMSESupported = isMSESupported as jestMockOf<typeof isMSESupported>;
 const mockIsHlsSupported = isHlsSupported as jestMockOf<typeof isHlsSupported>;
 
 const createPlayer = jest.fn();
+
+jest.mock('../../data/appData', () => ({
+  appData: {
+    video: {
+      description: 'Some description',
+      id: 'video-id',
+      is_ready_to_play: true,
+      show_download: false,
+      thumbnail: null,
+      timed_text_tracks: [
+        {
+          active_stamp: 1549385921,
+          id: 'ttt-1',
+          is_ready_to_play: true,
+          language: 'fr',
+          mode: 'st',
+          upload_state: 'ready',
+          url: 'https://example.com/timedtext/ttt-1.vtt',
+        },
+        {
+          active_stamp: 1549385922,
+          id: 'ttt-2',
+          is_ready_to_play: false,
+          language: 'fr',
+          mode: 'st',
+          upload_state: 'ready',
+          url: 'https://example.com/timedtext/ttt-2.vtt',
+        },
+        {
+          active_stamp: 1549385923,
+          id: 'ttt-3',
+          is_ready_to_play: true,
+          language: 'en',
+          mode: 'cc',
+          upload_state: 'ready',
+          url: 'https://example.com/timedtext/ttt-3.vtt',
+        },
+        {
+          active_stamp: 1549385924,
+          id: 'ttt-4',
+          is_ready_to_play: true,
+          language: 'fr',
+          mode: 'ts',
+          upload_state: 'ready',
+          url: 'https://example.com/timedtext/ttt-4.vtt',
+        },
+      ],
+      title: 'Some title',
+      upload_state: 'ready',
+      urls: {
+        manifests: {
+          dash: 'https://example.com/dash.mpd',
+          hls: 'https://example.com/hls.m3u8',
+        },
+        mp4: {
+          144: 'https://example.com/144p.mp4',
+          1080: 'https://example.com/1080p.mp4',
+        },
+        thumbnails: {
+          720: 'https://example.com/144p.jpg',
+        },
+      },
+    },
+  },
+}));
 
 describe('VideoPlayer', () => {
   beforeEach(() =>
@@ -61,80 +123,13 @@ describe('VideoPlayer', () => {
   afterEach(fetchMock.restore);
   afterEach(jest.clearAllMocks);
 
-  const video = {
-    description: 'Some description',
-    id: 'video-id',
-    is_ready_to_play: true,
-    show_download: false,
-    thumbnail: null,
-    timed_text_tracks: [
-      {
-        active_stamp: 1549385921,
-        id: 'ttt-1',
-        is_ready_to_play: true,
-        language: 'fr',
-        mode: timedTextMode.SUBTITLE,
-        upload_state: uploadState.READY,
-        url: 'https://example.com/timedtext/ttt-1.vtt',
-      },
-      {
-        active_stamp: 1549385922,
-        id: 'ttt-2',
-        is_ready_to_play: false,
-        language: 'fr',
-        mode: timedTextMode.SUBTITLE,
-        upload_state: uploadState.READY,
-        url: 'https://example.com/timedtext/ttt-2.vtt',
-      },
-      {
-        active_stamp: 1549385923,
-        id: 'ttt-3',
-        is_ready_to_play: true,
-        language: 'en',
-        mode: timedTextMode.CLOSED_CAPTIONING,
-        upload_state: uploadState.READY,
-        url: 'https://example.com/timedtext/ttt-3.vtt',
-      },
-      {
-        active_stamp: 1549385924,
-        id: 'ttt-4',
-        is_ready_to_play: true,
-        language: 'fr',
-        mode: timedTextMode.TRANSCRIPT,
-        upload_state: uploadState.READY,
-        url: 'https://example.com/timedtext/ttt-4.vtt',
-      },
-    ],
-    title: 'Some title',
-    upload_state: 'ready',
-    urls: {
-      manifests: {
-        dash: 'https://example.com/dash.mpd',
-        hls: 'https://example.com/hls.m3u8',
-      },
-      mp4: {
-        144: 'https://example.com/144p.mp4',
-        1080: 'https://example.com/1080p.mp4',
-      },
-      thumbnails: {
-        720: 'https://example.com/144p.jpg',
-      },
-    },
-  } as Video;
-
   it('starts up the player with DashJS and renders all the relevant sources', async () => {
     // Simulate a browser that supports MSE and will use DashJS
     mockIsMSESupported.mockReturnValue(true);
     mockIsHlsSupported.mockReturnValue(false);
-    const state = {
-      state: appState.INSTRUCTOR,
-      video,
-    } as any;
 
     const { container, getByText, queryByText } = render(
-      <Provider store={bootstrapStore(state)}>
-        <VideoPlayer createPlayer={createPlayer} video={video} />
-      </Provider>,
+      <VideoPlayer createPlayer={createPlayer} video={appData.video} />,
     );
     await wait();
 
@@ -175,18 +170,10 @@ describe('VideoPlayer', () => {
 
   it('allows video download when the video object specifies it', () => {
     mockIsMSESupported.mockReturnValue(false);
-    const state = {
-      state: appState.INSTRUCTOR,
-      video: {
-        ...video,
-        show_download: true,
-      },
-    } as any;
+    appData.video!.show_download = true;
 
     const { getByText } = render(
-      <Provider store={bootstrapStore(state)}>
-        <VideoPlayer createPlayer={createPlayer} video={video} />
-      </Provider>,
+      <VideoPlayer createPlayer={createPlayer} video={appData.video} />,
     );
 
     getByText(/Download this video/i);
@@ -196,15 +183,8 @@ describe('VideoPlayer', () => {
   it('does not use DashJS when MSE are not supported', async () => {
     // Simulate a browser that does not support MSE
     mockIsMSESupported.mockReturnValue(false);
-    const state = {
-      state: appState.INSTRUCTOR,
-      video,
-    } as any;
-
     const { container } = render(
-      <Provider store={bootstrapStore(state)}>
-        <VideoPlayer createPlayer={createPlayer} video={video} />
-      </Provider>,
+      <VideoPlayer createPlayer={createPlayer} video={appData.video} />,
     );
     await wait();
 
@@ -224,15 +204,8 @@ describe('VideoPlayer', () => {
   it('uses HLS source when browser support it', async () => {
     mockIsHlsSupported.mockReturnValue(true);
 
-    const state = {
-      state: appState.INSTRUCTOR,
-      video,
-    } as any;
-
     const { container } = render(
-      <Provider store={bootstrapStore(state)}>
-        <VideoPlayer createPlayer={createPlayer} video={video} />
-      </Provider>,
+      <VideoPlayer createPlayer={createPlayer} video={appData.video} />,
     );
     await wait();
 
