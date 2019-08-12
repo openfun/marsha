ca516c379e278692960b684a64b7301c2cc6ec07
Manuel Raynaud <manu@raynaud.io>
2019-08-12 16:39:56 +0200

    ➕(front) add intl and formatjs polyfills
    To ensure a good compatibility we need to install and configure a polyfill for
    intl.PluralRules and intl.RelativeTimeFormat.

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -14,6 +14,7 @@ import {
 import { AppRoutes } from './components/AppRoutes';
 import { appData } from './data/appData';
 import { DecodedJwt } from './types/jwt';
+import { report } from './utils/errors/report';
 // Load our style reboot into the DOM
 import { GlobalStyles } from './utils/theme/baseStyles';
 import { theme } from './utils/theme/theme';
@@ -29,6 +30,22 @@ export let intl: IntlShape;
 
 // Wait for the DOM to load before we scour it for an element that requires React to render
 document.addEventListener('DOMContentLoaded', async event => {
+  try {
+    if (!Intl.PluralRules) {
+      await import('intl-pluralrules');
+    }
+
+    if (!Intl.RelativeTimeFormat) {
+      await import('@formatjs/intl-relativetimeformat');
+      // Get `react-intl`/`formatjs` lang specific parameters and data
+      await import(
+        `@formatjs/intl-relativetimeformat/dist/locale-data/${localeCode}`
+      );
+    }
+  } catch (e) {
+    report(e);
+  }
+
   let translatedMessages = null;
   try {
     translatedMessages = await import(
