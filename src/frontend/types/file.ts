ac28a34dbaf19962ddbd66e17a7317a463432da9
Manuel Raynaud <manu@raynaud.io>
2019-09-09 18:31:06 +0200

    ✨(front) display filename in document dashboard
    When the document is ready we display the document filename. The
    user know wich filename the document has if he updates its title.

--- src/frontend/types/file.ts
+++ src/frontend/types/file.ts
@@ -2,6 +2,8 @@ import { Resource, uploadState } from './tracks';
 
 export interface Document extends Resource {
   description: string;
+  extension: string;
+  filename: string;
   is_ready_to_show: boolean;
   title: string;
   upload_state: uploadState;
