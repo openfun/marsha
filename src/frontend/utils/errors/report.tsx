094f49a48d684c46d36b7be5e7c83689076c188f
Manuel Raynaud <manu@raynaud.io>
2019-10-02 16:39:35 +0200

    ✨(front) configure and use sentry
    We want to use sentry to track errors in the front application. The dsn
    to use is expose by the back application using app_data in the LTI view.
    Once configured we use it in the report helper.

--- src/frontend/utils/errors/report.tsx
+++ src/frontend/utils/errors/report.tsx
@@ -1,4 +1,12 @@
+import * as Sentry from '@sentry/browser';
+
+import { appData } from '../../data/appData';
+
 export const report = (error: Error) => {
-  // tslint:disable:no-console
-  console.error(error);
+  if (appData.sentry_dsn) {
+    Sentry.captureException(error);
+  } else {
+    // tslint:disable:no-console
+    console.log(error);
+  }
 };
