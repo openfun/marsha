a6f61b69b06d3a224d997d2962c15754b64d2b09
Manuel Raynaud <manu@raynaud.io>
2019-10-03 17:41:05 +0200

    ✨(AWS) create a new lambda to run migrations on AWS
    We want to run migrations on our AWS infra. We choose to create a new
    lambda dedicated to this work. On each terraform apply this lambda will
    be trigger and will run the migrations declared in the environment
    variable MIGRATIONS. MIGRATIONS environment variable can contain a comma
    separated list of file without .js extension. Example:
    MIGRATIONS=0001_source_rename,0002_lambda_invoke

---
+++ src/aws/lambda-migrate/index.js
@@ -0,0 +1,44 @@
+exports.handler = async (event, context, callback) => {
+
+  let basePath = ''
+  if (process.env.NODE_ENV === 'production') {
+    basePath = 'src';
+  } else {
+    basePath = 'stubs'
+  }
+  let errors = [];
+  (process.env.MIGRATIONS || '')
+    .split(",")
+    .reduce((acc, migration) => {
+      if (migration.length > 0) {
+        acc.push(migration.trim());
+      }
+
+      return acc;
+    }, [])
+    .map(migration => `./${basePath}/migrations/${migration}.js`)
+    .map(migration => {
+      let toMigrate;
+      try {
+        toMigrate = require(migration);
+      } catch (e) {
+        console.error(`migration ${migration} does not exists`);
+        return;
+      }
+
+      try {
+        console.log(`executing migration ${migration}`);
+        toMigrate();
+        console.log(`end migration ${migration}`)
+      } catch (e){
+        errors.push(e);
+        console.error(e)
+      }
+    });
+
+  if (errors.length > 0) {
+    callback(errors);
+  } else {
+    callback(null, 200);
+  }
+};
---
+++ src/aws/lambda-migrate/index.spec.js
@@ -0,0 +1,30 @@
+const spyLog = jest.spyOn(console, 'log');
+const spyError = jest.spyOn(console, 'error');
+
+describe('lambda', () => {
+  beforeEach(() => {
+    jest.resetModules()
+    spyLog.mockReset();
+    spyLog.mockReset();
+    jest.resetAllMocks();
+  });
+  afterAll(() => {
+    spyLog.mockRestore();
+    spyError.mockRestore();
+  });
+
+  it('should execute nothing if MIGRATIONS env is missing', () => {
+    const lambda = require('./index.js').handler;
+    lambda(null, null, jest.fn());
+  });
+
+  it('should execute only existing migrations', () => {
+    process.env.MIGRATIONS = ' 0001_migration,foo';
+    const lambda = require('./index.js').handler;
+    lambda(null, null, jest.fn());
+    expect(spyError).toHaveBeenCalledWith('migration ./stubs/migrations/foo.js does not exists');
+    expect(spyLog).toHaveBeenNthCalledWith(1, 'executing migration ./stubs/migrations/0001_migration.js');
+    expect(spyLog).toHaveBeenNthCalledWith(2, 'foo');
+    expect(spyLog).toHaveBeenNthCalledWith(3, 'end migration ./stubs/migrations/0001_migration.js');
+  });
+});
