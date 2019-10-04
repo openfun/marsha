0a370b901af9d6c42206cdc8c26a0f9f33709903
Mehdi Benadda <me@mbenadda.com>
2019-10-04 17:49:25 +0200

    🔧(build) add a browserslist to babel config
    We did not specify a browserslist. This means we were using the
    default settings, which do not include Internet Explorer 11.
    
    Now that we decider to support IE11, we have to include it in our
    targets so that code is transpiled to run in it.

--- src/frontend/babel.config.js
+++ src/frontend/babel.config.js
@@ -16,6 +16,7 @@ module.exports = {
         forceAllTransforms: true,
         useBuiltIns: 'usage',
         corejs: 3,
+        targets: 'last 1 version, >0.2%, IE 11',
       },
     ],
     '@babel/preset-typescript',
