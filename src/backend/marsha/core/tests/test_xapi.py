cef6fbb51aec82aa833b1047b07d6678f2e7fb7c
Manuel Raynaud <manu@raynaud.io>
2020-05-04 16:46:15 +0200

    ♻️(back) create a dedicated class to compute xapi statements
    We want to separate how xapi statements are computed and enriched and
    how they are sent to a LRS. To do that a dedicated class XAPIStatement
    is created and its only job is to enrich a statement

--- src/backend/marsha/core/tests/test_xapi.py
+++ src/backend/marsha/core/tests/test_xapi.py
@@ -6,36 +6,24 @@ from django.test import TestCase
 from ..exceptions import MissingUserIdError
 from ..factories import VideoFactory
 from ..lti import LTIUser
-from ..xapi import XAPI, requests
+from ..xapi import XAPI, XAPIStatement, requests
 
 
 class MockLtiUser:
     """Mock LTIUser class."""
 
-    @property
-    def user_id(self):
-        """Force to raise a wanted exception."""
-        raise AttributeError("user_id")
 
+class XAPIStatmentTest(TestCase):
+    """Test the XAPIStatement class."""
 
-class XAPITest(TestCase):
-    """Test the xapi module."""
-
-    def test_xapi_missing_user_id(self):
+    def test_xapi_statement_missing_user_id(self):
         """Missing lti user_id should raise an exception."""
-        xapi = XAPI("https://lrs.example.com", "auth_token")
         video = VideoFactory()
-
         with self.assertRaises(MissingUserIdError):
-            xapi.send(video, {}, MockLtiUser())
+            XAPIStatement(video, {}, MockLtiUser())
 
-    @mock.patch.object(requests, "post")
-    def test_xapi_enrich_and_send_statement(self, mock_requests_post):
-        """XAPI statement sent by the front application should be enriched.
-
-        Before sending a statement, the xapi module is responsible for enriching it.
-        """
-        xapi = XAPI("https://lrs.example.com", "auth_token")
+    def test_xapi_statement_enrich_statement(self):
+        """XAPI statement sent by the front application should be enriched."""
         video = VideoFactory(
             id="68333c45-4b8c-4018-a195-5d5e1706b838",
             playlist__consumer_site__domain="example.com",
@@ -57,9 +45,6 @@ class XAPITest(TestCase):
 
         lti_user = LTIUser(mock_token_user)
 
-        mock_response = mock.MagicMock()
-        mock_response.raise_for_status.return_value = 200
-        mock_requests_post.return_value = mock_response
         base_statement = {
             "context": {
                 "extensions": {
@@ -83,30 +68,19 @@ class XAPITest(TestCase):
             "id": "17dfcd44-b3e0-403d-ab96-e3ef7da616d4",
         }
 
-        xapi.send(video, base_statement, lti_user)
+        xapi_statement = XAPIStatement(video, base_statement, lti_user)
+        statement = xapi_statement.get_statement()
 
-        args, kwargs = mock_requests_post.call_args_list[0]
-        self.assertEqual(args[0], "https://lrs.example.com")
+        self.assertIsNotNone(statement["timestamp"])
         self.assertEqual(
-            kwargs["headers"],
-            {
-                "Authorization": "auth_token",
-                "Content-Type": "application/json",
-                "X-Experience-API-Version": "1.0.3",
-            },
-        )
-        enriched_statement = kwargs["json"]
-
-        self.assertIsNotNone(enriched_statement["timestamp"])
-        self.assertEqual(
-            enriched_statement["actor"],
+            statement["actor"],
             {
                 "objectType": "Agent",
                 "account": {"name": "foo", "homePage": "http://example.com"},
             },
         )
         self.assertEqual(
-            enriched_statement["object"],
+            statement["object"],
             {
                 "definition": {
                     "type": "https://w3id.org/xapi/video/activity-type/video",
@@ -120,6 +94,40 @@ class XAPITest(TestCase):
                 "objectType": "Activity",
             },
         )
-        self.assertEqual(enriched_statement["verb"], base_statement["verb"])
-        self.assertEqual(enriched_statement["id"], base_statement["id"])
-        self.assertEqual(enriched_statement["result"], base_statement["result"])
+        self.assertEqual(statement["verb"], base_statement["verb"])
+        self.assertEqual(statement["id"], base_statement["id"])
+        self.assertEqual(statement["result"], base_statement["result"])
+
+
+class XAPITest(TestCase):
+    """Test the xapi module."""
+
+    @mock.patch.object(requests, "post")
+    def test_xapi_enrich_and_send_statement(self, mock_requests_post):
+        """XAPI statement sent by the front application should be enriched.
+
+        Before sending a statement, the xapi module is responsible for enriching it.
+        """
+        xapi = XAPI("https://lrs.example.com", "auth_token")
+
+        mock_response = mock.MagicMock()
+        mock_response.raise_for_status.return_value = 200
+        mock_requests_post.return_value = mock_response
+
+        statement = {"foo": "bar"}
+        mock_xapi_statement = mock.MagicMock()
+        mock_xapi_statement.get_statement.return_value = statement
+
+        xapi.send(mock_xapi_statement)
+
+        args, kwargs = mock_requests_post.call_args_list[0]
+        self.assertEqual(args[0], "https://lrs.example.com")
+        self.assertEqual(
+            kwargs["headers"],
+            {
+                "Authorization": "auth_token",
+                "Content-Type": "application/json",
+                "X-Experience-API-Version": "1.0.3",
+            },
+        )
+        self.assertEqual(kwargs["json"], statement)
