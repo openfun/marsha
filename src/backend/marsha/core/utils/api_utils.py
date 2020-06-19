a175be1fe034e453a5a2bf17c9ec8aec4af98c8b
Manuel Raynaud <manu@raynaud.io>
2020-06-19 17:58:41 +0200

    ✨(back) add a view to manage AWS request to update video live state
    We want to add the ability to an AWS lambda to update the video live
    state. We added a new view at endpoint /api/update-live-state, using the
    same signature algo from /api/update-state

---
+++ src/backend/marsha/core/utils/api_utils.py
@@ -0,0 +1,38 @@
+"""Utils used by the api module."""
+import hashlib
+import hmac
+
+from django.conf import settings
+
+
+def validate_signature(signature, message):
+    """Check if the provided signature is valid against any secret in our list.
+
+    We need to do this to support 2 or more versions of our infrastructure at the same time.
+    It then enables us to do updates and change the secret without incurring downtime.
+
+    Parameters
+    ----------
+    signature: string
+        The signature to validate.
+
+    message: string
+        The signed message to compare with the signature.
+
+    Return
+    ------
+    Boolean
+        Return true if the signature is valid. False otherwise.
+    """
+    print(
+        hmac.new(
+            "shared secret".encode("utf-8"), msg=message, digestmod=hashlib.sha256
+        ).hexdigest()
+    )
+    return any(
+        signature
+        == hmac.new(
+            secret.encode("utf-8"), msg=message, digestmod=hashlib.sha256
+        ).hexdigest()
+        for secret in settings.UPDATE_STATE_SHARED_SECRETS
+    )
