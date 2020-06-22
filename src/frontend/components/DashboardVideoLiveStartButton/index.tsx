678369c2107e4b5bd3cd5eb0380c382f2c68c1d7
Manuel Raynaud <manu@raynaud.io>
2020-06-22 11:14:06 +0200

    ✨(front) configure a button to start a live streaming
    To start a live we created a button to handle this action.

---
+++ src/frontend/components/DashboardVideoLiveStartButton/index.tsx
@@ -0,0 +1,59 @@
+import React, { useCallback, useState } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { Redirect } from 'react-router-dom';
+
+import { startLive } from '../../data/sideEffects/startLive';
+import { useVideo } from '../../data/stores/useVideo';
+import { Video } from '../../types/tracks';
+import { Nullable } from '../../utils/types';
+import { DashboardButton } from '../DashboardPaneButtons';
+import { ERROR_COMPONENT_ROUTE } from '../ErrorComponent/route';
+import { Loader } from '../Loader';
+
+const messages = defineMessages({
+  startLive: {
+    defaultMessage: 'start streaming',
+    description: 'Start a video streaming.',
+    id: 'components.DashboardVideoLive.startLive',
+  },
+});
+
+type startLiveStatus = 'pending' | 'error';
+
+interface DashboardVideoLiveStartButtonProps {
+  video: Video;
+}
+
+export const DashboardVideoLiveStartButton = ({
+  video,
+}: DashboardVideoLiveStartButtonProps) => {
+  const [status, setStatus] = useState<Nullable<startLiveStatus>>(null);
+  const { updateVideo } = useVideo((state) => ({
+    updateVideo: state.addResource,
+  }));
+
+  const startLiveAction = useCallback(async () => {
+    setStatus('pending');
+    try {
+      const updatedVideo = await startLive(video);
+      updateVideo(updatedVideo);
+    } catch (error) {
+      setStatus('error');
+    }
+  }, [video]);
+
+  if (status === 'error') {
+    return <Redirect push to={ERROR_COMPONENT_ROUTE('liveInit')} />;
+  }
+
+  return (
+    <React.Fragment>
+      {status === 'pending' && <Loader />}
+      <DashboardButton
+        label={<FormattedMessage {...messages.startLive} />}
+        primary={true}
+        onClick={startLiveAction}
+      />
+    </React.Fragment>
+  );
+};
---
+++ src/frontend/components/DashboardVideoLiveStopButton/index.spec.tsx
@@ -0,0 +1,160 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+import React from 'react';
+import { ImportMock } from 'ts-mock-imports';
+
+import * as useVideoModule from '../../data/stores/useVideo';
+import { uploadState, liveState } from '../../types/tracks';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import { wrapInRouter } from '../../utils/tests/router';
+import { ERROR_COMPONENT_ROUTE } from '../ErrorComponent/route';
+import { DashboardVideoLiveStopButton } from '.';
+
+jest.mock('jwt-decode', () => jest.fn());
+jest.mock('../../data/appData', () => ({
+  appData: { jwt: 'cool_token_m8' },
+}));
+
+jest.mock('../Loader', () => ({
+  Loader: () => <span>Loader</span>,
+}));
+
+const mockUpdateVideo = jest.fn();
+
+const useVideoStub = ImportMock.mockFunction(useVideoModule, 'useVideo');
+
+describe('components/DashboardVideoLiveStopButton', () => {
+  afterEach(() => {
+    fetchMock.restore();
+    jest.resetAllMocks();
+    useVideoStub.reset();
+  });
+
+  afterAll(useVideoStub.restore);
+
+  const video = {
+    description: '',
+    has_transcript: false,
+    id: 'f0e4835f-f126-457e-8e27-16898cd0b5d5',
+    is_ready_to_show: true,
+    show_download: true,
+    thumbnail: null,
+    timed_text_tracks: [],
+    title: '',
+    upload_state: uploadState.PENDING,
+    urls: {
+      manifests: {
+        dash: 'https://example.com/dash',
+        hls: 'https://example.com/hls',
+      },
+      mp4: {},
+      thumbnails: {},
+    },
+    should_use_subtitle_as_transcript: false,
+    playlist: {
+      title: 'foo',
+      lti_id: 'foo+context_id',
+    },
+    live_state: liveState.RUNNING,
+    live_info: {
+      medialive: {
+        input: {
+          endpoints: [
+            'rtmp://1.2.3.4:1935/stream-key-primary',
+            'rtmp://4.3.2.1:1935/stream-key-secondary',
+          ],
+        },
+      },
+    },
+  };
+
+  it('renders the stop button', () => {
+    useVideoStub.returns({
+      updateVideo: mockUpdateVideo,
+    });
+
+    render(
+      wrapInIntlProvider(
+        wrapInRouter(<DashboardVideoLiveStopButton video={video} />),
+      ),
+    );
+
+    screen.getByRole('button', { name: /stop streaming/i });
+  });
+
+  it('clicks on stop live button and fails.', async () => {
+    useVideoStub.returns({
+      updateVideo: mockUpdateVideo,
+    });
+
+    fetchMock.mock(
+      '/api/videos/f0e4835f-f126-457e-8e27-16898cd0b5d5/stop-live/',
+      400,
+      { method: 'POST' },
+    );
+
+    render(
+      wrapInIntlProvider(
+        wrapInRouter(<DashboardVideoLiveStopButton video={video} />, [
+          {
+            path: ERROR_COMPONENT_ROUTE('liveInit'),
+            render: () => <span>error</span>,
+          },
+        ]),
+      ),
+    );
+
+    // Loader should not be rendered yet
+    expect(screen.queryByText('Loader')).not.toBeInTheDocument();
+
+    const stopButton = screen.getByRole('button', {
+      name: /stop streaming/i,
+    });
+    fireEvent.click(stopButton);
+
+    // the state changes to pending, the loader is rendered
+    screen.getByText('Loader');
+
+    await waitFor(() => expect(fetchMock.called()).toBe(true));
+
+    screen.getByText('error');
+  });
+
+  it('clicks on stop live and it updates the video state', async () => {
+    useVideoStub.returns({
+      updateVideo: mockUpdateVideo,
+    });
+    fetchMock.mock(
+      '/api/videos/f0e4835f-f126-457e-8e27-16898cd0b5d5/stop-live/',
+      {
+        ...video,
+        live_state: liveState.STOPPED,
+      },
+      { method: 'POST' },
+    );
+
+    render(
+      wrapInIntlProvider(
+        wrapInRouter(<DashboardVideoLiveStopButton video={video} />),
+      ),
+    );
+
+    // Loader should not be rendered yet
+    expect(screen.queryByText('Loader')).not.toBeInTheDocument();
+
+    const stopButton = screen.getByRole('button', {
+      name: /stop streaming/i,
+    });
+    fireEvent.click(stopButton);
+
+    // the state changes to pending, the loader is rendered
+    screen.getByText('Loader');
+
+    await waitFor(() => expect(fetchMock.called()).toBe(true));
+
+    expect(mockUpdateVideo).toHaveBeenLastCalledWith({
+      ...video,
+      live_state: liveState.STOPPED,
+    });
+  });
+});
