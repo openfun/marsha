6b44a1ec133a206916e5da679a732b1d2c6d0e13
Manuel Raynaud <manu@raynaud.io>
2020-06-22 11:26:41 +0200

    ✨(front) create a dedicated dashboard to manage live video
    Live video have their own lifecycle. To manage it a dedicated dashboard
    is needed. In this Dashboard we found the information needed to
    configure a tool like OBS and the buttons to start, stop and watch the
    live streaming.

---
+++ src/frontend/components/DashboardVideoLive/index.spec.tsx
@@ -0,0 +1,188 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+import React from 'react';
+
+import { PLAYER_ROUTE } from '../routes';
+import { modelName } from '../../types/models';
+import { liveState, uploadState } from '../../types/tracks';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import { wrapInRouter } from '../../utils/tests/router';
+import { DashboardVideoLive } from '.';
+
+jest.mock('jwt-decode', () => jest.fn());
+jest.mock('../../data/appData', () => ({
+  appData: { jwt: 'cool_token_m8' },
+}));
+
+describe('components/DashboardVideoLive', () => {
+  beforeEach(() => jest.useFakeTimers());
+
+  afterEach(() => {
+    fetchMock.restore();
+    jest.resetAllMocks();
+  });
+
+  const video = {
+    description: '',
+    has_transcript: false,
+    id: '9e02ae7d-6c18-40ce-95e8-f87bbeae31c5',
+    is_ready_to_show: true,
+    show_download: true,
+    thumbnail: null,
+    timed_text_tracks: [],
+    title: '',
+    upload_state: uploadState.PENDING,
+    urls: {
+      manifests: {
+        dash: 'https://example.com/dash',
+        hls: 'https://example.com/hls',
+      },
+      mp4: {},
+      thumbnails: {},
+    },
+    should_use_subtitle_as_transcript: false,
+    playlist: {
+      title: 'foo',
+      lti_id: 'foo+context_id',
+    },
+    live_state: liveState.IDLE,
+    live_info: {
+      medialive: {
+        input: {
+          endpoints: [
+            'rtmp://1.2.3.4:1935/stream-key-primary',
+            'rtmp://4.3.2.1:1935/stream-key-secondary',
+          ],
+        },
+      },
+    },
+  };
+
+  it('displays steraming links', () => {
+    render(
+      wrapInIntlProvider(wrapInRouter(<DashboardVideoLive video={video} />)),
+    );
+
+    screen.getByText('Streaming link');
+    screen.getByText('url: rtmp://1.2.3.4:1935');
+    screen.getByText('stream key: stream-key-primary');
+    screen.getByText('url: rtmp://4.3.2.1:1935');
+    screen.getByText('stream key: stream-key-secondary');
+  });
+
+  it('shows the start button when the status id IDLE', () => {
+    render(
+      wrapInIntlProvider(wrapInRouter(<DashboardVideoLive video={video} />)),
+    );
+
+    screen.getByRole('button', { name: /start streaming/i });
+  });
+
+  it('shows the live and stop button when the status is LIVE', () => {
+    render(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <DashboardVideoLive
+            video={{ ...video, live_state: liveState.RUNNING }}
+          />,
+        ),
+      ),
+    );
+
+    screen.getByRole('button', { name: /show live/i });
+    screen.getByRole('button', { name: /stop streaming/i });
+  });
+
+  it('clicks on show live and redirects to the video player', () => {
+    render(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <DashboardVideoLive
+            video={{ ...video, live_state: liveState.RUNNING }}
+          />,
+          [
+            {
+              path: PLAYER_ROUTE(modelName.VIDEOS),
+              render: () => <span>video player</span>,
+            },
+          ],
+        ),
+      ),
+    );
+
+    const showLiveButton = screen.getByRole('button', { name: /show live/i });
+    expect(screen.queryByText('video player')).not.toBeInTheDocument();
+
+    fireEvent.click(showLiveButton);
+
+    screen.getByText('video player');
+  });
+
+  it('polls the video when live state is STARTING', async () => {
+    fetchMock.mock(
+      '/api/videos/9e02ae7d-6c18-40ce-95e8-f87bbeae31c5/',
+      JSON.stringify({ ...video, live_state: liveState.STARTING }),
+    );
+
+    const { rerender } = render(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <DashboardVideoLive
+            video={{ ...video, live_state: liveState.STARTING }}
+          />,
+        ),
+      ),
+    );
+
+    screen.getByText(
+      'Live streaming is starting. Wait before starting your stream.',
+    );
+    expect(fetchMock.called()).not.toBeTruthy();
+
+    // First backend call: the live is still starting
+    jest.advanceTimersByTime(1000 * 15 + 200);
+    await waitFor(() => expect(fetchMock.called()).toBe(true));
+
+    expect(fetchMock.lastCall()![0]).toEqual(
+      '/api/videos/9e02ae7d-6c18-40ce-95e8-f87bbeae31c5/',
+    );
+    expect(fetchMock.lastCall()![1]!.headers).toEqual({
+      Authorization: 'Bearer cool_token_m8',
+    });
+    screen.getByText(
+      'Live streaming is starting. Wait before starting your stream.',
+    );
+
+    // The live will be running in further response
+    fetchMock.restore();
+    fetchMock.mock(
+      '/api/videos/9e02ae7d-6c18-40ce-95e8-f87bbeae31c5/',
+      JSON.stringify({ ...video, live_state: liveState.RUNNING }),
+    );
+
+    // Second backend call
+    expect(fetchMock.called()).not.toBeTruthy();
+    jest.advanceTimersByTime(1000 * 15 + 200);
+    await waitFor(() => expect(fetchMock.called()).toBe(true));
+
+    expect(fetchMock.lastCall()![0]).toEqual(
+      '/api/videos/9e02ae7d-6c18-40ce-95e8-f87bbeae31c5/',
+    );
+    expect(fetchMock.lastCall()![1]!.headers).toEqual({
+      Authorization: 'Bearer cool_token_m8',
+    });
+
+    rerender(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <DashboardVideoLive
+            video={{ ...video, live_state: liveState.RUNNING }}
+          />,
+        ),
+      ),
+    );
+
+    screen.getByRole('button', { name: /show live/i });
+    screen.getByRole('button', { name: /stop streaming/i });
+  });
+});
