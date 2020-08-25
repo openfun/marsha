e9358429a0e2f5af94ac440689cf66a49a74532a
Manuel Raynaud <manu@raynaud.io>
2020-08-25 14:42:15 +0200

    ✨(aws) manage channel alerts in medialive lambda
    A case to manage channel alerts must be added. It does nothing more than
    calling the callback to end the lambda and it avoids an error and a
    lambda retry. Also the event log is not beautify anymore, it's harder to
    manipulate it afterward.

--- src/aws/lambda-medialive/index.js
+++ src/aws/lambda-medialive/index.js
@@ -3,7 +3,7 @@
 const channelStateChanged = require('./src/channelStateChanged');
 
 exports.handler = async (event, context, callback) => {
-  console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('Received event:', JSON.stringify(event));
   const type = event['detail-type'];
 
   switch(type) {
@@ -14,6 +14,10 @@ exports.handler = async (event, context, callback) => {
       } catch (error) {
         callback(error);
       }
+      break;
+    case 'MediaLive Channel Alert':
+      // just end the lambda, no treatment expected.
+      callback();
       break;
     default:
       callback('Unknown medialive event');
