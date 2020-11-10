a71c48efd366e94de9d85737312701e32692304f
Manuel Raynaud <manu@raynaud.io>
2020-11-10 09:34:35 +0100

    🚨(front) type guard value of decoded jwt
    jwt_token lib has its own type definition. This type returns unknown for
    jwt_decode function. This force us to rework how this value is handle by
    our function. Instead of casting it we choose to use a type guard, it's
    safer.

--- src/frontend/data/appData.ts
+++ src/frontend/data/appData.ts
@@ -9,12 +9,26 @@ export const appData = parseDataElements(
 
 // Jwt is lazily decoded only when needed and available
 let cacheDecodedJwt: DecodedJwt;
-export const getDecodedJwt = () => {
-  if (appData.jwt) {
-    return cacheDecodedJwt || (cacheDecodedJwt = jwt_decode(appData.jwt));
+const isDecodedJwt = (jwt: unknown): jwt is DecodedJwt => {
+  return (jwt as DecodedJwt).resource_id !== undefined;
+};
+export const getDecodedJwt = (): DecodedJwt => {
+  if (!appData.jwt) {
+    throw new Error(
+      'Impossible to decode JWT token, none present in the app state',
+    );
   }
 
-  throw new Error(
-    'Impossible to decode JWT token, none present in the app state',
-  );
+  if (cacheDecodedJwt) {
+    return cacheDecodedJwt;
+  }
+
+  const jwt = jwt_decode(appData.jwt);
+
+  if (isDecodedJwt(jwt)) {
+    cacheDecodedJwt = jwt;
+    return cacheDecodedJwt;
+  }
+
+  throw new Error('JWT token is invalid');
 };
