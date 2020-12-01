46eb7e02f2d2211a7ca26e8232ff51ea93813aa3
Manuel Raynaud <manu@raynaud.io>
2020-12-01 12:37:40 +0100

    ✨(front) hide configure live button when feature disabled
    Video live must be explicitely enabled in back environment in order to
    display the configuration button.

--- src/frontend/components/DashboardPaneButtons/index.spec.tsx
+++ src/frontend/components/DashboardPaneButtons/index.spec.tsx
@@ -1,22 +1,30 @@
 import { cleanup, render } from '@testing-library/react';
+import { mock } from 'fetch-mock';
 import React from 'react';
 
 import { DashboardPaneButtons } from '.';
+import { flags } from '../../types/AppData';
 import { modelName } from '../../types/models';
-import { uploadState } from '../../types/tracks';
+import { liveState, uploadState } from '../../types/tracks';
 import { videoMockFactory } from '../../utils/tests/factories';
 import { wrapInIntlProvider } from '../../utils/tests/intl';
 import { wrapInRouter } from '../../utils/tests/router';
 
 const { ERROR, PENDING, PROCESSING, READY, UPLOADING } = uploadState;
 
+let mockFlags = {};
 jest.mock('../../data/appData', () => ({
   appData: {
     video: {},
+    get flags() {
+      return mockFlags;
+    },
   },
 }));
 
 describe('<DashboardPaneButtons />', () => {
+  beforeEach(() => (mockFlags = {}));
+
   it('only renders the "Watch" button if the video is ready', async () => {
     const { getByText } = render(
       wrapInIntlProvider(
@@ -46,6 +54,62 @@ describe('<DashboardPaneButtons />', () => {
       expect(queryByText('Watch')).toBeNull();
       await cleanup();
     }
+  });
+
+  it('displays the configure live button', () => {
+    mockFlags = { [flags.VIDEO_LIVE]: true };
+    const { getByRole } = render(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <DashboardPaneButtons
+            object={videoMockFactory({ id: 'vid1', upload_state: PENDING })}
+            objectType={modelName.VIDEOS}
+          />,
+        ),
+      ),
+    );
+
+    getByRole('button', { name: 'Configure a live streaming' });
+  });
+
+  it('hides the configure live button when live state is not null', () => {
+    mockFlags = { [flags.VIDEO_LIVE]: false };
+    const { queryByRole } = render(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <DashboardPaneButtons
+            object={videoMockFactory({
+              id: 'vid1',
+              upload_state: PENDING,
+              live_state: liveState.IDLE,
+            })}
+            objectType={modelName.VIDEOS}
+          />,
+        ),
+      ),
+    );
+
+    expect(
+      queryByRole('button', { name: 'Configure a live streaming' }),
+    ).toBeNull();
+  });
+
+  it('hides the configure live button when the flag is disabled', () => {
+    mockFlags = { [flags.VIDEO_LIVE]: false };
+    const { queryByRole } = render(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <DashboardPaneButtons
+            object={videoMockFactory({ id: 'vid1', upload_state: PENDING })}
+            objectType={modelName.VIDEOS}
+          />,
+        ),
+      ),
+    );
+
+    expect(
+      queryByRole('button', { name: 'Configure a live streaming' }),
+    ).toBeNull();
   });
 
   it('adapts the text of the "Upload" button to the video state', () => {
--- src/frontend/components/DashboardPaneButtons/index.tsx
+++ src/frontend/components/DashboardPaneButtons/index.tsx
@@ -5,7 +5,9 @@ import styled from 'styled-components';
 
 import { Document } from '../../types/file';
 import { modelName } from '../../types/models';
+import { flags } from '../../types/AppData';
 import { uploadState, Video } from '../../types/tracks';
+import { isFeatureEnabled } from '../../utils/isFeatureEnabled';
 import { DashboardVideoLiveConfigureButton } from '../DashboardVideoLiveConfigureButton';
 import { PLAYER_ROUTE } from '../routes';
 import { UPLOAD_FORM_ROUTE } from '../UploadForm/route';
@@ -94,7 +96,8 @@ export const DashboardPaneButtons = ({
       margin={'small'}
     >
       {objectType === modelName.VIDEOS &&
-        object.upload_state === uploadState.PENDING && (
+        object.upload_state === uploadState.PENDING &&
+        isFeatureEnabled(flags.VIDEO_LIVE) && (
           <DashboardVideoLiveConfigureButton video={object as Video} />
         )}
       <DashboardButtonWithLink
