71888544a73c8b73b0cc8af46f4bb810f6f73056
Manuel Raynaud <manu@raynaud.io>
2021-01-06 15:11:55 +0100

    ♻️(aws) rename medialive-routing resources to elemental-routing
    All resrouces named medialive-routing in the shared-resources are
    renamed in elemental-routing. We will manage other resources than
    medialive, like mediapackage and they can be managed with the same
    lambda, we will not duplicate the code but mutualise them.

--- src/aws/shared_resources/cloudwatch.tf
+++ src/aws/shared_resources/cloudwatch.tf
@@ -1,6 +1,6 @@
-resource "aws_cloudwatch_event_rule" "marsha_medialive_routing_channel_state_change" {
-  name        = "marsha-medialive-routing-channel-state-change"
-  description = "Fires each time a medialive channel state changes."
+resource "aws_cloudwatch_event_rule" "marsha_elemental_routing_channel_state_change" {
+  name        = "marsha-elemental-routing-channel-state-change"
+  description = "Fires on every elemental events."
 
   event_pattern = <<PATTERN
 {
@@ -12,8 +12,8 @@ PATTERN
 }
 
 
-resource "aws_cloudwatch_event_target" "marsha_medialive_routing_channel_state_change_target" {
-  rule      = aws_cloudwatch_event_rule.marsha_medialive_routing_channel_state_change.name
-  target_id = "medialive"
-  arn       = aws_lambda_function.marsha_medialive_routing_lambda.arn
+resource "aws_cloudwatch_event_target" "marsha_elemental_routing_channel_state_change_target" {
+  rule      = aws_cloudwatch_event_rule.marsha_elemental_routing_channel_state_change.name
+  target_id = "elemental"
+  arn       = aws_lambda_function.marsha_elemental_routing_lambda.arn
 }
