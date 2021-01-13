21065dc36c2647bb49a034a7ae05066a6ccf18fd
Manuel Raynaud <manu@raynaud.io>
2021-01-13 18:47:00 +0100

    ✨(aws) manage mediapackage events in element routing lambda
    we need to manage new events from elemental products. When an harved job
    is succeeded, we want to trigger the mediapackage lambda

--- src/aws/lambda-elemental-routing/index.js
+++ src/aws/lambda-elemental-routing/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const medialive = require('./src/medialive');
+const mediapackage = require('./src/mediapackage');
 
 exports.handler = async (event) => {
   console.log('Received event:', JSON.stringify(event));
   switch (event.source) {
     case 'aws.medialive':
       return medialive(event);
+    case 'aws.mediapackage':
+      return mediapackage(event);
   }
 };
