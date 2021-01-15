aad87970da97213e6ec8a79894b268c8b65cad17
Mehdi Benadda <me@mbenadda.com>
2021-01-15 01:15:49 +0100

    ✨(frontend) add a data source for the current user
    As we build the marsha site, we're using an actual django user
    instead of an LTI session and a bunch of JWT claims. As a result,
    we need to use our new whoami endpoint to get some information
    about the current user.
    
    Create a User type matching what our API user serializer returns
    and build out a small utility to access and cache it with zustand.

---
+++ src/frontend/data/sideEffects/getCurrentUser/index.tsx
@@ -0,0 +1,40 @@
+import { API_ENDPOINT } from '../../../settings';
+import { requestStatus } from '../../../types/api';
+import { report } from '../../../utils/errors/report';
+import { appData } from '../../appData';
+import { AnonymousUser } from '../../stores/useCurrentUser';
+
+/**
+ * Makes and handles the GET request for the current user.
+ * @returns a promise for a request status, so the side effect caller can simply wait for it if needed.
+ */
+export const getCurrentUser = async (): Promise<requestStatus> => {
+  try {
+    const { useCurrentUser } = await import('../../stores/useCurrentUser');
+
+    const response = await fetch(`${API_ENDPOINT}/users/whoami/`, {
+      headers: {
+        Authorization: `Bearer ${appData.jwt}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.status === 401) {
+      useCurrentUser.getState().setCurrentUser(AnonymousUser.ANONYMOUS);
+      return requestStatus.SUCCESS;
+    }
+
+    if (!response.ok) {
+      // Push remote errors to the error channel for consistency
+      throw new Error(`Failed to get current user : ${response.status}.`);
+    }
+
+    const currentUser = await response.json();
+    useCurrentUser.getState().setCurrentUser(currentUser);
+
+    return requestStatus.SUCCESS;
+  } catch (error) {
+    report(error);
+    return requestStatus.FAILURE;
+  }
+};
