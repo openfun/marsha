2fe4acb3f9199a1d01013fde55e0bdfc94b376c1
Mehdi Benadda <me@mbenadda.com>
2021-02-08 18:23:46 +0100

    ♻️(api) update existing list requests with DRF pagination
    Before we added the default DRF pagination to our settings, there
    already existed one api route for a list of resources. We need to
    update its tests and call sites in the frontend to make sure it
    can work from the pagination-supporting object DRF returns, rather
    than a plain list.

--- src/backend/marsha/core/tests/test_api_timed_text_track.py
+++ src/backend/marsha/core/tests/test_api_timed_text_track.py
@@ -468,12 +468,15 @@ class TimedTextTrackAPITest(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         timed_text_track_list = json.loads(response.content)
-        self.assertEqual(len(timed_text_track_list), 2)
+        self.assertEqual(len(timed_text_track_list["results"]), 2)
+        self.assertEqual(timed_text_track_list["count"], 2)
         self.assertTrue(
-            str(timed_text_track_one.id) in (ttt["id"] for ttt in timed_text_track_list)
+            str(timed_text_track_one.id)
+            in (ttt["id"] for ttt in timed_text_track_list["results"])
         )
         self.assertTrue(
-            str(timed_text_track_two.id) in (ttt["id"] for ttt in timed_text_track_list)
+            str(timed_text_track_two.id)
+            in (ttt["id"] for ttt in timed_text_track_list["results"])
         )
 
     def test_api_timed_text_track_read_list_staff_or_user(self):
