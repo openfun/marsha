6d78e4d4276b07685b751be1fe717c3cedbb2711
Manuel Raynaud <manu@raynaud.io>
2021-02-15 12:02:09 +0100

    ✨(front) check browser MSE compat and fallback on MP4 if needed
    All browsers are not compatible with the Media Source Element API. When
    they are not, videojs does not fallback on other available sources. To
    force using them, we detect if the MSE API is available, if not only MP4
    sources are loaded and the quality selector plugin is activated.

--- src/frontend/Player/createVideojsPlayer.ts
+++ src/frontend/Player/createVideojsPlayer.ts
@@ -1,19 +1,23 @@
 import 'video.js/dist/video-js.css';
-import videojs, { VideoJsPlayer } from 'video.js';
+import videojs, { VideoJsPlayer, VideoJsPlayerOptions } from 'video.js';
 import 'videojs-contrib-quality-levels';
 import 'videojs-http-source-selector';
+import './videojs/qualitySelectorPlugin';
 
 import { appData, getDecodedJwt } from '../data/appData';
+import { QualityLevels } from '../types/libs/video.js/extend';
 import { Video } from '../types/tracks';
 import {
   InitializedContextExtensions,
   InteractedContextExtensions,
 } from '../types/XAPI';
 import { report } from '../utils/errors/report';
+import { isMSESupported } from '../utils/isMSESupported';
 import { Nullable } from '../utils/types';
 import { XAPIStatement } from '../XAPI/XAPIStatement';
 
 import { intl } from '../index';
+import { Events } from './videojs/qualitySelectorPlugin/types';
 
 export const createVideojsPlayer = (
   videoNode: HTMLVideoElement,
@@ -23,7 +27,7 @@ export const createVideojsPlayer = (
   // add the video-js class name to the video attribute.
   videoNode.classList.add('video-js', 'vjs-big-play-centered');
 
-  const player = videojs(videoNode, {
+  const options: VideoJsPlayerOptions = {
     controls: true,
     debug: false,
     fluid: true,
@@ -35,15 +39,27 @@ export const createVideojsPlayer = (
     language: intl.locale,
     liveui: video.live_state !== null,
     playbackRates: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 4],
-    plugins: {
-      httpSourceSelector: {
-        default: 'auto',
-      },
-    },
     responsive: true,
-  });
-  player.httpSourceSelector();
-  const qualityLevels = player.qualityLevels();
+  };
+
+  if (!isMSESupported()) {
+    options.plugins = {
+      qualitySelector: {
+        default: '480',
+      },
+    };
+  }
+
+  const player = videojs(videoNode, options);
+
+  if (isMSESupported()) {
+    player.httpSourceSelector();
+    const qualityLevels = player.qualityLevels();
+    qualityLevels.on('change', () => interacted(qualityLevels));
+  } else {
+    player.on(Events.PLAYER_SOURCES_CHANGED, () => interacted());
+  }
+
   const tracks = player.remoteTextTracks();
 
   /************************** XAPI **************************/
@@ -128,15 +144,22 @@ export const createVideojsPlayer = (
   });
 
   /**************** Interacted event *************************/
-  const interacted = (): void => {
+  const interacted = (qualityLevels?: QualityLevels): void => {
     if (!isInitialized) {
       return;
+    }
+    let quality: string | number;
+
+    if (qualityLevels) {
+      quality = qualityLevels[qualityLevels.selectedIndex]?.height;
+    } else {
+      quality = player.currentSource().size!;
     }
 
     const contextExtensions: InteractedContextExtensions = {
       ccSubtitleEnabled: currentTrack !== null,
       fullScreen: player.isFullscreen(),
-      quality: qualityLevels[qualityLevels.selectedIndex]?.height,
+      quality,
       speed: `${player.playbackRate()}x`,
       volume: player.volume(),
     };
@@ -151,7 +174,6 @@ export const createVideojsPlayer = (
   player.on('languagechange', () => interacted());
   player.on('ratechange', () => interacted());
   player.on('volumechange', () => interacted());
-  qualityLevels.on('change', () => interacted());
   tracks.addEventListener('change', () => {
     currentTrack = getCurrentTrack();
     interacted();
