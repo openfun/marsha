92556f5278e988ac80631da6e3ab06f66cefb0fd
Mehdi Benadda <me@mbenadda.com>
2021-03-01 17:27:34 +0100

    ♻️(frontend) update untitled object titles with file name
    There is currently a "fix" in the <UploadForm /> that updates
    objects which can receive a title with the file name as the object
    title upon download completion.
    
    This provides titles for objects without relying on user input. Is
    is not the way we intend to proceed with the new Marsha site. In our
    opinion, the LTI app will probably evolve in the same direction
    later on.
    
    However, for now we need to provide feature parity as we simply
    rework the LTI app to use <UploadManager />. To that end, we added
    a component at root tasked with applying this mutation on all
    relevant objects when uploads successfully end.

---
+++ src/frontend/components/UploadManager/LTIUploadHandlers.spec.tsx
@@ -0,0 +1,160 @@
+import { render, waitFor } from '@testing-library/react';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { updateResource } from '../../data/sideEffects/updateResource';
+import { addResource, getResource } from '../../data/stores/generics';
+import { modelName } from '../../types/models';
+import { Thumbnail, Video } from '../../types/tracks';
+import { jestMockOf } from '../../utils/types';
+import { LTIUploadHandlers } from './LTIUploadHandlers';
+import { UploadManagerContext, UploadManagerStatus } from '.';
+
+jest.mock('../../data/appData', () => ({}));
+jest.mock('../../data/sideEffects/updateResource', () => ({
+  updateResource: jest.fn(),
+}));
+jest.mock('../../data/stores/generics', () => ({
+  addResource: jest.fn(),
+  getResource: jest.fn(),
+}));
+
+const mockAddResource = addResource as jestMockOf<typeof addResource>;
+const mockGetResource = getResource as jestMockOf<typeof getResource>;
+const mockUpdateResource = updateResource as jestMockOf<typeof updateResource>;
+
+describe('<LTIUploadHandlers />', () => {
+  const file = new File(['(⌐□_□)'], 'course.mp4', { type: 'video/mp4' });
+  const objectState = {
+    file,
+    objectId: uuidv4(),
+    objectType: modelName.VIDEOS,
+    progress: 0,
+    status: UploadManagerStatus.UPLOADING,
+  };
+  const object = { id: objectState.objectId, title: '' } as Video;
+
+  beforeEach(() => jest.resetAllMocks());
+
+  it('updates the object name to the file name upon upload success', async () => {
+    mockGetResource.mockResolvedValue(object);
+
+    const { rerender } = render(
+      <UploadManagerContext.Provider
+        value={{
+          setUploadState: () => {},
+          uploadManagerState: { [objectState.objectId]: objectState },
+        }}
+      >
+        <LTIUploadHandlers />
+      </UploadManagerContext.Provider>,
+    );
+
+    expect(mockAddResource).not.toHaveBeenCalled();
+    expect(mockUpdateResource).not.toHaveBeenCalled();
+
+    rerender(
+      <UploadManagerContext.Provider
+        value={{
+          setUploadState: () => {},
+          uploadManagerState: {
+            [objectState.objectId]: {
+              ...objectState,
+              status: UploadManagerStatus.SUCCESS,
+            },
+          },
+        }}
+      >
+        <LTIUploadHandlers />
+      </UploadManagerContext.Provider>,
+    );
+
+    await waitFor(() => {
+      expect(mockAddResource).toHaveBeenCalledWith(modelName.VIDEOS, {
+        id: object.id,
+        title: file.name,
+      });
+      expect(mockUpdateResource).toHaveBeenCalledWith(
+        {
+          id: object.id,
+          title: file.name,
+        },
+        modelName.VIDEOS,
+      );
+    });
+  });
+
+  it('does not do anything unless the upload manager status for the object becomes SUCCESS', () => {
+    mockGetResource.mockResolvedValue(object);
+
+    const { rerender } = render(
+      <UploadManagerContext.Provider
+        value={{
+          setUploadState: () => {},
+          uploadManagerState: { [objectState.objectId]: objectState },
+        }}
+      >
+        <LTIUploadHandlers />
+      </UploadManagerContext.Provider>,
+    );
+
+    expect(mockAddResource).not.toHaveBeenCalled();
+    expect(mockUpdateResource).not.toHaveBeenCalled();
+
+    rerender(
+      <UploadManagerContext.Provider
+        value={{
+          setUploadState: () => {},
+          uploadManagerState: {
+            [objectState.objectId]: {
+              ...objectState,
+              status: UploadManagerStatus.ERR_UPLOAD,
+            },
+          },
+        }}
+      >
+        <LTIUploadHandlers />
+      </UploadManagerContext.Provider>,
+    );
+
+    expect(mockAddResource).not.toHaveBeenCalled();
+    expect(mockUpdateResource).not.toHaveBeenCalled();
+  });
+
+  it('does not do anything for objects which do not have a title', () => {
+    mockGetResource.mockResolvedValue({ id: object.id } as Thumbnail);
+
+    const { rerender } = render(
+      <UploadManagerContext.Provider
+        value={{
+          setUploadState: () => {},
+          uploadManagerState: { [objectState.objectId]: objectState },
+        }}
+      >
+        <LTIUploadHandlers />
+      </UploadManagerContext.Provider>,
+    );
+
+    expect(mockAddResource).not.toHaveBeenCalled();
+    expect(mockUpdateResource).not.toHaveBeenCalled();
+
+    rerender(
+      <UploadManagerContext.Provider
+        value={{
+          setUploadState: () => {},
+          uploadManagerState: {
+            [objectState.objectId]: {
+              ...objectState,
+              status: UploadManagerStatus.SUCCESS,
+            },
+          },
+        }}
+      >
+        <LTIUploadHandlers />
+      </UploadManagerContext.Provider>,
+    );
+
+    expect(mockAddResource).not.toHaveBeenCalled();
+    expect(mockUpdateResource).not.toHaveBeenCalled();
+  });
+});
