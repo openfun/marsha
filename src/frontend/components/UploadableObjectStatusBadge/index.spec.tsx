dbc13db9a9c62196c2613360dd5370fe6b741f56
Mehdi Benadda <me@mbenadda.com>
2021-03-15 18:48:53 +0100

    ✨(frontend) add a component to display lists of videos
    Displaying a list of videos is a need that is recurring in the new
    marsha site and that will probably arise in other places.
    
    As a result, we want to build a flexible component to consistently
    show that list, with differing parameters depending on the use case.
    We intend to upgrade it with search, filtering and pagination later
    on as we continue to build out Marsha.
    
    We also needed a video badge to compute and display a user-facing
    status for a given video.

---
+++ src/frontend/components/UploadableObjectStatusBadge/index.spec.tsx
@@ -0,0 +1,199 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+
+import { modelName } from '../../types/models';
+import { uploadState } from '../../types/tracks';
+import * as factories from '../../utils/tests/factories';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import { UploadManagerContext, UploadManagerStatus } from '../UploadManager';
+import { UploadableObjectStatusBadge } from '.';
+
+jest.mock('../../data/appData', () => ({}));
+
+describe('<UploadableObjectStatusBadge />', () => {
+  it('shows the status for an object that is READY', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.READY,
+    });
+    render(wrapInIntlProvider(<UploadableObjectStatusBadge object={object} />));
+
+    screen.getByRole('status');
+    screen.getByText('Ready');
+  });
+
+  it('shows the status for an object that is PROCESSING', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.PROCESSING,
+    });
+    render(wrapInIntlProvider(<UploadableObjectStatusBadge object={object} />));
+
+    screen.getByRole('status');
+    screen.getByText('Processing');
+  });
+
+  it('shows the status for an object that is in ERROR', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.ERROR,
+    });
+    render(wrapInIntlProvider(<UploadableObjectStatusBadge object={object} />));
+
+    screen.getByRole('status');
+    screen.getByText('Error');
+  });
+
+  it('shows the status for an object that is PENDING with no upload related information', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.PENDING,
+    });
+    render(wrapInIntlProvider(<UploadableObjectStatusBadge object={object} />));
+
+    screen.getByRole('status');
+    screen.getByText('Pending');
+  });
+
+  it('shows the status for an object that is PENDING with an upload just starting', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.PENDING,
+    });
+
+    const uploadManagerState = {
+      [object.id]: {
+        file: new File(['(⌐□_□)'], 'video.mp4'),
+        objectId: object.id,
+        objectType: modelName.VIDEOS,
+        progress: 0,
+        status: UploadManagerStatus.INIT,
+      },
+    };
+
+    render(
+      wrapInIntlProvider(
+        <UploadManagerContext.Provider
+          value={{ setUploadState: jest.fn(), uploadManagerState }}
+        >
+          <UploadableObjectStatusBadge object={object} />
+        </UploadManagerContext.Provider>,
+      ),
+    );
+
+    screen.getByRole('status');
+    screen.getByText('Uploading');
+  });
+
+  it('shows the status for an object that is PENDING with an upload in progress', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.PENDING,
+    });
+
+    const uploadManagerState = {
+      [object.id]: {
+        file: new File(['(⌐□_□)'], 'video.mp4'),
+        objectId: object.id,
+        objectType: modelName.VIDEOS,
+        progress: 0,
+        status: UploadManagerStatus.UPLOADING,
+      },
+    };
+
+    render(
+      wrapInIntlProvider(
+        <UploadManagerContext.Provider
+          value={{ setUploadState: jest.fn(), uploadManagerState }}
+        >
+          <UploadableObjectStatusBadge object={object} />
+        </UploadManagerContext.Provider>,
+      ),
+    );
+
+    screen.getByRole('status');
+    screen.getByText('Uploading');
+  });
+
+  it('shows the status for an object that is PENDING with a failed upload (policy)', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.PENDING,
+    });
+
+    const uploadManagerState = {
+      [object.id]: {
+        file: new File(['(⌐□_□)'], 'video.mp4'),
+        objectId: object.id,
+        objectType: modelName.VIDEOS,
+        progress: 0,
+        status: UploadManagerStatus.ERR_POLICY,
+      },
+    };
+
+    render(
+      wrapInIntlProvider(
+        <UploadManagerContext.Provider
+          value={{ setUploadState: jest.fn(), uploadManagerState }}
+        >
+          <UploadableObjectStatusBadge object={object} />
+        </UploadManagerContext.Provider>,
+      ),
+    );
+
+    screen.getByRole('status');
+    screen.getByText('Error');
+  });
+
+  it('shows the status for an object that is PENDING with a failed upload (upload)', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.PENDING,
+    });
+
+    const uploadManagerState = {
+      [object.id]: {
+        file: new File(['(⌐□_□)'], 'video.mp4'),
+        objectId: object.id,
+        objectType: modelName.VIDEOS,
+        progress: 0,
+        status: UploadManagerStatus.ERR_UPLOAD,
+      },
+    };
+
+    render(
+      wrapInIntlProvider(
+        <UploadManagerContext.Provider
+          value={{ setUploadState: jest.fn(), uploadManagerState }}
+        >
+          <UploadableObjectStatusBadge object={object} />
+        </UploadManagerContext.Provider>,
+      ),
+    );
+
+    screen.getByRole('status');
+    screen.getByText('Error');
+  });
+
+  it('shows the status for an object that is PENDING with an upload that succeeded', () => {
+    const object = factories.videoMockFactory({
+      upload_state: uploadState.PENDING,
+    });
+
+    const uploadManagerState = {
+      [object.id]: {
+        file: new File(['(⌐□_□)'], 'video.mp4'),
+        objectId: object.id,
+        objectType: modelName.VIDEOS,
+        progress: 0,
+        status: UploadManagerStatus.SUCCESS,
+      },
+    };
+
+    render(
+      wrapInIntlProvider(
+        <UploadManagerContext.Provider
+          value={{ setUploadState: jest.fn(), uploadManagerState }}
+        >
+          <UploadableObjectStatusBadge object={object} />
+        </UploadManagerContext.Provider>,
+      ),
+    );
+
+    screen.getByRole('status');
+    screen.getByText('Processing');
+  });
+});
---
+++ src/frontend/components/VideosList/index.spec.tsx
@@ -0,0 +1,134 @@
+import { act, render, screen } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { Deferred } from '../../utils/tests/Deferred';
+import * as factories from '../../utils/tests/factories';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import { VideosList } from '.';
+
+jest.mock('../../data/appData', () => ({
+  appData: {
+    jwt: 'cool_token_m8',
+  },
+}));
+
+beforeEach(() => fetchMock.restore());
+
+describe('<VideosList />', () => {
+  it('loads the list of videos and shows it in a table', async () => {
+    const queryClient = new QueryClient();
+
+    const deferred = new Deferred();
+    fetchMock.get('/api/videos/?limit=999', deferred.promise);
+
+    render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <VideosList />
+        </QueryClientProvider>,
+      ),
+    );
+
+    screen.getByRole('status', { name: 'Loading videos...' });
+    expect(fetchMock.lastCall()![0]).toEqual('/api/videos/?limit=999');
+    expect(fetchMock.lastCall()![1]).toEqual({
+      headers: {
+        Authorization: 'Bearer cool_token_m8',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const video1 = factories.videoMockFactory();
+    const video2 = factories.videoMockFactory();
+
+    await act(async () =>
+      deferred.resolve({
+        count: 2,
+        next: null,
+        previous: null,
+        results: [video1, video2],
+      }),
+    );
+
+    screen.getByRole('columnheader', { name: 'Title' });
+    screen.getByRole('columnheader', { name: 'Playlist' });
+    screen.getByRole('columnheader', { name: 'Upload state' });
+
+    screen.getByRole('rowheader', { name: video1.title });
+    screen.getByRole('cell', { name: video1.playlist.title });
+    screen.getByRole('rowheader', { name: video2.title });
+    screen.getByRole('cell', { name: video2.playlist.title });
+  });
+
+  it('shows an explanatory message when the list of playlists is empty', async () => {
+    const queryClient = new QueryClient();
+
+    const deferred = new Deferred();
+    fetchMock.get('/api/videos/?limit=999', deferred.promise);
+
+    render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <VideosList />
+        </QueryClientProvider>,
+      ),
+    );
+
+    screen.getByRole('status', { name: 'Loading videos...' });
+
+    await act(async () =>
+      deferred.resolve({
+        count: 0,
+        next: null,
+        previous: null,
+        results: [],
+      }),
+    );
+
+    screen.getByRole('table');
+    screen.getByText('There are no videos for this list yet.');
+  });
+
+  it('shows an error message when it fails to load the list of videos', async () => {
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+
+    const deferred = new Deferred();
+    fetchMock.get('/api/videos/?limit=999', deferred.promise);
+
+    jest.spyOn(console, 'error').mockImplementation(() => jest.fn());
+
+    render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <VideosList />
+        </QueryClientProvider>,
+      ),
+    );
+
+    screen.getByRole('status', { name: 'Loading videos...' });
+    expect(fetchMock.lastCall()![0]).toEqual('/api/videos/?limit=999');
+    expect(fetchMock.lastCall()![1]).toEqual({
+      headers: {
+        Authorization: 'Bearer cool_token_m8',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    await act(async () => deferred.resolve(500));
+
+    screen.getByText('There was an unexpected error');
+    // tslint:disable:no-console
+    expect(console.error).toHaveBeenCalledWith(
+      new Error('Failed to get list of videos.'),
+    );
+  });
+});
