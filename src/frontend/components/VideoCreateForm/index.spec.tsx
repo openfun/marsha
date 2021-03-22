f565e8579b03df7498259fdc442050ec8bdcd447
Mehdi Benadda <me@mbenadda.com>
2021-03-22 10:52:33 +0100

    ✨(frontend) add a 2-step form to create a new video
    In the Marsha LTI app, videos are automatically created when the
    LTI request is made to marsha by the LMS. This is not the case in
    the Marsha site, where we need to manually create the videos from
    the frontend.
    
    To help users find their videos later on, we need to make it
    mandatory to add a title for the video (and the playlist, if the
    context does not make it obvious). We made the form 2-step so they
    can first input the relevant values before they pick a file to
    upload. The third step is just a helpful message with a button to
    open the form for another video.

---
+++ src/frontend/components/VideoCreateForm/index.spec.tsx
@@ -0,0 +1,340 @@
+import { act, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import fetchMock from 'fetch-mock';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { modelName } from '../../types/models';
+import { Deferred } from '../../utils/tests/Deferred';
+import {
+  playlistMockFactory,
+  videoMockFactory,
+} from '../../utils/tests/factories';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import { UploadManagerContext, UploadManagerStatus } from '../UploadManager';
+import { VideoCreateForm } from '.';
+
+jest.mock('../../data/appData', () => ({
+  appData: {},
+}));
+
+describe('<VideoCreateForm />', () => {
+  jest.spyOn(console, 'error').mockImplementation(() => jest.fn());
+
+  beforeEach(() => {
+    fetchMock.restore();
+  });
+
+  it('lets the user create a video with the title and playlist ID through 3 steps', async () => {
+    const queryClient = new QueryClient();
+
+    const postStep1Deferred = new Deferred();
+    fetchMock.post('/api/videos/', postStep1Deferred.promise);
+
+    const playlist = playlistMockFactory();
+    const video = videoMockFactory({ title: 'new video title', playlist });
+
+    const { rerender } = render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <UploadManagerContext.Provider
+            value={{ setUploadState: jest.fn(), uploadManagerState: {} }}
+          >
+            <VideoCreateForm />
+          </UploadManagerContext.Provider>
+        </QueryClientProvider>,
+      ),
+    );
+
+    {
+      // Step 1
+      const titleField = screen.getByRole('textbox', { name: 'Title' });
+      const playlistField = screen.getByRole('textbox', {
+        name: 'Playlist ID',
+      });
+      const btn = screen.getByRole('button', { name: 'Create the video' });
+      screen.getByRole('list', { name: 'Video creation progress' });
+      {
+        const step1 = screen.getByRole('listitem', {
+          name: 'Current: Video details (1/3)',
+        });
+        expect(step1).toHaveAttribute('aria-current', 'step');
+        const step2 = screen.getByRole('listitem', {
+          name: 'File selection (2/3)',
+        });
+        expect(step2).toHaveAttribute('aria-current', 'false');
+        const step3 = screen.getByRole('listitem', {
+          name: 'Processing (3/3)',
+        });
+        expect(step3).toHaveAttribute('aria-current', 'false');
+      }
+      // The form does not send when the title field is not set
+      userEvent.click(btn);
+      screen.getByText('The title is required to create a new video.');
+      // The form does not send when the playlist is not passed and the playlist field is not set
+      userEvent.type(titleField, 'new video title');
+      userEvent.click(btn);
+      screen.getByText('The playlist ID is required to create a new video.');
+      expect(
+        screen.queryByText('The title is required to create a new video.'),
+      ).toBeNull();
+
+      userEvent.type(playlistField, playlist.id);
+      userEvent.click(btn);
+      expect(
+        screen.queryByText('The title is required to create a new video.'),
+      ).toBeNull();
+      expect(
+        screen.queryByText(
+          'The playlist ID is required to create a new video.',
+        ),
+      ).toBeNull();
+
+      await waitFor(() => {
+        expect(
+          fetchMock.called('/api/videos/', {
+            body: { title: 'new video title', playlist: playlist.id },
+            method: 'POST',
+          }),
+        ).toEqual(true);
+      });
+      screen.getByRole('status', { name: 'Creating video...' });
+
+      await act(async () => postStep1Deferred.resolve(video));
+    }
+
+    {
+      // Step 2 is the file upload field
+      screen.getByRole('button', {
+        name: 'Select a file to upload',
+      });
+      {
+        const step1 = screen.getByRole('listitem', {
+          name: 'Completed: Video details (1/3)',
+        });
+        expect(step1).toHaveAttribute('aria-current', 'false');
+        const step2 = screen.getByRole('listitem', {
+          name: 'Current: File selection (2/3)',
+        });
+        expect(step2).toHaveAttribute('aria-current', 'step');
+        const step3 = screen.getByRole('listitem', {
+          name: 'Processing (3/3)',
+        });
+        expect(step3).toHaveAttribute('aria-current', 'false');
+      }
+
+      // Rerender with a different value in upload manager to simulate an ongoing upload
+      rerender(
+        wrapInIntlProvider(
+          <QueryClientProvider client={queryClient}>
+            <UploadManagerContext.Provider
+              value={{
+                setUploadState: jest.fn(),
+                uploadManagerState: {
+                  [video.id]: {
+                    file: new File(['(⌐□_□)'], 'course.mp4', {
+                      type: 'video/mp4',
+                    }),
+                    objectId: video.id,
+                    objectType: modelName.VIDEOS,
+                    progress: 10,
+                    status: UploadManagerStatus.UPLOADING,
+                  },
+                },
+              }}
+            >
+              <VideoCreateForm />
+            </UploadManagerContext.Provider>
+          </QueryClientProvider>,
+        ),
+      );
+
+      // Step 3 is just the ongoing upload with some help text for users
+      screen.getByRole('button', { name: 'Create another video' });
+      screen.getByText('Video creation in progress');
+      screen.getByText(
+        'You can use the file uploads manager to monitor ongoing uploads, or go to the video page to start adding subtitles.',
+      );
+      {
+        const step1 = screen.getByRole('listitem', {
+          name: 'Completed: Video details (1/3)',
+        });
+        expect(step1).toHaveAttribute('aria-current', 'false');
+        const step2 = screen.getByRole('listitem', {
+          name: 'Completed: File selection (2/3)',
+        });
+        expect(step2).toHaveAttribute('aria-current', 'false');
+        const step3 = screen.getByRole('listitem', {
+          name: 'Current: Processing (3/3)',
+        });
+        expect(step3).toHaveAttribute('aria-current', 'step');
+      }
+    }
+  });
+
+  it('does not ask for the playlist ID it it is provided to the form as a prop', async () => {
+    const queryClient = new QueryClient();
+
+    const postStep1Deferred = new Deferred();
+    fetchMock.post('/api/videos/', postStep1Deferred.promise);
+
+    const playlist = playlistMockFactory();
+    const video = videoMockFactory({ title: 'new video title', playlist });
+
+    render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <VideoCreateForm playlist={playlist.id} />
+        </QueryClientProvider>,
+      ),
+    );
+
+    const titleField = screen.getByRole('textbox', { name: 'Title' });
+    expect(
+      screen.queryByRole('textbox', {
+        name: 'Playlist ID',
+      }),
+    ).toBeNull();
+    const btn = screen.getByRole('button', { name: 'Create the video' });
+    screen.getByRole('list', { name: 'Video creation progress' });
+
+    // The form does not send when the title field is not set
+    userEvent.click(btn);
+    screen.getByText('The title is required to create a new video.');
+
+    // As the playlist ID is available to the component, it does not ask it from the user
+    userEvent.type(titleField, 'new video title');
+    userEvent.click(btn);
+    expect(
+      screen.queryByText('The title is required to create a new video.'),
+    ).toBeNull();
+
+    await waitFor(() => {
+      expect(
+        fetchMock.called('/api/videos/', {
+          body: { title: 'new video title', playlist: playlist.id },
+          method: 'POST',
+        }),
+      ).toEqual(true);
+    });
+    screen.getByRole('status', { name: 'Creating video...' });
+
+    await act(async () => postStep1Deferred.resolve(video));
+
+    // We move on to step 2, which is covered by the previous test
+    screen.getByRole('button', {
+      name: 'Select a file to upload',
+    });
+  });
+
+  it('shows errors on the title field as they are returned by the API', async () => {
+    const queryClient = new QueryClient();
+
+    const postStep1Deferred = new Deferred();
+    fetchMock.post('/api/videos/', postStep1Deferred.promise);
+
+    const playlist = playlistMockFactory();
+
+    render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <VideoCreateForm />
+        </QueryClientProvider>,
+      ),
+    );
+
+    const titleField = screen.getByRole('textbox', { name: 'Title' });
+    const playlistField = screen.getByRole('textbox', {
+      name: 'Playlist ID',
+    });
+    const btn = screen.getByRole('button', { name: 'Create the video' });
+    screen.getByRole('list', { name: 'Video creation progress' });
+
+    userEvent.type(titleField, 'new video title');
+    userEvent.type(playlistField, playlist.id);
+    userEvent.click(btn);
+
+    await waitFor(() => {
+      expect(
+        fetchMock.called('/api/videos/', {
+          body: { title: 'new video title', playlist: playlist.id },
+          method: 'POST',
+        }),
+      ).toEqual(true);
+    });
+    screen.getByRole('status', { name: 'Creating video...' });
+
+    await act(async () =>
+      postStep1Deferred.resolve({
+        status: 400,
+        body: {
+          errors: [
+            {
+              title: ['Some title related error.', 'Another title error.'],
+            },
+          ],
+        },
+      }),
+    );
+
+    screen.getByText('Some title related error.');
+    screen.getByText('Another title error.');
+  });
+
+  it('shows errors on the playlist ID field as they are returned by the API', async () => {
+    const queryClient = new QueryClient();
+
+    const postStep1Deferred = new Deferred();
+    fetchMock.post('/api/videos/', postStep1Deferred.promise);
+
+    const playlist = playlistMockFactory();
+
+    render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <VideoCreateForm />
+        </QueryClientProvider>,
+      ),
+    );
+
+    const titleField = screen.getByRole('textbox', { name: 'Title' });
+    const playlistField = screen.getByRole('textbox', {
+      name: 'Playlist ID',
+    });
+    const btn = screen.getByRole('button', { name: 'Create the video' });
+    screen.getByRole('list', { name: 'Video creation progress' });
+
+    userEvent.type(titleField, 'new video title');
+    userEvent.type(playlistField, playlist.id);
+    userEvent.click(btn);
+
+    await waitFor(() => {
+      expect(
+        fetchMock.called('/api/videos/', {
+          body: { title: 'new video title', playlist: playlist.id },
+          method: 'POST',
+        }),
+      ).toEqual(true);
+    });
+    screen.getByRole('status', { name: 'Creating video...' });
+
+    await act(async () =>
+      postStep1Deferred.resolve({
+        status: 400,
+        body: {
+          errors: [
+            {
+              playlist: [
+                'Some playlist related error.',
+                'Another playlist error.',
+              ],
+            },
+          ],
+        },
+      }),
+    );
+
+    screen.getByText('Some playlist related error.');
+    screen.getByText('Another playlist error.');
+  });
+});
