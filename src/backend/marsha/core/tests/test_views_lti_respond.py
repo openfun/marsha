b756726d84d1703783797a830d90329f6b76579e
Nicolas Clerc <kernicpanel@nclerc.fr>
2021-04-29 13:09:54 +0200

    ♻️(tests-back) factorize LTI signature
    In our tests, we use 3 times the same code to generate signed LTI
    request.
    This code is now refactored in a new tests.utils module.

--- src/backend/marsha/core/tests/test_views_lti_respond.py
+++ src/backend/marsha/core/tests/test_views_lti_respond.py
@@ -1,12 +1,9 @@
 """Test the LTI select view."""
 import random
-from urllib.parse import unquote
 
 from django.test import TestCase
 
-from oauthlib import oauth1
-
-from ..factories import ConsumerSiteLTIPassportFactory
+from .utils import generate_passport_and_signed_lti_parameters
 
 
 # We don't enforce arguments documentation in tests
@@ -18,47 +15,17 @@ class RespondLTIViewTestCase(TestCase):
 
     maxDiff = None
 
-    @staticmethod
-    def _get_signed_lti_parameters():
-        """Generate signed LTI parameters."""
-        passport = ConsumerSiteLTIPassportFactory(consumer_site__domain="testserver")
-        lti_parameters = {
-            "roles": random.choice(["instructor", "administrator"]),
-            "content_item_return_url": "http://return.url/",
-            "content_items": "some content items",
-            "context_id": "unknown",
-        }
-        url = "http://testserver/lti/respond/"
-        client = oauth1.Client(
-            client_key=passport.oauth_consumer_key, client_secret=passport.shared_secret
-        )
-        # Compute Authorization header which looks like:
-        # Authorization: OAuth oauth_nonce="80966668944732164491378916897",
-        # oauth_timestamp="1378916897", oauth_version="1.0", oauth_signature_method="HMAC-SHA1",
-        # oauth_consumer_key="", oauth_signature="frVp4JuvT1mVXlxktiAUjQ7%2F1cw%3D"
-        _uri, headers, _body = client.sign(
-            url,
-            http_method="POST",
-            body=lti_parameters,
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-        )
-
-        # Parse headers to pass to template as part of context:
-        oauth_dict = dict(
-            param.strip().replace('"', "").split("=")
-            for param in headers["Authorization"].split(",")
-        )
-
-        signature = oauth_dict["oauth_signature"]
-        oauth_dict["oauth_signature"] = unquote(signature)
-        oauth_dict["oauth_nonce"] = oauth_dict.pop("OAuth oauth_nonce")
-
-        lti_parameters.update(oauth_dict)
-        return lti_parameters
-
     def test_views_lti_respond(self):
         """Validate the format of the response returned by the view for an instructor request."""
-        lti_parameters = self._get_signed_lti_parameters()
+        lti_parameters, _ = generate_passport_and_signed_lti_parameters(
+            url="http://testserver/lti/respond/",
+            lti_parameters={
+                "roles": random.choice(["instructor", "administrator"]),
+                "content_item_return_url": "http://return.url/",
+                "content_items": "some content items",
+                "context_id": "unknown",
+            },
+        )
         response = self.client.post(
             "/lti/respond/",
             lti_parameters,
@@ -98,7 +65,15 @@ class RespondLTIViewTestCase(TestCase):
 
     def test_views_lti_respond_verification_wrong_signature(self):
         """Wrong signature should raise a 403 error."""
-        lti_parameters = self._get_signed_lti_parameters()
+        lti_parameters, _ = generate_passport_and_signed_lti_parameters(
+            url="http://testserver/lti/respond/",
+            lti_parameters={
+                "roles": random.choice(["instructor", "administrator"]),
+                "content_item_return_url": "http://return.url/",
+                "content_items": "some content items",
+                "context_id": "unknown",
+            },
+        )
         lti_parameters["oauth_signature"] = "{:s}a".format(
             lti_parameters["oauth_signature"]
         )
@@ -112,7 +87,16 @@ class RespondLTIViewTestCase(TestCase):
 
     def test_views_lti_respond_verification_wrong_referer(self):
         """Wrong referer should raise a 403 error."""
-        lti_parameters = self._get_signed_lti_parameters()
+        lti_parameters, _ = generate_passport_and_signed_lti_parameters(
+            url="http://testserver/lti/respond/",
+            lti_parameters={
+                "roles": random.choice(["instructor", "administrator"]),
+                "content_item_return_url": "http://return.url/",
+                "content_items": "some content items",
+                "context_id": "unknown",
+            },
+        )
+
         response = self.client.post(
             "/lti/respond/",
             lti_parameters,
