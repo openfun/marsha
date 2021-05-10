3870f14b39004b348872306440dbc2cf27ab6241
Mehdi Benadda <me@mbenadda.com>
2021-05-11 00:12:48 +0200

    💄(site) rework the site layout
    Rework the site layout to make it cleaner and more usable. Take this
    opportunity to also simplify it and remove mostly empty components
    that did not have a real purpose.

--- src/frontend/components/SiteLayout/index.tsx
+++ src/frontend/components/SiteLayout/index.tsx
@@ -1,24 +1,110 @@
-import { Box, Grid } from 'grommet';
+import { Anchor, Box, Button, Main, Nav, Sidebar, Text } from 'grommet';
+import { normalizeColor } from 'grommet/utils';
 import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import { SiteHeader } from '../SiteHeader';
-import { SiteSidebar } from '../SiteSidebar';
+import {
+  AnonymousUser,
+  useCurrentUser,
+} from '../../data/stores/useCurrentUser';
+import { OrganizationAccessRole } from '../../types/User';
+import { theme } from '../../utils/theme/theme';
+import { Icon } from '../Icon';
+import { Loader } from '../Loader';
+import { SidebarUploadsIndicator } from '../SidebarUploadsIndicator';
+import { withLink } from '../withLink/withLink';
 
-export const SiteLayout: React.FC = ({ children }) => (
-  <Grid
-    fill
-    rows={['auto', 'flex']}
-    columns={['20%', '80%']}
-    areas={[
-      { name: 'header', start: [0, 0], end: [1, 0] },
-      { name: 'sidebar', start: [0, 1], end: [0, 1] },
-      { name: 'main', start: [1, 1], end: [1, 1] },
-    ]}
-  >
-    <SiteHeader />
-    <SiteSidebar />
-    <Box gridArea="main" justify="start" align="start">
-      {children}
+const messages = defineMessages({
+  logInBtn: {
+    defaultMessage: 'Log in',
+    description: 'Text for the login button in the header of the marsha site',
+    id: 'components.SiteHeader.logInBtn',
+  },
+});
+
+const TitleLink = withLink(Anchor);
+const SidebarLink = styled(Link)`
+  &,
+  &:active {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  &:hover {
+    background: white;
+    text-decoration: underline;
+    color: ${normalizeColor('brand', theme)};
+  }
+`;
+
+export const SiteLayout: React.FC = ({ children }) => {
+  const currentUser = useCurrentUser().getCurrentUser();
+
+  return (
+    <Box direction="row" pad="none" height="full">
+      <Sidebar
+        width="16rem"
+        height="full"
+        flex={false}
+        background="light-1"
+        border={{ color: 'light-6', side: 'right' }}
+        pad={{ horizontal: 'none', vertical: 'small' }}
+      >
+        <Box direction="column" gap="medium">
+          <Box pad={{ vertical: 'small' }} align="center">
+            <TitleLink to="/" size="large" color="white">
+              marsha.education
+            </TitleLink>
+          </Box>
+          {currentUser ? (
+            currentUser === AnonymousUser.ANONYMOUS ? (
+              <Button>
+                <FormattedMessage {...messages.logInBtn} />
+              </Button>
+            ) : (
+              <Box direction="row" pad="medium" gap="small" align="center">
+                <Icon name="icon-person-outline" />
+                <Text>{currentUser.email}</Text>
+              </Box>
+            )
+          ) : (
+            <Loader />
+          )}
+          <Nav>
+            {currentUser && currentUser !== AnonymousUser.ANONYMOUS ? (
+              <React.Fragment>
+                <SidebarUploadsIndicator />
+                {currentUser.organization_accesses
+                  .filter(
+                    (orgAccess) =>
+                      orgAccess.role === OrganizationAccessRole.ADMINISTRATOR,
+                  )
+                  .map((orgAccess) => (
+                    <SidebarLink
+                      key={orgAccess.organization}
+                      to={`/organization/${orgAccess.organization}`}
+                    >
+                      <Box
+                        direction="row"
+                        pad={{ horizontal: 'medium', vertical: 'small' }}
+                        gap="small"
+                        align="center"
+                      >
+                        <Icon name="icon-organization" />
+                        <Text>{orgAccess.organization_name}</Text>
+                      </Box>
+                    </SidebarLink>
+                  ))}
+              </React.Fragment>
+            ) : null}
+          </Nav>
+        </Box>
+      </Sidebar>
+      <Main overflow="auto" width="calc(100% - 16rem)">
+        {children}
+      </Main>
     </Box>
-  </Grid>
-);
+  );
+};
--- src/frontend/components/SiteRoutes/index.tsx
+++ src/frontend/components/SiteRoutes/index.tsx
@@ -1,19 +1,55 @@
-import { Heading, Main } from 'grommet';
+import { Box, Heading } from 'grommet';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import { appData } from '../../data/appData';
+import { BreadCrumbs, BreadCrumbsProvider } from '../BreadCrumbs';
+import { OrganizationView } from '../OrganizationView';
 import { SiteLayout } from '../SiteLayout';
+import { UploadManager } from '../UploadManager';
+import { UploadsView } from '../UploadsView';
 
-export const Routes = () => (
-  <BrowserRouter>
-    <SiteLayout>
-      <Switch>
-        <Route>
-          <Main>
-            <Heading margin="medium">The main content</Heading>
-          </Main>
-        </Route>
-      </Switch>
-    </SiteLayout>
+const queryClient = new QueryClient();
+
+const Wrappers = ({ children }: React.PropsWithChildren<{}>) => (
+  <BrowserRouter basename="/site">
+    <QueryClientProvider client={queryClient}>
+      <BreadCrumbsProvider>
+        <UploadManager>
+          <div
+            style={{ width: '100%', height: '100%' }}
+            className={`marsha-${appData.frontend}`}
+          >
+            {children}
+          </div>
+        </UploadManager>
+      </BreadCrumbsProvider>
+    </QueryClientProvider>
   </BrowserRouter>
 );
+
+export const Routes = () => {
+  return (
+    <Wrappers>
+      <SiteLayout>
+        <BreadCrumbs />
+        <Switch>
+          <Route path={'/uploads'}>
+            <UploadsView />
+          </Route>
+
+          <Route path={'/organization/:organizationId'}>
+            <OrganizationView />
+          </Route>
+
+          <Route>
+            <Box>
+              <Heading margin="medium">The main content</Heading>
+            </Box>
+          </Route>
+        </Switch>
+      </SiteLayout>
+    </Wrappers>
+  );
+};
