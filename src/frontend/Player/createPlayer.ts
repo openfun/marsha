404dddb6e5c81bc5d084182209c03c5a61a3f58f
Manuel Raynaud <manu@raynaud.io>
2021-06-08 19:12:47 +0200

    ✨(front) display a waiting message while live is not available
    When a live begins, you have to wait a few seconds before the hls
    manifest is created and available. If the player starts fetching this
    missing manifest, it fails and never restart. We choose to poll the
    manifest while it returns a 200 status code and then the player is
    created. A waiting message is display during this period.

--- src/frontend/Player/createPlayer.ts
+++ src/frontend/Player/createPlayer.ts
@@ -1,13 +1,34 @@
 import { VideoPlayerCreator } from '../types/VideoPlayer';
 import { createVideojsPlayer } from './createVideojsPlayer';
 import { report } from '../utils/errors/report';
+import { VideoUrls } from '../types/tracks';
 
-export const createPlayer: VideoPlayerCreator = (
+const pollForLive = async (urls: VideoUrls): Promise<null> => {
+  try {
+    const response = await fetch(urls.manifests.hls);
+    if (response.status === 404) {
+      throw new Error('missing manifest');
+    }
+  } catch (error) {
+    await new Promise((resolve) => window.setTimeout(resolve, 1000));
+    return await pollForLive(urls);
+  }
+
+  return null;
+};
+
+export const createPlayer: VideoPlayerCreator = async (
   type,
   ref,
   dispatchPlayerTimeUpdate,
   video,
 ) => {
+  if (video.live_state) {
+    ref.classList.add('offscreen');
+    await pollForLive(video.urls!);
+    ref.classList.remove('offscreen');
+  }
+
   switch (type) {
     case 'videojs':
       const player = createVideojsPlayer(
--- src/frontend/types/VideoPlayer.ts
+++ src/frontend/types/VideoPlayer.ts
@@ -11,4 +11,4 @@ export type VideoPlayerCreator = (
   ref: HTMLVideoElement,
   dispatchPlayerTimeUpdate: (time: number) => void,
   video: Video,
-) => Maybe<VideoPlayerInterface>;
+) => Promise<Maybe<VideoPlayerInterface>>;
