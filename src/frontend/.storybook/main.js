c38bc90240432b2e9d3199353d490642c3f6b3eb
Nicolas Clerc <kernicpanel@nclerc.fr>
2021-06-10 08:45:15 +0200

    🐛(storybook) add typescript docgen config
    A recent update on react-docgen-typescript dependancy broke storybook
    build.
    This configaration is a workaround until storybook 6.3 releases.
    
    Related issue: https://github.com/storybookjs/storybook/issues/15067

--- src/frontend/.storybook/main.js
+++ src/frontend/.storybook/main.js
@@ -1,4 +1,11 @@
 module.exports = {
   stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-essentials'],
+
+  // workaround for https://github.com/storybookjs/storybook/issues/15067
+  // storybook 6.3 should fix it
+  typescript: {
+    // also valid 'react-docgen-typescript' | false
+    reactDocgen: 'react-docgen',
+  },
 };
