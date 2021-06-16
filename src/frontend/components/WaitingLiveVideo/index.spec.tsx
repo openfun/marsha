fcaef0c41bfa2a2d6d72cb17cc21f66666b6109d
Manuel Raynaud <manu@raynaud.io>
2021-06-16 12:15:22 +0200

    ✨(front) create <WaitingLiveVideo /> component
    The `<WaitingLiveVideo />` component is reponsible to display a waiting
    message while a live is not available. It can be used with or without
    polling the manifest urls to know if the live is ready or not.

---
+++ src/frontend/components/WaitingLiveVideo/index.spec.tsx
@@ -0,0 +1,70 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+
+import { getResource } from '../../data/sideEffects/getResource';
+import { pollForLive } from '../../data/sideEffects/pollForLive';
+import { requestStatus } from '../../types/api';
+import { modelName } from '../../types/models';
+import { videoMockFactory } from '../../utils/tests/factories';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import { WaitingLiveVideo } from '.';
+
+jest.mock('../../data/appData', () => ({
+  appData: {},
+}));
+
+jest.mock('../../data/sideEffects/getResource', () => ({
+  getResource: jest.fn(),
+}));
+jest.mock('../../data/sideEffects/pollForLive', () => ({
+  pollForLive: jest.fn(),
+}));
+const mockGetResource = getResource as jest.MockedFunction<typeof getResource>;
+const mockPollForLive = pollForLive as jest.MockedFunction<typeof pollForLive>;
+
+describe('WaitingLiveVideo', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders the component without polling the hls manifest', () => {
+    render(wrapInIntlProvider(<WaitingLiveVideo />));
+
+    screen.getByText('Live will begin soon');
+    screen.getByText(
+      'The live is going to start. You can wait here, the player will start once the live is ready.',
+    );
+
+    expect(mockGetResource).not.toHaveBeenCalled();
+    expect(mockPollForLive).not.toHaveBeenCalled();
+  });
+
+  it('renders the component and poll the hls manifest', async () => {
+    const video = videoMockFactory({
+      urls: {
+        manifests: {
+          hls: 'https://marsha.education/live.m3u8',
+        },
+        mp4: {},
+        thumbnails: {},
+      },
+    });
+
+    mockPollForLive.mockResolvedValue(null);
+    mockGetResource.mockResolvedValue(requestStatus.SUCCESS);
+
+    render(wrapInIntlProvider(<WaitingLiveVideo video={video} />));
+
+    screen.getByText('Live will begin soon');
+    screen.getByText(
+      'The live is going to start. You can wait here, the player will start once the live is ready.',
+    );
+
+    await waitFor(() => {
+      expect(mockGetResource).toHaveBeenCalled();
+    });
+
+    expect(mockGetResource).toHaveBeenCalledWith(modelName.VIDEOS, video.id);
+    expect(mockPollForLive).toHaveBeenCalledWith(video.urls);
+  });
+});
