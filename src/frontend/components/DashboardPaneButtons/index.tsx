21567e871da5c1eef2870f95bacaa222cc4b755a
carofun <caroline.cramoisan@fun-mooc.fr>
2021-07-02 16:07:36 +0200

    💄(frontend) add a beta label on live configuration buttons
    The feature to start a live is still on a beta version. We want to alert users
    about this point. We add a temporary beta label on the two buttons.

--- src/frontend/components/DashboardPaneButtons/index.tsx
+++ src/frontend/components/DashboardPaneButtons/index.tsx
@@ -62,6 +62,7 @@ export const DashboardButton = styled(Button)`
   flex-grow: 1;
   flex-basis: 8rem;
   max-width: 50%;
+  position: relative;
 
   :first-child {
     margin-right: 1rem;
@@ -71,6 +72,17 @@ export const DashboardButton = styled(Button)`
     margin-left: 1rem;
   }
 `;
+
+export const DashboardButtonBeta = styled(DashboardButton)`
+  :after {
+    color: #ff6a00;
+    content: '(BETA)';
+    font-size: 0.5em;
+    position: absolute;
+    top: -0.4em;
+  }
+`;
+
 export const DashboardButtonWithLink = withLink(DashboardButton);
 
 /** Props shape for the DashboardVideoPaneButtons component. */
