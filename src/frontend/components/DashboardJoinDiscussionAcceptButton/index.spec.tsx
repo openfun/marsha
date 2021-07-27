cb7d6ffe5dfb4231852fd0f1bbf0ee56e799d445
Manuel Raynaud <manu@raynaud.io>
2021-07-27 10:47:59 +0200

    ✨(front) create components to join a discussion
    To join a discussion, a workflow is managed accross multiple components.
    Each component is responsible for one state in this workflow and
    DashboardJoinDiscussion orchestrate them.

---
+++ src/frontend/components/DashboardJoinDiscussionAcceptButton/index.spec.tsx
@@ -0,0 +1,42 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+
+import { useJoinParticipant } from '../../data/stores/useJoinParticipant';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import * as mockWindow from '../../utils/window';
+import { DashboardJoinDiscussionAcceptButton } from '.';
+
+jest.mock('../../utils/window', () => ({
+  converse: {
+    acceptParticipantToJoin: jest.fn(),
+  },
+}));
+
+describe('<DashboardJoinDiscussionAcceptButton />', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('renders the accept button and click on it', () => {
+    const participant = {
+      id: 'participant1',
+      name: 'John Doe',
+    };
+
+    render(
+      wrapInIntlProvider(
+        <DashboardJoinDiscussionAcceptButton participant={participant} />,
+      ),
+    );
+
+    const acceptButton = screen.getByRole('button', { name: 'accept' });
+
+    fireEvent.click(acceptButton);
+
+    expect(mockWindow.converse.acceptParticipantToJoin).toHaveBeenCalledWith(
+      participant,
+    );
+    expect(useJoinParticipant.getState().participantsInDiscussion).toContain(
+      participant,
+    );
+  });
+});
---
+++ src/frontend/components/DashboardJoinDiscussionKickButton/index.spec.tsx
@@ -0,0 +1,46 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+
+import { useJoinParticipant } from '../../data/stores/useJoinParticipant';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import * as mockWindow from '../../utils/window';
+import { DashboardJoinDiscussionKickButton } from '.';
+
+jest.mock('../../utils/window', () => ({
+  converse: {
+    kickParticipant: jest.fn(),
+  },
+}));
+
+describe('<DashboardJoinDiscussionKickButton />', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('renders the button and click on it', () => {
+    const participant = {
+      id: 'participant1',
+      name: 'John Doe',
+    };
+    // add participant in discussion
+    useJoinParticipant.getState().addParticipantToDiscussion(participant);
+
+    render(
+      wrapInIntlProvider(
+        <DashboardJoinDiscussionKickButton participant={participant} />,
+      ),
+    );
+
+    const kickButton = screen.getByRole('button', {
+      name: 'kick out participant',
+    });
+
+    fireEvent.click(kickButton);
+
+    expect(
+      useJoinParticipant.getState().participantsInDiscussion,
+    ).not.toContain(participant);
+    expect(mockWindow.converse.kickParticipant).toHaveBeenCalledWith(
+      participant,
+    );
+  });
+});
