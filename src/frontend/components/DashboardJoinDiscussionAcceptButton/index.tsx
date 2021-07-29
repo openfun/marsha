8df5bbb7881f3a96dced761a0c778e62d2d6fe95
Manuel Raynaud <manu@raynaud.io>
2021-07-29 17:26:15 +0200

    ✨(front) sync participant state between instructors
    When a participant want to join a discussion, all instructors receive
    the message and all can accept, reject or kick the participant. But the
    action is not sync between instructors so if one accept the participant,
    nothing will happen on others dashboard. This commit sync the state
    broadcasting messages between instructors.

--- src/frontend/components/DashboardJoinDiscussionAcceptButton/index.tsx
+++ src/frontend/components/DashboardJoinDiscussionAcceptButton/index.tsx
@@ -2,7 +2,6 @@ import { Button } from 'grommet';
 import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
-import { useJoinParticipant } from '../../data/stores/useJoinParticipant';
 import { Participant } from '../../types/Participant';
 import { converse } from '../../utils/window';
 
@@ -21,10 +20,6 @@ interface DashboardJoinDiscussionAcceptButtonProps {
 export const DashboardJoinDiscussionAcceptButton = ({
   participant,
 }: DashboardJoinDiscussionAcceptButtonProps) => {
-  const moveParticipantToDiscussion = useJoinParticipant(
-    (state) => state.moveParticipantToDiscussion,
-  );
-
   const onClick = () => {
     converse.acceptParticipantToJoin(participant, video);
   };
