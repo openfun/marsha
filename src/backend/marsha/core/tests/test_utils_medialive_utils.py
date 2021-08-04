3b70c3df2a49010d264e58f297f87c36f479559a
Nicolas Clerc <kernicpanel@nclerc.fr>
2021-08-04 17:53:48 +0200

    ✨(back) management command to delete orphan mediapackage channels
    When a mediapackage channel has no medialive channel, nor pending
    harvest job, it is considered useless, and thus can be deleted.
    We added a management command to delete them.

--- src/backend/marsha/core/tests/test_utils_medialive_utils.py
+++ src/backend/marsha/core/tests/test_utils_medialive_utils.py
@@ -789,3 +789,136 @@ class MediaLiveUtilsTestCase(TestCase):
             )
 
             medialive_utils.delete_aws_element_stack(video)
+
+    def test_list_mediapackage_channels(self):
+        """Should recursively get all mediapackage channels."""
+        with Stubber(
+            medialive_utils.mediapackage_client
+        ) as mediapackage_client_stubber:
+            mediapackage_client_stubber.add_response(
+                "list_channels",
+                service_response={"Channels": [{"Id": "1"}], "NextToken": "next_token"},
+                expected_params={},
+            )
+            mediapackage_client_stubber.add_response(
+                "list_channels",
+                service_response={"Channels": [{"Id": "2"}]},
+                expected_params={"NextToken": "next_token"},
+            )
+            channels = medialive_utils.list_mediapackage_channels()
+            mediapackage_client_stubber.assert_no_pending_responses()
+        self.assertEqual(channels, [{"Id": "1"}, {"Id": "2"}])
+
+    def test_list_mediapackage_channel_harvest_jobs(self):
+        """Should recursively get all harvest job related to a mediapackage channel."""
+        with Stubber(
+            medialive_utils.mediapackage_client
+        ) as mediapackage_client_stubber:
+            mediapackage_client_stubber.add_response(
+                "list_harvest_jobs",
+                service_response={
+                    "HarvestJobs": [{"Id": "1"}],
+                    "NextToken": "next_token",
+                },
+                expected_params={"IncludeChannelId": "1"},
+            )
+            mediapackage_client_stubber.add_response(
+                "list_harvest_jobs",
+                service_response={"HarvestJobs": [{"Id": "2"}]},
+                expected_params={"IncludeChannelId": "1", "NextToken": "next_token"},
+            )
+            harvest_jobs = medialive_utils.list_mediapackage_channel_harvest_jobs("1")
+            mediapackage_client_stubber.assert_no_pending_responses()
+        self.assertEqual(harvest_jobs, [{"Id": "1"}, {"Id": "2"}])
+
+    def test_list_mediapackage_channel_origin_endpoints(self):
+        """Should recursively get all origin endpoints related to a mediapackage channel."""
+        with Stubber(
+            medialive_utils.mediapackage_client
+        ) as mediapackage_client_stubber:
+            mediapackage_client_stubber.add_response(
+                "list_origin_endpoints",
+                service_response={
+                    "OriginEndpoints": [{"Id": "1"}],
+                    "NextToken": "next_token",
+                },
+                expected_params={"ChannelId": "1"},
+            )
+            mediapackage_client_stubber.add_response(
+                "list_origin_endpoints",
+                service_response={"OriginEndpoints": [{"Id": "2"}]},
+                expected_params={"ChannelId": "1", "NextToken": "next_token"},
+            )
+            origin_endpoints = (
+                medialive_utils.list_mediapackage_channel_origin_endpoints("1")
+            )
+            mediapackage_client_stubber.assert_no_pending_responses()
+        self.assertEqual(origin_endpoints, [{"Id": "1"}, {"Id": "2"}])
+
+    def test_list_medialive_channels(self):
+        """Should recursively get all medialive channels."""
+        with Stubber(medialive_utils.medialive_client) as medialive_client_stubber:
+            medialive_client_stubber.add_response(
+                "list_channels",
+                service_response={"Channels": [{"Id": "1"}], "NextToken": "next_token"},
+                expected_params={},
+            )
+            medialive_client_stubber.add_response(
+                "list_channels",
+                service_response={"Channels": [{"Id": "2"}]},
+                expected_params={"NextToken": "next_token"},
+            )
+            channels = medialive_utils.list_medialive_channels()
+            medialive_client_stubber.assert_no_pending_responses()
+        self.assertEqual(channels, [{"Id": "1"}, {"Id": "2"}])
+
+    def test_list_indexed_medialive_channels(self):
+        """Should recursively get all medialive channels and index them by their name."""
+        with Stubber(medialive_utils.medialive_client) as medialive_client_stubber:
+            medialive_client_stubber.add_response(
+                "list_channels",
+                service_response={
+                    "Channels": [{"Id": "1", "Name": "A"}],
+                    "NextToken": "next_token",
+                },
+                expected_params={},
+            )
+            medialive_client_stubber.add_response(
+                "list_channels",
+                service_response={"Channels": [{"Id": "2", "Name": "B"}]},
+                expected_params={"NextToken": "next_token"},
+            )
+            channels = medialive_utils.list_indexed_medialive_channels()
+            medialive_client_stubber.assert_no_pending_responses()
+        self.assertEqual(
+            channels, {"A": {"Id": "1", "Name": "A"}, "B": {"Id": "2", "Name": "B"}}
+        )
+
+    def test_delete_mediapackage_channel(self):
+        """Should delete a mediapackage channel and related enpoints."""
+        with Stubber(
+            medialive_utils.mediapackage_client
+        ) as mediapackage_client_stubber:
+            mediapackage_client_stubber.add_response(
+                "list_origin_endpoints",
+                service_response={"OriginEndpoints": [{"Id": "1"}, {"Id": "2"}]},
+                expected_params={"ChannelId": "1"},
+            )
+            mediapackage_client_stubber.add_response(
+                "delete_origin_endpoint",
+                service_response={},
+                expected_params={"Id": "1"},
+            )
+            mediapackage_client_stubber.add_response(
+                "delete_origin_endpoint",
+                service_response={},
+                expected_params={"Id": "2"},
+            )
+            mediapackage_client_stubber.add_response(
+                "delete_channel",
+                service_response={},
+                expected_params={"Id": "1"},
+            )
+            deleted_endpoints = medialive_utils.delete_mediapackage_channel("1")
+            mediapackage_client_stubber.assert_no_pending_responses()
+        self.assertEqual(deleted_endpoints, ["1", "2"])
