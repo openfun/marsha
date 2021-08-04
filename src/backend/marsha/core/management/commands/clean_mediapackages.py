2c1879d812ec5a82c3dde128c13a62e4152d661c
Nicolas Clerc <kernicpanel@nclerc.fr>
2021-08-04 17:53:48 +0200

    ✨(back) management command to delete orphan mediapackage channels
    When a mediapackage channel has no medialive channel, nor pending
    harvest job, it is considered useless, and thus can be deleted.
    We added a management command to delete them.

---
+++ src/backend/marsha/core/management/commands/clean_mediapackages.py
@@ -0,0 +1,52 @@
+"""Clean orphan mediapackages channel management command."""
+from django.core.management.base import BaseCommand
+
+from marsha.core.utils.medialive_utils import (
+    delete_mediapackage_channel,
+    list_indexed_medialive_channels,
+    list_mediapackage_channel_harvest_jobs,
+    list_mediapackage_channels,
+)
+
+
+class Command(BaseCommand):
+    """Remove orphan mediapackage channels."""
+
+    help = (
+        "Check every mediapackage channels and remove them if no related medialive "
+        "nor harvesting job exists."
+    )
+
+    def handle(self, *args, **options):
+        """Execute management command."""
+        mediapackage_channels = list_mediapackage_channels()
+        indexed_medialive_channels = list_indexed_medialive_channels()
+
+        for mediapackage_channel in mediapackage_channels:
+            mediapackage_channel_id = mediapackage_channel.get("Id")
+            self.stdout.write(
+                f"Processing mediapackage channel {mediapackage_channel_id}"
+            )
+
+            if mediapackage_channel_id in indexed_medialive_channels.keys():
+                continue
+
+            mediapackage_channel_should_be_deleted = True
+            mediapackage_channel_harvest_jobs = list_mediapackage_channel_harvest_jobs(
+                mediapackage_channel_id
+            )
+            for harvest_job in mediapackage_channel_harvest_jobs:
+                mediapackage_channel_should_be_deleted = (
+                    mediapackage_channel_should_be_deleted
+                    & (harvest_job.get("Status") in ("FAILED",))
+                )
+
+            if mediapackage_channel_should_be_deleted:
+                deleted_endpoints = delete_mediapackage_channel(mediapackage_channel_id)
+                for deleted_endpoint in deleted_endpoints:
+                    self.stdout.write(
+                        f"Mediapackage channel endpoint {deleted_endpoint} deleted"
+                    )
+                self.stdout.write(
+                    f"Mediapackage channel {mediapackage_channel_id} deleted"
+                )
---
+++ src/backend/marsha/core/tests/test_command_clean_mediapackages.py
@@ -0,0 +1,169 @@
+"""Tests for clean_mediapackages command."""
+from io import StringIO
+from unittest import mock
+
+from django.core.management import call_command
+from django.test import TestCase
+
+from ..management.commands import clean_mediapackages
+
+
+class CleanMediapackagesTest(TestCase):
+    """Test clean_mediapackages command."""
+
+    @mock.patch.object(clean_mediapackages, "list_indexed_medialive_channels")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channels")
+    def test_clean_mediapackages_no_mediapackage(
+        self, mock_mediapackage_channels, mock_medialive_indexed_channels
+    ):
+        """Command should do nothing when there is no mediapackage to process."""
+        out = StringIO()
+        mock_mediapackage_channels.return_value = []
+        mock_medialive_indexed_channels.return_value = {}
+
+        call_command("clean_mediapackages", stdout=out)
+
+        self.assertEqual("", out.getvalue())
+        out.close()
+
+    @mock.patch.object(clean_mediapackages, "list_indexed_medialive_channels")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channels")
+    def test_clean_mediapackages_related_medialive(
+        self, mock_mediapackage_channels, mock_medialive_indexed_channels
+    ):
+        """Command should do nothing when there is related medialives."""
+        out = StringIO()
+        mock_mediapackage_channels.return_value = [{"Id": "MP1"}, {"Id": "MP2"}]
+        mock_medialive_indexed_channels.return_value = {
+            "MP1": {"Id": "ML1", "Name": "MP1"},
+            "MP2": {"Id": "ML2", "Name": "MP2"},
+        }
+
+        call_command("clean_mediapackages", stdout=out)
+
+        self.assertIn("Processing mediapackage channel MP1", out.getvalue())
+        self.assertIn("Processing mediapackage channel MP2", out.getvalue())
+        self.assertNotIn("Mediapackage channel MP1 deleted", out.getvalue())
+        self.assertNotIn("Mediapackage channel MP2 deleted", out.getvalue())
+        out.close()
+
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channel_harvest_jobs")
+    @mock.patch.object(clean_mediapackages, "list_indexed_medialive_channels")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channels")
+    def test_clean_mediapackages_harvest_job_pending(
+        self,
+        mock_mediapackage_channels,
+        mock_medialive_indexed_channels,
+        mock_harvest_jobs,
+    ):
+        """Command should do nothing when there is a pending harvest job."""
+        out = StringIO()
+        mock_mediapackage_channels.return_value = [{"Id": "MP1"}]
+        mock_medialive_indexed_channels.return_value = {}
+        mock_harvest_jobs.return_value = [{"Status": "PENDING"}]
+
+        call_command("clean_mediapackages", stdout=out)
+
+        self.assertIn("Processing mediapackage channel MP1", out.getvalue())
+        self.assertNotIn("Mediapackage channel MP1 deleted", out.getvalue())
+        out.close()
+
+    @mock.patch.object(clean_mediapackages, "delete_mediapackage_channel")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channel_harvest_jobs")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channels")
+    @mock.patch.object(clean_mediapackages, "list_indexed_medialive_channels")
+    def test_clean_mediapackages_harvest_job_failed(
+        self,
+        mock_medialive_indexed_channels,
+        mock_mediapackage_channels,
+        mock_harvest_jobs,
+        mock_delete_mediapackage,
+    ):
+        """Command should delete channel when only a failed harvest job exists."""
+        out = StringIO()
+        mock_mediapackage_channels.return_value = [{"Id": "MP1"}]
+        mock_medialive_indexed_channels.return_value = {}
+        mock_harvest_jobs.return_value = [{"Status": "FAILED"}]
+        mock_delete_mediapackage.return_value = ["EP1", "EP2"]
+
+        call_command("clean_mediapackages", stdout=out)
+
+        self.assertIn("Processing mediapackage channel MP1", out.getvalue())
+        self.assertIn("Mediapackage channel endpoint EP1 deleted", out.getvalue())
+        self.assertIn("Mediapackage channel endpoint EP2 deleted", out.getvalue())
+        self.assertIn("Mediapackage channel MP1 deleted", out.getvalue())
+        out.close()
+
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channel_harvest_jobs")
+    @mock.patch.object(clean_mediapackages, "list_indexed_medialive_channels")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channels")
+    def test_clean_mediapackages_harvest_jobs_failed_and_pending(
+        self,
+        mock_mediapackage_channels,
+        mock_medialive_indexed_channels,
+        mock_harvest_jobs,
+    ):
+        """Command should do nothing when failed and pending harvest job exists."""
+        out = StringIO()
+        mock_mediapackage_channels.return_value = [{"Id": "MP1"}]
+        mock_medialive_indexed_channels.return_value = {}
+        mock_harvest_jobs.return_value = [
+            {"Status": "FAILED"},
+            {"Status": "PENDING"},
+        ]
+
+        call_command("clean_mediapackages", stdout=out)
+
+        self.assertIn("Processing mediapackage channel MP1", out.getvalue())
+        self.assertNotIn("Mediapackage channel MP1 deleted", out.getvalue())
+        out.close()
+
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channel_harvest_jobs")
+    @mock.patch.object(clean_mediapackages, "list_indexed_medialive_channels")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channels")
+    def test_clean_mediapackages_harvest_jobs_pending_and_failed(
+        self,
+        mock_mediapackage_channels,
+        mock_medialive_indexed_channels,
+        mock_harvest_jobs,
+    ):
+        """Command should do nothing when pending and failed harvest job exists."""
+        out = StringIO()
+        mock_mediapackage_channels.return_value = [{"Id": "MP1"}]
+        mock_medialive_indexed_channels.return_value = {}
+        mock_harvest_jobs.return_value = [
+            {"Status": "PENDING"},
+            {"Status": "FAILED"},
+        ]
+
+        call_command("clean_mediapackages", stdout=out)
+
+        self.assertIn("Processing mediapackage channel MP1", out.getvalue())
+        self.assertNotIn("Mediapackage channel MP1 deleted", out.getvalue())
+        out.close()
+
+    @mock.patch.object(clean_mediapackages, "delete_mediapackage_channel")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channel_harvest_jobs")
+    @mock.patch.object(clean_mediapackages, "list_indexed_medialive_channels")
+    @mock.patch.object(clean_mediapackages, "list_mediapackage_channels")
+    def test_clean_mediapackages_no_harvest_job(
+        self,
+        mock_mediapackage_channels,
+        mock_medialive_indexed_channels,
+        mock_harvest_jobs,
+        mock_delete_mediapackage,
+    ):
+        """Command should delete channel when no harvest job exists."""
+        out = StringIO()
+        mock_mediapackage_channels.return_value = [{"Id": "MP1"}]
+        mock_medialive_indexed_channels.return_value = {}
+        mock_harvest_jobs.return_value = []
+        mock_delete_mediapackage.return_value = ["EP1", "EP2"]
+
+        call_command("clean_mediapackages", stdout=out)
+
+        self.assertIn("Processing mediapackage channel MP1", out.getvalue())
+        self.assertIn("Mediapackage channel endpoint EP1 deleted", out.getvalue())
+        self.assertIn("Mediapackage channel endpoint EP2 deleted", out.getvalue())
+        self.assertIn("Mediapackage channel MP1 deleted", out.getvalue())
+        out.close()
