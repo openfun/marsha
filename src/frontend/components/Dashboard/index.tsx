7dfd557b249d5ea18cafd6aa1bae81631125b435
Nicolas Clerc <kernicpanel@nclerc.fr>
2021-08-25 07:46:07 +0200

    ♻️(frontend) cleanup Dashboard component
    DashboartDocument and DashboardVideo have been moved from Dashboard to
    LTIRoutes components.

--- src/frontend/components/Dashboard/index.tsx
+++ src/frontend/components/Dashboard/index.tsx
@@ -2,12 +2,8 @@ import React, { lazy } from 'react';
 import styled from 'styled-components';
 
 import { Document } from '../../types/file';
-import { modelName } from '../../types/models';
 import { Video } from '../../types/tracks';
 import { LTINav } from '../LTINav';
-
-const DashboardVideo = lazy(() => import('../DashboardVideo'));
-const DashboardDocument = lazy(() => import('../DashboardDocument'));
 
 const DashboardContainer = styled.div`
   display: flex;
@@ -19,9 +15,8 @@ const DashboardContainer = styled.div`
 
 /** Props shape for the Dashboard component. */
 interface DashboardProps {
-  video?: Video;
-  document?: Document;
-  objectType: modelName;
+  object: Document | Video;
+  children?: React.ReactNode;
 }
 
 /** Component. Displays a Dashboard with the state of the video in marsha's pipeline and provides links to
@@ -29,14 +24,11 @@ interface DashboardProps {
  * Will also be used to manage related tracks such as timed text when they are available.
  * @param video The video object from AppData. We need it to populate the component before polling starts.
  */
-const Dashboard = ({ document, video, objectType }: DashboardProps) => {
+const Dashboard = ({ object, children }: DashboardProps) => {
   return (
     <DashboardContainer>
-      <LTINav object={video! || document!} />
-      {objectType === modelName.VIDEOS && <DashboardVideo video={video!} />}
-      {objectType === modelName.DOCUMENTS && (
-        <DashboardDocument document={document!} />
-      )}
+      <LTINav object={object} />
+      {children}
     </DashboardContainer>
   );
 };
