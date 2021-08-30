f8fbf5301ee0d05fa3122c11e91f9c827d4ecfd1
Manuel Raynaud <manu@raynaud.io>
2021-08-30 16:35:54 +0200

    🚨(front) change catch variable to unknown type
    In typescript 4.4 catched variable have now the unknown type instead of
    any before. We decided to keep this change and adapt our code to deal
    with this new constraint.
    
    https://devblogs.microsoft.com/typescript/announcing-typescript-4-4

--- src/frontend/utils/errors/report.tsx
+++ src/frontend/utils/errors/report.tsx
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/browser';
 
 import { appData } from '../../data/appData';
 
-export const report = (error: Error) => {
+export const report = (error: unknown) => {
   if (appData.sentry_dsn) {
     Sentry.captureException(error);
   } else {
