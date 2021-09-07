cffb554dec6334e83c74a3ea921c433e122881c0
Manuel Raynaud <manu@raynaud.io>
2021-09-07 11:15:58 +0200

    ✨(front) connect user before asking to join discussion
    A user can ask to join the discussion while he is not connected to the
    chat. We must connect him first and ensure the username is not already
    used.

--- src/frontend/components/JoinDiscussionAskButton/index.tsx
+++ src/frontend/components/JoinDiscussionAskButton/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { defineMessages, FormattedMessage } from 'react-intl';
+import { Box, Button, Layer, Text, TextInput } from 'grommet';
+import React, { useEffect, useState } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
 import { Redirect } from 'react-router-dom';
 
 import { useParticipantWorkflow } from '../../data/stores/useParticipantWorkflow';
@@ -25,21 +26,77 @@ const messages = defineMessages({
     description: 'Text to tell participant his/her request is not accepted',
     id: 'components.JoinDiscussionAskButton.rejected',
   },
+  chooseUsername: {
+    defaultMessage: 'Choose a username',
+    description:
+      'Title displayed in the layer to ask the user to choose a username in order to connect to the chat',
+    id: 'components.JoinDiscussionAskButton.chooseUsername',
+  },
+  UsernameAlreadyExists: {
+    defaultMessage: 'Username already exists, please choose another one',
+    description:
+      'Error message shown when the connection to the chat failed if the username is already in use.',
+    id: 'components.JoinDiscussionAskButton.usernameAlreadyExists',
+  },
+  username: {
+    defaultMessage: 'username',
+    description: 'label used by the input text to write the username to choose',
+    id: 'components.JoinDiscussionAskButton.username',
+  },
+  confirm: {
+    defaultMessage: 'Confirm',
+    description: 'Label used by the confirm button',
+    id: 'components.JoinDiscussionAskButton.confirm',
+  },
+  cancel: {
+    defaultMessage: 'Cancel',
+    description: 'Label used by the cancel button',
+    id: 'components.JoinDiscussionAskButton.cancel',
+  },
 });
 
 export const JoinDiscussionAskButton = () => {
-  const { asked, accepted, rejected, setAsked } = useParticipantWorkflow(
-    (state) => ({
-      asked: state.asked,
-      accepted: state.accepted,
-      rejected: state.rejected,
-      setAsked: state.setAsked,
-    }),
-  );
+  const intl = useIntl();
+  const [username, setUsername] = useState('');
+  const [showUsernameForm, setShowUsernameForm] = useState(false);
+  const {
+    asked,
+    accepted,
+    rejected,
+    usernameAlreadyExisting,
+    reset,
+    setAsked,
+  } = useParticipantWorkflow((state) => ({
+    asked: state.asked,
+    accepted: state.accepted,
+    rejected: state.rejected,
+    usernameAlreadyExisting: state.usernameAlreadyExisting,
+    setAsked: state.setAsked,
+    reset: state.reset,
+  }));
 
-  const onClick = () => {
-    converse.askParticipantToJoin();
-    setAsked();
+  useEffect(() => {
+    if (usernameAlreadyExisting) {
+      setShowUsernameForm(true);
+    }
+  }, [usernameAlreadyExisting]);
+
+  const closeLayer = () => {
+    setUsername('');
+    setShowUsernameForm(false);
+    reset();
+  };
+
+  const onClick = async () => {
+    try {
+      await converse.askParticipantToJoin(username);
+      setAsked();
+      if (showUsernameForm) {
+        setShowUsernameForm(false);
+      }
+    } catch (error) {
+      setShowUsernameForm(true);
+    }
   };
 
   if (accepted) {
@@ -47,14 +104,49 @@ export const JoinDiscussionAskButton = () => {
   }
 
   if (rejected) {
-    return <FormattedMessage {...messages.rejected} />;
+    return <Text>{intl.formatMessage(messages.rejected)}</Text>;
+  }
+
+  if (showUsernameForm) {
+    return (
+      <Layer position="center" onClickOutside={closeLayer} onEsc={closeLayer}>
+        <Box pad="large" gap="medium">
+          <Text>{intl.formatMessage(messages.chooseUsername)}</Text>
+          {usernameAlreadyExisting && (
+            <Text color="status-error">
+              {intl.formatMessage(messages.UsernameAlreadyExists)}
+            </Text>
+          )}
+          <TextInput
+            aria-label="username"
+            name="username"
+            type="text"
+            placeholder={intl.formatMessage(messages.username)}
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <Box align="center" direction="row" gap="medium">
+            <Button
+              label={intl.formatMessage(messages.confirm)}
+              onClick={onClick}
+              primary={true}
+              disabled={!username.length}
+            />
+            <Button
+              label={intl.formatMessage(messages.cancel)}
+              onClick={closeLayer}
+            />
+          </Box>
+        </Box>
+      </Layer>
+    );
   }
 
   return asked ? (
-    <FormattedMessage {...messages.waitingInstructor} />
+    <Text>{intl.formatMessage(messages.waitingInstructor)}</Text>
   ) : (
     <DashboardButton
-      label={<FormattedMessage {...messages.askInstructor} />}
+      label={intl.formatMessage(messages.askInstructor)}
       primary={true}
       onClick={onClick}
     />
--- src/frontend/types/libs/window.d.ts
+++ src/frontend/types/libs/window.d.ts
@@ -1,24 +1,9 @@
-import { Participant } from '../Participant';
-import { Video } from '../tracks';
-
 // Ensure this is treated as a module.
 export {};
 
 declare global {
   interface Window {
-    converse: {
-      acceptParticipantToJoin: (participant: Participant, video: Video) => void;
-      askParticipantToJoin: () => void;
-      kickParticipant: (participant: Participant) => void;
-      rejectParticipantToJoin: (participant: Participant) => void;
-      participantLeaves: () => void;
-      insertInto: (container: HTMLElement) => void;
-      initialize: (options: any) => void;
-      env: any;
-      plugins: {
-        add: (name: string, plugin: any) => void;
-      };
-    };
+    converse: converse.Converse;
     JitsiMeetExternalAPI: JitsiMeetExternalAPI;
   }
 }
