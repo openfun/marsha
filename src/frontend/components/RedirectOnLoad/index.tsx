845be84021e466de913b0f9477a54415a2a96992
Manuel Raynaud <manu@raynaud.io>
2021-10-18 16:49:11 +0200

    ♻️(front) each resource is responsible to redirect the user on load
    Depending on the loaded resource, we want to redirect the user to different
    routes. Since now not having this architecture was enough because we
    didn't have specific cases based on the resource itself. Now we want to
    redirect the user on the dashboard if it's a video live and he has the
    update permission.

--- src/frontend/components/RedirectOnLoad/index.tsx
+++ src/frontend/components/RedirectOnLoad/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { Redirect } from 'react-router-dom';
 
-import { appData, getDecodedJwt } from '../../data/appData';
+import { appData } from '../../data/appData';
 import { appState } from '../../types/AppData';
-import { DASHBOARD_ROUTE } from '../Dashboard/route';
+import { modelName } from '../../types/models';
 import { FULL_SCREEN_ERROR_ROUTE } from '../ErrorComponents/route';
-import { PLAYER_ROUTE } from '../routes';
 import { SELECT_CONTENT_ROUTE } from '../SelectContent/route';
+import { RedirectVideo } from './RedirectVideo';
+import { RedirectDocument } from './RedirectDocument';
 
 // RedirectOnLoad assesses the initial state of the application using appData and determines the proper
 // route to load in the Router
@@ -25,15 +26,10 @@ export const RedirectOnLoad = () => {
     return <Redirect push to={FULL_SCREEN_ERROR_ROUTE('notFound')} />;
   }
 
-  if (resource && resource.is_ready_to_show) {
-    return <Redirect push to={PLAYER_ROUTE(appData.modelName)} />;
+  switch (appData.modelName) {
+    case modelName.DOCUMENTS:
+      return <RedirectDocument document={appData.document!} />;
+    case modelName.VIDEOS:
+      return <RedirectVideo video={appData.video!} />;
   }
-
-  if (getDecodedJwt().permissions.can_update) {
-    return <Redirect push to={DASHBOARD_ROUTE(appData.modelName)} />;
-  }
-
-  // For safety default to the 404 view: this is for users not able to edit the current resource
-  // when this one is not ready.
-  return <Redirect push to={FULL_SCREEN_ERROR_ROUTE('notFound')} />;
 };
