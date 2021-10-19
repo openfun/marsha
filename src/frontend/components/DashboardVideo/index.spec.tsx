5e402645b074d690c97c747ff2190ae9743f6321
Manuel Raynaud <manu@raynaud.io>
2021-10-19 18:50:22 +0200

    ♻️(front) hide timed text pane when video is not a VOD
    On the first screen, when the user have to choose between creating a
    live or a VOD, the timed text pane is shown. We want to hide and show
    it only when the user has decided to create a VOD.

--- src/frontend/components/DashboardVideo/index.spec.tsx
+++ src/frontend/components/DashboardVideo/index.spec.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
+import { UploadManagerContext, UploadManagerStatus } from '../UploadManager';
+import { modelName } from '../../types/models';
 import { liveState, uploadState, Video } from '../../types/tracks';
 import { videoMockFactory } from '../../utils/tests/factories';
 import { wrapInIntlProvider } from '../../utils/tests/intl';
@@ -106,5 +108,83 @@ describe('<DashboardVideo />', () => {
     expect(
       screen.queryByText('dashboard timed text pane'),
     ).not.toBeInTheDocument();
+  });
+  it('hides timed text pane when the upload state is PENDING', () => {
+    const videoProps = videoMockFactory({
+      id: 'dd43',
+      upload_state: uploadState.PENDING,
+    });
+    render(wrapInIntlProvider(<DashboardVideo video={videoProps} />));
+
+    screen.getByTitle('dd43');
+    expect(
+      screen.queryByText('dashboard timed text pane'),
+    ).not.toBeInTheDocument();
+  });
+  it('shows the timed text pane when the upload state is  PROCESSING', () => {
+    const videoProps = videoMockFactory({
+      id: 'dd43',
+      upload_state: uploadState.PROCESSING,
+    });
+    render(wrapInIntlProvider(<DashboardVideo video={videoProps} />));
+
+    screen.getByTitle('dd43');
+    screen.getByText('dashboard timed text pane');
+  });
+  it('shows the timed text pane when the updload state is PENDING and the uploadManagerState is ongoing', () => {
+    const videoProps = videoMockFactory({
+      id: 'dd43',
+      upload_state: uploadState.PENDING,
+    });
+    const file = new File(['(⌐□_□)'], 'course.mp4', { type: 'video/mp4' });
+    render(
+      <UploadManagerContext.Provider
+        value={{
+          setUploadState: jest.fn(),
+          uploadManagerState: {
+            [videoProps.id]: {
+              file,
+              objectId: videoProps.id,
+              objectType: modelName.VIDEOS,
+              progress: 60,
+              status: UploadManagerStatus.UPLOADING,
+            },
+          },
+        }}
+      >
+        {wrapInIntlProvider(<DashboardVideo video={videoProps} />)}
+      </UploadManagerContext.Provider>,
+    );
+
+    screen.getByTitle('dd43');
+    screen.getByText('dashboard timed text pane');
+  });
+  it('shows the timed text pane when the updload state is PENDING and the uploadManagerState is succeeded', () => {
+    const videoProps = videoMockFactory({
+      id: 'dd43',
+      upload_state: uploadState.PENDING,
+    });
+    const file = new File(['(⌐□_□)'], 'course.mp4', { type: 'video/mp4' });
+    render(
+      <UploadManagerContext.Provider
+        value={{
+          setUploadState: jest.fn(),
+          uploadManagerState: {
+            [videoProps.id]: {
+              file,
+              objectId: videoProps.id,
+              objectType: modelName.VIDEOS,
+              progress: 60,
+              status: UploadManagerStatus.SUCCESS,
+            },
+          },
+        }}
+      >
+        {wrapInIntlProvider(<DashboardVideo video={videoProps} />)}
+      </UploadManagerContext.Provider>,
+    );
+
+    screen.getByTitle('dd43');
+    screen.getByText('dashboard timed text pane');
   });
 });
