a4fc7254419b074dcfb0cece13df324646c04adf
Manuel Raynaud <manu@raynaud.io>
2021-10-19 18:50:22 +0200

    ♻️(front) hide timed text pane when video is not a VOD
    On the first screen, when the user have to choose between creating a
    live or a VOD, the timed text pane is shown. We want to hide and show
    it only when the user has decided to create a VOD.

--- src/frontend/components/DashboardVideo/index.tsx
+++ src/frontend/components/DashboardVideo/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useVideo } from '../../data/stores/useVideo';
 import { uploadState, Video } from '../../types/tracks';
 import { DashboardTimedTextPane } from '../DashboardTimedTextPane';
 import { DashboardVideoPane } from '../DashboardVideoPane';
-
-const { READY, PENDING } = uploadState;
+import { UploadManagerStatus, useUploadManager } from '../UploadManager';
 
 interface DashboardVideoProps {
   video: Video;
@@ -13,13 +12,25 @@ interface DashboardVideoProps {
 
 const DashboardVideo = (props: DashboardVideoProps) => {
   const video = useVideo((state) => state.getVideo(props.video));
-  const displayTimedTextPane =
-    video.live_state === null &&
-    ![
-      uploadState.DELETED,
-      uploadState.HARVESTED,
-      uploadState.HARVESTING,
-    ].includes(video.upload_state);
+  const { uploadManagerState } = useUploadManager();
+  const [displayTimedTextPane, setDisplayTimedTextPane] = useState(false);
+
+  useEffect(() => {
+    setDisplayTimedTextPane(
+      video.live_state === null &&
+        (![
+          uploadState.DELETED,
+          uploadState.HARVESTED,
+          uploadState.HARVESTING,
+          uploadState.PENDING,
+        ].includes(video.upload_state) ||
+          (video.upload_state === uploadState.PENDING &&
+            (uploadManagerState[video.id]?.status ===
+              UploadManagerStatus.UPLOADING ||
+              uploadManagerState[video.id]?.status ===
+                UploadManagerStatus.SUCCESS))),
+    );
+  }, [video.live_state, video.upload_state, uploadManagerState]);
 
   return (
     <React.Fragment>
