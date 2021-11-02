ed6264c8c4986e676d52a4686cd83d57ac33c0be
Manuel Raynaud <manu@raynaud.io>
2021-11-02 19:21:35 +0100

    🔥(front) remove usage of type in useTranscriptTimeSelector store
    Zustand middleware are able now to infer the type to use witout
    declaring a state and use it as a generic. The type used in
    useTranscriptTimeSelector store must be removed and let zustand infers
    it.

--- src/frontend/data/stores/useTranscriptTimeSelector/index.tsx
+++ src/frontend/data/stores/useTranscriptTimeSelector/index.tsx
@@ -1,14 +1,9 @@
 import create from 'zustand';
 import { subscribeWithSelector } from 'zustand/middleware';
 
-type State = {
-  time: number;
-  setTime: (time: number) => void;
-};
-
 export const useTranscriptTimeSelector = create(
-  subscribeWithSelector<State>((set) => ({
-    setTime: (time) => set({ time }),
+  subscribeWithSelector((set) => ({
+    setTime: (time: number) => set({ time }),
     time: 0,
   })),
 );
