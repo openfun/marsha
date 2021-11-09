6b005008a48253f4762e38afde7da3126769a6a1
Manuel Raynaud <manu@raynaud.io>
2021-11-09 10:40:00 +0100

    ✨(aws) send aws request id to udpate-live-state endpoint
    In the medialive lambda we want to send the aws request id to the
    update-live-state endpoint. With this info we can better handle retried
    requests.

--- src/aws/lambda-medialive/src/channelStateChanged.js
+++ src/aws/lambda-medialive/src/channelStateChanged.js
@@ -18,12 +18,13 @@ module.exports = async (channel, event, context) => {
   const videoId = channel.Name.split('_')[1];
   const body = {
     logGroupName: context.logGroupName,
+    requestId: context.awsRequestId,
     state: correspondingStatus[status],
   };
 
   const signature = computeSignature(SHARED_SECRET, JSON.stringify(body));
 
-  return await sendRequest(
+  return sendRequest(
     body,
     signature,
     DISABLE_SSL_VALIDATION ? false : true,
