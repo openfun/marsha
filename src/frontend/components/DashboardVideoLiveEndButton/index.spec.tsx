3e58302179b1e461c7f43da34badccfb9d3f17b8
Manuel Raynaud <manu@raynaud.io>
2021-11-12 19:30:34 +0100

    ✨(front) add component DashboardVideoLiveEndButton
    The component DashboardVideoLiveEndButton is responsible to end a live
    by calling the end-live endpoint and update the state once ended.

---
+++ src/frontend/components/DashboardVideoLiveEndButton/index.spec.tsx
@@ -0,0 +1,148 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+import { Grommet } from 'grommet';
+import React from 'react';
+
+import { useVideo } from '../../data/stores/useVideo';
+import { liveState } from '../../types/tracks';
+import { videoMockFactory } from '../../utils/tests/factories';
+import { wrapInIntlProvider } from '../../utils/tests/intl';
+import { wrapInRouter } from '../../utils/tests/router';
+import { FULL_SCREEN_ERROR_ROUTE } from '../ErrorComponents/route';
+import { DashboardVideoLiveEndButton } from '.';
+
+jest.mock('jwt-decode', () => jest.fn());
+jest.mock('../../data/appData', () => ({
+  appData: { jwt: 'cool_token_m8' },
+}));
+
+jest.mock('../Loader', () => ({
+  Loader: () => <span>Loader</span>,
+}));
+
+describe('DashboardVideoLiveEndButton', () => {
+  afterEach(() => {
+    fetchMock.restore();
+    jest.resetAllMocks();
+  });
+
+  beforeEach(() => {
+    /*
+        make sure to remove all body children, grommet layer gets rendered twice, known issue
+        https://github.com/grommet/grommet/issues/5200
+      */
+    document.body.innerHTML = '';
+    document.body.appendChild(document.createElement('div'));
+  });
+
+  it('renders the end button', () => {
+    const video = videoMockFactory({
+      live_state: liveState.PAUSED,
+    });
+
+    render(
+      wrapInIntlProvider(
+        wrapInRouter(<DashboardVideoLiveEndButton video={video} />),
+      ),
+    );
+
+    screen.getByRole('button', { name: /end live/i });
+  });
+
+  it('clicks on end live button and fails.', async () => {
+    const video = videoMockFactory({
+      live_state: liveState.PAUSED,
+    });
+
+    fetchMock.mock(`/api/videos/${video.id}/end-live/`, 400, {
+      method: 'POST',
+    });
+
+    render(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <Grommet>
+            <DashboardVideoLiveEndButton video={video} />
+          </Grommet>,
+          [
+            {
+              path: FULL_SCREEN_ERROR_ROUTE('liveInit'),
+              render: () => <span>error</span>,
+            },
+          ],
+        ),
+      ),
+    );
+
+    // Loader should not be rendered yet
+    expect(screen.queryByText('Loader')).not.toBeInTheDocument();
+
+    const endButton = screen.getByRole('button', {
+      name: /end live/i,
+    });
+    fireEvent.click(endButton);
+
+    // Confirmation layer should show up
+    const confirmButton = screen.getByRole('button', { name: /ok/i });
+    screen.getByRole('button', { name: /cancel/i });
+
+    // Clicks on ok button, confirmation layer should show up
+    fireEvent.click(confirmButton);
+
+    // the state changes to pending, the loader is rendered
+    screen.getByText('Loader');
+
+    await waitFor(() => expect(fetchMock.called()).toBe(true));
+
+    screen.getByText('error');
+  });
+
+  it('clicks on end live and it updates the video state', async () => {
+    const video = videoMockFactory({
+      live_state: liveState.PAUSED,
+    });
+    fetchMock.mock(
+      `/api/videos/${video.id}/end-live/`,
+      {
+        ...video,
+        live_state: liveState.STOPPED,
+      },
+      { method: 'POST' },
+    );
+
+    render(
+      wrapInIntlProvider(
+        wrapInRouter(
+          <Grommet>
+            <DashboardVideoLiveEndButton video={video} />
+          </Grommet>,
+        ),
+      ),
+    );
+
+    // Loader should not be rendered yet
+    expect(screen.queryByText('Loader')).not.toBeInTheDocument();
+
+    const endButton = screen.getByRole('button', {
+      name: /end live/i,
+    });
+    fireEvent.click(endButton);
+
+    // Confirmation layer should show up
+    const confirmButton = screen.getByRole('button', { name: /ok/i });
+    screen.getByRole('button', { name: /cancel/i });
+
+    // Clicks on ok button, confirmation layer should show up
+    fireEvent.click(confirmButton);
+
+    // the state changes to pending, the loader is rendered
+    screen.getByText('Loader');
+
+    await waitFor(() => expect(fetchMock.called()).toBe(true));
+
+    expect(useVideo.getState().videos[video.id]).toEqual({
+      ...video,
+      live_state: liveState.STOPPED,
+    });
+  });
+});
---
+++ src/frontend/components/DashboardVideoLiveEndButton/index.tsx
@@ -0,0 +1,65 @@
+import React, { useCallback, useState } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { Redirect } from 'react-router-dom';
+
+import { endLive } from '../../data/sideEffects/endLive';
+import { useVideo } from '../../data/stores/useVideo';
+import { Video } from '../../types/tracks';
+import { Nullable } from '../../utils/types';
+import { DashboardConfirmButton } from '../DashboardConfirmButton';
+import { FULL_SCREEN_ERROR_ROUTE } from '../ErrorComponents/route';
+import { Loader } from '../Loader';
+
+const messages = defineMessages({
+  endLive: {
+    defaultMessage: 'End live',
+    description: 'End a video streaming.',
+    id: 'components.DashboardVideoLiveEndButton.endLive',
+  },
+  confirmEndLive: {
+    defaultMessage:
+      'Are you sure you want to end the video streaming ? This action is definitive',
+    description: 'Confirmation to end the video streaming.',
+    id: 'components.DashboardVideoLiveEndButton.confirmEndLive',
+  },
+});
+
+type endLiveStatus = 'pending' | 'error';
+
+interface DashboardVideoLiveEndButtonProps {
+  video: Video;
+}
+
+export const DashboardVideoLiveEndButton = ({
+  video,
+}: DashboardVideoLiveEndButtonProps) => {
+  const [status, setStatus] = useState<Nullable<endLiveStatus>>(null);
+  const { updateVideo } = useVideo((state) => ({
+    updateVideo: state.addResource,
+  }));
+
+  const endLiveAction = useCallback(async () => {
+    setStatus('pending');
+    try {
+      const updatedVideo = await endLive(video);
+      updateVideo(updatedVideo);
+    } catch (error) {
+      setStatus('error');
+    }
+  }, [video]);
+
+  if (status === 'error') {
+    return <Redirect push to={FULL_SCREEN_ERROR_ROUTE('liveInit')} />;
+  }
+
+  return (
+    <React.Fragment>
+      {status === 'pending' && <Loader />}
+      <DashboardConfirmButton
+        label={<FormattedMessage {...messages.endLive} />}
+        confirmationLabel={<FormattedMessage {...messages.confirmEndLive} />}
+        onConfirm={endLiveAction}
+      />
+    </React.Fragment>
+  );
+};
