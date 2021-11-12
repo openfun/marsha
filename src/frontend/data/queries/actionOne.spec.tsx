eb5b33a8631e12b874f7117217e38c0e1d89aca7
Nicolas Clerc <kernicpanel@nclerc.fr>
2021-11-12 17:53:15 +0100

    ✨(frontend) update actionOne query
    Update query actionOne to permit
      - usage of any request method
      - usage of empty body request

--- src/frontend/data/queries/actionOne.spec.tsx
+++ src/frontend/data/queries/actionOne.spec.tsx
@@ -37,6 +37,52 @@ describe('queries/actionOne', () => {
     expect(response).toEqual({ key: 'value' });
   });
 
+  it('allows empty request body', async () => {
+    mockAppData = { jwt: 'some token' };
+    fetchMock.mock('/api/model-name/1/action/', { key: 'value' });
+
+    const response = await actionOne({
+      name: 'model-name',
+      id: '1',
+      action: 'action',
+    });
+
+    expect(fetchMock.lastCall()![0]).toEqual('/api/model-name/1/action/');
+    expect(fetchMock.lastCall()![1]).toEqual({
+      headers: {
+        Authorization: 'Bearer some token',
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+    });
+    expect(response).toEqual({ key: 'value' });
+  });
+
+  it('allows defining request method', async () => {
+    mockAppData = { jwt: 'some token' };
+    const objectToUpdate = { object: 'data' };
+    fetchMock.mock('/api/model-name/1/action/', { key: 'value' });
+
+    const response = await actionOne({
+      name: 'model-name',
+      id: '1',
+      action: 'action',
+      method: 'GET',
+      object: objectToUpdate,
+    });
+
+    expect(fetchMock.lastCall()![0]).toEqual('/api/model-name/1/action/');
+    expect(fetchMock.lastCall()![1]).toEqual({
+      headers: {
+        Authorization: 'Bearer some token',
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+      body: JSON.stringify(objectToUpdate),
+    });
+    expect(response).toEqual({ key: 'value' });
+  });
+
   it('requests the resource list without JWT token', async () => {
     mockAppData = {};
     const objectToUpdate = { object: 'data' };
