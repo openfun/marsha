e4d73a20c86db6b2b4cd7342d5122a229c1da56c
Manuel Raynaud <manu@raynaud.io>
2021-11-18 21:31:11 +0100

    ✨(front) detect when a live is resumed to update video store
    When a live is paused, the manifest contains `#EXT-X-ENDLIST`. This tag
    means no more media segments will be added to the manifest. Fortunately,
    mediapackage will remove this tag once the live resumed, meaning the
    live_state has changed in RUNNING and we can fetch the video object to
    refresh its state in the application store. To do this, we fetch the
    manifest to parse it while this tag is present. Once removed, we stop
    fetching and the video is updated in the store.

---
+++ src/frontend/utils/resumeLive.spec.ts
@@ -0,0 +1,137 @@
+import { waitFor } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+
+import { getResource } from 'data/sideEffects/getResource';
+import { requestStatus } from 'types/api';
+import { modelName } from 'types/models';
+
+import { videoMockFactory } from './tests/factories';
+import { resumeLive } from './resumeLive';
+
+jest.mock('data/appData', () => ({
+  appData: {
+    jwt: 'some token',
+  },
+}));
+
+jest.mock('data/sideEffects/getResource', () => ({
+  getResource: jest.fn(),
+}));
+
+const mockGetResource = getResource as jest.MockedFunction<typeof getResource>;
+
+describe('resumeLive', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useFakeTimers('modern');
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+    fetchMock.restore();
+  });
+  it('waits until manifest is not ended', async () => {
+    const video = videoMockFactory({
+      urls: {
+        manifests: {
+          hls: 'https://c223d9abb67d57c7.mediapackage.eu-west-1.amazonaws.com/out/v1/0a5924bddfff4fe68e8c10a2a671a503/dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls.m3u8',
+        },
+        mp4: {},
+        thumbnails: {},
+      },
+    });
+    mockGetResource.mockResolvedValue(requestStatus.SUCCESS);
+
+    fetchMock.mock(
+      'https://c223d9abb67d57c7.mediapackage.eu-west-1.amazonaws.com/out/v1/0a5924bddfff4fe68e8c10a2a671a503/dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls.m3u8',
+      `
+      #EXTM3U
+      #EXT-X-VERSION:3
+      #EXT-X-INDEPENDENT-SEGMENTS
+      #EXT-X-STREAM-INF:BANDWIDTH=1404480,AVERAGE-BANDWIDTH=1205600,RESOLUTION=854x480,FRAME-RATE=24.000,CODECS="avc1.640029,mp4a.40.2"
+      dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_1.m3u8
+      #EXT-X-STREAM-INF:BANDWIDTH=4440449,AVERAGE-BANDWIDTH=3735564,RESOLUTION=1280x720,FRAME-RATE=24.000,CODECS="avc1.640029,mp4a.40.2"
+      dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_2.m3u8
+      #EXT-X-STREAM-INF:BANDWIDTH=518276,AVERAGE-BANDWIDTH=455345,RESOLUTION=426x240,FRAME-RATE=24.000,CODECS="avc1.4D401E,mp4a.40.2"
+      dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_3.m3u8
+      `,
+    );
+
+    fetchMock.mock(
+      'https://c223d9abb67d57c7.mediapackage.eu-west-1.amazonaws.com/out/v1/0a5924bddfff4fe68e8c10a2a671a503/dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_1.m3u8',
+      `
+      #EXTM3U
+      #EXT-X-VERSION:3
+      #EXT-X-TARGETDURATION:4
+      #EXT-X-MEDIA-SEQUENCE:848
+      #EXT-X-DISCONTINUITY-SEQUENCE:21
+      #EXTINF:1.500,
+      dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_1_848.ts?m=1637050263
+      #EXT-X-ENDLIST
+      `,
+    );
+
+    resumeLive(video);
+
+    await waitFor(() => {
+      expect(
+        fetchMock.calls(
+          'https://c223d9abb67d57c7.mediapackage.eu-west-1.amazonaws.com/out/v1/0a5924bddfff4fe68e8c10a2a671a503/dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls.m3u8',
+          {
+            method: 'GET',
+          },
+        ),
+      ).toHaveLength(1);
+    });
+    expect(mockGetResource).not.toHaveBeenCalled();
+
+    await waitFor(() => {
+      expect(
+        fetchMock.calls(
+          'https://c223d9abb67d57c7.mediapackage.eu-west-1.amazonaws.com/out/v1/0a5924bddfff4fe68e8c10a2a671a503/dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_1.m3u8',
+          {
+            method: 'GET',
+          },
+        ),
+      ).toHaveLength(1);
+    });
+    expect(mockGetResource).not.toHaveBeenCalled();
+
+    jest.advanceTimersToNextTimer();
+
+    await waitFor(() => {
+      expect(
+        fetchMock.calls(
+          'https://c223d9abb67d57c7.mediapackage.eu-west-1.amazonaws.com/out/v1/0a5924bddfff4fe68e8c10a2a671a503/dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_1.m3u8',
+          {
+            method: 'GET',
+          },
+        ),
+      ).toHaveLength(2);
+    });
+    expect(mockGetResource).not.toHaveBeenCalled();
+
+    fetchMock.mock(
+      'https://c223d9abb67d57c7.mediapackage.eu-west-1.amazonaws.com/out/v1/0a5924bddfff4fe68e8c10a2a671a503/dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_1.m3u8',
+      `
+      #EXTM3U
+      #EXT-X-VERSION:3
+      #EXT-X-TARGETDURATION:4
+      #EXT-X-MEDIA-SEQUENCE:848
+      #EXT-X-DISCONTINUITY-SEQUENCE:21
+      #EXTINF:1.500,
+      dev-manu_030aaea4-bb0b-4915-88a4-521fc8b59366_1637050264_hls_1_848.ts?m=1637050263
+      `,
+      {
+        overwriteRoutes: true,
+      },
+    );
+
+    jest.advanceTimersToNextTimer();
+
+    await waitFor(() => {
+      expect(mockGetResource).toHaveBeenCalledWith(modelName.VIDEOS, video.id);
+    });
+  });
+});
