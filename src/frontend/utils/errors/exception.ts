fa8a9e0d9865e9f5a03304be42e3bdb59489cc02
vassili <vassili.rezvoy@polyconseil.fr>
2021-11-30 15:16:57 +0100

    ✅(frontend) add exception to typecheck switch enum
    Create a new exception type to raise error while transpiling the code if an enum
    value is not covered in a switch case.

---
+++ src/frontend/utils/errors/exception.ts
@@ -0,0 +1,6 @@
+// To use for default statement in switch where all cases must be covered
+export class ShouldNotHappen extends Error {
+  constructor(val: never) {
+    super(`this should not happen: ${val}`);
+  }
+}
