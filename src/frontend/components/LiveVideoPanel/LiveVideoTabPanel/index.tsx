2b0a2f7a310c21e60cdee83b849d23584c762410
vassili <vassili.rezvoy@polyconseil.fr>
2021-12-01 10:44:38 +0100

    ✨(frontend) create live panel
    Create component to display live panel and controls with tabs.

---
+++ src/frontend/components/LiveVideoPanel/LiveVideoTabPanel/index.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Tab, Text } from 'grommet';
+import { defineMessages, useIntl } from 'react-intl';
+import styled from 'styled-components';
+
+import { LivePanelItem } from 'data/stores/useLivePanelState';
+import { theme } from 'utils/theme/theme';
+
+interface StyledTabProps {
+  selected?: boolean;
+}
+
+const colors = {
+  active: theme.global.colors['blue-active'],
+  counterPart: theme.global.colors.white,
+  disable: theme.global.colors['blue-off'],
+  focus: theme.global.colors['blue-focus'],
+};
+
+const StyledTab = styled(Tab)`
+  background: ${colors.counterPart};
+  box-shadow: ${({ selected }: StyledTabProps) =>
+    selected
+      ? `inset 0 -2px ${colors.active}`
+      : `inset 0 -1px ${colors.disable}`};
+  flex: 1;
+  padding: 3px 10px;
+
+  div span {
+    color: ${({ selected }: StyledTabProps) =>
+      selected ? colors.active : colors.disable};
+  }
+
+  :hover {
+    box-shadow: ${({ selected }: StyledTabProps) =>
+      selected
+        ? `inset 0 -2px ${colors.active}`
+        : `inset 0 -2px ${colors.focus}`};
+
+    div span {
+      color: ${({ selected }: StyledTabProps) =>
+        selected ? colors.active : colors.focus};
+    }
+  }
+  :focus:not(:focus-visible) {
+    box-shadow: ${({ selected }: StyledTabProps) =>
+      selected
+        ? `inset 0 -2px ${colors.active}`
+        : `inset 0 -2px ${colors.focus}`};
+
+    div span {
+      color: ${({ selected }: StyledTabProps) =>
+        selected ? colors.active : colors.focus};
+    }
+  }
+`;
+
+const StyledText = styled(Text)`
+  font-family: Roboto-Bold;
+  font-size: 10px;
+  font-weight: bold;
+  letter-spacing: -0.23px;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+const messages = defineMessages({
+  [LivePanelItem.APPLICATION]: {
+    defaultMessage: 'application',
+    description: 'Application tab title in live panel',
+    id: 'component.LiveVideoPanel.LiveVideoTabPanel.application',
+  },
+  [LivePanelItem.CHAT]: {
+    defaultMessage: 'chat',
+    description: 'Chat tab title in live panel',
+    id: 'component.LiveVideoPanel.LiveVideoTabPanel.chat',
+  },
+  [LivePanelItem.JOIN_DISCUSSION]: {
+    defaultMessage: 'viewers',
+    description: 'Viewers and join discussion tab title in live panel',
+    id: 'component.LiveVideoPanel.LiveVideoTabPanel.joinDiscussion',
+  },
+});
+
+interface LiveVideoTabPanelProps {
+  item: LivePanelItem;
+  selected: boolean;
+}
+
+export const LiveVideoTabPanel = ({
+  item,
+  selected,
+}: LiveVideoTabPanelProps) => {
+  const intl = useIntl();
+
+  return (
+    <StyledTab
+      title={
+        <StyledText role="tab_title">
+          {intl.formatMessage(messages[item])}
+        </StyledText>
+      }
+      plain
+      selected={selected}
+    />
+  );
+};
---
+++ src/frontend/components/LiveVideoPanel/index.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect } from 'react';
+import { Tabs, Box, Grommet } from 'grommet';
+
+import { Chat } from 'components/Chat';
+import {
+  LivePanelItem,
+  useLivePanelState,
+} from 'data/stores/useLivePanelState';
+import { Video } from 'types/tracks';
+import { ShouldNotHappen } from 'utils/errors/exception';
+import { breakpointSize, theme } from 'utils/theme/theme';
+
+import { LiveVideoTabPanel } from './LiveVideoTabPanel';
+
+interface LiveVideoPanelProps {
+  video: Video;
+}
+
+export const LiveVideoPanel = ({ video }: LiveVideoPanelProps) => {
+  const { currentItem, availableItems, setPanelVisibility } = useLivePanelState(
+    (state) => ({
+      currentItem: state.currentItem,
+      availableItems: state.availableItems,
+      setPanelVisibility: state.setPanelVisibility,
+    }),
+  );
+
+  //  close panel if there is nothing to display
+  useEffect(() => {
+    if (!currentItem) {
+      setPanelVisibility(false);
+    }
+  }, [currentItem]);
+
+  if (!currentItem) {
+    return <React.Fragment />;
+  }
+
+  let header;
+  if (availableItems.length > 1) {
+    header = (
+      <Tabs
+        activeIndex={availableItems.indexOf(currentItem)}
+        onActive={(index) => setPanelVisibility(true, availableItems[index])}
+      >
+        {availableItems.map((item) => (
+          <LiveVideoTabPanel
+            item={item}
+            selected={item === currentItem}
+            key={`tab_${item}`}
+          />
+        ))}
+      </Tabs>
+    );
+  }
+
+  let content;
+  switch (currentItem) {
+    case LivePanelItem.CHAT:
+      content = <Chat video={video} />;
+      break;
+    case LivePanelItem.APPLICATION:
+      //  TODO : implement this item
+      content = <p>application content</p>;
+      break;
+    case LivePanelItem.JOIN_DISCUSSION:
+      //  TODO : implement this item
+      content = <p>join discussion content</p>;
+      break;
+    default:
+      throw new ShouldNotHappen(currentItem);
+  }
+
+  return (
+    <Grommet
+      theme={{
+        ...theme,
+        tabs: {
+          header: {
+            extend: `white-space: nowrap; \
+            display: flex; \
+            flex-wrap: nowrap;`,
+          },
+          extend: `@media (max-width: ${breakpointSize.tablet}) { \
+            display: none; \
+          }`,
+          gap: 'none',
+        },
+      }}
+      style={{ height: '100%' }}
+    >
+      {header}
+      <Box fill={true}>{content}</Box>
+    </Grommet>
+  );
+};
