3f663d3620c6f8fcacab9e702a8b7891a63d511e
Raphael <rfer92@gmail.com>
2021-12-03 17:50:48 +0100

    ✨(front) create component button
    create a default component button that can take an icon and text

--- src/frontend/testSetup.ts
+++ src/frontend/testSetup.ts
@@ -5,3 +5,7 @@ import '@testing-library/jest-dom';
 
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
 expect.extend({ toMatchImageSnapshot });
+
+beforeEach(() => {
+  global.ResizeObserver = require('resize-observer-polyfill');
+});
