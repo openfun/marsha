98df1734849561a54e081d4b0de643415a311d72
Raphael <rfer92@gmail.com>
2021-12-06 13:05:24 +0100

    ✨(front) create badge component
    create a badge to display a number over some buttons

---
+++ src/frontend/components/Badge/index.spec.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { imageSnapshot } from 'utils/tests/imageSnapshot';
+
+import { Badge } from '.';
+
+describe('<StudentShowChatButton />', () => {
+  it('renders the badge component', async () => {
+    render(<Badge value={24} />);
+
+    screen.getByText('24');
+
+    const badge = screen.getByRole('badge_container');
+    expect(badge).toHaveStyle('background-color: #ffffff;');
+    expect(badge).toHaveStyle('border: 1px solid #031963;');
+    expect(badge).toHaveStyle('border-radius: 6px;');
+    expect(badge).toHaveStyle('bottom: 2px;');
+    expect(badge).toHaveStyle('color: #031963;');
+    expect(badge).toHaveStyle('font-weight: bold;');
+    expect(badge).toHaveStyle('padding: 3px 6px;');
+    expect(badge).toHaveStyle('position: absolute;');
+    expect(badge).toHaveStyle('right: -8px;');
+
+    await imageSnapshot();
+  });
+});
