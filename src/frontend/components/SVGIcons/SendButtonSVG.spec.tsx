df5afbb7603e233326cdfd75cd57c8c67bfeb1cc
roro-lv <ronan.le-viennesse@polyconseil.fr>
2021-12-06 12:47:35 +0100

    🍱(frontend) add a new svg icon
    This svg icon is from the mockup and is on the send button

---
+++ src/frontend/components/SVGIcons/SendButtonSVG.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { renderIconSnapshot } from 'utils/tests/imageSnapshot';
+
+import { SendButtonSVG } from './SendButtonSVG';
+
+it('renders SendButtonSVG correctly', async () => {
+  await renderIconSnapshot(
+    <SendButtonSVG
+      iconColor={'blue-chat'}
+      containerStyle={{
+        width: '25px',
+        height: '22px',
+      }}
+    />,
+  );
+});
