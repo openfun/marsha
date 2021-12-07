9d66effeeb7a11b580d9f4ae8b9ef1221efc14c5
roro-lv <ronan.le-viennesse@polyconseil.fr>
2021-12-07 15:46:05 +0100

    💄(frontend) add ChatConversationDisplayer component
    This component displays all messages received by the front and recorded in
    the zustand store.

---
+++ src/frontend/components/Chat/SharedChatComponents/ChatConversationDisplayer/index.tsx
@@ -0,0 +1,57 @@
+import { Box, Spinner } from 'grommet';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { ChatMessageItem } from 'components/Chat/SharedChatComponents/ChatMessageItem';
+import { useMessagesState } from 'data/stores/useMessagesStore';
+
+export const ChatConversationDisplayer = () => {
+  const { messages } = useMessagesState();
+  const [loading, setLoading] = useState(true);
+  const scrollableContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      scrollableContainerRef.current!.scrollTo({
+        top: scrollableContainerRef.current!.scrollHeight,
+      });
+    }, 3000);
+  }, []);
+
+  const isScrollInBottom = scrollableContainerRef.current
+    ? scrollableContainerRef.current!.scrollHeight -
+        Math.round(scrollableContainerRef.current!.scrollTop) ===
+      scrollableContainerRef.current!.clientHeight
+    : false;
+
+  useEffect(() => {
+    if (scrollableContainerRef.current && isScrollInBottom) {
+      scrollableContainerRef.current.scrollTo({
+        top: scrollableContainerRef.current.scrollHeight,
+      });
+    }
+  }, [messages]);
+
+  return (
+    <Box
+      overflow={{
+        vertical: 'auto',
+        horizontal: 'hidden',
+      }}
+      ref={scrollableContainerRef}
+      height="100%"
+    >
+      {loading ? (
+        <Box align="center" height="100%" justify="center" width="100%">
+          <Spinner size="large" />
+        </Box>
+      ) : (
+        <React.Fragment>
+          {messages.map((msg, idx) => (
+            <ChatMessageItem key={idx} msg={msg} />
+          ))}
+        </React.Fragment>
+      )}
+    </Box>
+  );
+};
