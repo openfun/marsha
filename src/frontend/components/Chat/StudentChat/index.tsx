b629ef2d0f8a2fa3bc5daa320a8a930f3ee6cafa
roro-lv <ronan.le-viennesse@polyconseil.fr>
2021-12-27 12:16:48 +0100

    ✨(frontend) add chat's changes to integrate new authentification feature
    Add a new feature to the chat. An incoming user is now automatically login as
    an anonymous one when he arrived on the chat. He can display messages but if he
    wants to join it and be able to write his own he has to authenticate himself.
    A dedicated overlay component is added to fulfill this role.

--- src/frontend/components/Chat/StudentChat/index.tsx
+++ src/frontend/components/Chat/StudentChat/index.tsx
@@ -1,21 +1,75 @@
 import { Box } from 'grommet';
-import React from 'react';
+import React, { useState } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
 
 import { ChatConversationDisplayer } from 'components/Chat/SharedChatComponents/ChatConversationDisplayer';
-import { ChatInputBar } from 'components/Chat/SharedChatComponents/ChatInputBar';
+import { InputBar } from 'components/Chat/SharedChatComponents/InputBar';
+import { InputDisplayNameOverlay } from 'components/Chat/SharedChatComponents/InputDisplayNameOverlay';
+import { JoinChatButton } from 'components/Chat/SharedChatComponents/JoinChatButton';
+import { converse } from 'utils/window';
 
 export const StudentChat = () => {
+  const [overlay, setOverlay] = useState(false);
+  const [isInputBarActive, setInputBarActive] = useState(false);
+  const intl = useIntl();
+
+  const processChatMessage = (chatMsg: string) => {
+    converse.sendMessage(chatMsg);
+    return true;
+  };
+
+  const messages = defineMessages({
+    inputBarPlaceholder: {
+      defaultMessage: 'Message...',
+      description:
+        'The input bar to write, edit and send messages to the chat conversation.',
+      id: 'components.InputBar.inputBarPlaceholder',
+    },
+  });
+
+  const handleJoinChatButton = () => {
+    setOverlay(true);
+  };
+
   return (
     <Box
-      direction="column"
       border={{
         color: 'blue',
         size: 'small',
       }}
+      direction="column"
       height="medium"
     >
-      <ChatConversationDisplayer />
-      <ChatInputBar />
+      {overlay && (
+        <InputDisplayNameOverlay
+          setInputBarActive={setInputBarActive}
+          setOverlay={setOverlay}
+        />
+      )}
+
+      {
+        // ChatConversationDisplayer display a spinner each time it is mounted,
+        // so it can't be unmounted for now
+      }
+      <Box
+        height="100%"
+        style={{
+          display: overlay ? 'none' : 'flex',
+        }}
+      >
+        <ChatConversationDisplayer />
+        <Box margin="10px">
+          {isInputBarActive ? (
+            <InputBar
+              handleUserInput={processChatMessage}
+              isChatInput={true}
+              placeholderText={intl.formatMessage(messages.inputBarPlaceholder)}
+            />
+          ) : (
+            <JoinChatButton handleClick={handleJoinChatButton} />
+          )}
+        </Box>
+      </Box>
     </Box>
   );
 };
