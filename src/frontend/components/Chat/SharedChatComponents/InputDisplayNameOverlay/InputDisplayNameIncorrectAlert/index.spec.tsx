32b29def7b74e312ff5637f2740cdb5e80eb6e0e
roro-lv <ronan.le-viennesse@polyconseil.fr>
2021-12-27 11:48:54 +0100

    💄(frontend) add InputDisplayNameIncorrectAlert component
    This component is used by InputDisplayNameOverlay (which will be committed in
    next commit). It is in charge of displaying alert message when typed input
    value doesn't match defined rules.

---
+++ src/frontend/components/Chat/SharedChatComponents/InputDisplayNameOverlay/InputDisplayNameIncorrectAlert/index.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { renderImageSnapshot } from 'utils/tests/imageSnapshot';
+import { wrapInIntlProvider } from 'utils/tests/intl';
+import { InputDisplayNameIncorrectAlert } from '.';
+
+describe('<InputDisplayNameIncorrectAlert />', () => {
+  it('displays the alert message given in props.', () => {
+    render(
+      wrapInIntlProvider(
+        <InputDisplayNameIncorrectAlert alertMsg="This is an example alert message." />,
+      ),
+    );
+    screen.getByText('This is an example alert message.');
+  });
+
+  it('displays the component and compares it with previous render.', async () => {
+    await renderImageSnapshot(
+      <InputDisplayNameIncorrectAlert alertMsg="This is an example alert message.'" />,
+    );
+  });
+});
