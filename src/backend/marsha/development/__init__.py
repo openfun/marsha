007ef6d65acfa55d574cc96b0860a7d8e1c1cc84
Manuel Raynaud <manu@raynaud.io>
2021-12-27 15:12:01 +0100

    🐛(back) remove development views from init module
    The development init module import all views. With this import, marsha
    is not working in non development environment. This commit complete
    commit 3e4a8ed1a0ce3d71ba5283613169e50c0bbc562d.

--- src/backend/marsha/development/__init__.py
+++ src/backend/marsha/development/__init__.py
@@ -1,4 +1,3 @@
 """Make all endpoints and views available from marsha.development."""
 # pylint: disable=wildcard-import,unused-wildcard-import
 from .api import *  # noqa isort:skip
-from .views import *  # noqa isort:skip
