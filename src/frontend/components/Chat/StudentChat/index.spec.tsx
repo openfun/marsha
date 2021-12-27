bc1e10f7e15f234ad5357684a2b8d93b37a504f4
roro-lv <ronan.le-viennesse@polyconseil.fr>
2021-12-27 12:16:48 +0100

    ✨(frontend) add chat's changes to integrate new authentification feature
    Add a new feature to the chat. An incoming user is now automatically login as
    an anonymous one when he arrived on the chat. He can display messages but if he
    wants to join it and be able to write his own he has to authenticate himself.
    A dedicated overlay component is added to fulfill this role.

--- src/frontend/components/Chat/StudentChat/index.spec.tsx
+++ src/frontend/components/Chat/StudentChat/index.spec.tsx
@@ -1,48 +1,54 @@
-import { DateTime } from 'luxon';
 import React from 'react';
 import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { useMessagesState } from 'data/stores/useMessagesStore';
 import { wrapInIntlProvider } from 'utils/tests/intl';
 import { converse } from 'utils/window';
 import { StudentChat } from '.';
 
 window.HTMLElement.prototype.scrollTo = jest.fn();
 
-const sendMessageMock = (msgContent: string) =>
-  act(() =>
-    useMessagesState.getState().addMessage({
-      sentAt: DateTime.fromISO('2020-01-01T12:12:12'),
-      sender: 'John Doe',
-      content: msgContent,
-    }),
-  );
-
 jest.mock('utils/window', () => ({
   converse: {
-    sendMessage: jest.fn(sendMessageMock),
+    claimNewNicknameInChatRoom: jest.fn(),
+    sendMessage: jest.fn(),
   },
 }));
 
 describe('<StudentChat />', () => {
-  it('checks that a message is displayed after having been send with the input bar', async () => {
+  it('connects an anonymous user and sends a message.', async () => {
     jest.useFakeTimers();
     render(wrapInIntlProvider(<StudentChat />));
     await act(async () => {
       jest.advanceTimersByTime(4000);
     });
-    const chatInputText = screen.getByRole('textbox');
-    const chatSendButton = screen.getByRole('button');
-    userEvent.type(chatInputText, 'A simple message.');
-    await act(async () => {
-      userEvent.click(chatSendButton);
+    const joinChatButton = screen.getByRole('button', {
+      name: 'Join the chat',
     });
-    expect(converse.sendMessage).toHaveBeenCalledWith('A simple message.');
-    expect(chatInputText).toHaveValue('');
-
-    screen.getByText('(12:12)');
-    screen.getByText('John Doe');
-    screen.getByText('A simple message.');
+    await act(async () => {
+      userEvent.click(joinChatButton);
+    });
+    const textBoxDisplayName = screen.getByRole('textbox');
+    const buttonSendDisplayName = screen.getByRole('button');
+    userEvent.type(textBoxDisplayName, 'John Doe');
+    await act(async () => {
+      userEvent.click(buttonSendDisplayName);
+    });
+    expect(converse.claimNewNicknameInChatRoom).toHaveBeenNthCalledWith(
+      1,
+      'John Doe',
+    );
+    expect(converse.claimNewNicknameInChatRoom).toHaveBeenCalledTimes(1);
+    const sendChatButton = screen.getByRole('button');
+    const textboxChatInput = screen.getByRole('textbox');
+    userEvent.type(textboxChatInput, 'This is an example message.');
+    await act(async () => {
+      userEvent.click(sendChatButton);
+    });
+    expect(converse.sendMessage).toHaveBeenNthCalledWith(
+      1,
+      'This is an example message.',
+    );
+    expect(converse.sendMessage).toHaveBeenCalledTimes(1);
   });
 });
