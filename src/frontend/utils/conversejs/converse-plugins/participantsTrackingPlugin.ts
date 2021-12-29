2a62a17b286f5f3abb1910022527d782e91c0b9b
roro-lv <ronan.le-viennesse@polyconseil.fr>
2021-12-29 16:58:18 +0100

    ✨(frontend) add a converse plugin for monitoring presences
    This plugin receives the presence stanzas and updates the participants list
    depending of the stanza's content & nature.

---
+++ src/frontend/utils/conversejs/converse-plugins/participantsTrackingPlugin.ts
@@ -0,0 +1,70 @@
+import { useParticipantsStore } from 'data/stores/useParticipantsStore';
+import { converse } from 'utils/window';
+
+const PLUGIN_NAME = 'participants-tracking-plugin';
+
+const addParticipantsTrackingPlugin = () =>
+  converse.plugins.add(PLUGIN_NAME, {
+    dependencies: ['converse-muc'],
+    initialize() {
+      const _converse = this._converse;
+
+      _converse.on('initialized', () => {
+        _converse.connection.addHandler(
+          (stanza: HTMLElement) => {
+            if (
+              stanza.getAttribute('from') !== null &&
+              stanza.getAttribute('to') !== null
+            ) {
+              const participantDisplayName = getNameFromJID(
+                stanza.getAttribute('from')!,
+              );
+              if (
+                stanza.getAttribute('type') !== null &&
+                stanza.getAttribute('type') === 'unavailable'
+              ) {
+                useParticipantsStore
+                  .getState()
+                  .removeParticipant(participantDisplayName);
+              } else if (
+                stanza.getElementsByTagName('item')[0] !== null &&
+                stanza
+                  .getElementsByTagName('item')[0]
+                  .getAttribute('affiliation') !== null &&
+                stanza
+                  .getElementsByTagName('item')[0]
+                  .getAttribute('affiliation') !== 'none'
+              ) {
+                const participantIsInstructor =
+                  stanza
+                    .getElementsByTagName('item')[0]
+                    .getAttribute('affiliation') === 'owner'
+                    ? true
+                    : false;
+                useParticipantsStore.getState().addParticipant({
+                  isInstructor: participantIsInstructor,
+                  isOnStage: false,
+                  name: participantDisplayName,
+                });
+              }
+            }
+            return true;
+          },
+          null,
+          'presence',
+          null,
+          null,
+          null,
+        );
+      });
+    },
+  });
+
+const getNameFromJID = (jid: string): string => {
+  return converse.env.Strophe.getResourceFromJid(jid);
+};
+
+export const participantsTrackingPlugin = {
+  name: PLUGIN_NAME,
+  addPlugin: addParticipantsTrackingPlugin,
+};
