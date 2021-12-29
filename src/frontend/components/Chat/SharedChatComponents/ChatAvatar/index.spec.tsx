f77b8acbccf84d8c03b1d2d0a6cc62263d0fa5bb
roro-lv <ronan.le-viennesse@polyconseil.fr>
2021-12-29 17:04:42 +0100

    💄(frontend) update ChatAvatar component UI
    Add a boolean prop to outline or not the avatar depending on if the user is an
    instructor.

--- src/frontend/components/Chat/SharedChatComponents/ChatAvatar/index.spec.tsx
+++ src/frontend/components/Chat/SharedChatComponents/ChatAvatar/index.spec.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { renderIconSnapshot } from 'utils/tests/imageSnapshot';
 import { screen } from '@testing-library/react';
 
+import { renderIconSnapshot } from 'utils/tests/imageSnapshot';
 import { ChatAvatar } from './index';
 
 describe('<ChatAvatar />', () => {
-  it('renders the avatar and compares it with snapshot', () => {
-    renderIconSnapshot(<ChatAvatar />);
+  it('renders the avatar for student and compares it with snapshot', async () => {
+    await renderIconSnapshot(<ChatAvatar isInstructor={false} />);
+    screen.getByTitle("The user's avatar");
+  });
+
+  it('renders the avatar for instructor and compares it with snapshot', async () => {
+    await renderIconSnapshot(<ChatAvatar isInstructor={true} />);
     screen.getByTitle("The user's avatar");
   });
 });
