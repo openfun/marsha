b25932548a7fa711e8b298c33a79f4e91bc58ff5
roro-lv <ronan.le-viennesse@polyconseil.fr>
2022-01-03 11:30:16 +0100

    💄(frontend) add InputDisplayNameOverlay component
    This component can overlay the chat and allows user to choose an username for
    joining it.

---
+++ src/frontend/components/Chat/SharedChatComponents/InputDisplayNameOverlay/index.spec.tsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { ANONYMOUS_ID_PREFIX } from 'utils/chat/chat';
+import { renderImageSnapshot } from 'utils/tests/imageSnapshot';
+import { wrapInIntlProvider } from 'utils/tests/intl';
+import { converse } from 'utils/window';
+import { InputDisplayNameOverlay } from './index';
+
+const mockSetInputBarActive = jest.fn();
+const mockSetOverlay = jest.fn();
+
+jest.mock('utils/window', () => ({
+  converse: {
+    claimNewNicknameInChatRoom: jest.fn(),
+  },
+}));
+
+describe('<InputDisplayNameOverlay />', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("controls input and shows error when input contains 'Anonymous'", () => {
+    render(
+      wrapInIntlProvider(
+        <InputDisplayNameOverlay
+          setInputBarActive={mockSetInputBarActive}
+          setOverlay={mockSetOverlay}
+        />,
+      ),
+    );
+    const inputTextbox = screen.getByRole('textbox');
+    const validateButton = screen.getByRole('button');
+    userEvent.type(inputTextbox, 'Anonymous-John');
+    act(() => userEvent.click(validateButton));
+    screen.getByText(`Keyword "${ANONYMOUS_ID_PREFIX}" is not allowed.`);
+    expect(screen.queryByText('Min length is 3 characters.')).toBeNull();
+    expect(screen.queryByText('Max length is 15 characters.')).toBeNull();
+    expect(inputTextbox).toHaveValue('Anonymous-John');
+    expect(mockSetInputBarActive).not.toHaveBeenCalled();
+    expect(mockSetOverlay).not.toHaveBeenCalled();
+  });
+
+  it('controls input and shows error when input contains less than 3 characters.', () => {
+    render(
+      wrapInIntlProvider(
+        <InputDisplayNameOverlay
+          setInputBarActive={mockSetInputBarActive}
+          setOverlay={mockSetOverlay}
+        />,
+      ),
+    );
+    const inputTextbox = screen.getByRole('textbox');
+    const validateButton = screen.getByRole('button');
+    userEvent.type(inputTextbox, 'JD');
+    act(() => userEvent.click(validateButton));
+    screen.getByText('Min length is 3 characters.');
+    expect(
+      screen.queryByText(`Keyword "${ANONYMOUS_ID_PREFIX}" is not allowed.`),
+    ).toBeNull();
+    expect(screen.queryByText('Max length is 15 characters.')).toBeNull();
+    expect(inputTextbox).toHaveValue('JD');
+    expect(mockSetInputBarActive).not.toHaveBeenCalled();
+    expect(mockSetOverlay).not.toHaveBeenCalled();
+  });
+
+  it('controls input and shows error when input contains more than 15 characters.', () => {
+    render(
+      wrapInIntlProvider(
+        <InputDisplayNameOverlay
+          setInputBarActive={mockSetInputBarActive}
+          setOverlay={mockSetOverlay}
+        />,
+      ),
+    );
+    const inputTextbox = screen.getByRole('textbox');
+    const validateButton = screen.getByRole('button');
+    userEvent.type(inputTextbox, 'John Doe the legend');
+    act(() => userEvent.click(validateButton));
+    screen.getByText('Max length is 15 characters.');
+    expect(
+      screen.queryByText(`Keyword "${ANONYMOUS_ID_PREFIX}" is not allowed.`),
+    ).toBeNull();
+    expect(screen.queryByText('Min length is 3 characters.')).toBeNull();
+    expect(inputTextbox).toHaveValue('John Doe the legend');
+    expect(mockSetInputBarActive).not.toHaveBeenCalled();
+    expect(mockSetOverlay).not.toHaveBeenCalled();
+  });
+
+  it('enters a valid nickname and validates it.', () => {
+    render(
+      wrapInIntlProvider(
+        <InputDisplayNameOverlay
+          setInputBarActive={mockSetInputBarActive}
+          setOverlay={mockSetOverlay}
+        />,
+      ),
+    );
+    const inputTextbox = screen.getByRole('textbox');
+    const validateButton = screen.getByRole('button');
+    userEvent.type(inputTextbox, 'John_Doe');
+    act(() => userEvent.click(validateButton));
+    expect(mockSetInputBarActive).toHaveBeenCalledTimes(1);
+    expect(mockSetOverlay).toHaveBeenCalledTimes(1);
+    expect(converse.claimNewNicknameInChatRoom).toHaveBeenNthCalledWith(
+      1,
+      'John_Doe',
+    );
+    expect(converse.claimNewNicknameInChatRoom).toHaveBeenCalledTimes(1);
+  });
+
+  it('closes the window.', () => {
+    render(
+      wrapInIntlProvider(
+        <InputDisplayNameOverlay
+          setInputBarActive={mockSetInputBarActive}
+          setOverlay={mockSetOverlay}
+        />,
+      ),
+    );
+    const closeButton = screen.getByTitle(
+      'Click this button to close the overlay.',
+    );
+    act(() => userEvent.click(closeButton));
+    expect(mockSetInputBarActive).not.toHaveBeenCalled();
+    expect(mockSetOverlay).toHaveBeenCalledTimes(1);
+    expect(converse.claimNewNicknameInChatRoom).not.toHaveBeenCalled();
+  });
+
+  it('displays the component and compares it with previous render.', async () => {
+    await renderImageSnapshot(
+      <InputDisplayNameOverlay
+        setInputBarActive={mockSetInputBarActive}
+        setOverlay={mockSetOverlay}
+      />,
+    );
+  });
+});
---
+++ src/frontend/components/Chat/SharedChatComponents/InputDisplayNameOverlay/index.tsx
@@ -0,0 +1,173 @@
+import { Box, Text, Tip } from 'grommet';
+import React, { useState } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+
+import { InputBar } from 'components/Chat/SharedChatComponents/InputBar';
+import { ExitCrossSVG } from 'components/SVGIcons/ExitCrossSVG';
+import { QuestionMarkSVG } from 'components/SVGIcons/QuestionMarkSVG';
+import { ANONYMOUS_ID_PREFIX } from 'utils/chat/chat';
+import { converse } from 'utils/window';
+import { InputDisplayNameIncorrectAlert } from './InputDisplayNameIncorrectAlert/index';
+
+const messages = defineMessages({
+  closeButtonTitle: {
+    defaultMessage: 'Click this button to close the overlay.',
+    description: 'A title describing the close button action',
+    id: 'components.InputDiplayNameOverlay.closeButtonTitle',
+  },
+  inputAnonymousKeywordForbiddenAlertMessage: {
+    defaultMessage: 'Keyword "anonymous" is not allowed.',
+    description:
+      'An alert message explaining why the entered display name is invalid.',
+    id: 'components.InputDisplayNameOverlay.inputAnonymousKeywordForbiddenAlertMessage',
+  },
+  inputDisplayNameInformative: {
+    defaultMessage:
+      "The display name is the pseudonym you will be authenticated with on this live. You won't be able to change it during the live. Other participants will see you with this name. The instructor will however be able to see your genuine identity if you have previously identified yourself with the LTI.",
+    description:
+      'An informative text about the display name which is asked to enter.',
+    id: 'components.InputDisplayNameOverlay.inputDisplayNameInformative',
+  },
+  inputDisplayNameLabel: {
+    defaultMessage: 'Display name',
+    description: 'An label describing the input below.',
+    id: 'components.InputDisplayNameOverlay.inputDisplayNameLabel',
+  },
+  inputDisplayNamePlaceholder: {
+    defaultMessage: 'Enter your desired display name...',
+    description: 'The input bar to fill your display name.',
+    id: 'components.InputBar.inputDisplayNamePlaceholder',
+  },
+  inputTooShortAlertMessage: {
+    defaultMessage: 'Min length is 3 characters.',
+    description:
+      'An alert message explaining why the entered display name is invalid.',
+    id: 'components.InputDisplayNameOverlay.inputTooShortAlertMessage',
+  },
+  inputTooLongAlertMessage: {
+    defaultMessage: 'Max length is 15 characters.',
+    description:
+      'An alert message explaining why the entered display name is invalid.',
+    id: 'components.InputDisplayNameOverlay.inputTooLongAlertMessage',
+  },
+});
+
+interface InputDisplayNameOverlayProps {
+  setInputBarActive: React.Dispatch<React.SetStateAction<boolean>>;
+  setOverlay: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const InputDisplayNameOverlay = ({
+  setInputBarActive,
+  setOverlay,
+}: InputDisplayNameOverlayProps) => {
+  const intl = useIntl();
+  const [alertsState, setAlertsState] = useState<string[]>([]);
+
+  const processDisplayName = (displayName: string) => {
+    const alerts: string[] = [];
+    if (displayName.toLowerCase().includes(ANONYMOUS_ID_PREFIX)) {
+      alerts.push(
+        intl.formatMessage(messages.inputAnonymousKeywordForbiddenAlertMessage),
+      );
+    }
+    if (displayName.length < 3) {
+      alerts.push(intl.formatMessage(messages.inputTooShortAlertMessage));
+    }
+    if (displayName.length > 15) {
+      alerts.push(intl.formatMessage(messages.inputTooLongAlertMessage));
+    }
+    if (alerts.length === 0) {
+      converse.claimNewNicknameInChatRoom(displayName);
+      setInputBarActive(true);
+      setOverlay(false);
+      return true;
+    } else {
+      setAlertsState(alerts);
+      return false;
+    }
+  };
+
+  const handleExitCrossClick = () => {
+    setOverlay(false);
+  };
+
+  return (
+    <Box height="100%">
+      <Box
+        direction="row-reverse"
+        margin={{
+          right: '5px',
+          top: '5px',
+        }}
+      >
+        <Box
+          onClick={handleExitCrossClick}
+          title={intl.formatMessage(messages.closeButtonTitle)}
+        >
+          <ExitCrossSVG
+            containerStyle={{
+              height: '20px',
+              width: '20px',
+            }}
+            iconColor={'#031963'}
+          />
+        </Box>
+      </Box>
+      <Box
+        margin={{
+          horizontal: '20px',
+          vertical: '30px',
+        }}
+        pad="3px"
+      >
+        <Box background="#edf5fa" gap="8px" pad="8px" round="6px">
+          <Box direction="row">
+            <Text
+              margin={{
+                right: '5px',
+              }}
+              size="0.875rem"
+            >
+              {intl.formatMessage(messages.inputDisplayNameLabel)}
+            </Text>
+            <Box>
+              <Tip
+                content={
+                  <Box background="#ffffff" pad="2px" round="6px" width="150px">
+                    <Text size="0.625rem">
+                      {intl.formatMessage(messages.inputDisplayNameInformative)}
+                    </Text>
+                  </Box>
+                }
+                plain
+              >
+                <Box>
+                  <QuestionMarkSVG
+                    containerStyle={{
+                      height: '15px',
+                      width: '15px',
+                    }}
+                    iconColor="#031963"
+                  />
+                </Box>
+              </Tip>
+            </Box>
+          </Box>
+          <InputBar
+            handleUserInput={processDisplayName}
+            isChatInput={false}
+            placeholderText={intl.formatMessage(
+              messages.inputDisplayNamePlaceholder,
+            )}
+          />
+        </Box>
+        <Box>
+          {alertsState.map((msg, index) => (
+            <InputDisplayNameIncorrectAlert alertMsg={msg} key={index} />
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
