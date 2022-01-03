718cc8219a8fb134827af67d8c58be7eaad0be7d
roro-lv <ronan.le-viennesse@polyconseil.fr>
2022-01-03 18:26:28 +0100

    💄(frontend) add JoinChatButton component
    A button offering to join the chat.

---
+++ src/frontend/components/Chat/SharedChatComponents/JoinChatButton/index.tsx
@@ -0,0 +1,39 @@
+import { Box, Button, Text } from 'grommet';
+import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+
+const messages = defineMessages({
+  joinChatButton: {
+    defaultMessage: 'Join the chat',
+    description: "Button's label offering the user to join the chat.",
+    id: 'components.InputBar.joinChatButton',
+  },
+});
+
+interface JoinChatButtonProps {
+  handleClick: () => void;
+}
+
+export const JoinChatButton = ({ handleClick }: JoinChatButtonProps) => {
+  const intl = useIntl();
+
+  return (
+    <Box
+      align="center"
+      border={{
+        color: 'blue-chat',
+        size: 'xsmall',
+      }}
+      height="50px"
+      round="6px"
+    >
+      <Button fill onClick={handleClick} plain>
+        <Box>
+          <Text color="blue-chat" size="1.063rem" textAlign="center" truncate>
+            {intl.formatMessage(messages.joinChatButton)}
+          </Text>
+        </Box>
+      </Button>
+    </Box>
+  );
+};
