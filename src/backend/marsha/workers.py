fb0694b544ec22ed5ca38faabc2c9922277a3810
Manuel Raynaud <manu@raynaud.io>
2022-01-10 14:15:12 +0100

    ✨(back) configure gunicorn to work with Uvicorn
    We want to use Uvicorn as ASGI server. Uvicorn provides a Gunicorn
    worker and we already use Gunicorn server. We create our own worker
    inheriting UvicornWorker to allow its configuration using environment
    variable.

---
+++ src/backend/marsha/workers.py
@@ -0,0 +1,18 @@
+"""Configurable with environment variable UvicornWorker."""
+import os
+
+from uvicorn.workers import UvicornWorker
+
+
+class MarshaUvicornWorker(UvicornWorker):
+    """Configurable with environment variable UvicornWorker."""
+
+    CONFIG_KWARGS = {
+        "http": os.environ.get("UVICORN_HTTP_PROTOCOL", "auto"),
+        "log_level": os.environ.get("UVICORN_LOG_LEVEL", "info"),
+        "loop": os.environ.get("UVICORN_LOOP_PROTOCOL", "auto"),
+        "proxy_headers": os.environ.get("UVICORN_PROXY_HEADERS", True),
+        "ws": os.environ.get("UVICORN_WEBSOCKET_PROTOCOL", "auto"),
+        "ws_ping_interval": os.environ.get("UVICORN_WEBSOCKET_PING_INTERVAL", 20),
+        "ws_ping_timeout": os.environ.get("UVICORN_WEBSOCKET_PING_TIMEOUT", 30),
+    }
