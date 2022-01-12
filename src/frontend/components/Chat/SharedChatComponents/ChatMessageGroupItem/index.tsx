4e1de67091c527ab76be15f4b88badccd48149d4
roro-lv <ronan.le-viennesse@polyconseil.fr>
2022-01-12 15:10:52 +0100

    💄(front) add ChatMessageGroupItem component
    This component displays a group of messages. It displays also metadata about
    messages it contains, such as the name of the sender, its avatar and the
    datetime at which the first message of the group was sent.A group of message
    has necessarily the same sender.

---
+++ src/frontend/components/Chat/SharedChatComponents/ChatMessageGroupItem/index.tsx
@@ -0,0 +1,38 @@
+import { Box } from 'grommet';
+import React, { useMemo } from 'react';
+
+import { ChatMessage } from 'components/Chat/SharedChatComponents/ChatMessage';
+import { ChatMessageMetadatas } from 'components/Chat/SharedChatComponents/ChatMessageMetadatas';
+import { ChatMessageGroupType } from 'data/stores/useChatItemsStore';
+
+interface ChatMessageGroupItemProps {
+  msgGroup: ChatMessageGroupType;
+}
+
+export const ChatMessageGroupItem = ({
+  msgGroup,
+}: ChatMessageGroupItemProps) => {
+  const firstMessage = msgGroup.messages[0];
+  const memo = useMemo(
+    () => (
+      <Box
+        margin={{
+          vertical: '5px',
+          horizontal: '20px',
+        }}
+      >
+        <ChatMessageMetadatas
+          msgDatetime={firstMessage.sentAt}
+          msgSender={msgGroup.sender}
+        />
+        <Box gap="5px">
+          {msgGroup.messages.map((msg, index) => (
+            <ChatMessage key={index} message={msg} />
+          ))}
+        </Box>
+      </Box>
+    ),
+    [msgGroup.sender, msgGroup.messages.length],
+  );
+  return memo;
+};
