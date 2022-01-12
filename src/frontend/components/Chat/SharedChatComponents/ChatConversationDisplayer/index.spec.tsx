2a207fcc98eacba9911b2a3a542893330f111cb9
roro-lv <ronan.le-viennesse@polyconseil.fr>
2022-01-12 15:26:34 +0100

    ✨(front) add grouped messages and join & left label in the chat
    The way the chat works changes. Consecutive messages (same sender in a short
    period)  are displayed together and a label is displayed when a user joins or
    leaves the chat. Also, the spinner displayed while history message are received
    is now waiting for all history message to have been received, before going
    hidden.
    
    CCD

--- src/frontend/components/Chat/SharedChatComponents/ChatConversationDisplayer/index.spec.tsx
+++ src/frontend/components/Chat/SharedChatComponents/ChatConversationDisplayer/index.spec.tsx
@@ -1,13 +1,21 @@
+import { act, render, screen } from '@testing-library/react';
+import { Grommet } from 'grommet';
 import { DateTime } from 'luxon';
 import React from 'react';
 
-import { useMessagesState } from 'data/stores/useMessagesStore';
+import { presenceType, useChatItemState } from 'data/stores/useChatItemsStore';
+import { report } from 'utils/errors/report';
+import { imageSnapshot } from 'utils/tests/imageSnapshot';
 import { wrapInIntlProvider } from 'utils/tests/intl';
+import { theme } from 'utils/theme/theme';
 import { ChatConversationDisplayer } from '.';
-import { act, render, screen } from '@testing-library/react';
 
 const mockScrollTo = jest.fn();
 window.HTMLElement.prototype.scrollTo = mockScrollTo;
+
+jest.mock('utils/errors/report', () => ({
+  report: jest.fn(),
+}));
 
 describe('<ChatConversationDisplayer />', () => {
   beforeEach(() => {
@@ -19,47 +27,141 @@ describe('<ChatConversationDisplayer />', () => {
     jest.useRealTimers();
   });
 
-  it('checks spinner is displayed first, and then chat messages', async () => {
+  it('retrieves correctly message history and new presences.', () => {
     render(wrapInIntlProvider(<ChatConversationDisplayer />));
-
-    await act(async () => {
-      jest.advanceTimersByTime(4000);
-    });
 
     for (let i = 0; i < 5; i++) {
       act(() =>
-        useMessagesState.getState().addMessage({
-          sentAt: DateTime.fromISO(`2020-01-01T1${i}:1${i}:12`),
-          sender: `John Doe n°${i}`,
+        useChatItemState.getState().addMessage({
           content: `This is example message n°${i}`,
+          sender: `John Doe n°${i}`,
+          sentAt: DateTime.fromISO(`2020-01-01T12:1${i}:12`),
+        }),
+      );
+    }
+    for (let i = 0; i < 2; i++) {
+      act(() =>
+        useChatItemState.getState().addPresence({
+          receivedAt: DateTime.fromISO(`2020-01-01T12:2${i}:12`),
+          sender: `John Doe n°${i}`,
+          type: presenceType.ARRIVAL,
         }),
       );
     }
 
     for (let i = 0; i < 5; i++) {
-      screen.getByText(`(1${i}:1${i})`);
+      expect(screen.queryByText(`(12:1${i})`)).not.toBeInTheDocument();
+      expect(screen.queryByText(`John Doe n°${i}`)).not.toBeInTheDocument();
+      expect(
+        screen.queryByText(`This is example message n°${i}`),
+      ).not.toBeInTheDocument();
+    }
+    expect(
+      screen.queryByText('John Doe n°1 has joined'),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText('John Doe n°2 has joined'),
+    ).not.toBeInTheDocument();
+
+    act(() => useChatItemState.getState().setHasReceivedMessageHistory(true));
+
+    for (let i = 0; i < 5; i++) {
+      screen.getByText(`(12:1${i})`);
       screen.getByText(`John Doe n°${i}`);
       screen.getByText(`This is example message n°${i}`);
     }
+    screen.queryByText('John Doe n°1 has joined');
+    screen.queryByText('John Doe n°2 has joined');
   });
 
-  it('checks that scroll bar sticks to bottom when new message comes ', async () => {
+  it('is unable to retrieve message history.', async () => {
     render(wrapInIntlProvider(<ChatConversationDisplayer />));
 
     await act(async () => {
-      jest.advanceTimersByTime(4000);
+      jest.advanceTimersByTime(5000);
     });
-    expect(mockScrollTo).toHaveBeenCalledTimes(1);
+
+    expect(report).toHaveBeenNthCalledWith(
+      1,
+      'Unable to retrieve message history.',
+    );
+    expect(report).toHaveBeenCalledTimes(1);
+  });
+
+  it('scrolls down to bottom, when scroll bar is previously set to bottom.', async () => {
+    render(wrapInIntlProvider(<ChatConversationDisplayer />));
+
+    expect(mockScrollTo).toHaveBeenCalledTimes(0);
+
+    act(() => useChatItemState.getState().setHasReceivedMessageHistory(true));
+
+    expect(mockScrollTo).toHaveBeenCalledTimes(2);
 
     for (let i = 0; i < 5; i++) {
       act(() =>
-        useMessagesState.getState().addMessage({
-          sentAt: DateTime.fromISO(`2020-01-01T12:12:1${i}`),
-          sender: `John Doe n°${i}`,
+        useChatItemState.getState().addMessage({
           content: `This is example message n°${i}`,
+          sender: `John Doe n°${i}`,
+          sentAt: DateTime.fromISO(`2020-01-01T12:1${i}:12`),
         }),
       );
     }
-    expect(mockScrollTo).toHaveBeenCalledTimes(6);
+    expect(mockScrollTo).toHaveBeenCalledTimes(7);
+  });
+
+  it('renders components with messages and presences and compares it with previous render.', async () => {
+    render(
+      wrapInIntlProvider(
+        <Grommet theme={theme}>
+          <ChatConversationDisplayer />
+        </Grommet>,
+      ),
+    );
+
+    for (let i = 0; i < 5; i++) {
+      act(() =>
+        useChatItemState.getState().addMessage({
+          content: `This is example message n°${i}`,
+          sender: `John Doe n°${i}`,
+          sentAt: DateTime.fromISO(`2020-01-01T12:1${i}:12`),
+        }),
+      );
+    }
+
+    act(() => useChatItemState.getState().setHasReceivedMessageHistory(true));
+
+    act(() =>
+      useChatItemState.getState().addPresence({
+        receivedAt: DateTime.fromISO('2020-01-01T12:12:00'),
+        sender: 'John_Doe',
+        type: presenceType.ARRIVAL,
+      }),
+    );
+
+    act(() =>
+      useChatItemState.getState().addMessage({
+        content: 'Hello everyone !',
+        sender: 'John_Doe',
+        sentAt: DateTime.fromISO('2020-01-01T12:12:10'),
+      }),
+    );
+
+    act(() =>
+      useChatItemState.getState().addMessage({
+        content: 'Hello John !',
+        sender: 'Jane_Doe',
+        sentAt: DateTime.fromISO('2020-01-01T12:12:25'),
+      }),
+    );
+
+    act(() =>
+      useChatItemState.getState().addPresence({
+        receivedAt: DateTime.fromISO('2020-01-01T12:12:27'),
+        sender: 'John_Doe',
+        type: presenceType.DEPARTURE,
+      }),
+    );
+
+    await imageSnapshot(800, 1000);
   });
 });
