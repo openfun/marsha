ec03988d8541bbda076f34d2bcd853ba3f4c9805
Manuel Raynaud <manu@raynaud.io>
2022-01-20 11:01:35 +0100

    ✨(front) create an helper to connect to the websocket
    We use robust websocket library to connect to the websocket. Once
    connected, on each received message we update the zustand store accoring
    to the updated resource.

---
+++ src/frontend/data/websocket.ts
@@ -0,0 +1,29 @@
+import RobustWebSocket from 'altamoon-robust-websocket';
+
+import { addResource } from 'data/stores/generics';
+import { WS_ENPOINT } from 'settings';
+import { modelName } from 'types/models';
+import { UploadableObject, Video } from 'types/tracks';
+
+import { appData } from './appData';
+
+type WSMessageType = {
+  resource: UploadableObject;
+  type: modelName;
+};
+
+let videoWebsocket: RobustWebSocket;
+
+export const initVideoWebsocket = (video: Video) => {
+  if (videoWebsocket) return;
+  const location = window.location;
+  const wsProto = location.protocol === 'https' ? 'wss' : 'ws';
+  videoWebsocket = new RobustWebSocket(
+    `${wsProto}://${location.host}${WS_ENPOINT}/video/${video.id}/?jwt=${appData.jwt}`,
+  );
+
+  videoWebsocket.addEventListener('message', async (message) => {
+    const data: WSMessageType = JSON.parse(message.data);
+    await addResource(data.type, data.resource);
+  });
+};
--- src/frontend/settings.ts
+++ src/frontend/settings.ts
@@ -1,4 +1,5 @@
 export const API_ENDPOINT = '/api';
+export const WS_ENPOINT = '/ws';
 export const XAPI_ENDPOINT = '/xapi';
 
 export const API_LIST_DEFAULT_PARAMS = {
