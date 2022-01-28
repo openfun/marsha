6bbdbcd84f80117ab2bd4a6ed7be939c03c74bee
Manuel Raynaud <manu@raynaud.io>
2022-01-28 20:49:10 +0100

    ♻️(front) use websocket instead of polling timed text to update state
    When a timed text track upload_state is updated, a message is dispatched
    in the websocket and the zustand store is updated. The UI will be
    updated accordingly, there is no need anymore to poll the timed text
    track endpoint to detect when the state is updated. To simulated this
    update, the jest websocket mock is used to send an updated timed text
    track.

--- src/frontend/components/DashboardTimedTextPane/index.spec.tsx
+++ src/frontend/components/DashboardTimedTextPane/index.spec.tsx
@@ -1,30 +1,59 @@
 import {
   getByText as getByTextInContainer,
+  findByText,
   render,
   screen,
 } from '@testing-library/react';
+import WS from 'jest-websocket-mock';
 import fetchMock from 'fetch-mock';
 import React from 'react';
 
+import { FULL_SCREEN_ERROR_ROUTE } from 'components/ErrorComponents/route';
+import { timedTextMode, uploadState } from 'types/tracks';
+import { report } from 'utils/errors/report';
+import { wrapInIntlProvider } from 'utils/tests/intl';
+import { wrapInRouter } from 'utils/tests/router';
 import { DashboardTimedTextPane } from '.';
-import { timedTextMode, uploadState } from '../../types/tracks';
-import { report } from '../../utils/errors/report';
-import { wrapInIntlProvider } from '../../utils/tests/intl';
-import { wrapInRouter } from '../../utils/tests/router';
-import { FULL_SCREEN_ERROR_ROUTE } from '../ErrorComponents/route';
+import { timedTextMockFactory, videoMockFactory } from 'utils/tests/factories';
+import { initVideoWebsocket } from 'data/websocket';
+import { modelName } from 'types/models';
 
-jest.mock('../../data/appData', () => ({
+jest.mock('data/appData', () => ({
   appData: {
     jwt: 'foo',
   },
 }));
 
-jest.mock('../../utils/errors/report', () => ({
+jest.mock('utils/errors/report', () => ({
   report: jest.fn(),
 }));
 
+const video = videoMockFactory();
+let server: WS;
+
 describe('<DashboardTimedTextPane />', () => {
-  beforeEach(() =>
+  beforeAll(async () => {
+    server = new WS(`ws://localhost:1234/ws/video/${video.id}/`);
+
+    global.window = Object.create(window);
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: 'http://localhost:1234/',
+        host: 'localhost:1234',
+        protocol: 'http',
+      },
+    });
+
+    initVideoWebsocket(video);
+    await server.connected;
+  });
+
+  afterAll(() => {
+    server.close();
+    WS.clean();
+  });
+
+  beforeEach(() => {
     fetchMock.mock(
       '/api/timedtexttracks/',
       {
@@ -40,50 +69,69 @@ describe('<DashboardTimedTextPane />', () => {
         },
       },
       { method: 'OPTIONS' },
-    ),
-  );
+    );
+  });
 
-  afterEach(jest.resetAllMocks);
-  afterEach(() => fetchMock.restore());
+  afterEach(() => {
+    jest.resetAllMocks();
+    fetchMock.restore();
+  });
 
   it('gets the list of timedtexttracks and displays them by mode', async () => {
+    const track1 = timedTextMockFactory({
+      active_stamp: 2094219242,
+      id: '142',
+      is_ready_to_show: true,
+      language: 'en',
+      mode: timedTextMode.SUBTITLE,
+      upload_state: uploadState.READY,
+      source_url: 'https://example.com/ttt/142',
+      url: 'https://example.com/ttt/142.vtt',
+      video: '43',
+    });
+    const track2 = timedTextMockFactory({
+      active_stamp: 2094219242,
+      id: '144',
+      is_ready_to_show: true,
+      language: 'fr',
+      mode: timedTextMode.CLOSED_CAPTIONING,
+      upload_state: uploadState.PROCESSING,
+      source_url: 'https://example.com/ttt/144',
+      url: 'https://example.com/ttt/144.vtt',
+      video: '43',
+    });
     fetchMock.get('/api/timedtexttracks/?limit=20&offset=0', {
       count: 2,
       next: null,
       previous: null,
-      results: [
-        {
-          active_stamp: 2094219242,
-          id: '142',
-          is_ready_to_show: true,
-          language: 'en',
-          mode: timedTextMode.SUBTITLE,
-          upload_state: uploadState.READY,
-          source_url: 'https://example.com/ttt/142',
-          url: 'https://example.com/ttt/142.vtt',
-          video: '43',
-        },
-        {
-          active_stamp: 2094219242,
-          id: '144',
-          is_ready_to_show: true,
-          language: 'fr',
-          mode: timedTextMode.CLOSED_CAPTIONING,
-          upload_state: uploadState.READY,
-          source_url: 'https://example.com/ttt/144',
-          url: 'https://example.com/ttt/144.vtt',
-          video: '43',
-        },
-      ],
+      results: [track1, track2],
     });
+    await server.connected;
 
     render(wrapInIntlProvider(wrapInRouter(<DashboardTimedTextPane />)));
 
     const closedCaptions = await screen.findByText('Closed captions');
     getByTextInContainer(closedCaptions.parentElement!, 'French');
+    getByTextInContainer(closedCaptions.parentElement!, 'Processing');
     const subtitles = screen.getByText('Subtitles');
     getByTextInContainer(subtitles.parentElement!, 'English');
     screen.getByText('Transcripts');
+
+    // Send updated timed text track with upload_state ready.
+    // Display should be updated to reflect this change.
+    server.send(
+      JSON.stringify({
+        type: modelName.TIMEDTEXTTRACKS,
+        resource: {
+          ...track2,
+          upload_state: uploadState.READY,
+        },
+      }),
+    );
+
+    await findByText(closedCaptions.parentElement!, (content) =>
+      content.startsWith('Ready'),
+    );
   });
 
   it('redirects to the error view when the timedtexttrack list request fails', async () => {
