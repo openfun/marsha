bb12ffbf3d2a7e078219a6c0373dc82946a404b7
Manuel Raynaud <manu@raynaud.io>
2022-02-01 18:15:26 +0100

    🐛(front) better detect https protocol to connect to the websocket
    To know if we have to connect to the websocket using the ws or wss
    protocol, we check the current protocol used to connect to the page,
    this protocol is present in window.location.protocol. Depending the
    browser implementation, the procotol can include the final `:`

--- src/frontend/data/websocket.spec.ts
+++ src/frontend/data/websocket.spec.ts
@@ -4,7 +4,6 @@ import WS from 'jest-websocket-mock';
 import { modelName } from 'types/models';
 import { videoMockFactory } from 'utils/tests/factories';
 import { useVideo } from './stores/useVideo';
-import { initVideoWebsocket } from './websocket';
 
 jest.mock('data/appData', () => ({
   appData: { jwt: 'cool_token_m8' },
@@ -12,6 +11,10 @@ jest.mock('data/appData', () => ({
 
 describe('initVideoWebsocket', () => {
   it('connects to the websocket and updates video zustand store when message is received', async () => {
+    let initVideoWebsocket: any;
+    jest.isolateModules(() => {
+      initVideoWebsocket = require('./websocket').initVideoWebsocket;
+    });
     const video = videoMockFactory();
     useVideo.getState().addResource(video);
     const server = new WS(`ws://localhost:1234/ws/video/${video.id}/`);
@@ -23,14 +26,57 @@ describe('initVideoWebsocket', () => {
         host: 'localhost:1234',
         protocol: 'http',
       },
+      writable: true,
     });
 
-    initVideoWebsocket(video);
+    initVideoWebsocket!(video);
     await server.connected;
 
     const updatedVideo = {
       ...video,
       title: 'updated title',
+    };
+
+    server.send(
+      JSON.stringify({
+        type: modelName.VIDEOS,
+        resource: updatedVideo,
+      }),
+    );
+
+    await waitFor(() => {
+      expect(useVideo.getState().getVideo(video)).toEqual(updatedVideo);
+    });
+
+    server.close();
+    WS.clean();
+  });
+
+  it('connects to the websocket using https protocol', async () => {
+    let initVideoWebsocket: any;
+    jest.isolateModules(() => {
+      initVideoWebsocket = require('./websocket').initVideoWebsocket;
+    });
+    const video = videoMockFactory();
+    useVideo.getState().addResource(video);
+    const server = new WS(`wss://localhost:4321/ws/video/${video.id}/`);
+
+    global.window = Object.create(window);
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: 'https://localhost:4321/',
+        host: 'localhost:4321',
+        protocol: 'https:',
+      },
+      writable: true,
+    });
+
+    initVideoWebsocket!(video);
+    await server.connected;
+
+    const updatedVideo = {
+      ...video,
+      title: 'updated title with https',
     };
 
     server.send(
