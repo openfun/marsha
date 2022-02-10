1286d5d7532dd0c4219d93f287172473c279299a
vassili <vassili.rezvoy@polyconseil.fr>
2022-02-10 13:38:40 +0100

    ✨(frontend) create form and form field with error handling
    Create wrapper to handle errors from backend and display these errors in the
    form.

---
+++ src/frontend/components/Form/index.tsx
@@ -0,0 +1,88 @@
+import {
+  Form as GrommetForm,
+  FormExtendedEvent as GrommetFormExtendedEvent,
+  FormField as GrommetFormField,
+  FormFieldProps as GrommetFormFieldProps,
+  FormProps as GrommetFormProps,
+} from 'grommet';
+import React, {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
+
+import { Maybe } from 'utils/types';
+
+type ErrorStateType<T> = Partial<Record<keyof T, string>>;
+
+const FormErrorContext = createContext<
+  { [key: string]: string | undefined } | undefined
+>(undefined);
+const useFormErrorContext = () => {
+  const context = useContext(FormErrorContext);
+  if (context === undefined) {
+    throw new Error('You are not wrap in FormContext.');
+  }
+
+  return context;
+};
+
+type CleanFormProps<T> = Omit<GrommetFormProps<T>, 'children' | 'onSubmit'>;
+interface FormProps<T = {}> extends CleanFormProps<T> {
+  initialErrors?: ErrorStateType<T>;
+  onSubmit: (
+    event: GrommetFormExtendedEvent<T, Element>,
+  ) => Promise<void> | void;
+  onSubmitError: (values: T, error: any) => Maybe<ErrorStateType<T>>;
+}
+
+export const Form = <T extends {}>({
+  children,
+  initialErrors,
+  onSubmit,
+  onSubmitError,
+  ...props
+}: PropsWithChildren<FormProps<T>>) => {
+  const [errors, setErrors] = useState<ErrorStateType<T>>(initialErrors ?? {});
+  const isSubmitOngoing = useRef(false);
+
+  return (
+    <GrommetForm
+      {...props}
+      onSubmit={async (submitValues) => {
+        if (isSubmitOngoing.current) {
+          //  a submit is already in progress
+          return;
+        }
+
+        isSubmitOngoing.current = true;
+        try {
+          setErrors({});
+          await onSubmit(submitValues);
+        } catch (e) {
+          setErrors(onSubmitError(submitValues.value, e) ?? {});
+        }
+        isSubmitOngoing.current = false;
+      }}
+    >
+      <FormErrorContext.Provider value={errors}>
+        {children}
+      </FormErrorContext.Provider>
+    </GrommetForm>
+  );
+};
+
+export const FormField = (props: PropsWithChildren<GrommetFormFieldProps>) => {
+  const errors = useFormErrorContext();
+
+  return (
+    <GrommetFormField
+      error={props.name ? errors[props.name] : undefined}
+      {...props}
+    >
+      {props.children}
+    </GrommetFormField>
+  );
+};
