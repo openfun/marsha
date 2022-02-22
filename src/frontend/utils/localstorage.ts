70196793f41ae296dadbaf467ebc15ad75f4e453
Manuel Raynaud <manu@raynaud.io>
2022-02-22 16:13:27 +0100

    ✨(front) add a helper to store anonymous_id in localStorage
    localStorage is widely used by navigators but sometimes the access to it
    can be blocked. We added a little helper managing case where the
    localStorage is not available.

---
+++ src/frontend/utils/localstorage.ts
@@ -0,0 +1,37 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { report } from './errors/report';
+
+export const ANONYMOUS_ID_KEY = 'marsha_anonymous_id';
+
+const localStorageEnabled = (): boolean => {
+  try {
+    localStorage.setItem('enabled', '1');
+    localStorage.removeItem('enabled');
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const isLocaleStorageEnabled = localStorageEnabled();
+
+export const getAnonymousId = (): string => {
+  if (!isLocaleStorageEnabled) {
+    return uuidv4();
+  }
+
+  let anonymousId = localStorage.getItem(ANONYMOUS_ID_KEY);
+
+  if (!anonymousId) {
+    anonymousId = uuidv4();
+    try {
+      localStorage.setItem(ANONYMOUS_ID_KEY, anonymousId);
+    } catch (e) {
+      report(e);
+      return anonymousId;
+    }
+  }
+
+  return anonymousId;
+};
