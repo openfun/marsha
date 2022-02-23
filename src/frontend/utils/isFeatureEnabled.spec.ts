2a74eeca166dc6705cc79bac1522856eca13dfc4
vassili <vassili.rezvoy@polyconseil.fr>
2022-02-23 19:04:31 +0100

    🚩(frontend) remove JITSI flag
    A flag JITSI was defined but never used in the frontend.

--- src/frontend/utils/isFeatureEnabled.spec.ts
+++ src/frontend/utils/isFeatureEnabled.spec.ts
@@ -1,8 +1,9 @@
+import { flags } from 'types/AppData';
+
 import { isFeatureEnabled } from './isFeatureEnabled';
-import { flags } from '../types/AppData';
 
 let mockAppData = {};
-jest.mock('../data/appData', () => ({
+jest.mock('data/appData', () => ({
   get appData() {
     return mockAppData;
   },
@@ -12,30 +13,30 @@ describe('isFeatureEnabled', () => {
   it('returns true is flag is enabled', () => {
     mockAppData = {
       flags: {
-        [flags.JITSI]: true,
+        [flags.BBB]: true,
       },
     };
-    expect(isFeatureEnabled(flags.JITSI)).toBe(true);
+    expect(isFeatureEnabled(flags.BBB)).toBe(true);
   });
 
   it('returns false if flag is disabled', () => {
     mockAppData = {
       flags: {
-        [flags.JITSI]: false,
+        [flags.BBB]: false,
       },
     };
-    expect(isFeatureEnabled(flags.JITSI)).toBe(false);
+    expect(isFeatureEnabled(flags.BBB)).toBe(false);
   });
 
   it('returns false when the wanted flag is undefined', () => {
     mockAppData = {
       flags: {},
     };
-    expect(isFeatureEnabled(flags.JITSI)).toBe(false);
+    expect(isFeatureEnabled(flags.BBB)).toBe(false);
   });
 
   it('returns false when flag is undefined', () => {
     mockAppData = {};
-    expect(isFeatureEnabled(flags.JITSI)).toBe(false);
+    expect(isFeatureEnabled(flags.BBB)).toBe(false);
   });
 });
