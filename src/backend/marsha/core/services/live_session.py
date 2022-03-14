74de839c11c4129edd8bc8decfd9b925711828aa
Manuel Raynaud <manu@raynaud.io>
2022-03-14 10:29:26 +0100

    ♻️(back) extract generic function from live_session API to services
    Some functions used in the live_session API can be extracted in the
    services in order to use them from other contexts. We plan to use them
    with the websocket to link a channel name to a live session.

---
+++ src/backend/marsha/core/services/live_session.py
@@ -0,0 +1,61 @@
+"""Live session services."""
+from marsha.core.models import ConsumerSite, LiveSession, Video
+from marsha.core.models.account import NONE
+
+
+class NotLtiTokenException(Exception):
+    """Raised when the submitted token is not a LTI one."""
+
+
+class NotPublicTokenException(Exception):
+    """Raised when the submited token is not a public one."""
+
+
+def is_lti_token(token):
+    """Read the token and confirms if the user is identified by LTI"""
+
+    return (
+        token.payload
+        and token.payload.get("context_id")
+        and token.payload.get("consumer_site")
+        and token.payload.get("user")
+        and token.payload["user"].get("id")
+    )
+
+
+def is_public_token(token):
+    """Read the token and check it was made to fetch a public resource."""
+    return (
+        token.payload
+        and token.payload.get("roles") == [NONE]
+        and token.payload.get("context_id") is None
+        and token.payload.get("consumer_site") is None
+    )
+
+
+def get_livesession_from_lti(token):
+    """Get or create livesession for a LTI connection."""
+    if not is_lti_token(token):
+        raise NotLtiTokenException()
+
+    video = Video.objects.get(pk=token.payload["resource_id"])
+    token_user = token.payload.get("user")
+    consumer_site = ConsumerSite.objects.get(pk=token.payload["consumer_site"])
+
+    return LiveSession.objects.get_or_create(
+        consumer_site=consumer_site,
+        lti_id=token.payload.get("context_id"),
+        lti_user_id=token_user.get("id"),
+        video=video,
+        defaults={
+            "email": token_user.get("email"),
+            "username": token_user.get("username"),
+        },
+    )
+
+
+def get_livesession_from_anonymous_id(video_id, anonymous_id):
+    """Get or create a livesession for an anonymous id"""
+    video = Video.objects.get(pk=video_id)
+
+    return LiveSession.objects.get_or_create(video=video, anonymous_id=anonymous_id)
