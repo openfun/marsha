2a143a1bbb78739c3c0effd30e9464b12fda51d8
Manuel Raynaud <manu@raynaud.io>
2022-03-18 11:49:33 +0100

    ✨(front) manage anonymous_id with websocket
    To connect to a websocket, we need to add the anonymous_id in the query
    string when the current token in a public one.

--- src/frontend/components/DashboardTimedTextPane/index.spec.tsx
+++ src/frontend/components/DashboardTimedTextPane/index.spec.tsx
@@ -22,6 +22,24 @@ jest.mock('data/appData', () => ({
   appData: {
     jwt: 'foo',
   },
+  getDecodedJwt: () => ({
+    context_id: 'course-v1:ufr+mathematics+0001',
+    consumer_site: '112cf553-b8c3-4b98-9d47-d0793284b9b3',
+    locale: 'en_US',
+    maintenance: false,
+    permissions: {
+      can_access_dashboard: false,
+      can_update: false,
+    },
+    resource_id: '26debfee-8c3b-4c23-b08f-67f223de9832',
+    roles: ['student'],
+    session_id: '6bbb8d1d-442d-4575-a0ad-d1e34f37cae3',
+    user: {
+      email: 'sarah@test-mooc.fr',
+      id: 'aaace992-49e3-4e01-b809-7a84b1b55b72',
+      username: null,
+    },
+  }),
 }));
 
 jest.mock('utils/errors/report', () => ({
