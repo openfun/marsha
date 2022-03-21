163aa3460b71b35b916dfd3d423dc4ec9965352a
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-03-21 11:06:02 +0100

    ✨(frontend) add meeting scheduling interfaces
    On a BBB meeting creation view, we added a scheduling form.
    When a meeting is scheduled, students will see a countdown before the
    start.

--- src/frontend/apps/bbb/DashboardMeetingForm/index.spec.tsx
+++ src/frontend/apps/bbb/DashboardMeetingForm/index.spec.tsx
@@ -1,6 +1,8 @@
 import { act, fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import fetchMock from 'fetch-mock';
 import MatchMediaMock from 'jest-matchmedia-mock';
+import { DateTime, Duration, Settings } from 'luxon';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -22,14 +24,25 @@ jest.mock('data/appData', () => ({
   },
 }));
 
+Settings.defaultLocale = 'en';
+Settings.defaultZone = 'utc';
+const currentDate = DateTime.local(2022, 1, 27, 14, 22, 15);
+
 describe('<DashboardMeetingForm />', () => {
   beforeAll(() => {
     matchMedia = new MatchMediaMock();
+  });
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(currentDate.toJSDate());
   });
   afterEach(() => {
     matchMedia.clear();
     jest.resetAllMocks();
     fetchMock.restore();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
   });
 
   it('creates a meeting with current values', async () => {
@@ -77,7 +90,6 @@ describe('<DashboardMeetingForm />', () => {
     const { getByText } = render(
       wrapInIntlProvider(
         <QueryClientProvider client={queryClient}>
-          <Toaster />
           <DashboardMeetingForm meeting={meeting} />
         </QueryClientProvider>,
       ),
@@ -88,24 +100,71 @@ describe('<DashboardMeetingForm />', () => {
     const inputTitle = screen.getByRole('textbox', {
       name: /title/i,
     });
-    fireEvent.change(inputTitle, { target: { value: 'updated title' } });
+    userEvent.clear(inputTitle);
+    userEvent.type(inputTitle, 'updated title');
+    fireEvent.blur(inputTitle);
+
+    // simulate updated meeting
+    rerender(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm
+            meeting={{
+              ...meeting,
+              title: 'updated title',
+            }}
+          />
+        </QueryClientProvider>,
+      ),
+    );
 
     const inputWelcomeText = screen.getByRole('textbox', {
       name: /welcome text/i,
     });
-    fireEvent.change(inputWelcomeText, {
-      target: { value: 'updated welcome text' },
+    userEvent.type(inputWelcomeText, 'updated welcome text', {
+      initialSelectionStart: 0,
+      initialSelectionEnd: meeting.welcome_text.length,
     });
+
+    // wait for debounce to update the meeting
+    fetchMock.patch('/api/meetings/1/', {
+      ...meeting,
+      title: 'updated title',
+      welcomeText: 'updated welcome text',
+    });
+    jest.runAllTimers();
 
     fireEvent.click(screen.getByText('Create meeting in BBB'));
     await act(async () =>
       deferredPatch.resolve({ message: 'Meeting created.' }),
     );
 
-    expect(fetchMock.calls()).toHaveLength(1);
+    expect(fetchMock.calls()).toHaveLength(3);
 
-    expect(fetchMock.calls()[0]![0]).toEqual('/api/meetings/1/create/');
+    expect(fetchMock.calls()[0]![0]).toEqual('/api/meetings/1/');
     expect(fetchMock.calls()[0]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: 'updated title',
+      }),
+    });
+
+    expect(fetchMock.calls()[1]![0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[1]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        welcome_text: 'updated welcome text',
+      }),
+    });
+
+    expect(fetchMock.lastCall()![0]).toEqual('/api/meetings/1/create/');
+    expect(fetchMock.lastCall()![1]).toEqual({
       headers: {
         'Content-Type': 'application/json',
       },
@@ -113,6 +172,256 @@ describe('<DashboardMeetingForm />', () => {
       body: JSON.stringify({
         ...meeting,
         title: 'updated title',
+        welcome_text: 'updated welcome text',
+      }),
+    });
+  });
+
+  it('schedules a meeting', async () => {
+    const startingAt = currentDate.plus({ days: 2, hours: 2 }).startOf('hour');
+    const estimatedDuration = Duration.fromObject({ minutes: 30 });
+    const meeting = meetingMockFactory({ id: '1', started: false });
+    const queryClient = new QueryClient();
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/meetings/1/', deferredPatch.promise);
+
+    const { getByText, rerender } = render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm meeting={meeting} />
+        </QueryClientProvider>,
+      ),
+    );
+    getByText('Title');
+    getByText('Welcome text');
+
+    const inputTitle = screen.getByRole('textbox', {
+      name: /title/i,
+    });
+    userEvent.clear(inputTitle);
+    userEvent.type(inputTitle, 'updated title');
+    fireEvent.blur(inputTitle);
+
+    // simulate meeting update
+    rerender(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm
+            meeting={{ ...meeting, title: 'updated title' }}
+          />
+        </QueryClientProvider>,
+      ),
+    );
+
+    const inputWelcomeText = screen.getByRole('textbox', {
+      name: /welcome text/i,
+    });
+    userEvent.type(inputWelcomeText, 'updated welcome text', {
+      initialSelectionStart: 0,
+      initialSelectionEnd: meeting.welcome_text.length,
+    });
+    fireEvent.blur(inputWelcomeText);
+
+    jest.runAllTimers();
+    await act(async () =>
+      deferredPatch.resolve({ message: 'Meeting scheduled.' }),
+    );
+
+    expect(fetchMock.calls()).toHaveLength(2);
+
+    expect(fetchMock.calls()[0]![0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[0]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: 'updated title',
+      }),
+    });
+
+    expect(fetchMock.calls()[1]![0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[1]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        welcome_text: 'updated welcome text',
+      }),
+    });
+
+    // simulate meeting update
+    rerender(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm
+            meeting={{
+              ...meeting,
+              title: 'updated title',
+              welcome_text: 'updated welcome text',
+            }}
+          />
+        </QueryClientProvider>,
+      ),
+    );
+
+    const inputStartingAtDate = screen.getByLabelText(/starting date/i);
+    fireEvent.change(inputStartingAtDate, {
+      target: { value: startingAt.toFormat('dd/MM/yyyy') },
+    });
+
+    // using userEvent.type with following input doesn't work
+    const inputStartingAtTime = screen.getByLabelText(/starting time/i);
+    fireEvent.change(inputStartingAtTime, {
+      target: { value: startingAt.toLocaleString(DateTime.TIME_24_SIMPLE) },
+    });
+    fireEvent.blur(inputStartingAtTime);
+
+    // simulate meeting update
+    rerender(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm
+            meeting={{
+              ...meeting,
+              title: 'updated title',
+              starting_at: startingAt.toISO(),
+            }}
+          />
+        </QueryClientProvider>,
+      ),
+    );
+
+    const inputEstimatedDuration = screen.getByLabelText(/estimated duration/i);
+    userEvent.type(inputEstimatedDuration, estimatedDuration.toFormat('h:mm'));
+    fireEvent.blur(inputEstimatedDuration);
+
+    // simulate meeting update
+    rerender(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm
+            meeting={{
+              ...meeting,
+              title: 'updated title',
+              starting_at: startingAt.toISO(),
+              estimated_duration: estimatedDuration.toFormat('hh:mm:ss'),
+            }}
+          />
+        </QueryClientProvider>,
+      ),
+    );
+
+    await act(async () =>
+      deferredPatch.resolve({ message: 'Meeting scheduled.' }),
+    );
+
+    expect(fetchMock.calls()).toHaveLength(4);
+
+    expect(fetchMock.calls()[2]![0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[2]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        starting_at: startingAt.toISO(),
+      }),
+    });
+
+    expect(fetchMock.calls()[3]![0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[3]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        estimated_duration: estimatedDuration.toFormat('hh:mm:ss'),
+      }),
+    });
+  });
+
+  it('updates a meeting scheduled', async () => {
+    const startingAt = currentDate.plus({ days: 2, hours: 2 }).startOf('hour');
+    const estimatedDuration = Duration.fromObject({ minutes: 30 });
+    const meeting = meetingMockFactory({
+      id: '1',
+      started: false,
+      starting_at: startingAt.toISO(),
+      estimated_duration: estimatedDuration.toFormat('hh:mm:ss'),
+    });
+    const queryClient = new QueryClient();
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/meetings/1/', deferredPatch.promise);
+
+    const { getByText, rerender } = render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm meeting={meeting} />
+        </QueryClientProvider>,
+      ),
+    );
+    getByText('Title');
+    getByText('Welcome text');
+
+    const inputTitle = screen.getByRole('textbox', {
+      name: /title/i,
+    });
+    userEvent.clear(inputTitle);
+    userEvent.type(inputTitle, 'updated title');
+
+    jest.runAllTimers();
+    await act(async () =>
+      deferredPatch.resolve({ message: 'Meeting scheduled.' }),
+    );
+
+    // simulate meeting update
+    rerender(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm
+            meeting={{ ...meeting, title: 'updated title' }}
+          />
+        </QueryClientProvider>,
+      ),
+    );
+
+    const inputWelcomeText = screen.getByRole('textbox', {
+      name: /welcome text/i,
+    });
+    userEvent.type(inputWelcomeText, 'updated welcome text', {
+      initialSelectionStart: 0,
+      initialSelectionEnd: meeting.welcome_text?.length,
+    });
+
+    jest.runAllTimers();
+    await act(async () =>
+      deferredPatch.resolve({ message: 'Meeting scheduled.' }),
+    );
+
+    expect(fetchMock.calls()).toHaveLength(2);
+
+    expect(fetchMock.calls()[0]![0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[0]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: 'updated title',
+      }),
+    });
+
+    expect(fetchMock.calls()[1]![0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[1]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
         welcome_text: 'updated welcome text',
       }),
     });
--- src/frontend/apps/bbb/DashboardMeetingStudent/index.spec.tsx
+++ src/frontend/apps/bbb/DashboardMeetingStudent/index.spec.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { DateTime, Duration, Settings } from 'luxon';
 import React from 'react';
 
 import { wrapInIntlProvider } from 'utils/tests/intl';
@@ -6,8 +7,33 @@ import { wrapInIntlProvider } from 'utils/tests/intl';
 import { meetingMockFactory } from 'apps/bbb/utils/tests/factories';
 import DashboardMeetingStudent from '.';
 
+jest.mock('data/appData', () => ({
+  appData: {
+    static: {
+      img: {
+        bbbBackground: 'some_url',
+      },
+    },
+  },
+}));
+
+Settings.defaultLocale = 'en';
+Settings.defaultZone = 'utc';
+const currentDate = DateTime.local(2022, 1, 27, 14, 22, 15);
+
 describe('<DashboardMeetingStudent />', () => {
-  it('Displays message and triggers callbacks depending on meeting state', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(currentDate.toJSDate());
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+  it('Displays message and triggers callbacks depending on meeting state', async () => {
     const meeting = meetingMockFactory({
       id: '1',
       started: false,
@@ -29,7 +55,59 @@ describe('<DashboardMeetingStudent />', () => {
 
     getByText('Meeting not started yet.');
     expect(joinMeetingAction).toHaveBeenCalledTimes(0);
-    expect(meetingEnded).toHaveBeenCalledTimes(1);
+    expect(meetingEnded).toHaveBeenCalledTimes(0);
+
+    // meeting scheduled
+    const startingAt = currentDate.plus({ days: 2, hours: 2 }).startOf('hour');
+    const exstimatedDuration = Duration.fromObject({ hours: 3 });
+    const endingDateTime = startingAt.plus(exstimatedDuration);
+    rerender(
+      wrapInIntlProvider(
+        <DashboardMeetingStudent
+          meeting={{
+            ...meeting,
+            started: false,
+            ended: false,
+            starting_at: startingAt.toISO(),
+            estimated_duration: exstimatedDuration.toFormat('hh:mm:ss'),
+          }}
+          joinedAs={false}
+          joinMeetingAction={joinMeetingAction}
+          meetingEnded={meetingEnded}
+        />,
+      ),
+    );
+    getByText(meeting.title!);
+    getByText(meeting.description);
+    const displayedStartingDate = startingAt.toLocaleString(DateTime.DATE_HUGE);
+    const displayedStartingTime = startingAt.toLocaleString(
+      DateTime.TIME_24_SIMPLE,
+    );
+    const displayedEndingTime = endingDateTime.toLocaleString(
+      DateTime.TIME_24_SIMPLE,
+    );
+    getByText(
+      `${displayedStartingDate} - ${displayedStartingTime} > ${displayedEndingTime}`,
+    );
+    expect(meetingEnded).toHaveBeenCalledTimes(0);
+
+    // meeting scheduled without estimated duration
+    rerender(
+      wrapInIntlProvider(
+        <DashboardMeetingStudent
+          meeting={{
+            ...meeting,
+            started: false,
+            ended: false,
+            starting_at: startingAt.toISO(),
+          }}
+          joinedAs={false}
+          joinMeetingAction={joinMeetingAction}
+          meetingEnded={meetingEnded}
+        />,
+      ),
+    );
+    getByText(`${displayedStartingDate} - ${displayedStartingTime}`);
 
     // meeting starts
     rerender(
@@ -44,7 +122,7 @@ describe('<DashboardMeetingStudent />', () => {
     );
     fireEvent.click(screen.getByText('Join meeting'));
     expect(joinMeetingAction).toHaveBeenCalledTimes(1);
-    expect(meetingEnded).toHaveBeenCalledTimes(1);
+    expect(meetingEnded).toHaveBeenCalledTimes(0);
 
     // meeting joined
     rerender(
@@ -59,7 +137,7 @@ describe('<DashboardMeetingStudent />', () => {
     );
     getByText('You have joined the meeting as John Doe.');
     expect(joinMeetingAction).toHaveBeenCalledTimes(1);
-    expect(meetingEnded).toHaveBeenCalledTimes(1);
+    expect(meetingEnded).toHaveBeenCalledTimes(0);
 
     // meeting ends
     rerender(
@@ -74,6 +152,6 @@ describe('<DashboardMeetingStudent />', () => {
     );
     getByText('Meeting ended.');
     expect(joinMeetingAction).toHaveBeenCalledTimes(1);
-    expect(meetingEnded).toHaveBeenCalledTimes(2);
+    expect(meetingEnded).toHaveBeenCalledTimes(1);
   });
 });
