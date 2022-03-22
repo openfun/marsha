0b48e5880aa7c6991ec524b28616325268784371
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-03-22 12:48:36 +0100

    ♻️(frontend) extract scheduling fields to generic component
    AS we have two similar forms for sheduling both live video and BBB
    meeting, related fields have been extracted to a generic component.

--- src/frontend/apps/bbb/DashboardMeetingForm/index.tsx
+++ src/frontend/apps/bbb/DashboardMeetingForm/index.tsx
@@ -3,16 +3,14 @@ import {
   Button,
   Form,
   FormField,
-  MaskedInput,
   TextArea,
   TextInput,
 } from 'grommet';
 import React, { useRef, useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import { toast } from 'react-hot-toast';
 import { defineMessages, useIntl } from 'react-intl';
 
+import { SchedulingFields } from 'components/SchedulingFields';
 import { Maybe } from 'utils/types';
 
 import { DashboardMeetingLayout } from 'apps/bbb/DashboardMeetingLayout';
@@ -24,18 +22,6 @@ const messages = defineMessages({
     defaultMessage: 'Meeting not created!',
     description: 'Message when meeting creation failed.',
     id: 'component.DashboardMeetingForm.createMeetingFail',
-  },
-  startingAtTextLabel: {
-    defaultMessage: 'Starting date and time',
-    description:
-      'Label of the input field to add/update the date and time of the scheduled meeting',
-    id: 'component.DashboardMeetingForm.startingAtTextLabel',
-  },
-  estimatedDurationTextLabel: {
-    defaultMessage: 'Estimated duration',
-    description:
-      'Label of the input field to add/update the estimated duration of the scheduled meeting',
-    id: 'component.DashboardMeetingForm.estimatedDurationTextLabel',
   },
   titleLabel: {
     defaultMessage: 'Title',
@@ -170,70 +156,20 @@ const DashboardMeetingForm = ({ meeting }: DashboardMeetingFormProps) => {
           }}
         />
       </FormField>
-      <Box direction="row" justify="between" margin="medium" gap="medium">
-        <FormField
-          label={intl.formatMessage(messages.startingAtTextLabel)}
-          htmlFor="starting_at"
-        >
-          <DatePicker
-            autoComplete="off"
-            id="starting_at"
-            name="starting_at"
-            className="scheduled-date-picker"
-            dateFormat="dd/MM/yyyy H:mm aa"
-            dayClassName={() => 'scheduled-date-picker-day'}
-            onChange={(date) => {
-              // setDateEvent(date as Date)
-              setUpdatedMeetingState({
-                ...updatedMeetingState,
-                starting_at: date ? date.toISOString() : null,
-              });
-            }}
-            placeholderText="dd/MM/yyyy H:mm"
-            selected={
-              updatedMeetingState.starting_at
-                ? new Date(updatedMeetingState.starting_at)
-                : null
-            }
-            showTimeSelect
-            timeFormat="H:mm"
-            timeIntervals={5}
-            timeInputLabel="Time:"
-            wrapperClassName="wrapper-scheduled-date-picker"
-          />
-        </FormField>
-        <FormField
-          label={intl.formatMessage(messages.estimatedDurationTextLabel)}
-          htmlFor="estimated_duration"
-        >
-          <MaskedInput
-            name="estimated_duration"
-            id="estimated_duration"
-            mask={[
-              {
-                length: [1, 2],
-                options: Array.from({ length: 12 }, (_v, k) => k),
-                regexp: /^1[0,1-2]$|^0?[1-9]$|^0$/,
-                placeholder: 'hh',
-              },
-              { fixed: ':' },
-              {
-                length: 2,
-                options: ['00', '15', '30', '45'],
-                regexp: /^[0-5][0-9]$|^[0-9]$/,
-                placeholder: 'mm',
-              },
-            ]}
-            value={updatedMeetingState.estimated_duration?.slice(0, -3) || ''}
-            onChange={(event) => {
-              setUpdatedMeetingState({
-                ...updatedMeetingState,
-                estimated_duration: event.target.value + ':00',
-              });
-            }}
-          />
-        </FormField>
-      </Box>
+      <SchedulingFields
+        startingAt={updatedMeetingState.starting_at || meeting.starting_at}
+        estimatedDuration={updatedMeetingState.estimated_duration || meeting.estimated_duration}
+        onStartingAtChange={(startingAt) => {
+          return handleChange({
+            starting_at: startingAt,
+          });
+        }}
+        onEstimatedDurationChange={(estimatedDuration) => {
+          return handleChange({
+            estimated_duration: estimatedDuration,
+          });
+        }}
+      />
       <Box align="center">
         <Button
           fill="horizontal"
