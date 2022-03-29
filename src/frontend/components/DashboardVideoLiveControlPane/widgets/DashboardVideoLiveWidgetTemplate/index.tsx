ef990fd5d7285b424b02383531a7cd84d9e53f46
roro-lv <ronan.le-viennesse@polyconseil.fr>
2022-03-29 15:52:21 +0200

    💄(front) add template component for widgets
    This component is used to construct widgets. It comes with a modal component,
    in charge of displaying info on the widget.

---
+++ src/frontend/components/DashboardVideoLiveControlPane/widgets/DashboardVideoLiveWidgetTemplate/index.tsx
@@ -0,0 +1,97 @@
+import { Box, Button, Collapsible, Text } from 'grommet';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import { DownArrowSVG } from 'components/SVGIcons/DownArrowSVG';
+import { InfoCircleSVG } from 'components/SVGIcons/InfoCircleSVG';
+import { defineMessages, useIntl } from 'react-intl';
+import { DashboardVideoLiveInfoModal } from 'components/DashboardVideoLiveInfoModal';
+
+const messages = defineMessages({
+  infoTitle: {
+    defaultMessage: "Widget's function",
+    description: 'The title displays in the info modal.',
+    id: 'component.DashboardVideoLiveWidgetTemplate.infoTitle',
+  },
+});
+
+const StyledTitleText = styled(Text)`
+  font-family: 'Roboto-Bold';
+`;
+
+interface DashboardVideoLiveWidgetTemplateProps {
+  children: React.ReactNode;
+  infoText?: string;
+  initialOpenValue: boolean;
+  title: string;
+}
+
+export const DashboardVideoLiveWidgetTemplate = ({
+  children,
+  infoText,
+  initialOpenValue,
+  title,
+}: DashboardVideoLiveWidgetTemplateProps) => {
+  const [open, setOpen] = useState(initialOpenValue);
+  const [showInfoTextModal, setShowInfoTextModal] = useState(false);
+  const intl = useIntl();
+
+  return (
+    <Box
+      background="white"
+      direction="column"
+      round="6px"
+      style={{
+        boxShadow: '0px 0px 6px 0px rgba(2, 117, 180, 0.3)',
+        minHeight: '70px',
+      }}
+    >
+      <Box pad={{ horizontal: '6px', top: '6px' }}>
+        <Button
+          disabled={!infoText}
+          margin={{ left: 'auto' }}
+          onClick={() => setShowInfoTextModal(true)}
+          plain
+          style={{ display: 'flex', padding: 0 }}
+        >
+          <InfoCircleSVG height="17px" iconColor="blue-active" width="17px" />
+        </Button>
+      </Box>
+
+      {infoText && showInfoTextModal && (
+        <DashboardVideoLiveInfoModal
+          text={infoText}
+          title={intl.formatMessage(messages.infoTitle)}
+          onModalClose={() => setShowInfoTextModal(false)}
+        />
+      )}
+
+      <Box pad={{ horizontal: '20px' }}>
+        <Button onClick={() => setOpen(!open)} plain style={{ padding: 0 }}>
+          <Box align="center" direction="row" gap="25px">
+            <Box>
+              <DownArrowSVG
+                containerStyle={{
+                  transform: open ? undefined : 'rotate(-90deg)',
+                  transition: '150ms linear all',
+                }}
+                iconColor="blue-active"
+                height="9px"
+                width="14.5px"
+              />
+            </Box>
+            <Box style={{ minWidth: '0' }}>
+              <StyledTitleText color="blue-active" size="1.125rem" truncate>
+                {title}
+              </StyledTitleText>
+            </Box>
+          </Box>
+        </Button>
+      </Box>
+
+      <Collapsible open={open}>
+        <Box pad="33px">{children}</Box>
+      </Collapsible>
+    </Box>
+  );
+};
---
+++ src/frontend/components/DashboardVideoLiveInfoModal/index.spec.tsx
@@ -0,0 +1,61 @@
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { ResponsiveContext } from 'grommet';
+
+import { DashboardVideoLiveInfoModal } from './index';
+
+const mockModalOnClose = jest.fn();
+const genericTitle = 'A generic title';
+const genericContent =
+  'A generic content, which has for purpose to represent an example of an average string used in this modal. It has too be not too short, but also not too long. The idea is to be the average string the modal will displayed.';
+
+describe('<DashboardVideoLiveInfoModal />', () => {
+  beforeEach(() => {
+    /*
+          make sure to remove all body children, grommet layer gets rendered twice, known issue
+          https://github.com/grommet/grommet/issues/5200
+        */
+    document.body.innerHTML = '';
+    document.body.appendChild(document.createElement('div'));
+    jest.resetAllMocks();
+  });
+
+  it('renders the modal and closes it with esc key', () => {
+    render(
+      <ResponsiveContext.Provider value="large">
+        <DashboardVideoLiveInfoModal
+          text={genericContent}
+          title={genericTitle}
+          onModalClose={mockModalOnClose}
+        />
+      </ResponsiveContext.Provider>,
+    );
+    screen.getByText(genericTitle);
+    screen.getByText(genericContent);
+    screen.getByRole('button');
+    act(() => {
+      userEvent.keyboard('{esc}');
+    });
+    expect(mockModalOnClose).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders the modal and closes it with close button', () => {
+    render(
+      <ResponsiveContext.Provider value="large">
+        <DashboardVideoLiveInfoModal
+          text={genericContent}
+          title={genericTitle}
+          onModalClose={mockModalOnClose}
+        />
+      </ResponsiveContext.Provider>,
+    );
+    screen.getByText(genericTitle);
+    screen.getByText(genericContent);
+    const closeButton = screen.getByRole('button');
+    act(() => {
+      userEvent.click(closeButton);
+    });
+    expect(mockModalOnClose).toHaveBeenCalledTimes(1);
+  });
+});
