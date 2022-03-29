2d6c07d38ddaafdd3119ebe52df0a4b36a024ecc
roro-lv <ronan.le-viennesse@polyconseil.fr>
2022-03-29 15:50:18 +0200

    🩹(front) fix width of the dashboard
    The width need to be given in % not in vw, because otherwise when the vertical
    scrollbar appears (when the height of the application > the height of the
    viewport), a horizontal scrollbar appears too, because vw units don't take into
    account width of the vertical scrollbar.

--- src/frontend/components/Styled/DashboardContainer.ts
+++ src/frontend/components/Styled/DashboardContainer.ts
@@ -4,6 +4,6 @@ export const DashboardContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: stretch;
-  width: 100vw;
+  width: 100%;
   min-height: 56.25vw; /* Default to LayoutMainArea aspect ratio, ie. 16/9. */
 `;
