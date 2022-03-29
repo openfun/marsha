23c381a91f901697c852db3ca1ac40661573d95a
roro-lv <ronan.le-viennesse@polyconseil.fr>
2022-03-29 15:52:21 +0200

    💄(front) add template component for widgets
    This component is used to construct widgets. It comes with a modal component,
    in charge of displaying info on the widget.

---
+++ src/frontend/components/DashboardVideoLiveInfoModal/index.tsx
@@ -0,0 +1,69 @@
+import { Box, Button, Layer, ResponsiveContext, Text } from 'grommet';
+import React from 'react';
+import styled from 'styled-components';
+
+import { RoundCrossSVG } from 'components/SVGIcons/RoundCrossSVG';
+import { normalizeColor } from 'grommet/utils';
+import { theme } from 'utils/theme/theme';
+
+const StyledTitleText = styled(Text)`
+  font-family: 'Roboto-Medium';
+`;
+
+const StyledText = styled(Text)`
+  font-family: 'Roboto-Regular';
+  line-height: 20px;
+`;
+
+interface DashboardVideoLiveInfoModalProps {
+  text: string;
+  title: string;
+  onModalClose: () => void;
+}
+
+export const DashboardVideoLiveInfoModal = ({
+  text,
+  title,
+  onModalClose,
+}: DashboardVideoLiveInfoModalProps) => {
+  const size = React.useContext(ResponsiveContext);
+
+  return (
+    <Layer
+      onEsc={() => onModalClose()}
+      onClickOutside={() => onModalClose()}
+      responsive={false}
+      style={{
+        width: size === 'small' ? '95%' : '500px',
+        border: `1px solid ${normalizeColor('blue-active', theme)}`,
+      }}
+    >
+      <Box background="bg-info" direction="column" round="6px">
+        <Box
+          direction="row-reverse"
+          pad={{ horizontal: 'small', top: 'small' }}
+        >
+          <Button
+            onClick={() => onModalClose()}
+            plain
+            style={{ display: 'block', padding: 0 }}
+          >
+            <RoundCrossSVG height="20px" iconColor="blue-active" width="20px" />
+          </Button>
+        </Box>
+        <Box
+          direction="column"
+          gap="medium"
+          pad={{ horizontal: 'large', bottom: '30px' }}
+        >
+          <StyledTitleText color="blue-active" size="1.125rem" truncate>
+            {title}
+          </StyledTitleText>
+          <StyledText color="blue-active" size="0.875rem">
+            {text}
+          </StyledText>
+        </Box>
+      </Box>
+    </Layer>
+  );
+};
