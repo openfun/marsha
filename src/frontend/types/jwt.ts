ea57ba75511e2e0d100f410832633ebc45f0fdc3
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-04-01 11:57:06 +0200

    🧑‍💻(frontend) add mock factories for tokens
    When testing, we need to mock tokens to authenticate requests.
    Three most common tokens have been added : public, LTI student, and LTI
    instructor. We can override them for testing purposes.

--- src/frontend/types/jwt.ts
+++ src/frontend/types/jwt.ts
@@ -1,23 +1,27 @@
 import { Nullable } from '../utils/types';
 
+export interface DecodedJwtPermission {
+  can_access_dashboard: boolean;
+  can_update: boolean;
+}
+
+export interface DecodedJwtUser {
+  anonymous_id?: string;
+  email: Nullable<string>;
+  id?: string;
+  username?: Nullable<string>;
+  user_fullname?: Nullable<string>;
+}
+
 export interface DecodedJwt {
   context_id?: string;
   consumer_site?: string;
   locale: string;
   maintenance: boolean;
-  permissions: {
-    can_access_dashboard: boolean;
-    can_update: boolean;
-  };
+  permissions: DecodedJwtPermission;
   playlist_id?: string;
   resource_id: string;
   roles: string[];
   session_id: string;
-  user?: {
-    anonymous_id?: string;
-    email: Nullable<string>;
-    id?: string;
-    username?: Nullable<string>;
-    user_fullname?: Nullable<string>;
-  };
+  user?: DecodedJwtUser;
 }
