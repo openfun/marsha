be0c09b9904d29645496a80896b6161529b01ce5
Quentin BEY <quentin.bey@polyconseil.fr>
2022-04-03 20:41:53 +0200

    ✨(backend) add markdown models & admin & factory
    This commit provides the models to represent a Markdown document.
    The fields are translated with django-parler, the administration
    panel also relies on django-parler.
    The factories are provided for tests.

---
+++ src/backend/marsha/markdown/admin.py
@@ -0,0 +1,50 @@
+"""Admin of the ``markdown`` app of the Marsha project."""
+
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from parler.admin import TranslatableAdmin
+
+from marsha.core.admin import admin_site, link_field
+
+from .models import MarkdownDocument
+
+
+@admin.register(MarkdownDocument, site=admin_site)
+class MarkdownDocumentAdmin(TranslatableAdmin):
+    """Admin class for the MarkdownDocument model."""
+
+    verbose_name = _("Markdown document")
+    list_display = (
+        "pk",
+        "title",
+        link_field("playlist"),
+        "lti_id",
+        "is_draft",
+    )
+    list_select_related = ("playlist__consumer_site",)
+    fields = (
+        "pk",
+        "is_draft",
+        "playlist",
+        "lti_id",
+        "rendering_options",
+        "title",
+        "content",
+        "rendered_content",
+    )
+    readonly_fields = [
+        "pk",
+    ]
+    list_filter = ("playlist__consumer_site__domain",)
+    search_fields = (
+        "pk",
+        "lti_id",
+        "playlist__consumer_site__domain",
+        "playlist__consumer_site__name",
+        "playlist_id",
+        "playlist__lti_id",
+        "playlist__title",
+        "playlist__organization__name",
+        "title",
+    )
---
+++ src/backend/marsha/markdown/factories.py
@@ -0,0 +1,46 @@
+"""Factories for the ``markdown`` app of the Marsha project."""
+
+from django.conf import settings
+from django.utils import lorem_ipsum
+
+import factory
+from factory.django import DjangoModelFactory
+
+from marsha.core.factories import PlaylistFactory
+
+from . import models
+
+
+# usage of format function here is wanted in this file
+# pylint: disable=consider-using-f-string
+class MarkdownDocumenTranslationFactory(DjangoModelFactory):
+    """Factory for the Markdown document's translation model."""
+
+    class Meta:  # noqa
+        # pylint: disable-next=protected-access
+        model = models.MarkdownDocument._parler_meta.root.model
+
+    master = factory.SubFactory(
+        "marsha.markdown.factories.MarkdownDocumentFactory", translations=None
+    )
+
+    # Only translate in one language for test purpose
+    language_code = settings.LANGUAGES[0][0]  # en
+    title = factory.Sequence("Markdown Document's title {:03d}".format)
+    content = lorem_ipsum.sentence()
+    rendered_content = lorem_ipsum.sentence()
+
+
+class MarkdownDocumentFactory(DjangoModelFactory):
+    """Factory for the Markdown document model."""
+
+    class Meta:  # noqa
+        model = models.MarkdownDocument
+
+    lti_id = factory.Faker("uuid4")
+    playlist = factory.SubFactory(PlaylistFactory)
+
+    translations = factory.RelatedFactory(
+        MarkdownDocumenTranslationFactory,
+        factory_related_name="master",
+    )
