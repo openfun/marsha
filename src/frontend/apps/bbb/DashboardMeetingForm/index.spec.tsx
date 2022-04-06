7b42d4390b5b84375ff133dc5d015d3d276602b3
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-04-06 16:41:21 +0200

    🐛(frontend) make meeting estimated duration clearable
    When clearing the estimated duration field, a luxon duration was used,
    and raised an error with an empty value.

--- src/frontend/apps/bbb/DashboardMeetingForm/index.spec.tsx
+++ src/frontend/apps/bbb/DashboardMeetingForm/index.spec.tsx
@@ -449,4 +449,74 @@ describe('<DashboardMeetingForm />', () => {
       }),
     });
   });
+
+  it('clears a meeting scheduled', async () => {
+    const startingAt = currentDate.plus({ days: 2, hours: 2 }).startOf('hour');
+    const estimatedDuration = Duration.fromObject({ minutes: 30 });
+    const meeting = meetingMockFactory({
+      id: '1',
+      started: false,
+      starting_at: startingAt.toISO(),
+      estimated_duration: estimatedDuration.toFormat('hh:mm:ss'),
+    });
+    const queryClient = new QueryClient();
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/meetings/1/', deferredPatch.promise);
+
+    const { getByText, rerender } = render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm meeting={meeting} />
+        </QueryClientProvider>,
+      ),
+    );
+    getByText('Title');
+    getByText('Welcome text');
+
+    const inputStartingAtDate = screen.getByLabelText(/starting date/i);
+    userEvent.clear(inputStartingAtDate);
+    fireEvent.blur(inputStartingAtDate);
+
+    // simulate meeting update
+    rerender(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <DashboardMeetingForm meeting={{ ...meeting, starting_at: null }} />
+        </QueryClientProvider>,
+      ),
+    );
+
+    const inputEstimatedDuration = screen.getByLabelText(/estimated duration/i);
+    userEvent.clear(inputEstimatedDuration);
+
+    jest.runAllTimers();
+    await act(async () =>
+      deferredPatch.resolve({ message: 'Meeting scheduled.' }),
+    );
+
+    expect(fetchMock.calls()).toHaveLength(2);
+
+    expect(fetchMock.calls()[0][0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[0][1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        starting_at: null,
+      }),
+    });
+
+    expect(fetchMock.calls()[1][0]).toEqual('/api/meetings/1/');
+    expect(fetchMock.calls()[1][1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        estimated_duration: null,
+      }),
+    });
+  });
 });
