e7abd279a2690a35866938a8fd4865024cffd2ca
Manuel Raynaud <manu@raynaud.io>
2022-04-15 08:11:01 +0200

    ✨(front) implement useUpdateVideo with react-query
    We want to use react-query mutation to update an existing video
    using a PATCH request to partially update a video resource.

--- src/frontend/data/queries/index.tsx
+++ src/frontend/data/queries/index.tsx
@@ -149,6 +149,40 @@ export const useCreateVideo = (options?: UseCreateVideoOptions) => {
   );
 };
 
+type UseUpdateVideoData = Partial<Video>;
+type UseUpdateVideoError =
+  | { code: 'exception' }
+  | {
+      code: 'invalid';
+      errors: { [key in keyof UseUpdateVideoData]?: string[] }[];
+    };
+type UseUpdateVideoOptions = UseMutationOptions<
+  Video,
+  UseUpdateVideoError,
+  UseUpdateVideoData
+>;
+export const useUpdateVideo = (id: string, options?: UseUpdateVideoOptions) => {
+  const queryClient = useQueryClient();
+  return useMutation<Video, UseUpdateVideoError, UseUpdateVideoData>(
+    (updatedVideo) => updateOne({ name: 'videos', id, object: updatedVideo }),
+    {
+      ...options,
+      onSuccess: (data, variables, context) => {
+        queryClient.invalidateQueries('videos');
+        if (options?.onSuccess) {
+          options.onSuccess(data, variables, context);
+        }
+      },
+      onError: (error, variables, context) => {
+        queryClient.invalidateQueries('videos');
+        if (options?.onError) {
+          options.onError(error, variables, context);
+        }
+      },
+    },
+  );
+};
+
 type VideosResponse = APIList<Video>;
 type UseVideosParams = {} | { organization: string };
 export const useVideos = (
