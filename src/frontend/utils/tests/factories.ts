20827f48dd59aefd2713c2dbaef3258d79f7b486
Manuel Raynaud <manu@raynaud.io>
2022-04-19 18:03:27 +0200

    🏷️(front) update video type with estimated_duration
    The estimated_duration was not defined on the video definition. We add
    it as a nullable string.

--- src/frontend/utils/tests/factories.ts
+++ src/frontend/utils/tests/factories.ts
@@ -110,6 +110,7 @@ export const videoMockFactory = (video: Partial<Video> = {}): Video => {
     lti_url: `https://example.com/lti/videos/${id}`,
     show_download: true,
     starting_at: null,
+    estimated_duration: null,
     thumbnail: null,
     timed_text_tracks: [],
     title: faker.lorem.words(5),
