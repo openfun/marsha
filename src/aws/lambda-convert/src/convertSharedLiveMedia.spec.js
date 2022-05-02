46c83639790f8e9569fbf1c94104412892fecfd2
Manuel Raynaud <manu@raynaud.io>
2022-05-02 16:30:02 +0200

    🐛(aws) copy source shared live media to destination bucket
    The source shared live media was not copied from the source bucket to
    the destination one. We need to copy it in order to download it from the
    video dashboard.

--- src/aws/lambda-convert/src/convertSharedLiveMedia.spec.js
+++ src/aws/lambda-convert/src/convertSharedLiveMedia.spec.js
@@ -13,11 +13,13 @@ const svgFiles = [
 // Don't pollute tests with logs intended for CloudWatch
 jest.spyOn(console, "log");
 
-// Mock the AWS SDK calls used in encodeTimedTextTrack
+// Mock the AWS SDK calls used in convertSharedLiveMedia
 const mockGetObject = jest.fn();
 const mockPutObject = jest.fn();
+const mockCopyObject = jest.fn();
 jest.mock("aws-sdk", () => ({
   S3: function () {
+    this.copyObject = mockCopyObject;
     this.getObject = mockGetObject;
     this.putObject = mockPutObject;
   },
@@ -38,10 +40,13 @@ describe("lambda-convert/src/convertSharedLiveMedia", () => {
     mockPutObject.mockReturnValue({
       promise: () => new Promise((resolve) => resolve()),
     });
+    mockCopyObject.mockReturnValue({
+      promise: () => new Promise((resolve) => resolve()),
+    });
 
     const { nbPages, extension } = await convertSharedLiveMedia(
       "ed08da34-7447-4141-96ff-5740315d7b99/sharedlivemedia/c5cad053-111a-4e0e-8f78-fe43dec11512/1638403200.pdf",
-      "source bucket"
+      "source_bucket"
     );
 
     expect(nbPages).toBe(3);
@@ -61,6 +66,12 @@ describe("lambda-convert/src/convertSharedLiveMedia", () => {
       Body: fs.readFileSync(svgFiles[2]),
       Key: "ed08da34-7447-4141-96ff-5740315d7b99/sharedlivemedia/c5cad053-111a-4e0e-8f78-fe43dec11512/1638403200_3.svg",
       Bucket: "destination_bucket",
+    });
+    expect(mockCopyObject).toHaveBeenCalledWith({
+      Bucket: "destination_bucket",
+      Key: "ed08da34-7447-4141-96ff-5740315d7b99/sharedlivemedia/c5cad053-111a-4e0e-8f78-fe43dec11512/1638403200.pdf",
+      CopySource:
+        "source_bucket/ed08da34-7447-4141-96ff-5740315d7b99/sharedlivemedia/c5cad053-111a-4e0e-8f78-fe43dec11512/1638403200.pdf",
     });
   });
 });
