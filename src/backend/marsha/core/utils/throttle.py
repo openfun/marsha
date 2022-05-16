22de9396fc759a6e224a873cf935a63894c6ef9e
Manuel Raynaud <manu@raynaud.io>
2022-05-16 16:03:30 +0200

    ⚡️(back) create fallback cache strategy when redis is down
    When redis is down and used as cache backend we want to fallback on an
    other backend cache to avoid denial of service. For this we have copied
    the fallback cache used in the openfun/richie project and we adapt it to
    our use case. For example we don't need to clear any cache if redis is
    up again, the cache we are using has a lifetime.

---
+++ src/backend/marsha/core/utils/throttle.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from functools import wraps
+
+
+class throttle(object):
+    """
+    Throttle Decorator
+    Limit execution of a function to a defined interval
+    """
+
+    def __init__(self, interval):
+        """
+        Initialize throttle decorator
+        Define the throttle_interval with the provided interval (in seconds)
+        and set time_of_last_call to the earliest representable datetime
+        """
+        self.throttle_interval = timedelta(seconds=interval)
+        self.time_of_last_call = datetime.min
+
+    def __call__(self, callback):
+        """
+        Process `elapsed_since_last_call`,
+        if it is greater than `throttle_interval`, callback is executed.
+        """
+
+        @wraps(callback)
+        def wrapper(*args, **kwargs):
+            now = datetime.now()
+            elapsed_since_last_call = now - self.time_of_last_call
+
+            if elapsed_since_last_call > self.throttle_interval:
+                self.time_of_last_call = now
+                return callback(*args, **kwargs)
+
+        return wrapper
