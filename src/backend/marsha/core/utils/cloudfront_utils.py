541d8b0a15d28d700ca28f8d6ef2b2a5cddcc5b0
Manuel Raynaud <manu@raynaud.io>
2022-05-20 10:05:35 +0200

    ♻️(back) use custom policy to generate cloudfront signed url
    Since the beginning of marsha we used predefined signed url to request
    cloudfront. So we needed to generate a signed url for each resources we
    want to fetch. Signing a url is a low operation and when we introduced
    shared_live_media resources we had a bottleneck on the video and
    shared_live_media serializer. To fix this issue, we decided to use a
    custom policy[1]. This custom policy target at once all the resource
    belonging to a given video. We sign this this policy once and then we
    catch all the needed parameters to use them with all the resources
    related to this video.
    
    [1]: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/
    private-content-signed-urls.html#private-content-choosing-canned-custom-policy

--- src/backend/marsha/core/utils/cloudfront_utils.py
+++ src/backend/marsha/core/utils/cloudfront_utils.py
@@ -4,8 +4,11 @@ Utils to sign CloudFront urls and cookies.
 Following boto3's documentation to sign CloudFront urls
 https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudfront.html
 """
+import base64
+
 from django.conf import settings
 
+from botocore.signers import CloudFrontSigner
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
@@ -42,3 +45,44 @@ def rsa_signer(message):
     # The following line is excluded from bandit security check because cloudfront supports
     # only sha1 hash for signed URLs.
     return private_key.sign(message, padding.PKCS1v15(), hashes.SHA1())  # nosec
+
+
+def generate_cloudfront_urls_signed_parameters(resource, date_less_than):
+    """
+    Generate all parameters use by a cloudfront signed url.
+    Mainly extracted from CloudFrontSigner class.
+    """
+    cloudfront_signer = CloudFrontSigner(
+        settings.CLOUDFRONT_SIGNED_PUBLIC_KEY_ID,
+        rsa_signer,
+    )
+    policy = cloudfront_signer.build_policy(
+        resource=resource, date_less_than=date_less_than
+    ).encode("utf8")
+    signature = cloudfront_signer.rsa_signer(policy)
+    return [
+        f"Policy={_url_b64encode(policy).decode('utf8')}",
+        f"Signature={_url_b64encode(signature).decode('utf8')}",
+        f"Key-Pair-Id={cloudfront_signer.key_id}",
+    ]
+
+
+def build_signed_url(base_url, extra_params):
+    """
+    Build an url by concatening the base url and the parameters needed to sign it.
+    Extracted from CloudFrontSigner class
+    """
+    separator = "&" if "?" in base_url else "?"
+    return base_url + separator + "&".join(extra_params)
+
+
+def _url_b64encode(data):
+    """Extracted from CloudFrontSigner class"""
+    # Required by CloudFront. See also:
+    # http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-linux-openssl.html
+    return (
+        base64.b64encode(data)
+        .replace(b"+", b"-")
+        .replace(b"=", b"_")
+        .replace(b"/", b"~")
+    )
