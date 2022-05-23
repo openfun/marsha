07c73fb8cd8d39f7ad2f1f0e3d99bd0a10d50925
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-05-23 14:07:33 +0200

    ⬆️(dependencies) resolve estree-walker import with jest
    Since version 3, estree-walker supports commonJS export.
    We need to update our jest resolver accordingly.

--- src/frontend/utils/tests/resolver.js
+++ src/frontend/utils/tests/resolver.js
@@ -4,7 +4,8 @@ module.exports = (path, options) => {
     ...options,
     // Use packageFilter to process parsed `package.json` before the resolution (see https://www.npmjs.com/package/resolve#resolveid-opts-cb)
     packageFilter: (pkg) => {
-      // This is a workaround for https://github.com/uuidjs/uuid/pull/616
+      // This is a workaround for https://github.com/uuidjs/uuid/pull/616 and other packages
+      // that not use commonJS style export.
       //
       // jest-environment-jsdom 28+ tries to use browser exports instead of default exports,
       // but uuid only offers an ESM browser export and not a CommonJS one. Jest does not yet
@@ -17,11 +18,7 @@ module.exports = (path, options) => {
       // Once we're able to migrate our Jest config to ESM and a browser crypto
       // implementation is available for the browser+ESM version of uuid to use (eg, via
       // https://github.com/jsdom/jsdom/pull/3352 or a similar polyfill), this can go away.
-      if (pkg.name === 'uuid') {
-        delete pkg['exports'];
-        delete pkg['module'];
-      }
-      if (['estree-walker', 'periscopic'].includes(pkg.name)) {
+      if (['uuid', 'periscopic'].includes(pkg.name)) {
         delete pkg['exports'];
         delete pkg['module'];
       }
