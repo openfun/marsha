e5c9440a9ec2ae351faa1c7a2c05a992029bc352
Quentin BEY <quentin.bey@polyconseil.fr>
2022-05-24 14:54:49 +0200

    ✨(backend) add Idp to Organization association
    This new model will be used to store the link between and Identity Provider
    and its organization. This will allow to directly link a new user to its
    organization, or init the Organization on-the-fly if it doesn't already exist.
    For now it not easy to tell whether we will have other authentication process
    for the same Organization, that's why the model is really simple.

---
+++ src/backend/marsha/account/models.py
@@ -0,0 +1,35 @@
+"""This module holds the models for marsha's `account` application."""
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from marsha.core.models import BaseModel, Organization
+
+
+class IdpOrganizationAssociation(BaseModel):
+    """
+    Simple association between an Identity Provider and an Organization.
+
+    Initially implemented for Renater, we identify the IdP using the SAML
+    entity ID as we don't store them in database. Any "key" may be later
+    used to identify uniquely an Identity Provider.
+    """
+
+    idp_identifier = models.CharField(
+        max_length=255,
+        db_index=True,
+        verbose_name=_("identity provider ID"),
+        help_text=_("allows to know which IdP is linked (eg. the Entity ID for SAML)"),
+    )
+    organization = models.ForeignKey(
+        to=Organization,
+        related_name="identity_providers",
+        verbose_name=_("organization"),
+        help_text=_("organization linked to the identity provider"),
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        app_label = "account"
+        unique_together = ("idp_identifier", "organization")
+        db_table = "idp_organization_association"
