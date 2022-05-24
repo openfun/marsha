c334c70b989b6508b85b9739d8194a8124695c71
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-05-24 10:10:20 +0200

    👔(frontend) create meeting on lti select
    We want to store moodle activity title and description in the new
    meeting when we add one through LTI select (deeplinking).
    So, we need to use meetings API to create them.

--- src/frontend/apps/bbb/SelectContentTab/index.spec.tsx
+++ src/frontend/apps/bbb/SelectContentTab/index.spec.tsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import fetchMock from 'fetch-mock';
 import React, { Suspense } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -36,7 +37,7 @@ const meeting = meetingMockFactory({
   playlist: currentPlaylist,
 });
 const selectMeetingResponse = {
-  new_url: 'https://example.com/meetings/new_uuid/',
+  new_url: 'https://example.com/lti/meetings/',
   meetings: [meeting],
 };
 
@@ -45,7 +46,7 @@ fetchMock.get('/api/meetings/lti-select/', selectMeetingResponse);
 describe('<SelectContent />', () => {
   afterEach(jest.resetAllMocks);
 
-  it('select content', async () => {
+  it('selects content', async () => {
     const queryClient = new QueryClient();
 
     const { container } = render(
@@ -67,8 +68,8 @@ describe('<SelectContent />', () => {
     const meetingTab = await screen.findByRole('tab', {
       name: 'Meetings',
     });
-    fireEvent.click(meetingTab);
-    fireEvent.click(screen.getByTitle(`Select ${meeting.title}`));
+    userEvent.click(meetingTab);
+    userEvent.click(screen.getByTitle(`Select ${meeting.title}`));
 
     expect(window.HTMLFormElement.prototype.submit).toHaveBeenCalledTimes(1);
 
@@ -90,7 +91,7 @@ describe('<SelectContent />', () => {
     });
   });
 
-  it('add new content', async () => {
+  it('selects content with activity title and description', async () => {
     const queryClient = new QueryClient();
 
     const { container } = render(
@@ -98,6 +99,107 @@ describe('<SelectContent />', () => {
         <QueryClientProvider client={queryClient}>
           <Suspense fallback={<div>Loading...</div>}>
             <SelectContent
+              lti_select_form_action_url={appData.lti_select_form_action_url!}
+              lti_select_form_data={{
+                lti_response_url: 'https://example.com/lti',
+                lti_message_type: 'ContentItemSelection',
+                activity_title: 'Activity title',
+                activity_description: 'Activity description',
+              }}
+            />
+          </Suspense>
+        </QueryClientProvider>,
+      ),
+    );
+
+    const meetingTab = await screen.findByRole('tab', {
+      name: 'Meetings',
+    });
+    userEvent.click(meetingTab);
+    userEvent.click(screen.getByTitle(`Select ${meeting.title}`));
+
+    expect(window.HTMLFormElement.prototype.submit).toHaveBeenCalledTimes(1);
+
+    expect(container.querySelector('form')).toHaveFormValues({
+      lti_response_url: 'https://example.com/lti',
+      lti_message_type: 'ContentItemSelection',
+      content_items: JSON.stringify({
+        '@context': 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
+        '@graph': [
+          {
+            '@type': 'ContentItem',
+            url: meeting.lti_url,
+            frame: [],
+            title: 'Activity title',
+            text: 'Activity description',
+          },
+        ],
+      }),
+    });
+  });
+
+  it('selects content with empty activity title and description', async () => {
+    const queryClient = new QueryClient();
+
+    const { container } = render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <SelectContent
+              lti_select_form_action_url={appData.lti_select_form_action_url!}
+              lti_select_form_data={{
+                lti_response_url: 'https://example.com/lti',
+                lti_message_type: 'ContentItemSelection',
+                activity_title: '',
+                activity_description: '',
+              }}
+            />
+          </Suspense>
+        </QueryClientProvider>,
+      ),
+    );
+
+    const meetingTab = await screen.findByRole('tab', {
+      name: 'Meetings',
+    });
+    userEvent.click(meetingTab);
+    userEvent.click(screen.getByTitle(`Select ${meeting.title}`));
+
+    expect(window.HTMLFormElement.prototype.submit).toHaveBeenCalledTimes(1);
+
+    expect(container.querySelector('form')).toHaveFormValues({
+      lti_response_url: 'https://example.com/lti',
+      lti_message_type: 'ContentItemSelection',
+      content_items: JSON.stringify({
+        '@context': 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
+        '@graph': [
+          {
+            '@type': 'ContentItem',
+            url: meeting.lti_url,
+            frame: [],
+            title: meeting.title,
+            text: meeting.description,
+          },
+        ],
+      }),
+    });
+  });
+
+  it('adds new content', async () => {
+    const queryClient = new QueryClient();
+    const playlist = playlistMockFactory();
+    const newMeeting = meetingMockFactory({
+      title: null,
+      description: null,
+    });
+    fetchMock.post('/api/meetings/', newMeeting);
+
+    const { container } = render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <SelectContent
+              playlist={playlist}
               lti_select_form_action_url={appData.lti_select_form_action_url!}
               lti_select_form_data={{
                 lti_response_url: 'https://example.com/lti',
@@ -112,10 +214,21 @@ describe('<SelectContent />', () => {
     const meetingTab = await screen.findByRole('tab', {
       name: 'Meetings',
     });
-    fireEvent.click(meetingTab);
-    fireEvent.click(screen.getByText('Add a meeting'));
+    userEvent.click(meetingTab);
+    userEvent.click(screen.getByText('Add a meeting'));
 
-    expect(window.HTMLFormElement.prototype.submit).toHaveBeenCalledTimes(1);
+    await waitFor(() => {
+      expect(
+        fetchMock.called('/api/meetings/', {
+          body: {
+            playlist: playlist.id,
+          },
+          method: 'POST',
+        }),
+      ).toBe(true);
+
+      expect(window.HTMLFormElement.prototype.submit).toHaveBeenCalledTimes(1);
+    });
 
     const form = container.querySelector('form');
     expect(form).toHaveFormValues({
@@ -124,7 +237,143 @@ describe('<SelectContent />', () => {
         '@graph': [
           {
             '@type': 'ContentItem',
-            url: 'https://example.com/meetings/new_uuid/',
+            url: `https://example.com/lti/meetings/${newMeeting.id}`,
+            frame: [],
+          },
+        ],
+      }),
+    });
+    expect(form).toHaveAttribute('action', '/lti/select/');
+  });
+
+  it('adds new content with activity title and description', async () => {
+    const queryClient = new QueryClient();
+    const playlist = playlistMockFactory();
+    const newMeeting = meetingMockFactory({
+      title: null,
+      description: null,
+    });
+    fetchMock.post('/api/meetings/', newMeeting, {
+      overwriteRoutes: true,
+    });
+
+    const { container } = render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <SelectContent
+              playlist={playlist}
+              lti_select_form_action_url={appData.lti_select_form_action_url!}
+              lti_select_form_data={{
+                lti_response_url: 'https://example.com/lti',
+                lti_message_type: 'ContentItemSelection',
+                activity_title: 'Activity title',
+                activity_description: 'Activity description',
+              }}
+            />
+          </Suspense>
+        </QueryClientProvider>,
+      ),
+    );
+
+    const meetingTab = await screen.findByRole('tab', {
+      name: 'Meetings',
+    });
+    userEvent.click(meetingTab);
+    userEvent.click(screen.getByText('Add a meeting'));
+
+    await waitFor(() => {
+      expect(
+        fetchMock.called('/api/meetings/', {
+          body: {
+            playlist: playlist.id,
+            title: 'Activity title',
+            description: 'Activity description',
+          },
+          method: 'POST',
+        }),
+      ).toBe(true);
+
+      expect(window.HTMLFormElement.prototype.submit).toHaveBeenCalledTimes(1);
+    });
+
+    const form = container.querySelector('form');
+    expect(form).toHaveFormValues({
+      content_items: JSON.stringify({
+        '@context': 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
+        '@graph': [
+          {
+            '@type': 'ContentItem',
+            url: `https://example.com/lti/meetings/${newMeeting.id}`,
+            frame: [],
+            title: 'Activity title',
+            text: 'Activity description',
+          },
+        ],
+      }),
+    });
+    expect(form).toHaveAttribute('action', '/lti/select/');
+  });
+
+  it('adds new content with empty activity title and description', async () => {
+    const queryClient = new QueryClient();
+    const playlist = playlistMockFactory();
+    const newMeeting = meetingMockFactory({
+      title: null,
+      description: null,
+    });
+    fetchMock.post('/api/meetings/', newMeeting, {
+      overwriteRoutes: true,
+    });
+
+    const { container } = render(
+      wrapInIntlProvider(
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <SelectContent
+              playlist={playlist}
+              lti_select_form_action_url={appData.lti_select_form_action_url!}
+              lti_select_form_data={{
+                lti_response_url: 'https://example.com/lti',
+                lti_message_type: 'ContentItemSelection',
+                activity_title: '',
+                activity_description: '',
+              }}
+            />
+          </Suspense>
+        </QueryClientProvider>,
+      ),
+    );
+
+    const meetingTab = await screen.findByRole('tab', {
+      name: 'Meetings',
+    });
+    userEvent.click(meetingTab);
+    userEvent.click(screen.getByText('Add a meeting'));
+
+    await waitFor(() => {
+      expect(
+        fetchMock.called('/api/meetings/', {
+          body: {
+            playlist: playlist.id,
+            title: '',
+            description: '',
+          },
+          method: 'POST',
+        }),
+      ).toBe(true);
+
+      expect(window.HTMLFormElement.prototype.submit).toHaveBeenCalledTimes(1);
+    });
+
+    const form = container.querySelector('form');
+    expect(form).toHaveFormValues({
+      content_items: JSON.stringify({
+        '@context': 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
+        '@graph': [
+          {
+            '@type': 'ContentItem',
+            url: `https://example.com/lti/meetings/${newMeeting.id}`,
             frame: [],
           },
         ],
