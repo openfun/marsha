0cc65919f1f2c498fec3c57db3b709dc120cffd3
Quentin BEY <quentin.bey@polyconseil.fr>
2022-05-24 14:29:40 +0200

    ♻️(backend) move django login views in submodule
    The views module will be split into several submodule to
    ease readability when adding other authentication modes.

---
+++ src/backend/marsha/account/views/__init__.py
@@ -0,0 +1,6 @@
+"""
+    Module containing the marsha's account views:
+     - the `model_auth` submodule contains views for the simple Django Model authentication.
+"""
+# pylint: disable=wildcard-import,unused-wildcard-import
+from .model_auth import *  # noqa isort:skip
