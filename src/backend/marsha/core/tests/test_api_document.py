82a73aea9f72b247f672be546012ab7d5d927cb2
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-05-24 13:29:23 +0200

    ✨(backend) ignore default activity title on lti select
    When an LTI select request is made from Moodle, if the activity has no
    title, the provider name is sent.
    So, we choose to ignore this case, and we act as if no title was sent.

--- src/backend/marsha/core/tests/test_api_document.py
+++ src/backend/marsha/core/tests/test_api_document.py
@@ -169,6 +169,21 @@ class DocumentAPITest(TestCase):
         )
         self.assertEqual(response.status_code, 403)
 
+    def test_api_document_create_student_with_playlist_token(self):
+        """A student with a playlist token should not be able to create a document."""
+        playlist = PlaylistFactory()
+
+        jwt_token = AccessToken()
+        jwt_token.payload["resource_id"] = "None"
+        jwt_token.payload["roles"] = ["student"]
+        jwt_token.payload["permissions"] = {"can_update": True}
+        jwt_token.payload["playlist_id"] = str(playlist.id)
+
+        response = self.client.post(
+            "/api/documents/", HTTP_AUTHORIZATION=f"Bearer {jwt_token}"
+        )
+        self.assertEqual(response.status_code, 403)
+
     def test_api_document_create_instructor(self):
         """An instrustor should not be able to create a document."""
         document = DocumentFactory()
