8c37c71bd880302df85996d04e84865ca4a62fd1
Manuel Raynaud <manu@raynaud.io>
2022-05-31 17:17:22 +0200

    ✨(front) fetch video data after websocket reconnection
    When the websocket reconnect to the server after a disconnection, we
    fetch the video data in order to refresh them in the zustand store.
    Doing this, we will not miss data between disconnection.
    Also, the robustwebsocket library type are wrong. We choose to create
    our own and overrde the reconnection strategy. By default, the lib does
    not reconnect to the server after three attempts. We do not limit the
    number of attempt anymore but we increse the delay to 500 ms after 10
    attempts.

--- src/frontend/data/websocket.ts
+++ src/frontend/data/websocket.ts
@@ -8,11 +8,16 @@ import { checkLtiToken } from 'utils/checkLtiToken';
 import { getOrInitAnonymousId } from 'utils/getOrInitAnonymousId';
 
 import { appData, getDecodedJwt } from './appData';
+import { getResource } from './sideEffects/getResource';
 
 type WSMessageType = {
   resource: UploadableObject;
   type: modelName;
 };
+
+interface OpenEvent extends Event {
+  reconnects?: number;
+}
 
 let videoWebsocket: RobustWebSocket;
 
@@ -25,10 +30,27 @@ export const initVideoWebsocket = (video: Video) => {
     const anonymousId = getOrInitAnonymousId();
     url = `${url}&anonymous_id=${anonymousId}`;
   }
-  videoWebsocket = new RobustWebSocket(url);
+
+  videoWebsocket = new RobustWebSocket(url, null, {
+    shouldReconnect: (_, ws) => {
+      // On the first 10 attempts we try to reconnect immediatly.
+      // Then the delay between each attempts is 500ms
+      if (ws.attempts < 10) {
+        return 0;
+      }
+
+      return 500;
+    },
+  });
 
   videoWebsocket.addEventListener('message', async (message) => {
     const data: WSMessageType = JSON.parse(message.data);
     await addResource(data.type, data.resource);
+  });
+
+  videoWebsocket.addEventListener('open', async (event: OpenEvent) => {
+    if (event.reconnects && event.reconnects > 0) {
+      await getResource(modelName.VIDEOS, video.id);
+    }
   });
 };
