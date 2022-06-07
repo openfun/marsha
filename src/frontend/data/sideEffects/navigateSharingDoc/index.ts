9e9c2e3fa7f88a88bbbd04a8165421fe93c399ff
vassili <vassili.rezvoy@polyconseil.fr>
2022-06-07 15:54:57 +0200

    ✨(frontend) implement navigate-sharing
    Implement endpoint to update current page of shared component

---
+++ src/frontend/data/sideEffects/navigateSharingDoc/index.ts
@@ -0,0 +1,28 @@
+import { appData } from 'data/appData';
+import { API_ENDPOINT } from 'settings';
+import { Video } from 'types/tracks';
+
+export const navigateSharingDoc = async (
+  video: Video,
+  targetPage: number,
+): Promise<Video> => {
+  const response = await fetch(
+    `${API_ENDPOINT}/videos/${video.id}/navigate-sharing/`,
+    {
+      headers: {
+        Authorization: `Bearer ${appData.jwt}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        target_page: targetPage,
+      }),
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed to update shared page for video ${video.id}.`);
+  }
+
+  return await response.json();
+};
