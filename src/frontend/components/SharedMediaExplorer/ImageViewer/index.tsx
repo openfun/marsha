01a0e481301b707f691b7c84b1783f819a4b3449
vassili <vassili.rezvoy@polyconseil.fr>
2022-06-07 15:46:47 +0200

    ✨(frontend) add components to support sharing a document
    Add component to render the shared document during a live.

---
+++ src/frontend/components/SharedMediaExplorer/ImageViewer/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+
+import { useSharedMediaCurrentPage } from 'data/stores/useSharedMediaCurrentPage';
+import { Box, Image } from 'grommet';
+
+const messages = defineMessages({
+  altMessage: {
+    defaultMessage: 'Shared document page {url}',
+    description: 'Alt message for a shared document page',
+    id: 'component.ImageViewer.altMessage',
+  },
+});
+
+export const ImageViewer = () => {
+  const intl = useIntl();
+  const [currentPage] = useSharedMediaCurrentPage();
+
+  return (
+    <Box background="white">
+      <Image
+        alt={intl.formatMessage(messages.altMessage, {
+          url: currentPage.imageUrl,
+        })}
+        src={currentPage.imageUrl}
+        style={{ maxWidth: '100%' }}
+      />
+    </Box>
+  );
+};
