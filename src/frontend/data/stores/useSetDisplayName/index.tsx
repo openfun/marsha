a7c83fbb8ee5bd486b706c649033dfd0ec2a21b1
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-06-08 14:34:51 +0200

    🧑‍💻(frontend) clearly display missing context
    When a context is missing, we didn't see which one is concerned in the
    error output.

--- src/frontend/data/stores/useSetDisplayName/index.tsx
+++ src/frontend/data/stores/useSetDisplayName/index.tsx
@@ -1,6 +1,6 @@
 import { createStore } from 'utils/createContext';
 
-const store = createStore<boolean>();
+const store = createStore<boolean>('useSetDisplayName');
 
 export const SetDisplayNameProvider = store.Provider;
 export const useSetDisplayName = store.useStore;
