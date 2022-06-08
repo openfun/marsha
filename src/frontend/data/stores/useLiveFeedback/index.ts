a051bfb28acc47ba37e7f7c78da31e26195fba9c
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-06-08 14:34:51 +0200

    🧑‍💻(frontend) clearly display missing context
    When a context is missing, we didn't see which one is concerned in the
    error output.

--- src/frontend/data/stores/useLiveFeedback/index.ts
+++ src/frontend/data/stores/useLiveFeedback/index.ts
@@ -1,6 +1,6 @@
 import { createStore } from 'utils/createContext';
 
-const store = createStore<boolean>();
+const store = createStore<boolean>('useLiveFeedback');
 
 export const LiveFeedbackProvider = store.Provider;
 export const useLiveFeedback = store.useStore;
--- src/frontend/data/stores/useLiveModale/index.ts
+++ src/frontend/data/stores/useLiveModale/index.ts
@@ -2,7 +2,7 @@ import { LiveModaleProps } from 'components/LiveModale';
 import { createStore } from 'utils/createContext';
 import { Nullable } from 'utils/types';
 
-const store = createStore<Nullable<LiveModaleProps>>();
+const store = createStore<Nullable<LiveModaleProps>>('useLiveModale');
 
 export const LiveModaleConfigurationProvider = store.Provider;
 export const useLiveModaleConfiguration = store.useStore;
