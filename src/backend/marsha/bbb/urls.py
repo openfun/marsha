af835b030a671f85acee4e6a3ba21f364e08f130
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-06-16 14:21:11 +0200

    🩹(bbb) allow legacy urls to work
    As we renamed our resource, we need to suppport urls that may be used in
    various LMS.

--- src/backend/marsha/bbb/urls.py
+++ src/backend/marsha/bbb/urls.py
@@ -13,11 +13,22 @@ app_name = "bbb"
 router = DefaultRouter()
 router.register("classrooms", ClassroomViewSet, basename="classrooms")
 
+# The following URL patterns are used to support legacy model name.
+# They must be removed in the future.
+router.register("meetings", ClassroomViewSet, basename="meetings")
+
 urlpatterns = [
     path(
         "lti/classrooms/<uuid:uuid>",
         ClassroomLTIView.as_view(),
         name="classroom_lti_view",
+    ),
+    # The following URL pattern is used to support legacy model name.
+    # It must be removed in the future.
+    path(
+        "lti/meetings/<uuid:uuid>",
+        ClassroomLTIView.as_view(),
+        name="meeting_lti_view",
     ),
     path("api/", include(router.urls)),
 ]
