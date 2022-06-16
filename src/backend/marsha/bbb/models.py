e839be1b3ee27fd494bda37de2c7c8844887c620
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-06-16 13:06:38 +0200

    🗃️(bbb) rename meeting database table to classroom
    To finalize the renaming of our meeting resource to classroom, only the
    database table needs to be altered.

--- src/backend/marsha/bbb/models.py
+++ src/backend/marsha/bbb/models.py
@@ -17,8 +17,8 @@ from marsha.core.models import BaseModel, Playlist
 logger = logging.getLogger(__name__)
 
 
-class MeetingManager(models.Manager):
-    """Model manager for a BBB meeting."""
+class ClassroomManager(models.Manager):
+    """Model manager for a BBB classroom."""
 
     def create(self, *args, **kwargs):
         """Remove unexisting parameters added by `lti.utils.get_or_create_resource`."""
@@ -27,18 +27,18 @@ class MeetingManager(models.Manager):
         return super().create(*args, **kwargs)
 
 
-class Meeting(BaseModel):
-    """Model representing a BBB meeting."""
+class Classroom(BaseModel):
+    """Model representing a BBB classroom."""
 
-    RESOURCE_NAME = "meetings"
-    objects = MeetingManager()
+    RESOURCE_NAME = "classrooms"
+    objects = ClassroomManager()
 
     playlist = models.ForeignKey(
         to=Playlist,
         related_name="%(class)ss",
         verbose_name=_("playlist"),
-        help_text=_("playlist to which this meeting belongs"),
-        # don't allow hard deleting a playlist if it still contains meeting
+        help_text=_("playlist to which this classroom belongs"),
+        # don't allow hard deleting a playlist if it still contains classroom
         on_delete=models.PROTECT,
     )
     lti_id = models.CharField(
@@ -51,25 +51,25 @@ class Meeting(BaseModel):
     title = models.CharField(
         max_length=255,
         verbose_name=_("title"),
-        help_text=_("title of the meeting"),
+        help_text=_("title of the classroom"),
         null=True,
         blank=True,
     )
     description = models.TextField(
         verbose_name=_("description"),
-        help_text=_("description of the meeting"),
+        help_text=_("description of the classroom"),
         null=True,
         blank=True,
     )
     position = models.PositiveIntegerField(
         verbose_name=_("position"),
-        help_text=_("position of this meeting in the playlist"),
+        help_text=_("position of this classroom in the playlist"),
         default=0,
     )
 
     meeting_id = models.UUIDField(
-        verbose_name=_("meeting id"),
-        help_text=_("BBB id for the meeting as UUID"),
+        verbose_name=_("BBB meeting id"),
+        help_text=_("BBB meeting id as UUID"),
         default=uuid.uuid4,
         editable=False,
     )
@@ -108,40 +108,40 @@ class Meeting(BaseModel):
     starting_at = models.DateTimeField(
         blank=True,
         verbose_name=_("starting at date and time"),
-        help_text=_("Start date and time at which a meeting is scheduled."),
+        help_text=_("Start date and time at which a classroom is scheduled."),
         null=True,
     )
     estimated_duration = models.DurationField(
         blank=True,
         null=True,
         verbose_name=_("estimated duration"),
-        help_text=_("Estimated duration of the meeting in seconds."),
+        help_text=_("Estimated duration of the classroom in seconds."),
     )
 
     class Meta:
-        """Options for the ``Meeting`` model."""
+        """Options for the ``Classroom`` model."""
 
-        db_table = "meeting"
-        verbose_name = _("meeting")
-        verbose_name_plural = _("meetings")
+        db_table = "classroom"
+        verbose_name = _("classroom")
+        verbose_name_plural = _("classrooms")
         constraints = [
             models.UniqueConstraint(
                 fields=["lti_id", "playlist"],
                 condition=models.Q(deleted=None),
-                name="meeting_unique_idx",
+                name="classroom_unique_idx",
             )
         ]
 
     @property
     def slideshows(self):
-        """Get slideshows for current meeting."""
+        """Get slideshows for current classroom."""
         return []
 
     @staticmethod
     def get_ready_clause():
-        """Clause used in lti.utils.get_or_create_resource to filter the meetings.
+        """Clause used in lti.utils.get_or_create_resource to filter the classrooms.
 
-        Only show meetings that have successfully been created.
+        Only show classrooms that have successfully been created.
 
         Returns
         -------
@@ -157,7 +157,3 @@ class Meeting(BaseModel):
         if self.deleted:
             result = _("{:s} [deleted]").format(result)
         return result
-
-
-Classroom = Meeting
-Classroom.RESOURCE_NAME = "classrooms"
