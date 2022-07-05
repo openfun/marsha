913de39a97c5db63ec55b8acd85c3dc42f4f1d0c
carofun <caroline.cramoisan@fun-mooc.fr>
2022-07-05 15:33:53 +0200

    🗃️(marsha) migration live_attendance's key are timestamps
    Previous commit 67f8233b transformed milliseconds keys to
    keys in seconds. For data existing in the database, a migration
    is needed to do this conversion.

---
+++ src/backend/marsha/core/migrations/0052_migrate_livesession_liveattendance.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from django.db import migrations
+
+
+def migrate_live_attendance(apps, schema_editor):
+    """Update existing records to set live_attendance with timestamps
+    keys."""
+    try:
+        livesessions = apps.get_model("core", "LiveSession")
+    except LookupError:
+        return
+    livesessions_to_migrate = livesessions.objects.filter(live_attendance__isnull=False)
+    for livesession in livesessions_to_migrate.iterator():
+        for key in list(livesession.live_attendance):
+            try:
+                datetime.fromtimestamp(int(key))
+            except ValueError as error:
+                try:
+                    new_key = int(key) // 1000
+                    datetime.fromtimestamp(new_key)
+                    livesession.live_attendance[
+                        str(new_key)
+                    ] = livesession.live_attendance.pop(key)
+                except ValueError as error:  # shouldn't happen
+                    livesession.live_attendance.pop(key)
+
+        livesession.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0051_migrate_live_state"),
+    ]
+
+    operations = [migrations.RunPython(migrate_live_attendance)]
