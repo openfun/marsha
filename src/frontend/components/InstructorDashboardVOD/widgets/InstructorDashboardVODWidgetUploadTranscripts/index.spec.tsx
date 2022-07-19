52e47d089042cca21a536c7093083e2fa50e40e9
roro-lv <ronan.le-viennesse@polyconseil.fr>
2022-07-19 18:08:46 +0200

    ✨(front) add 3 widgets to upload timed text tracks
    Using the UploadWidgetGeneric, 3 widgets are added to the instructor dashboard
    VOD, in order to upload subtitles, transcripts and closed captions to the video.

---
+++ src/frontend/components/InstructorDashboardVOD/widgets/InstructorDashboardVODWidgetUploadTranscripts/index.spec.tsx
@@ -0,0 +1,56 @@
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import { useInfoWidgetModal } from 'data/stores/useInfoWidgetModal';
+import { useJwt } from 'data/stores/useJwt';
+import { useTimedTextTrackLanguageChoices } from 'data/stores/useTimedTextTrackLanguageChoices';
+import render from 'utils/tests/render';
+import { InstructorDashboardVODWidgetUploadTranscripts } from '.';
+
+jest.mock('data/stores/useInfoWidgetModal', () => ({
+  useInfoWidgetModal: jest.fn(),
+}));
+
+const mockUseInfoWidgetModal = useInfoWidgetModal as jest.MockedFunction<
+  typeof useInfoWidgetModal
+>;
+
+const languageChoices = [
+  { label: 'English', value: 'en' },
+  { label: 'French', value: 'fr' },
+];
+
+describe('<DashboardVODWidgetUploadTranscripts />', () => {
+  beforeEach(() => {
+    useJwt.setState({
+      jwt: 'some token',
+      getDecodedJwt: () => ({ locale: 'fr_FR' } as any),
+    });
+  });
+
+  it('renders the DashboardVODWidgetUploadTranscripts', () => {
+    const mockSetInfoWidgetModalProvider = jest.fn();
+    mockUseInfoWidgetModal.mockReturnValue([
+      null,
+      mockSetInfoWidgetModalProvider,
+    ]);
+
+    useTimedTextTrackLanguageChoices.setState({
+      choices: languageChoices,
+    });
+
+    render(<InstructorDashboardVODWidgetUploadTranscripts />);
+
+    screen.getByText('Transcripts');
+
+    const infoButton = screen.getByRole('button', { name: 'help' });
+    act(() => userEvent.click(infoButton));
+
+    expect(mockSetInfoWidgetModalProvider).toHaveBeenCalledTimes(1);
+    expect(mockSetInfoWidgetModalProvider).toHaveBeenLastCalledWith({
+      title: 'Transcripts',
+      text: 'This widget allows you upload transcripts for the video.',
+    });
+  });
+});
