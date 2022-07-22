984d1cf64ba1befdf6cd0e6d838dbc9300228812
vassili <vassili.rezvoy@polyconseil.fr>
2022-07-22 14:27:20 +0200

    ✨(frontend) create stores to substitute appData global variable
    Currently, appData are loaded from a dom node, parsed and set in an exposed
    global variable.
    We would rather use contexts and hooks to expose those data.

---
+++ src/frontend/data/stores/useSentry/index.ts
@@ -0,0 +1,12 @@
+import create from 'zustand';
+
+interface SentryStore {
+  isSentryReady: boolean;
+  setIsSentryReady: (config: boolean) => void;
+}
+
+export const useSentry = create<SentryStore>((set) => ({
+  isSentryReady: false,
+  setIsSentryReady: (config: boolean) =>
+    set((state) => ({ ...state, isSentryReady: config })),
+}));
