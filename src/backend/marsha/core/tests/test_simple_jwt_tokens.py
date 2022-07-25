4321e60f0db455bb27e104289e48e589eb87d716
Quentin BEY <quentin.bey@polyconseil.fr>
2022-07-25 18:22:00 +0200

    ✅(backend) add tests for JWT classes
    This adds test for the various JWT possible creations.

---
+++ src/backend/marsha/core/tests/test_simple_jwt_tokens.py
@@ -0,0 +1,289 @@
+"""Test Marsha JWTs"""
+import uuid
+
+from django.test import RequestFactory, TestCase
+
+from rest_framework_simplejwt.exceptions import TokenError
+
+from marsha.core.factories import LiveSessionFactory, UserFactory, VideoFactory
+from marsha.core.lti import LTI
+from marsha.core.models import INSTRUCTOR, NONE, STUDENT
+from marsha.core.simple_jwt.tokens import (
+    LTISelectFormAccessToken,
+    ResourceAccessToken,
+    UserAccessToken,
+)
+from marsha.core.tests.utils import generate_passport_and_signed_lti_parameters
+
+
+class LTISelectFormAccessTokenTestCase(TestCase):
+    """Test suite for the LTISelectFormAccessToken"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Provides test case with some common `lti_select_form_data`"""
+        super().setUpClass()
+
+        # `lti_select_form_data` should be a `self.request.POST` which behaves like a dict
+        cls.lti_select_form_data = {"lti_message_type": "ContentItemSelection"}
+
+    def test_for_lti_select_form_data(self):
+        """Test JWT initialization from `for_lti_select_form_data` method"""
+        # The `lti_select_form_data` content is not tested here
+        token = LTISelectFormAccessToken.for_lti_select_form_data(
+            self.lti_select_form_data
+        )
+
+        # Assert the payload contains the expected value
+        self.assertDictEqual(
+            token.payload["lti_select_form_data"], self.lti_select_form_data
+        )
+
+    def test_verify(self):
+        """Test JWT `verify` method"""
+
+        # The `lti_select_form_data` content is not tested here
+        token = LTISelectFormAccessToken.for_lti_select_form_data(
+            self.lti_select_form_data
+        )
+
+        token.verify()  # Should not raise
+
+        handmade_token = LTISelectFormAccessToken()  # new empty token
+        with self.assertRaises(TokenError):
+            handmade_token.verify()
+
+        handmade_token.payload["lti_select_form_data"] = self.lti_select_form_data
+        handmade_token.verify()  # Should not raise
+
+
+class ResourceAccessTokenTestCase(TestCase):
+    """Test suite for the ResourceAccessToken"""
+
+    def make_lti_instance(self, resource_id=None):
+        """Helper to init some LTI context."""
+        url = f"http://testserver/lti/videos/{resource_id or uuid.uuid4()}"
+        lti_parameters, passport = generate_passport_and_signed_lti_parameters(
+            url=url,
+            lti_parameters={
+                "resource_link_id": "df7",
+                "context_id": "course-v1:ufr+mathematics+0001",
+                "roles": INSTRUCTOR,
+                "user_id": "56255f3807599c377bf0e5bf072359fd",
+                "lis_person_sourcedid": "jane_doe",
+                "lis_person_contact_email_primary": "jane@test-mooc.fr",
+            },
+        )
+
+        request = RequestFactory().post(
+            url, lti_parameters, HTTP_REFERER="https://testserver"
+        )
+        lti = LTI(request, resource_id)
+        self.assertTrue(lti.verify())
+
+        return lti, passport
+
+    def test_for_resource_id(self):
+        """Test JWT initialization from `for_resource_id` method without LTI"""
+        permissions = {"can_access_dashboard": False, "can_update": False}
+        session_id = str(uuid.uuid4())
+        resource_id = str(uuid.uuid4())
+
+        token = ResourceAccessToken.for_resource_id(
+            permissions, session_id, resource_id=resource_id
+        )
+
+        self.assertEqual(token.payload["session_id"], session_id)
+        self.assertEqual(token.payload["resource_id"], resource_id)
+        self.assertListEqual(token.payload["roles"], [NONE])
+        self.assertEqual(token.payload["locale"], "en_US")  # settings.REACT_LOCALES[0]
+        self.assertDictEqual(token.payload["permissions"], permissions)
+        self.assertFalse(token.payload["maintenance"])  # settings.MAINTENANCE_MODE
+
+        # Same, with a playlist given
+        playlist_id = str(uuid.uuid4())
+        token_with_playlist = ResourceAccessToken.for_resource_id(
+            permissions,
+            session_id,
+            resource_id=resource_id,
+            playlist_id=playlist_id,
+        )
+        self.assertEqual(token_with_playlist.payload["session_id"], session_id)
+        self.assertEqual(token_with_playlist.payload["resource_id"], resource_id)
+        self.assertListEqual(token_with_playlist.payload["roles"], [NONE])
+        self.assertEqual(token_with_playlist.payload["locale"], "en_US")
+        self.assertDictEqual(token_with_playlist.payload["permissions"], permissions)
+        self.assertFalse(token_with_playlist.payload["maintenance"])
+        self.assertEqual(token_with_playlist.payload["playlist_id"], playlist_id)
+
+    def test_for_resource_id_with_lti(self):
+        """Test JWT initialization from `for_resource_id` method with LTI"""
+        permissions = {"can_access_dashboard": False, "can_update": False}
+        session_id = str(uuid.uuid4())
+        resource_id = str(uuid.uuid4())
+        lti, passport = self.make_lti_instance(resource_id=resource_id)
+
+        token = ResourceAccessToken.for_resource_id(permissions, session_id, lti=lti)
+
+        self.assertEqual(token.payload["session_id"], session_id)
+        self.assertEqual(token.payload["resource_id"], resource_id)
+        self.assertEqual(token.payload["roles"], [INSTRUCTOR])
+        self.assertEqual(token.payload["locale"], "en_US")
+        self.assertDictEqual(token.payload["permissions"], permissions)
+        self.assertFalse(token.payload["maintenance"])  # settings.MAINTENANCE_MODE
+
+        self.assertEqual(token.payload["context_id"], "course-v1:ufr+mathematics+0001")
+        self.assertEqual(token.payload["consumer_site"], str(passport.consumer_site.pk))
+        self.assertDictEqual(
+            token.payload["user"],
+            {
+                "email": "jane@test-mooc.fr",
+                "id": "56255f3807599c377bf0e5bf072359fd",
+                "user_fullname": None,
+                "username": "jane_doe",
+            },
+        )
+
+        # Same, with a playlist given
+        playlist_id = str(uuid.uuid4())
+        token_with_playlist = ResourceAccessToken.for_resource_id(
+            permissions,
+            session_id,
+            lti=lti,
+            playlist_id=playlist_id,
+        )
+
+        self.assertEqual(token_with_playlist.payload["session_id"], session_id)
+        self.assertEqual(token_with_playlist.payload["resource_id"], resource_id)
+        self.assertEqual(token_with_playlist.payload["roles"], [INSTRUCTOR])
+        self.assertEqual(token_with_playlist.payload["locale"], "en_US")
+        self.assertDictEqual(token_with_playlist.payload["permissions"], permissions)
+        self.assertFalse(
+            token_with_playlist.payload["maintenance"]
+        )  # settings.MAINTENANCE_MODE
+
+        self.assertEqual(
+            token_with_playlist.payload["context_id"],
+            "course-v1:ufr+mathematics+0001",
+        )
+        self.assertEqual(
+            token_with_playlist.payload["consumer_site"],
+            str(passport.consumer_site.pk),
+        )
+        self.assertDictEqual(
+            token_with_playlist.payload["user"],
+            {
+                "email": "jane@test-mooc.fr",
+                "id": "56255f3807599c377bf0e5bf072359fd",
+                "user_fullname": None,
+                "username": "jane_doe",
+            },
+        )
+        self.assertEqual(token_with_playlist.payload["playlist_id"], playlist_id)
+
+    def test_for_live_session_anonymous(self):
+        """Test JWT initialization from `for_live_session` method with public session."""
+        permissions = {"can_access_dashboard": False, "can_update": False}
+        session_id = str(uuid.uuid4())
+        anonymous_id = str(uuid.uuid4())
+        live_session = LiveSessionFactory(
+            anonymous_id=anonymous_id,
+            email="chantal@test-fun-mooc.fr",
+        )
+
+        token = ResourceAccessToken.for_live_session(live_session, session_id)
+
+        self.assertEqual(token.payload["session_id"], session_id)
+        self.assertEqual(token.payload["locale"], "en_US")  # settings.REACT_LOCALES[0]
+        self.assertDictEqual(token.payload["permissions"], permissions)
+        self.assertFalse(token.payload["maintenance"])  # settings.MAINTENANCE_MODE
+
+        self.assertEqual(token.payload["resource_id"], str(live_session.video.pk))
+        self.assertDictEqual(
+            token.payload["user"],
+            {
+                "email": "chantal@test-fun-mooc.fr",
+                "anonymous_id": anonymous_id,
+            },
+        )
+        self.assertListEqual(token.payload["roles"], [NONE])
+
+    def test_for_live_session_lti(self):
+        """Test JWT initialization from `for_live_session` method with LTI session."""
+        permissions = {"can_access_dashboard": False, "can_update": False}
+        session_id = str(uuid.uuid4())
+        video = VideoFactory()
+        live_session = LiveSessionFactory(
+            consumer_site=video.playlist.consumer_site,
+            email="chantal@test-fun-mooc.fr",
+            lti_id="Maths",
+            lti_user_id="56255f3807599c377bf0e5bf072359fd",
+            username="chantal",
+            video=video,
+        )
+
+        self.assertTrue(live_session.is_from_lti_connection)
+
+        token = ResourceAccessToken.for_live_session(live_session, session_id)
+
+        self.assertEqual(token.payload["session_id"], session_id)
+        self.assertEqual(token.payload["locale"], "en_US")  # settings.REACT_LOCALES[0]
+        self.assertDictEqual(token.payload["permissions"], permissions)
+        self.assertFalse(token.payload["maintenance"])  # settings.MAINTENANCE_MODE
+
+        self.assertEqual(
+            token.payload["consumer_site"], str(video.playlist.consumer_site.pk)
+        )
+        self.assertEqual(token.payload["context_id"], str(video.playlist.lti_id))
+        self.assertEqual(token.payload["resource_id"], str(video.pk))
+        self.assertDictEqual(
+            token.payload["user"],
+            {
+                "email": "chantal@test-fun-mooc.fr",
+                "id": "56255f3807599c377bf0e5bf072359fd",
+                "username": "chantal",
+            },
+        )
+        self.assertListEqual(token.payload["roles"], [STUDENT])
+
+
+class UserAccessTokenTestCase(TestCase):
+    """Test suite for the ResourceAccessToken"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Provides test case with some common `user` data"""
+        super().setUpClass()
+
+        cls.user = UserFactory()
+
+    def test_for_user(self):
+        """Test JWT initialization from `for_user` method"""
+        token = UserAccessToken.for_user(self.user)
+        self.assertEqual(token.payload["resource_id"], str(self.user.pk))
+        self.assertDictEqual(token.payload["user"], {"id": str(self.user.pk)})
+
+    def test_verify(self):
+        """Test JWT `verify` method"""
+
+        token = UserAccessToken.for_user(self.user)
+        token.verify()  # Should not raise
+
+        handmade_token = UserAccessToken()  # new empty token
+        with self.assertRaises(TokenError):
+            # missing in payload: resource_id, user
+            handmade_token.verify()
+
+        handmade_token.payload["resource_id"] = self.user.pk
+
+        with self.assertRaises(TokenError):
+            # missing in payload: user
+            handmade_token.verify()
+
+        handmade_token.payload["user"] = self.user.pk
+        handmade_token.verify()  # Should not raise
+
+        del handmade_token.payload["resource_id"]
+        # missing in payload: resource_id, but not tested in verify
+        # (this is done in the authentication backend for now)
+        handmade_token.verify()
