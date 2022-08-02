e79eb306292b492dff1efe578e9e289e03719c58
Quentin BEY <quentin.bey@polyconseil.fr>
2022-08-02 18:40:13 +0200

    🧑‍💻(backend) add factories for resource JWT
    This provides a set of factories to generate `ResourceAccessToken`
    in tests.

--- src/backend/marsha/core/simple_jwt/factories.py
+++ src/backend/marsha/core/simple_jwt/factories.py
@@ -1,10 +1,16 @@
 """Factories for the Marsha JWTs module"""
 import types
+import uuid
+
+from django.conf import settings
 
 import factory
+from faker.utils.text import slugify
 
-from marsha.core.factories import UserFactory
-from marsha.core.simple_jwt.tokens import UserAccessToken
+from marsha.core.factories import LiveSessionFactory, PlaylistFactory, UserFactory
+from marsha.core.models import ADMINISTRATOR, INSTRUCTOR, NONE, STUDENT
+from marsha.core.simple_jwt.permissions import ResourceAccessPermissions
+from marsha.core.simple_jwt.tokens import ResourceAccessToken, UserAccessToken
 
 
 class BaseTokenFactory(factory.Factory):
@@ -82,3 +88,168 @@ class UserAccessTokenFactory(BaseTokenFactory):
 
     class Meta:  # pylint:disable=missing-class-docstring
         model = UserAccessToken.for_user
+
+
+class ResourcePermissionsFactory(factory.DictFactory):
+    """Factory for resource access permissions."""
+
+    can_access_dashboard = False
+    can_update = False
+
+    class Meta:  # pylint:disable=missing-class-docstring
+        model = ResourceAccessPermissions
+
+    @classmethod
+    def _build(cls, model_class, *args, **kwargs):
+        """Build permissions from the dataclass to enforce consistency."""
+        permissions = super()._build(model_class, *args, **kwargs)
+        return permissions.as_dict()
+
+
+class BaseResourceTokenFactory(BaseTokenFactory):
+    """
+    Base class for all resource token factories.
+    This allows to provide a resource (video, document, ...)
+    to forge the JWT, or nothing to use a random UUID.
+    """
+
+    resource_id = factory.LazyAttribute(
+        lambda o: str(o.resource.id if o.resource else uuid.uuid4())
+    )
+
+    class Meta:  # pylint:disable=missing-class-docstring
+        abstract = True
+
+    class Params:  # pylint:disable=missing-class-docstring
+        resource = None
+
+
+class ResourceAccessTokenFactory(BaseResourceTokenFactory):
+    """Simple resource token. Looks like a public resource token."""
+
+    session_id = factory.Faker("uuid4")
+
+    class Meta:  # pylint:disable=missing-class-docstring
+        model = ResourceAccessToken.for_resource_id
+
+
+class LTIResourceAccessTokenFactory(BaseResourceTokenFactory):
+    """
+    LTI resource forged token.
+
+    This token's payload is forged to remove a heavy call to
+    `generate_passport_and_signed_lti_parameters` each time
+    we want to have an LTI token.
+    """
+
+    # for_resource_id payload
+    session_id = factory.Faker("uuid4")
+    permissions = factory.SubFactory(ResourcePermissionsFactory)
+    roles = factory.fuzzy.FuzzyChoice(
+        [ADMINISTRATOR, INSTRUCTOR, STUDENT, NONE],
+        getter=lambda x: [x],
+    )
+    locale = factory.fuzzy.FuzzyChoice(settings.REACT_LOCALES)
+    maintenance = factory.fuzzy.FuzzyChoice([True, False])
+
+    # for_lti specific parameters
+    context_id = factory.Faker("uuid4")
+    consumer_site = factory.Faker("uuid4")
+
+    # user details
+    user = factory.Dict(
+        {
+            "id": factory.Faker("uuid4"),
+            "user_fullname": factory.Faker("name"),
+            "email": factory.LazyAttribute(lambda o: f"{o.username}@example.org"),
+            "username": factory.LazyAttribute(lambda o: slugify(o.user_fullname)),
+        }
+    )
+
+    class Meta:  # pylint:disable=missing-class-docstring
+        model = ResourceAccessToken
+
+
+class StudentLtiTokenFactory(LTIResourceAccessTokenFactory):
+    """LTI resource forged token for student."""
+
+    roles = [STUDENT]
+
+
+class InstructorOrAdminLtiTokenFactory(LTIResourceAccessTokenFactory):
+    """
+    LTI resource forged token for instructor or administrators.
+    See `marsha.core.views.BaseLTIView`.
+
+    Note: the `can_update` permission is set to True
+    to mean "the LTI request context ID is the resource playlist LTI ID".
+    When this is not the case, the test must explicitly add `permissions__can_update=False`.
+    """
+
+    roles = factory.fuzzy.FuzzyChoice([ADMINISTRATOR, INSTRUCTOR], getter=lambda x: [x])
+    permissions = factory.SubFactory(
+        ResourcePermissionsFactory,
+        can_access_dashboard=True,
+        can_update=True,
+    )
+
+
+class PlaylistLtiTokenFactory(InstructorOrAdminLtiTokenFactory):
+    """
+    LTI resource forged token for instructor or administrators with a playlist access.
+    See `marsha.core.views.LTISelectView`.
+    """
+
+    playlist_id = factory.LazyAttribute(lambda o: str(o.playlist.id))
+    permissions = factory.SubFactory(ResourcePermissionsFactory, can_update=True)
+
+    class Params:  # pylint:disable=missing-class-docstring
+        playlist = factory.SubFactory(PlaylistFactory)
+
+
+class LiveSessionResourceAccessTokenFactory(BaseTokenFactory):
+    """Generates a resource access token from a live session."""
+
+    live_session = factory.SubFactory(LiveSessionFactory)
+    session_id = factory.Faker("uuid4")
+
+    class Meta:  # pylint:disable=missing-class-docstring
+        model = ResourceAccessToken.for_live_session
+
+
+class LiveSessionLtiTokenFactory(LTIResourceAccessTokenFactory):
+    """
+    Prefer the use of LiveSessionResourceAccessTokenFactory,
+    but this one allows to deeply customize the final JWT.
+    """
+
+    resource_id = factory.LazyAttribute(lambda o: str(o.live_session.video.id))
+    roles = factory.fuzzy.FuzzyChoice([STUDENT, NONE], getter=lambda x: [x])
+
+    consumer_site = factory.LazyAttribute(
+        lambda o: str(o.live_session.consumer_site.id)
+    )
+    context_id = factory.LazyAttribute(lambda o: str(o.live_session.lti_id))
+
+    user = factory.Dict(
+        {
+            "id": factory.SelfAttribute("..live_session.lti_user_id"),
+            "username": factory.SelfAttribute("..live_session.username"),
+            "email": factory.SelfAttribute("..live_session.email"),
+        }
+    )
+
+    class Meta:  # pylint:disable=missing-class-docstring
+        model = ResourceAccessToken
+
+    class Params:  # pylint:disable=missing-class-docstring
+        live_session = factory.SubFactory(
+            LiveSessionFactory,
+            is_from_lti_connection=True,
+        )
+        any_role = factory.Trait(
+            roles=factory.fuzzy.FuzzyChoice(
+                [ADMINISTRATOR, INSTRUCTOR, STUDENT, NONE],
+                getter=lambda x: [x],
+            )
+        )
---
+++ src/backend/marsha/core/tests/test_simple_jwt_factories.py
@@ -0,0 +1,72 @@
+"""Tests for Marsha JWT factories"""
+import uuid
+
+from django.test import RequestFactory, TestCase
+
+from marsha.core.factories import LiveSessionFactory
+from marsha.core.lti import LTI
+from marsha.core.models import INSTRUCTOR
+from marsha.core.simple_jwt.factories import (
+    LiveSessionLtiTokenFactory,
+    LTIResourceAccessTokenFactory,
+)
+from marsha.core.simple_jwt.tokens import ResourceAccessToken
+from marsha.core.tests.utils import generate_passport_and_signed_lti_parameters
+
+
+class LTIResourceAccessTokenFactoryTestCase(TestCase):
+    """Test suite for the LTIResourceAccessTokenFactory"""
+
+    def test_payload_keys(self):
+        """Tests the factory generates a payload with the same keys as the original method."""
+        # generate an LTI request
+        resource_id = uuid.uuid4()
+        url = f"http://testserver/lti/videos/{resource_id}"
+        lti_parameters, _passport = generate_passport_and_signed_lti_parameters(
+            url=url,
+            lti_parameters={
+                "resource_link_id": "df7",
+                "context_id": "course-v1:ufr+mathematics+0001",
+                "roles": INSTRUCTOR,
+                "user_id": "56255f3807599c377bf0e5bf072359fd",
+                "lis_person_sourcedid": "jane_doe",
+                "lis_person_contact_email_primary": "jane@test-mooc.fr",
+            },
+        )
+
+        request = RequestFactory().post(
+            url,
+            lti_parameters,
+            HTTP_REFERER="https://testserver",
+        )
+        lti = LTI(request, resource_id)
+        self.assertTrue(lti.verify())
+
+        session_id = uuid.uuid4()
+        jwt = ResourceAccessToken.for_lti(lti, {}, session_id)
+
+        jwt_from_factory = LTIResourceAccessTokenFactory()
+
+        self.assertSetEqual(
+            set(jwt.payload.keys()),
+            set(jwt_from_factory.payload.keys()),
+        )
+
+
+class LiveSessionLtiTokenFactoryTestCase(TestCase):
+    """Test suite for the LiveSessionLtiTokenFactory"""
+
+    def test_payload_keys(self):
+        """Tests the factory generates a payload with the same keys as the original method."""
+        session_id = uuid.uuid4()
+        jwt = ResourceAccessToken.for_live_session(
+            LiveSessionFactory(is_from_lti_connection=True),
+            session_id,
+        )
+
+        jwt_from_factory = LiveSessionLtiTokenFactory()
+
+        self.assertSetEqual(
+            set(jwt.payload.keys()),
+            set(jwt_from_factory.payload.keys()),
+        )
