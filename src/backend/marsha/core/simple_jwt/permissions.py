ae2dd5d19509fe0f5cfbe334f2faae7b4685d77c
Quentin BEY <quentin.bey@polyconseil.fr>
2022-08-02 18:40:13 +0200

    🧑‍💻(backend) add factories for resource JWT
    This provides a set of factories to generate `ResourceAccessToken`
    in tests.

--- src/backend/marsha/core/simple_jwt/permissions.py
+++ src/backend/marsha/core/simple_jwt/permissions.py
@@ -1,5 +1,5 @@
 """Module for the permissions linked to Marsha's JWT"""
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 
 @dataclass
@@ -8,3 +8,10 @@ class ResourceAccessPermissions:
 
     can_access_dashboard: bool = False
     can_update: bool = False
+
+    def as_dict(self):
+        """
+        Return permissions as a dict.
+        This prevents importing `dataclasses.asdict` everywhere.
+        """
+        return asdict(self)
