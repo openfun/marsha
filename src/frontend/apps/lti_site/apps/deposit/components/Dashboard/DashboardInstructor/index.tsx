557489ba4cd2d46de77c9f34b238a1a7a3244a39
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-08-24 11:44:00 +0200

    ✨(frontend) filter instructor file depository by read
    Instructors should be able to filter deposited files by read state.

--- src/frontend/apps/lti_site/apps/deposit/components/Dashboard/DashboardInstructor/index.tsx
+++ src/frontend/apps/lti_site/apps/deposit/components/Dashboard/DashboardInstructor/index.tsx
@@ -1,7 +1,9 @@
-import { Box, Heading, Pagination, Text } from 'grommet';
+import { Box, Heading, Pagination, Select, Text } from 'grommet';
 import React, { useState } from 'react';
 
 import { Loader } from 'components/Loader';
+
+import { Maybe } from 'utils/types';
 
 import { DepositedFileRow } from 'apps/deposit/components/Dashboard/common/DepositedFileRow';
 import { useDepositedFiles } from 'apps/deposit/data/queries';
@@ -18,14 +20,34 @@ export const DashboardInstructor = ({
 }: DashboardInstructorProps) => {
   const [depositedFilesOffset, setDepositedFilesOffset] = useState(0);
   const [indices, setIndices] = useState([0, PAGE_SIZE]);
+  const [readFilter, setReadFilter] = useState<Maybe<string>>(undefined);
 
-  const { data, isError, isLoading } = useDepositedFiles(
+  const { data, isError, isLoading, isSuccess, refetch } = useDepositedFiles(
     fileDepository.id,
-    { limit: PAGE_SIZE, offset: depositedFilesOffset },
+    { limit: PAGE_SIZE, offset: depositedFilesOffset, read: readFilter },
     {
       keepPreviousData: true,
     },
   );
+  const readFilterOptions = [
+    {
+      label: 'All',
+      value: undefined,
+    },
+    {
+      label: 'Unread',
+      value: 'false',
+    },
+    {
+      label: 'Read',
+      value: 'true',
+    },
+  ];
+
+  const onReadFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setReadFilter(event.target.value);
+    refetch();
+  };
 
   const onPageChange = async (event: any) => {
     setDepositedFilesOffset(event.startIndex);
@@ -42,14 +64,30 @@ export const DashboardInstructor = ({
         round="xsmall"
       >
         <Box align="center" direction="row" justify="between">
-          <Text>
-            Showing {indices[0] + 1} - {indices[1]} of {data?.count}
-          </Text>
-          <Pagination
-            step={PAGE_SIZE}
-            numberItems={data?.count}
-            onChange={onPageChange}
-          />
+          {isSuccess && (
+            <React.Fragment>
+              <Select
+                a11yTitle="Filter files"
+                id="readFilterSelect"
+                name="read"
+                placeholder={'Filter'}
+                value={readFilter}
+                options={readFilterOptions}
+                labelKey="label"
+                valueKey={{ key: 'value', reduce: true }}
+                onChange={onReadFilterChange}
+              />
+              <Text>
+                Showing {indices[0] + 1} - {Math.min(indices[1], data!.count)}{' '}
+                of {data!.count}
+              </Text>
+              <Pagination
+                step={PAGE_SIZE}
+                numberItems={data?.count}
+                onChange={onPageChange}
+              />
+            </React.Fragment>
+          )}
         </Box>
       </Box>
       <Box
