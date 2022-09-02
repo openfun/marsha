b9ee2b5e78abac5e3b3338cd8902b4d094059c31
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-09-02 11:36:53 +0200

    🐛(frontend) store deposited file filename and size
    When creating a deposited file, we need to store his filename and size.

--- src/frontend/apps/lti_site/apps/deposit/data/sideEffects/createDepositedFile/index.ts
+++ src/frontend/apps/lti_site/apps/deposit/data/sideEffects/createDepositedFile/index.ts
@@ -3,13 +3,17 @@ import { API_ENDPOINT } from 'settings';
 
 import { DepositedFile, modelName } from 'apps/deposit/types/models';
 
-export const createDepositedFile = async () => {
+export const createDepositedFile = async (file: {
+  size: number;
+  filename: string;
+}) => {
   const response = await fetch(`${API_ENDPOINT}/${modelName.DepositedFiles}/`, {
     headers: {
       Authorization: `Bearer ${useJwt.getState().jwt}`,
       'Content-Type': 'application/json',
     },
     method: 'POST',
+    body: JSON.stringify(file),
   });
 
   if (!response.ok) {
