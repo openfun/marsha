e74b04d0b18f2bec72d54cd868e7f44988630b42
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-09-21 14:34:08 +0200

    ✨(aws) copy classroom document to destination
    When an instructor uploads a classroom document, we need to copy it to
    the destination bucket.

---
+++ src/aws/lambda-convert/src/copyClassroomDocument.spec.js
@@ -0,0 +1,38 @@
+process.env.S3_DESTINATION_BUCKET = 'destination_bucket';
+
+// Don't pollute tests with logs intended for CloudWatch
+jest.spyOn(console, 'log');
+
+// Mock the AWS SDK calls used in encodeTimedTextTrack
+const mockCopyObject = jest.fn();
+jest.mock('aws-sdk', () => ({
+  S3: function () {
+    this.copyObject = mockCopyObject;
+  },
+}));
+
+const copyClassroomDocument = require('./copyClassroomDocument');
+
+describe('lambda-encore/src/copyClassroomDocument', () => {
+  beforeEach(() => {
+    console.log.mockReset();
+    jest.resetAllMocks();
+  });
+
+  it('copy a classroom document from a source to a destination bucket', async () => {
+    mockCopyObject.mockReturnValue({
+      promise: () => new Promise((resolve) => resolve()),
+    });
+
+    await copyClassroomDocument(
+      'a3e213a7-9c56-4bd3-b71c-fe567b0cfe12/classroomdocument/a3e213a7-9c56-4bd3-b71c-fe567b0cfe12/1606230873.pdf',
+      'source_bucket',
+    );
+
+    expect(mockCopyObject).toHaveBeenCalledWith({
+      Bucket: 'destination_bucket',
+      Key: 'a3e213a7-9c56-4bd3-b71c-fe567b0cfe12/classroomdocument/a3e213a7-9c56-4bd3-b71c-fe567b0cfe12/1606230873.pdf',
+      CopySource: `source_bucket/a3e213a7-9c56-4bd3-b71c-fe567b0cfe12/classroomdocument/a3e213a7-9c56-4bd3-b71c-fe567b0cfe12/1606230873.pdf`,
+    });
+  });
+});
