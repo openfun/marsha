d6630724283768927fd64cb81775903f4da5e95d
ALC Consulting <freelance.anthony@gmail.com>
2022-09-27 17:51:33 +0200

    ✨(frontend) integrate router link with menu feature
    Integrate the router link with the menu feature,
    when the user clicks on the menu item,
    the page will be redirected to the corresponding page.

--- src/frontend/apps/standalone_site/src/App.spec.tsx
+++ src/frontend/apps/standalone_site/src/App.spec.tsx
@@ -13,24 +13,31 @@ jest.mock('react-router-dom', () => ({
   useLocation: () => mockedLocation,
 }));
 
-test('renders learn react link', async () => {
-  fetchMock.post('/api/auth/challenge/', { access: 'some-access' });
-  fetchMock.get('/api/users/whoami/', {
-    date_joined: 'date_joined',
-    email: 'email',
-    first_name: 'first_name',
-    id: 'id',
-    is_staff: false,
-    is_superuser: false,
-    last_name: 'last_name',
-    organization_accesses: [],
+fetchMock.post('/api/auth/challenge/', { access: 'some-access' });
+fetchMock.get('/api/users/whoami/', {
+  date_joined: 'date_joined',
+  email: 'email',
+  first_name: 'first_name',
+  id: 'id',
+  is_staff: false,
+  is_superuser: false,
+  last_name: 'last_name',
+  organization_accesses: [],
+});
+
+describe('<App />', () => {
+  afterEach(() => {
+    fetchMock.restore();
+    jest.resetAllMocks();
   });
 
-  render(<App />);
+  test('renders <App />', async () => {
+    render(<App />);
 
-  expect(
-    await screen.findByRole(/menuitem/i, { name: /Favorites/i }),
-  ).toBeInTheDocument();
-  expect(screen.getByText(/John Doe/i)).toBeInTheDocument();
-  expect(screen.getByText(/Accueil/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Accueil/i)).toBeInTheDocument();
+    expect(screen.getByText(/John Doe/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole(/menuitem/i, { name: /Favorites/i }),
+    ).toBeInTheDocument();
+  });
 });
