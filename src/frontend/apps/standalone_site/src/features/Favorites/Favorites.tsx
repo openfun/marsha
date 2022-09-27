f22d6760008b13b24ed87a111c61fe63e011b217
Anthony LC <anthony.le-courric@polyconseil.fr>
2022-09-27 17:50:59 +0200

    ✨(frontend) integrate browser router to our app
    Integrate the router with the application, this will allow us
    to navigate between screens

---
+++ src/frontend/apps/standalone_site/src/features/Favorites/Favorites.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from 'grommet';
+import React from 'react';
+
+function Favorites() {
+  return (
+    <Box>
+      <Text>My favorites</Text>
+    </Box>
+  );
+}
+
+export default Favorites;
