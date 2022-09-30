48176af0683880feb61b96d7cdf6425f15dcee2b
Quentin BEY <quentin.bey@polyconseil.fr>
2022-09-30 17:03:36 +0200

    WIP: try to run terraform with moto
    This was an attempt to run:
     - moto `docker run --rm  --name local-aws -p 5000:5000 motoserver/moto:latest`
     - make init
     - make apply
    
    but the last one fails with
    ```
    aws_vpc.fargate_ffmpeg_transmux_service_vpc: Creating...
    ╷
    │ Error: error creating EC2 VPC: VpcLimitExceeded: The maximum number of VPCs has been reached.
    │ 	status code: 400, request id: bb317b26-2adb-4fb5-b843-50d37e0cf468
    │
    │   with aws_vpc.fargate_ffmpeg_transmux_service_vpc,
    │   on vpc.tf line 3, in resource "aws_vpc" "fargate_ffmpeg_transmux_service_vpc":
    │    3: resource "aws_vpc" "fargate_ffmpeg_transmux_service_vpc" {
    │
    ╵
    ```
    and I didn't found why.

--- src/aws/shared_resources/provider.tf
+++ src/aws/shared_resources/provider.tf
@@ -1,3 +1,25 @@
 provider "aws" {
-  version = "~> 3.25"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    cloudwatch = "http://10.1.0.1:5000"
+    cloudfront = "http://10.1.0.1:5000"
+    ec2 = "http://10.1.0.1:5000"
+    ecr = "http://10.1.0.1:5000"
+    ecs = "http://10.1.0.1:5000"
+    eks = "http://10.1.0.1:5000"
+    iam = "http://10.1.0.1:5000"
+    lambda = "http://10.1.0.1:5000"
+    medialive = "http://10.1.0.1:5000"
+    mediapackage = "http://10.1.0.1:5000"
+    mediaconvert = "http://10.1.0.1:5000"
+    s3  = "http://10.1.0.1:5000"
+    route53 = "http://10.1.0.1:5000"
+  }
 }
