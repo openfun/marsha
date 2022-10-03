90832063dd956cd0cd2582e943e6314bc77e7848
Quentin BEY <quentin.bey@polyconseil.fr>
2022-10-03 13:37:05 +0200

    ♻️(frontend) use redirect on load for cookiecutter
    This adds different check on the root route to behave more like the
    `LTISite` and allow to easily add checks on the `app_data.state`.

---
+++ src/frontend/apps/lti_site/apps/.cookiecutter/{{cookiecutter.app_name}}/components/Dashboard{{cookiecutter.model}}/route.ts
@@ -0,0 +1,4 @@
+/**
+ * Route for the `<Dashboard{{cookiecutter.model}} />` component.
+ */
+export const DASHBOARD_{{ cookiecutter.app_name|upper }}_ROUTE = () => '/dashboard/{{cookiecutter.model}}';
