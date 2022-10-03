0cb1e877fdd019813c2b3917c543a0aecdbdd17d
Quentin BEY <quentin.bey@polyconseil.fr>
2022-10-03 13:37:05 +0200

    ♻️(frontend) use redirect on load for cookiecutter
    This adds different check on the root route to behave more like the
    `LTISite` and allow to easily add checks on the `app_data.state`.

---
+++ src/frontend/apps/lti_site/apps/.cookiecutter/{{cookiecutter.app_name}}/components/RedirectOnLoad/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import { FULL_SCREEN_ERROR_ROUTE } from 'components/ErrorComponents/route';
+import { useIsFeatureEnabled } from 'data/hooks/useIsFeatureEnabled';
+import { useAppConfig } from 'data/stores/useAppConfig';
+
+import { appState, flags } from 'types/AppData';
+
+import { DASHBOARD_{{ cookiecutter.app_name|upper }}_ROUTE } from '../Dashboard{{cookiecutter.model}}/route';
+
+// RedirectOnLoad assesses the initial state of the application using appData and determines the proper
+// route to load in the Router
+export const RedirectOnLoad = () => {
+  const appData = useAppConfig();
+  const isFeatureEnabled = useIsFeatureEnabled();
+
+  // Get LTI errors out of the way
+  if (appData.state === appState.ERROR) {
+    return <Redirect push to={FULL_SCREEN_ERROR_ROUTE('lti')} />;
+  }
+
+  if (!isFeatureEnabled(flags.{{ cookiecutter.flag }})) {
+    return <Redirect push to={FULL_SCREEN_ERROR_ROUTE('notFound')} />;
+  }
+
+  return <Redirect push to={DASHBOARD_{{ cookiecutter.app_name|upper }}_ROUTE()} />;
+};
