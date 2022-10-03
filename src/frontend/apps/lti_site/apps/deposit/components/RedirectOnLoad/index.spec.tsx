5840020e0b67453d1edca61a73e087f9639b7739
Quentin BEY <quentin.bey@polyconseil.fr>
2022-10-03 13:36:09 +0200

    ♻️(frontend) use redirect on load for deposit
    This adds different check on the root route to behave more like the
    `LTISite` and allow to easily add checks on the `app_data.state`.

---
+++ src/frontend/apps/lti_site/apps/deposit/components/RedirectOnLoad/index.spec.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import { FULL_SCREEN_ERROR_ROUTE } from 'components/ErrorComponents/route';
+import { useAppConfig } from 'data/stores/useAppConfig';
+import { appState } from 'types/AppData';
+import render from 'utils/tests/render';
+
+import { RedirectOnLoad } from '.';
+import { DASHBOARD_ROUTE } from '../Dashboard/route';
+
+jest.mock('data/stores/useAppConfig', () => ({
+  useAppConfig: jest.fn(),
+}));
+const mockedUseAppConfig = useAppConfig as jest.MockedFunction<
+  typeof useAppConfig
+>;
+
+describe('<RedirectOnLoad />', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('redirects users to the error view on LTI error', () => {
+    mockedUseAppConfig.mockReturnValue({
+      state: appState.ERROR,
+      flags: { deposit: true },
+    } as any);
+
+    const { getByText } = render(<RedirectOnLoad />, {
+      routerOptions: {
+        routes: [
+          {
+            path: FULL_SCREEN_ERROR_ROUTE(),
+            render: ({ match }) => (
+              <span>{`Error Component: ${match.params.code}`}</span>
+            ),
+          },
+        ],
+      },
+    });
+
+    getByText('Error Component: lti');
+  });
+
+  it('shows not found error when feature is disabled', () => {
+    mockedUseAppConfig.mockReturnValue({
+      flags: { deposit: false },
+    } as any);
+
+    const { getByText } = render(<RedirectOnLoad />, {
+      routerOptions: {
+        routes: [
+          {
+            path: FULL_SCREEN_ERROR_ROUTE(),
+            render: ({ match }) => (
+              <span>{`Error Component: ${match.params.code}`}</span>
+            ),
+          },
+        ],
+      },
+    });
+
+    getByText('Error Component: notFound');
+  });
+
+  it('shows dashboard', async () => {
+    mockedUseAppConfig.mockReturnValue({
+      flags: { deposit: true },
+    } as any);
+
+    const { getByText } = render(<RedirectOnLoad />, {
+      routerOptions: {
+        routes: [
+          {
+            path: DASHBOARD_ROUTE(),
+            render: () => <span>Dashboard</span>,
+          },
+        ],
+      },
+    });
+
+    getByText('Dashboard');
+  });
+});
