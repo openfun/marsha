bc266b6785c2728ca9ab2f655af5b8d7162a1e1c
Quentin BEY <quentin.bey@polyconseil.fr>
2022-10-03 13:33:14 +0200

    ♻️(frontend) use redirect on load for bbb
    This adds different check on the root route to behave more like the
    `LTISite` and allow to easily add checks on the `app_data.state`.

--- src/frontend/apps/lti_site/apps/bbb/components/Routes.tsx
+++ src/frontend/apps/lti_site/apps/bbb/components/Routes.tsx
@@ -1,12 +1,17 @@
 import React, { lazy, Suspense } from 'react';
-import { MemoryRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 
+import {
+  ErrorComponentsProps,
+  FullScreenError,
+} from 'components/ErrorComponents';
 import { FULL_SCREEN_ERROR_ROUTE } from 'components/ErrorComponents/route';
 import { Loader } from 'lib-components';
-import { useIsFeatureEnabled } from 'data/hooks/useIsFeatureEnabled';
-import { flags } from 'types/AppData';
 
 import { bbbAppData } from 'apps/bbb/data/bbbAppData';
+import { DASHBOARD_CLASSROOM_ROUTE } from './DashboardClassroom/route';
+import { REDIRECT_ON_LOAD_ROUTE } from './RedirectOnLoad/route';
+import { RedirectOnLoad } from './RedirectOnLoad';
 
 const DashboardClassroom = lazy(() => import('./DashboardClassroom'));
 
@@ -17,25 +22,31 @@ const Wrappers = ({ children }: React.PropsWithChildren<{}>) => (
 );
 
 const Routes = () => {
-  const isFeatureEnabled = useIsFeatureEnabled();
+  return (
+    <Wrappers>
+      <Suspense fallback={<Loader />}>
+        <MemoryRouter>
+          <Route
+            exact
+            path={DASHBOARD_CLASSROOM_ROUTE()}
+            render={() => <DashboardClassroom />}
+          />
 
-  if (isFeatureEnabled(flags.BBB)) {
-    return (
-      <Wrappers>
-        <Suspense fallback={<Loader />}>
-          <Switch>
-            <Route exact path="/" render={() => <DashboardClassroom />} />
-          </Switch>
-        </Suspense>
-      </Wrappers>
-    );
-  } else {
-    return (
-      <Wrappers>
-        <Redirect push to={FULL_SCREEN_ERROR_ROUTE('notFound')} />
-      </Wrappers>
-    );
-  }
+          <Route
+            exact
+            path={FULL_SCREEN_ERROR_ROUTE()}
+            render={({ match }) => (
+              <FullScreenError
+                code={match.params.code as ErrorComponentsProps['code']}
+              />
+            )}
+          />
+
+          <Route path={REDIRECT_ON_LOAD_ROUTE()} component={RedirectOnLoad} />
+        </MemoryRouter>
+      </Suspense>
+    </Wrappers>
+  );
 };
 
 export default Routes;
