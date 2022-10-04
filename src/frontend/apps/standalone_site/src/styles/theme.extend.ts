441ea0073eaa05f80b75e3b5c1866d3d7909a329
vassili <vassili.rezvoy@polyconseil.fr>
2022-10-04 12:43:17 +0200

    ✨(frontend) create a new component for white cards
    Create a component for white cards with rounded corner and elevation.

--- src/frontend/apps/standalone_site/src/styles/theme.extend.ts
+++ src/frontend/apps/standalone_site/src/styles/theme.extend.ts
@@ -3,12 +3,24 @@ import { deepMerge } from 'grommet/utils';
 import { theme } from 'lib-common';
 
 export const themeExtend: ThemeType = deepMerge(theme, {
+  box: {
+    extend: '',
+  },
   global: {
     colors: {
+      'content-background': '#E5EEFA',
       text: {
         light: 'blue-active',
       },
       'bg-menu-hover': '#CDDEF5',
+    },
+    elevation: {
+      light: {
+        even: '0px 0px 6px 0px rgba(2, 117, 180, 0.3)',
+      },
+      dark: {
+        even: '0px 0px 6px 0px rgba(2, 117, 180, 0.3)',
+      },
     },
   },
 });
