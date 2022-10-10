bfc18462394f0d722456f906619cd9327f063957
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-10-10 13:40:40 +0200

    ✨(backend) allow site users to get file depository update api
    We want the site users to update their file depository.

--- src/backend/marsha/deposit/tests/api/filedepositories/test_update.py
+++ src/backend/marsha/deposit/tests/api/filedepositories/test_update.py
@@ -4,9 +4,16 @@ import json
 from django.test import TestCase, override_settings
 
 from marsha.core import factories as core_factories
+from marsha.core.factories import (
+    OrganizationAccessFactory,
+    PlaylistAccessFactory,
+    PlaylistFactory,
+)
+from marsha.core.models import ADMINISTRATOR
 from marsha.core.simple_jwt.factories import (
     InstructorOrAdminLtiTokenFactory,
     StudentLtiTokenFactory,
+    UserAccessTokenFactory,
 )
 from marsha.core.tests.utils import reload_urlconf
 from marsha.deposit.factories import FileDepositoryFactory
@@ -80,6 +87,61 @@ class FileDepositoryUpdateAPITest(TestCase):
         """An instructor should be able to update a file_depository."""
         file_depository = FileDepositoryFactory()
         jwt_token = InstructorOrAdminLtiTokenFactory(resource=file_depository)
+        data = {"title": "new title", "description": "Hello"}
+
+        response = self.client.patch(
+            f"/api/filedepositories/{file_depository.id!s}/",
+            data,
+            HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+        file_depository.refresh_from_db()
+        self.assertEqual("new title", file_depository.title)
+        self.assertEqual("Hello", file_depository.description)
+
+    def test_api_file_depository_update_user_access_token(self):
+        """A user with UserAccessToken user should not be able to update a file_depository."""
+        organization_access = OrganizationAccessFactory()
+        playlist = PlaylistFactory(organization=organization_access.organization)
+        file_depository = FileDepositoryFactory(playlist=playlist)
+        jwt_token = UserAccessTokenFactory(user=organization_access.user)
+        data = {"title": "new title"}
+
+        response = self.client.patch(
+            f"/api/filedepositories/{file_depository.id!s}/",
+            json.dumps(data),
+            HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_api_file_depository_update_user_access_token_organization_admin(self):
+        """An organization administrator should be able to update a file depository."""
+        organization_access = OrganizationAccessFactory(role=ADMINISTRATOR)
+        playlist = PlaylistFactory(organization=organization_access.organization)
+        file_depository = FileDepositoryFactory(playlist=playlist)
+        jwt_token = UserAccessTokenFactory(user=organization_access.user)
+        data = {"title": "new title", "description": "Hello"}
+
+        response = self.client.patch(
+            f"/api/filedepositories/{file_depository.id!s}/",
+            data,
+            HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+        file_depository.refresh_from_db()
+        self.assertEqual("new title", file_depository.title)
+        self.assertEqual("Hello", file_depository.description)
+
+    def test_api_file_depository_update_user_access_token_playlist_admin(self):
+        """A playlist administrator should be able to update a file depository."""
+        playlist_access = PlaylistAccessFactory(role=ADMINISTRATOR)
+        file_depository = FileDepositoryFactory(playlist=playlist_access.playlist)
+        jwt_token = UserAccessTokenFactory(user=playlist_access.user)
         data = {"title": "new title", "description": "Hello"}
 
         response = self.client.patch(
