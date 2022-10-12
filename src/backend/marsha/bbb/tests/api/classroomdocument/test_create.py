9b5c6c3fdc3c730906dfd8a2bb90bcdf416d3359
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-10-12 17:57:50 +0200

    ✨(backend) allow site users classroom documents api calls
    We want the site users to manage classroom documents through API.

--- src/backend/marsha/bbb/tests/api/classroomdocument/test_create.py
+++ src/backend/marsha/bbb/tests/api/classroomdocument/test_create.py
@@ -5,9 +5,16 @@ from django.test import TestCase, override_settings
 
 from marsha.bbb.factories import ClassroomDocumentFactory, ClassroomFactory
 from marsha.bbb.models import ClassroomDocument
+from marsha.core.factories import (
+    OrganizationAccessFactory,
+    PlaylistAccessFactory,
+    PlaylistFactory,
+)
+from marsha.core.models import ADMINISTRATOR
 from marsha.core.simple_jwt.factories import (
     InstructorOrAdminLtiTokenFactory,
     StudentLtiTokenFactory,
+    UserAccessTokenFactory,
 )
 from marsha.core.tests.utils import reload_urlconf
 
@@ -129,3 +136,105 @@ class ClassroomDocumentCreateAPITest(TestCase):
         )
         first_document.refresh_from_db()
         self.assertTrue(first_document.is_default)
+
+    def test_api_classroom_document_create_user_access_token(self):
+        """
+        A user with UserAccessToken should not be able to create a document
+        for an existing classroom.
+        """
+        organization_access = OrganizationAccessFactory()
+        playlist = PlaylistFactory(organization=organization_access.organization)
+        classroom = ClassroomFactory(playlist=playlist)
+        jwt_token = UserAccessTokenFactory(user=organization_access.user)
+
+        response = self.client.post(
+            "/api/classroomdocuments/",
+            HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
+            content_type="application/json",
+            data=json.dumps(
+                {
+                    "filename": "test.pdf",
+                }
+            ),
+        )
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(ClassroomDocument.objects.count(), 0)
+        self.assertEqual(classroom.classroom_documents.count(), 0)
+
+    def test_api_classroom_document_create_user_access_token_organization_admin(self):
+        """
+        An organization administrator should be able to create a document
+        for an existing classroom.
+
+        First created document should be the default one.
+        """
+        organization_access = OrganizationAccessFactory(role=ADMINISTRATOR)
+        playlist = PlaylistFactory(organization=organization_access.organization)
+        classroom = ClassroomFactory(playlist=playlist)
+        jwt_token = UserAccessTokenFactory(user=organization_access.user)
+
+        response = self.client.post(
+            "/api/classroomdocuments/",
+            HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
+            content_type="application/json",
+            data=json.dumps(
+                {
+                    "filename": "test.pdf",
+                    "classroom": str(classroom.id),
+                }
+            ),
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(ClassroomDocument.objects.count(), 1)
+        self.assertEqual(
+            response.json(),
+            {
+                "classroom": str(classroom.id),
+                "filename": "test.pdf",
+                "id": str(ClassroomDocument.objects.first().id),
+                "is_default": True,
+                "upload_state": "pending",
+                "uploaded_on": None,
+                "url": None,
+            },
+        )
+
+    def test_api_classroom_document_create_user_access_token_playlist_admin(self):
+        """
+        A playlist administrator should be able to create a document
+        for an existing classroom.
+
+        First created document should be the default one.
+        """
+        playlist_access = PlaylistAccessFactory(role=ADMINISTRATOR)
+        classroom = ClassroomFactory(playlist=playlist_access.playlist)
+        jwt_token = UserAccessTokenFactory(user=playlist_access.user)
+
+        response = self.client.post(
+            "/api/classroomdocuments/",
+            HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
+            content_type="application/json",
+            data=json.dumps(
+                {
+                    "filename": "test.pdf",
+                    "classroom": str(classroom.id),
+                }
+            ),
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(ClassroomDocument.objects.count(), 1)
+        self.assertEqual(
+            response.json(),
+            {
+                "classroom": str(classroom.id),
+                "filename": "test.pdf",
+                "id": str(ClassroomDocument.objects.first().id),
+                "is_default": True,
+                "upload_state": "pending",
+                "uploaded_on": None,
+                "url": None,
+            },
+        )
