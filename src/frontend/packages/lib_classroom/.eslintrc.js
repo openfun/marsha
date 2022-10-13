09432392d90bb47a7d0dc475b362eab08157a055
ALC Consulting <freelance.anthony@gmail.com>
2022-10-13 10:10:45 +0200

    📦️(lib_classroom) create package lib_classroom
    Create a new package called lib-classroom based on lib-component.
    This package will contain everything related to the classroom.

---
+++ src/frontend/packages/lib_classroom/.eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true,
+    },
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['import'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    'array-callback-return': 'error',
+    'block-scoped-var': 'error',
+    curly: 2,
+    'default-case': 'error',
+    eqeqeq: 2,
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '+(common|hooks|types|utils)/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
+      },
+    ],
+    'no-alert': 1,
+    'react/prop-types': 0,
+  },
+  ignorePatterns: ['node_modules/'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
