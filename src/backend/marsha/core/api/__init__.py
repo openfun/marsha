04e126d515c49c43752ba34a1e280100665fdc97
Quentin BEY <quentin.bey@polyconseil.fr>
2022-10-13 22:02:48 +0200

    ✨(backend) provide API for LTI user association
    This add a creation endpoint to make possible the association between a
    stand-alone site user and an LTI user.

--- src/backend/marsha/core/api/__init__.py
+++ src/backend/marsha/core/api/__init__.py
@@ -4,6 +4,7 @@ from .account import *  # noqa isort:skip
 from .base import *  # noqa isort:skip
 from .file import *  # noqa isort:skip
 from .live_session import *  # noqa isort:skip
+from .lti_user_association import *  # noqa isort:skip
 from .pairing_challenge import *  # noqa isort:skip
 from .playlist import *  # noqa isort:skip
 from .shared_live_media import *  # noqa isort:skip
