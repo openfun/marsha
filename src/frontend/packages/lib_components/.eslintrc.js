b1bdb388b38c2f60cda66c43a7ca5a1794535b29
Anthony LC <anthony.le-courric@polyconseil.fr>
2022-10-14 14:56:41 +0200

    🔧(lib_components) adapt settings base project
    - Adapt jest settings to find modules in base project
    - Adapt regexp in eslint import plugin

--- src/frontend/packages/lib_components/.eslintrc.js
+++ src/frontend/packages/lib_components/.eslintrc.js
@@ -42,7 +42,7 @@ module.exports = {
         ],
         pathGroups: [
           {
-            pattern: '+(common|hooks|types|utils)/**',
+            pattern: '+(common|hooks|types|utils|data)/**',
             group: 'internal',
           },
         ],
