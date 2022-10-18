511b211d90612cfcffa09f7b458aa91593d53d9a
Manuel Raynaud <manu@raynaud.io>
2022-10-18 09:23:52 +0200

    ✨(front) deploy site app in production
    The site application will be serve by django in production on the same
    domain name. For this, the build must be made in the django static and
    the site view will serve the generate index file modified to replace
    relative path to main js and css file by the django static url. A
    postbuild script is created to copy all needed media static at the root
    of django static directory.

---
+++ src/frontend/apps/standalone_site/postbuild.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DJANGO_STATIC_DIR="${DJANGO_STATIC_DIR:-"../../../backend/marsha/static"}"
+BUILD_DIR="${DJANGO_STATIC_DIR}/js/build/site/"
+
+# copy static files from build to django ones.
+cp -rf "${BUILD_DIR}static/media/" "${DJANGO_STATIC_DIR}"
