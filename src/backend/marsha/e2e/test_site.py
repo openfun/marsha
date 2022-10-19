91d11f28b70a181bcd10c69f646cb23be88cc4fa
Nicolas Clerc <kernicpanel@nclerc.fr>
2022-10-19 12:20:45 +0200

    ✅(e2e) test site login
    We want to ensure that the site build process is working.
    Some starter tests has been added to ease further ones.

---
+++ src/backend/marsha/e2e/test_site.py
@@ -0,0 +1,60 @@
+"""e2e tests for site."""
+from django.test import override_settings
+
+from playwright.sync_api import Page
+import pytest
+from pytest_django.live_server_helper import LiveServer
+from waffle.testutils import override_switch
+
+from marsha.core import factories
+
+
+@pytest.mark.django_db()
+@pytest.fixture(scope="function", autouse=True)
+def site_settings(live_server: LiveServer):
+    """Override frontend settings."""
+    with override_settings(
+        CORS_ALLOWED_ORIGINS=[live_server.url],
+        FRONTEND_HOME_URL=live_server.url,
+    ), override_switch("site", active=True):
+        yield
+
+
+# @pytest.mark.django_db()
+@pytest.fixture(scope="function")
+def user_logged_in(page: Page, live_server: LiveServer):
+    """Create a user and log him in."""
+    user = factories.UserFactory(username="jane")
+    user.set_password("password")
+    page.goto(live_server.url)
+    page.click("text=Log in")
+    page.fill("input[name=username]", "jane")
+    page.fill("input[name=password]", "password")
+    page.click("text=Log in")
+    yield
+
+
+def test_site_login(page: Page, live_server: LiveServer):
+    """Test site login."""
+    user = factories.UserFactory(username="john")
+    user.set_password("password")
+
+    page.goto(live_server.url)
+    page.click("text=Log in")
+    page.fill("input[name=username]", "john")
+    page.fill("input[name=password]", "password")
+    page.click("text=Log in")
+
+    page.wait_for_selector("text=Dashboard")
+    page.wait_for_selector("text=Favorites")
+    page.wait_for_selector("text=My profile")
+
+
+@pytest.mark.usefixtures("user_logged_in")
+def test_site_logged_in(page: Page, live_server: LiveServer):
+    """Test site already logged in."""
+    page.goto(live_server.url)
+
+    page.wait_for_selector("text=Dashboard")
+    page.wait_for_selector("text=Favorites")
+    page.wait_for_selector("text=My profile")
