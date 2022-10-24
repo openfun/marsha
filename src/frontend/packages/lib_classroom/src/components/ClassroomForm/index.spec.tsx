9ef51d516d7476a9673cb05629d95b5f831cefd3
ALC Consulting <freelance.anthony@gmail.com>
2022-10-24 16:41:50 +0200

    ♻️(lib-classroom) refacto DashboardClassroomForm to ClassroomForm
    - Extract the form from DashboardClassroomForm
    - Moved it to lib-classroom
    - Adapt LTI Classroom Apps

---
+++ src/frontend/packages/lib_classroom/src/components/ClassroomForm/index.spec.tsx
@@ -0,0 +1,435 @@
+import { act, fireEvent, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import fetchMock from 'fetch-mock';
+import { render, Deferred } from 'lib-tests';
+import { DateTime, Duration, Settings } from 'luxon';
+import React from 'react';
+
+import { classroomMockFactory } from 'utils/tests/factories';
+
+import { ClassroomForm } from '.';
+
+jest.mock('lib-components', () => ({
+  ...jest.requireActual('lib-components'),
+  useAppConfig: () => ({
+    modelName: 'classrooms',
+    resource: {
+      id: '1',
+    },
+    static: {
+      img: {
+        bbbBackground: 'some_url',
+      },
+    },
+  }),
+}));
+
+jest.mock('data/classroomAppData', () => ({
+  classroomAppData: {
+    modelName: 'classrooms',
+    classroom: {
+      id: '1',
+    },
+  },
+}));
+
+jest.mock('components/UploadDocuments', () => ({
+  UploadDocuments: () => <p>Upload Documents.</p>,
+}));
+
+Settings.defaultLocale = 'en';
+Settings.defaultZone = 'Europe/Paris';
+const currentDate = DateTime.local(2022, 1, 27, 14, 22, 15);
+
+describe('<ClassroomForm />', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(currentDate.toJSDate());
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+    fetchMock.restore();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it('creates a classroom with updated values', async () => {
+    const classroom = classroomMockFactory({ id: '1', started: false });
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/classrooms/1/create/', deferredPatch.promise);
+
+    const { getByText, rerender } = render(
+      <ClassroomForm classroom={classroom} />,
+    );
+    getByText('Title');
+    getByText('Welcome text');
+
+    const inputTitle = screen.getByRole('textbox', {
+      name: /title/i,
+    });
+    userEvent.clear(inputTitle);
+    userEvent.type(inputTitle, 'updated title');
+    fireEvent.blur(inputTitle);
+
+    // simulate updated classroom
+    rerender(
+      <ClassroomForm
+        classroom={{
+          ...classroom,
+          title: 'updated title',
+        }}
+      />,
+    );
+
+    const inputWelcomeText = screen.getByRole('textbox', {
+      name: /welcome text/i,
+    });
+    userEvent.type(inputWelcomeText, 'updated welcome text', {
+      initialSelectionStart: 0,
+      initialSelectionEnd: classroom.welcome_text.length,
+    });
+
+    // wait for debounce to update the classroom
+    fetchMock.patch('/api/classrooms/1/', {
+      ...classroom,
+      title: 'updated title',
+      welcomeText: 'updated welcome text',
+    });
+    jest.runAllTimers();
+
+    // simulate updated classroom
+    rerender(
+      <ClassroomForm
+        classroom={{
+          ...classroom,
+          title: 'updated title',
+          welcome_text: 'updated welcome text',
+        }}
+      />,
+    );
+
+    deferredPatch.resolve({ message: 'Classroom updated.' });
+
+    await waitFor(() => expect(fetchMock.calls()).toHaveLength(2));
+
+    expect(fetchMock.calls()[0]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[0]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: 'updated title',
+      }),
+    });
+
+    expect(fetchMock.calls()[1]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[1]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        welcome_text: 'updated welcome text',
+      }),
+    });
+  });
+
+  it('schedules a classroom', async () => {
+    const startingAt = currentDate.plus({ days: 2, hours: 2 }).startOf('hour');
+    const estimatedDuration = Duration.fromObject({ minutes: 30 });
+    const classroom = classroomMockFactory({ id: '1', started: false });
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/classrooms/1/', deferredPatch.promise);
+
+    const { getByText, rerender } = render(
+      <ClassroomForm classroom={classroom} />,
+    );
+    getByText('Title');
+    getByText('Welcome text');
+
+    const inputTitle = screen.getByRole('textbox', {
+      name: /title/i,
+    });
+    userEvent.clear(inputTitle);
+    userEvent.type(inputTitle, 'updated title');
+    fireEvent.blur(inputTitle);
+
+    // simulate classroom update
+    rerender(
+      <ClassroomForm classroom={{ ...classroom, title: 'updated title' }} />,
+    );
+
+    const inputWelcomeText = screen.getByRole('textbox', {
+      name: /welcome text/i,
+    });
+    userEvent.type(inputWelcomeText, 'updated welcome text', {
+      initialSelectionStart: 0,
+      initialSelectionEnd: classroom.welcome_text.length,
+    });
+    fireEvent.blur(inputWelcomeText);
+
+    jest.runAllTimers();
+
+    deferredPatch.resolve({ message: 'Classroom scheduled.' });
+
+    await waitFor(() => expect(fetchMock.calls()).toHaveLength(2));
+
+    expect(fetchMock.calls()[0]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[0]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: 'updated title',
+      }),
+    });
+
+    expect(fetchMock.calls()[1]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[1]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        welcome_text: 'updated welcome text',
+      }),
+    });
+
+    // simulate classroom update
+    rerender(
+      <ClassroomForm
+        classroom={{
+          ...classroom,
+          title: 'updated title',
+          welcome_text: 'updated welcome text',
+        }}
+      />,
+    );
+
+    const inputStartingAtDate = screen.getByLabelText(/starting date/i);
+    userEvent.type(inputStartingAtDate, startingAt.toFormat('yyyy/MM/dd'));
+    fireEvent.blur(inputStartingAtDate);
+
+    deferredPatch.resolve({ message: 'Classroom scheduled.' });
+
+    // using userEvent.type with following input doesn't work
+    const inputStartingAtTime = await screen.findByLabelText(/starting time/i);
+    fireEvent.change(inputStartingAtTime, {
+      target: { value: startingAt.toLocaleString(DateTime.TIME_24_SIMPLE) },
+    });
+    fireEvent.blur(inputStartingAtTime);
+
+    // simulate classroom update
+    rerender(
+      <ClassroomForm
+        classroom={{
+          ...classroom,
+          title: 'updated title',
+          starting_at: startingAt.toISO(),
+        }}
+      />,
+    );
+
+    const inputEstimatedDuration = screen.getByLabelText(/estimated duration/i);
+    userEvent.type(inputEstimatedDuration, estimatedDuration.toFormat('h:mm'));
+    fireEvent.blur(inputEstimatedDuration);
+
+    // simulate classroom update
+    rerender(
+      <ClassroomForm
+        classroom={{
+          ...classroom,
+          title: 'updated title',
+          starting_at: startingAt.toISO(),
+          estimated_duration: estimatedDuration.toFormat('hh:mm:ss'),
+        }}
+      />,
+    );
+
+    deferredPatch.resolve({ message: 'Classroom scheduled.' });
+
+    await waitFor(() => expect(fetchMock.calls()).toHaveLength(4));
+
+    expect(fetchMock.calls()[2]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[2]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        starting_at: startingAt.toISO(),
+      }),
+    });
+
+    expect(fetchMock.calls()[3]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[3]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        estimated_duration: estimatedDuration.toFormat('hh:mm:ss'),
+      }),
+    });
+  });
+
+  it('updates a classroom scheduled', async () => {
+    const startingAt = currentDate.plus({ days: 2, hours: 2 }).startOf('hour');
+    const estimatedDuration = Duration.fromObject({ minutes: 30 });
+    const classroom = classroomMockFactory({
+      id: '1',
+      started: false,
+      starting_at: startingAt.toISO(),
+      estimated_duration: estimatedDuration.toFormat('hh:mm:ss'),
+    });
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/classrooms/1/', deferredPatch.promise);
+
+    const { getByText, rerender } = render(
+      <ClassroomForm classroom={classroom} />,
+    );
+    getByText('Title');
+    getByText('Welcome text');
+
+    const inputTitle = screen.getByRole('textbox', {
+      name: /title/i,
+    });
+    userEvent.clear(inputTitle);
+    userEvent.type(inputTitle, 'updated title');
+
+    // Blur event should trigger update
+    fireEvent.blur(inputTitle);
+    act(() => deferredPatch.resolve({ message: 'Classroom scheduled.' }));
+
+    // simulate classroom update
+    rerender(
+      <ClassroomForm classroom={{ ...classroom, title: 'updated title' }} />,
+    );
+
+    const inputWelcomeText = screen.getByRole('textbox', {
+      name: /welcome text/i,
+    });
+    userEvent.type(inputWelcomeText, 'updated welcome text', {
+      initialSelectionStart: 0,
+      initialSelectionEnd: classroom.welcome_text?.length,
+    });
+
+    jest.runAllTimers();
+
+    act(() => {
+      deferredPatch.resolve({ message: 'Classroom scheduled.' });
+    });
+
+    await waitFor(() => expect(fetchMock.calls()).toHaveLength(2));
+
+    expect(fetchMock.calls()[0]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[0]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: 'updated title',
+      }),
+    });
+
+    expect(fetchMock.calls()[1]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[1]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        welcome_text: 'updated welcome text',
+      }),
+    });
+  });
+
+  it('clears a classroom scheduled', async () => {
+    const startingAt = currentDate.plus({ days: 2, hours: 2 }).startOf('hour');
+    const estimatedDuration = Duration.fromObject({ minutes: 30 });
+    const classroom = classroomMockFactory({
+      id: '1',
+      started: false,
+      starting_at: startingAt.toISO(),
+      estimated_duration: estimatedDuration.toFormat('hh:mm:ss'),
+    });
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/classrooms/1/', deferredPatch.promise);
+
+    const { getByText, rerender } = render(
+      <ClassroomForm classroom={classroom} />,
+    );
+    getByText('Title');
+    getByText('Welcome text');
+
+    const inputStartingAtDate = screen.getByLabelText(/starting date/i);
+    userEvent.clear(inputStartingAtDate);
+    fireEvent.blur(inputStartingAtDate);
+
+    // simulate classroom update
+    rerender(<ClassroomForm classroom={{ ...classroom, starting_at: null }} />);
+
+    const inputEstimatedDuration = screen.getByLabelText(/estimated duration/i);
+    userEvent.clear(inputEstimatedDuration);
+
+    jest.runAllTimers();
+
+    deferredPatch.resolve({ message: 'Classroom scheduled.' });
+
+    await waitFor(() => expect(fetchMock.calls()).toHaveLength(2));
+
+    expect(fetchMock.calls()[0][0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[0][1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        starting_at: null,
+      }),
+    });
+
+    expect(fetchMock.calls()[1][0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[1][1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        estimated_duration: null,
+      }),
+    });
+  });
+
+  it('shows an error when classroom title is missing', () => {
+    const classroom = classroomMockFactory({ title: null, id: '1' });
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/classrooms/1/create/', deferredPatch.promise);
+
+    const { getByText, queryByText } = render(
+      <ClassroomForm classroom={classroom} />,
+    );
+
+    // title empty, error message should be shown
+    getByText('Title is required to launch the classroom.');
+
+    // title filled, error message should be hidden
+    const inputTitle = screen.getByRole('textbox', {
+      name: /title/i,
+    });
+    userEvent.type(inputTitle, 'updated title');
+    const titleError = queryByText(
+      'Title is required to launch the classroom.',
+    );
+    expect(titleError).not.toBeInTheDocument();
+  });
+});
---
+++ src/frontend/packages/lib_classroom/src/components/ClassroomForm/index.tsx
@@ -0,0 +1,194 @@
+import { Form, FormField, Text, TextArea, TextInput } from 'grommet';
+import { Maybe } from 'lib-common';
+import { SchedulingFields, UploadManager, Classroom } from 'lib-components';
+import React, { useEffect, useRef, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+
+import { UploadDocuments } from 'components/UploadDocuments';
+import { useUpdateClassroom } from 'data/queries';
+
+const DEBOUNCE_TIME_MS = 1500;
+
+const messages = defineMessages({
+  titleLabel: {
+    defaultMessage: 'Title',
+    description: 'Label for title in classroom creation form.',
+    id: 'component.ClassroomForm.titleLabel',
+  },
+  descriptionLabel: {
+    defaultMessage: 'Description',
+    description: 'Label for description in classroom creation form.',
+    id: 'component.ClassroomForm.descriptionLabel',
+  },
+  welcomeTextLabel: {
+    defaultMessage: 'Welcome text',
+    description: 'Label for welcome text in classroom creation form.',
+    id: 'component.ClassroomForm.welcomeTextLabel',
+  },
+  scheduleClassroomLabel: {
+    defaultMessage: 'Update classroom scheduling',
+    description: 'Label for confirm button in classroom scheduling form.',
+    id: 'component.ClassroomForm.scheduleClassroomLabel',
+  },
+  updateClassroomSuccess: {
+    defaultMessage: 'Classroom updated.',
+    description: 'Message when classroom update is successful.',
+    id: 'component.ClassroomForm.updateClassroomSuccess',
+  },
+  updateClassroomFail: {
+    defaultMessage: 'Classroom not updated!',
+    description: 'Message when classroom update failed.',
+    id: 'component.ClassroomForm.updateClassroomFail',
+  },
+  requiredTitle: {
+    defaultMessage: 'Title is required to launch the classroom.',
+    description: 'Message when classroom title is missing.',
+    id: 'component.ClassroomForm.requiredTitle',
+  },
+});
+
+interface ClassroomFormProps {
+  classroom: Classroom;
+}
+
+export const ClassroomForm = ({ classroom }: ClassroomFormProps) => {
+  const intl = useIntl();
+  const updateClassroomMutation = useUpdateClassroom(classroom.id, {
+    onSuccess: () => {
+      toast.success(intl.formatMessage(messages.updateClassroomSuccess));
+    },
+    onError: () => {
+      toast.error(intl.formatMessage(messages.updateClassroomFail));
+      setUpdatedClassroomState(classroom);
+    },
+  });
+  const [updatedClassroomState, setUpdatedClassroomState] = useState<
+    Partial<Classroom>
+  >({});
+
+  const timeoutId = useRef<Maybe<number>>();
+
+  const debounce = (
+    fn: (updatedClassroom: Partial<Classroom>) => void,
+    ms = DEBOUNCE_TIME_MS,
+  ) => {
+    return (updatedClassroom: Partial<Classroom>) => {
+      window.clearTimeout(timeoutId.current);
+      timeoutId.current = window.setTimeout(() => fn(updatedClassroom), ms);
+    };
+  };
+
+  const debouncedUpdateClassroom = React.useRef(
+    debounce((updatedClassroom: Partial<Classroom>) => {
+      if (JSON.stringify(updatedClassroom) !== '{}') {
+        updateClassroomMutation.mutate(updatedClassroom);
+      }
+    }),
+  ).current;
+
+  const handleChange = (updatedClassroomAttribute: Partial<Classroom>) => {
+    const updatedClassroom = {
+      ...updatedClassroomState,
+      ...updatedClassroomAttribute,
+    };
+    setUpdatedClassroomState(updatedClassroom);
+    window.clearTimeout(timeoutId.current);
+    debouncedUpdateClassroom(updatedClassroom);
+  };
+
+  const handleBlur = () => {
+    if (JSON.stringify(updatedClassroomState) !== '{}') {
+      window.clearTimeout(timeoutId.current);
+      updateClassroomMutation.mutate(updatedClassroomState);
+    }
+  };
+
+  useEffect(() => {
+    setUpdatedClassroomState({});
+  }, [classroom]);
+
+  const titleError = !{ ...classroom, ...updatedClassroomState }.title && (
+    <Text size="small" color="status-error">
+      <FormattedMessage {...messages.requiredTitle} />
+    </Text>
+  );
+
+  return (
+    <UploadManager>
+      <Form
+        value={{ ...classroom, ...updatedClassroomState }}
+        onBlur={handleBlur}
+      >
+        <FormField
+          label={intl.formatMessage(messages.titleLabel)}
+          htmlFor="title"
+          required={true}
+          error={titleError}
+          background={titleError ? 'status-error-off' : 'white'}
+          margin={{ bottom: 'medium' }}
+        >
+          <TextInput
+            name="title"
+            id="title"
+            value={{ ...classroom, ...updatedClassroomState }.title || ''}
+            onChange={(e) => {
+              handleChange({ title: e.target.value });
+            }}
+          />
+        </FormField>
+        <FormField
+          label={intl.formatMessage(messages.descriptionLabel)}
+          htmlFor="description"
+          margin={{ bottom: 'medium' }}
+        >
+          <TextArea
+            name="description"
+            id="description"
+            value={{ ...classroom, ...updatedClassroomState }.description || ''}
+            onChange={(e) => {
+              handleChange({ description: e.target.value });
+            }}
+          />
+        </FormField>
+        <FormField
+          label={intl.formatMessage(messages.welcomeTextLabel)}
+          htmlFor="welcome_text"
+          margin={{ bottom: 'medium' }}
+        >
+          <TextArea
+            name="welcome_text"
+            id="welcome_text"
+            value={
+              { ...classroom, ...updatedClassroomState }.welcome_text || ''
+            }
+            onChange={(e) => {
+              handleChange({ welcome_text: e.target.value });
+            }}
+          />
+        </FormField>
+        <SchedulingFields
+          margin="none"
+          startingAt={
+            updatedClassroomState.starting_at || classroom.starting_at
+          }
+          estimatedDuration={
+            updatedClassroomState.estimated_duration ||
+            classroom.estimated_duration
+          }
+          onStartingAtChange={(startingAt) => {
+            handleChange({
+              starting_at: startingAt,
+            });
+          }}
+          onEstimatedDurationChange={(estimatedDuration) => {
+            handleChange({
+              estimated_duration: estimatedDuration,
+            });
+          }}
+        />
+      </Form>
+      <UploadDocuments />
+    </UploadManager>
+  );
+};
