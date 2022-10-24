a7462101a391362b1dc0693d429c54dade684990
Anthony LC <anthony.le-courric@polyconseil.fr>
2022-10-24 16:41:50 +0200

    ♻️(lib-classroom) refacto DashboardClassroomForm to ClassroomForm
    - Extract the form from DashboardClassroomForm
    - Moved it to lib-classroom
    - Adapt LTI Classroom Apps

--- src/frontend/apps/lti_site/apps/classroom/components/DashboardClassroomForm/index.tsx
+++ src/frontend/apps/lti_site/apps/classroom/components/DashboardClassroomForm/index.tsx
@@ -1,16 +1,11 @@
-import { Button, Form, FormField, Text, TextArea, TextInput } from 'grommet';
-import { Maybe } from 'lib-common';
-import React, { useEffect, useRef, useState } from 'react';
+import { Button } from 'grommet';
+import React from 'react';
 import { toast } from 'react-hot-toast';
-import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
-
-import { SchedulingFields, UploadManager, Classroom } from 'lib-components';
+import { defineMessages, useIntl } from 'react-intl';
 
 import { DashboardClassroomLayout } from 'apps/classroom/components/DashboardClassroomLayout';
-import { UploadDocuments } from 'lib-classroom';
-import { useCreateClassroomAction, useUpdateClassroom } from 'lib-classroom';
-
-const DEBOUNCE_TIME_MS = 1500;
+import { useCreateClassroomAction, ClassroomForm } from 'lib-classroom';
+import { Classroom } from 'lib-components/src/types/apps/classroom/models';
 
 const messages = defineMessages({
   createClassroomFail: {
@@ -18,45 +13,10 @@ const messages = defineMessages({
     description: 'Message when classroom creation failed.',
     id: 'component.DashboardClassroomForm.createClassroomFail',
   },
-  titleLabel: {
-    defaultMessage: 'Title',
-    description: 'Label for title in classroom creation form.',
-    id: 'component.DashboardClassroomForm.titleLabel',
-  },
-  descriptionLabel: {
-    defaultMessage: 'Description',
-    description: 'Label for description in classroom creation form.',
-    id: 'component.DashboardClassroomForm.descriptionLabel',
-  },
-  welcomeTextLabel: {
-    defaultMessage: 'Welcome text',
-    description: 'Label for welcome text in classroom creation form.',
-    id: 'component.DashboardClassroomForm.welcomeTextLabel',
-  },
   startClassroomLabel: {
     defaultMessage: 'Launch the classroom now in BBB',
     description: 'Label for the button starting the classroom creation in BBB.',
     id: 'component.DashboardClassroomForm.startClassroomLabel',
-  },
-  scheduleClassroomLabel: {
-    defaultMessage: 'Update classroom scheduling',
-    description: 'Label for confirm button in classroom scheduling form.',
-    id: 'component.DashboardClassroomForm.scheduleClassroomLabel',
-  },
-  updateClassroomSuccess: {
-    defaultMessage: 'Classroom updated.',
-    description: 'Message when classroom update is successful.',
-    id: 'component.DashboardClassroomForm.updateClassroomSuccess',
-  },
-  updateClassroomFail: {
-    defaultMessage: 'Classroom not updated!',
-    description: 'Message when classroom update failed.',
-    id: 'component.DashboardClassroomForm.updateClassroomFail',
-  },
-  requiredTitle: {
-    defaultMessage: 'Title is required to launch the classroom.',
-    description: 'Message when classroom title is missing.',
-    id: 'component.DashboardClassroomForm.requiredTitle',
   },
 });
 
@@ -74,145 +34,7 @@ const DashboardClassroomForm = ({ classroom }: DashboardClassroomFormProps) => {
       toast.error(intl.formatMessage(messages.createClassroomFail));
     },
   });
-  const updateClassroomMutation = useUpdateClassroom(classroom.id, {
-    onSuccess: () => {
-      toast.success(intl.formatMessage(messages.updateClassroomSuccess));
-    },
-    onError: () => {
-      toast.error(intl.formatMessage(messages.updateClassroomFail));
-      setUpdatedClassroomState(classroom);
-    },
-  });
-  const [updatedClassroomState, setUpdatedClassroomState] = useState<
-    Partial<Classroom>
-  >({});
 
-  const timeoutId = useRef<Maybe<number>>();
-
-  const debounce = (
-    fn: (updatedClassroom: Partial<Classroom>) => void,
-    ms = DEBOUNCE_TIME_MS,
-  ) => {
-    return (updatedClassroom: Partial<Classroom>) => {
-      window.clearTimeout(timeoutId.current);
-      timeoutId.current = window.setTimeout(() => fn(updatedClassroom), ms);
-    };
-  };
-
-  const debouncedUpdateClassroom = React.useRef(
-    debounce(async (updatedClassroom: Partial<Classroom>) => {
-      if (JSON.stringify(updatedClassroom) !== '{}') {
-        updateClassroomMutation.mutate(updatedClassroom);
-      }
-    }),
-  ).current;
-
-  const handleChange = async (
-    updatedClassroomAttribute: Partial<Classroom>,
-  ) => {
-    const updatedClassroom = {
-      ...updatedClassroomState,
-      ...updatedClassroomAttribute,
-    };
-    setUpdatedClassroomState(updatedClassroom);
-    window.clearTimeout(timeoutId.current);
-    debouncedUpdateClassroom(updatedClassroom);
-  };
-
-  const handleBlur = () => {
-    if (JSON.stringify(updatedClassroomState) !== '{}') {
-      window.clearTimeout(timeoutId.current);
-      updateClassroomMutation.mutate(updatedClassroomState);
-    }
-  };
-
-  useEffect(() => {
-    setUpdatedClassroomState({});
-  }, [classroom]);
-
-  const titleError = !{ ...classroom, ...updatedClassroomState }.title && (
-    <Text size="small" color="status-error">
-      <FormattedMessage {...messages.requiredTitle} />
-    </Text>
-  );
-
-  const left = (
-    <UploadManager>
-      <Form
-        value={{ ...classroom, ...updatedClassroomState }}
-        onBlur={handleBlur}
-      >
-        <FormField
-          label={intl.formatMessage(messages.titleLabel)}
-          htmlFor="title"
-          required={true}
-          error={titleError}
-          background={titleError ? 'status-error-off' : 'white'}
-          margin={{ bottom: 'medium' }}
-        >
-          <TextInput
-            name="title"
-            id="title"
-            value={{ ...classroom, ...updatedClassroomState }.title || ''}
-            onChange={(e) => {
-              handleChange({ title: e.target.value });
-            }}
-          />
-        </FormField>
-        <FormField
-          label={intl.formatMessage(messages.descriptionLabel)}
-          htmlFor="description"
-          margin={{ bottom: 'medium' }}
-        >
-          <TextArea
-            name="description"
-            id="description"
-            value={{ ...classroom, ...updatedClassroomState }.description || ''}
-            onChange={(e) => {
-              handleChange({ description: e.target.value });
-            }}
-          />
-        </FormField>
-        <FormField
-          label={intl.formatMessage(messages.welcomeTextLabel)}
-          htmlFor="welcome_text"
-          margin={{ bottom: 'medium' }}
-        >
-          <TextArea
-            name="welcome_text"
-            id="welcome_text"
-            value={
-              { ...classroom, ...updatedClassroomState }.welcome_text || ''
-            }
-            onChange={(e) => {
-              handleChange({ welcome_text: e.target.value });
-            }}
-          />
-        </FormField>
-        <SchedulingFields
-          margin="none"
-          startingAt={
-            updatedClassroomState.starting_at || classroom.starting_at
-          }
-          estimatedDuration={
-            updatedClassroomState.estimated_duration ||
-            classroom.estimated_duration
-          }
-          onStartingAtChange={(startingAt) => {
-            return handleChange({
-              starting_at: startingAt,
-            });
-          }}
-          onEstimatedDurationChange={(estimatedDuration) => {
-            return handleChange({
-              estimated_duration: estimatedDuration,
-            });
-          }}
-        />
-      </Form>
-      <UploadDocuments />
-    </UploadManager>
-  );
   const right = (
     <Button
       type="submit"
@@ -227,7 +49,12 @@ const DashboardClassroomForm = ({ classroom }: DashboardClassroomFormProps) => {
     />
   );
 
-  return <DashboardClassroomLayout left={left} right={right} />;
+  return (
+    <DashboardClassroomLayout
+      left={<ClassroomForm classroom={classroom} />}
+      right={right}
+    />
+  );
 };
 
 export default DashboardClassroomForm;
--- src/frontend/packages/lib_classroom/src/components/index.ts
+++ src/frontend/packages/lib_classroom/src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './UploadDocuments';
+export * from './ClassroomForm';
