b2557f88a0e86317e4725d5bd5a57c72dfcba463
Manuel Raynaud <manu@raynaud.io>
2022-10-24 11:20:49 +0200

    ✨(back) allow site users organizations api calls
    Site users should be allowed to make api calls on the organizations
    resource.
    Users can list ornizations they are administrator for. And update them.
    They still can not create organizations in the site application.

--- src/backend/marsha/core/tests/api/organizations/test_list.py
+++ src/backend/marsha/core/tests/api/organizations/test_list.py
@@ -1,5 +1,6 @@
 """Tests for the Organization list API of the Marsha project."""
 from django.test import TestCase
+from django.utils import timezone
 
 from marsha.core import factories, models
 from marsha.core.simple_jwt.factories import UserAccessTokenFactory
@@ -15,7 +16,7 @@ class OrganizationListAPITest(TestCase):
         self.assertEqual(response.status_code, 401)
 
     def test_list_organizations_by_random_logged_in_user(self):
-        """Random logged-in users cannot list organizations."""
+        """Random logged-in users can list organizations but always empty."""
         factories.OrganizationFactory()
 
         jwt_token = UserAccessTokenFactory()
@@ -24,14 +25,26 @@ class OrganizationListAPITest(TestCase):
             "/api/organizations/",
             HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
         )
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["count"], 0)
+        self.assertEqual(response.json()["results"], [])
 
     def test_list_organizations_by_admin(self):
         """Organization admins cannot list organizations."""
         user = factories.UserFactory()
-        organization = factories.OrganizationFactory()
+        created_on = timezone.now()
+        organization = factories.OrganizationFactory(created_on=created_on)
         factories.OrganizationAccessFactory(
             user=user, organization=organization, role=models.ADMINISTRATOR
+        )
+
+        # Other organization not linked to the current user
+        factories.OrganizationFactory()
+
+        # Other organization linked to the current user but not as admin
+        factories.OrganizationAccessFactory(
+            user=user,
+            role=models.INSTRUCTOR,
         )
 
         jwt_token = UserAccessTokenFactory(user=user)
@@ -39,5 +52,20 @@ class OrganizationListAPITest(TestCase):
         response = self.client.get(
             "/api/organizations/",
             HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
+            content_type="application/json",
         )
-        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["count"], 1)
+        self.assertEqual(
+            response.json()["results"],
+            [
+                {
+                    "consumer_sites": [],
+                    # remove offset in number and replace with Z like DRF serializer did
+                    "created_on": created_on.isoformat()[:-6] + "Z",
+                    "id": str(organization.pk),
+                    "name": organization.name,
+                    "users": [str(user.pk)],
+                }
+            ],
+        )
