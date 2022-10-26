5e9791a3ddb680ee7ce058f649502d6083435e52
ALC Consulting <freelance.anthony@gmail.com>
2022-10-26 18:21:40 +0200

    ♻️(lib-classroom) refactored UploadableObjectProgress
    - Problem: I had problem to test UploadableObjectProgress from lib-classroom
    because of the use of useUploadManager that was difficult to mock.
    
    - Solution: I refactored UploadableObjectProgress to get the
    progress variable from props instead of using useUploadManager.
    It make it easier to test it.
    
    - Refactored component
    - Adapt Classroom packages
    - Adapt LTI App

--- src/frontend/packages/lib_components/src/common/UploadableObjectProgress/index.tsx
+++ src/frontend/packages/lib_components/src/common/UploadableObjectProgress/index.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 import styled from 'styled-components';
 
-import { useUploadManager } from 'common/UploadManager';
-import { Resource } from 'types/tracks';
-
 const messages = defineMessages({
   progressLabel: {
     defaultMessage: 'Upload progress',
@@ -19,15 +16,13 @@ export const StyledMeter = styled(Meter)`
 `;
 
 interface UploadableObjectProgressProps {
-  objectId: Resource['id'];
+  progress: number;
 }
 
 export const UploadableObjectProgress = ({
-  objectId,
+  progress,
 }: UploadableObjectProgressProps) => {
   const intl = useIntl();
-  const { uploadManagerState } = useUploadManager();
-  const progress = uploadManagerState[objectId]?.progress || 0;
 
   // There is a conflict in the type of the `values` prop between `react` and `grommet`
   // Use the const type to ensure correctness and the `any` escape hatch for the actual value
