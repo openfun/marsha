defe1969146b1d46d56ef6b25e72df26001e82d5
vassili <vassili.rezvoy@polyconseil.fr>
2022-10-31 10:32:15 +0100

    ✨(frontend) create a new component modale in the site
    Create a new component to control our modales.

---
+++ src/frontend/apps/standalone_site/src/components/Modal/index.spec.tsx
@@ -0,0 +1,73 @@
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Box, Button } from 'grommet';
+import { render } from 'lib-tests';
+import { createRef } from 'react';
+
+import Modale, { ModalControlMethods } from '.';
+
+describe('<Modale />', () => {
+  it('opens the modal on init', async () => {
+    const onClose = jest.fn();
+    const { rerender } = render(
+      <Box>
+        <Button>Open</Button>
+        <Modale isOpen onClose={onClose}>
+          Some awesome content
+        </Modale>
+      </Box>,
+    );
+
+    screen.getByRole('button', { name: 'Open' });
+    screen.getByText('Some awesome content');
+    expect(onClose).not.toHaveBeenCalled();
+
+    rerender(
+      <Box>
+        <Button>Open</Button>
+        <Modale isOpen={false}>Some awesome content</Modale>
+      </Box>,
+    );
+
+    await waitForElementToBeRemoved(screen.queryByText('Some awesome content'));
+    expect(onClose).not.toHaveBeenCalled();
+  });
+
+  it('populates the ref API', async () => {
+    const ref = createRef<ModalControlMethods>();
+    const onClose = jest.fn();
+
+    render(
+      <Box>
+        <Button onClick={() => ref.current?.open()}>Open</Button>
+        <Modale controlMethods={ref} onClose={onClose}>
+          <Button onClick={() => ref.current?.close()}>Close</Button>
+          Some awesome content
+        </Modale>
+      </Box>,
+    );
+
+    expect(screen.getByRole('button', { name: 'Open' })).toBeInTheDocument();
+    expect(screen.queryByText('Some awesome content')).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('button', { name: 'Open' }));
+
+    expect(
+      await screen.findByRole('button', { name: 'Close' }),
+    ).toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('button', { name: 'Close' }));
+
+    await waitForElementToBeRemoved(screen.queryByText('Some awesome content'));
+    expect(onClose).not.toHaveBeenCalled();
+
+    userEvent.click(screen.getByRole('button', { name: 'Open' }));
+
+    userEvent.click(
+      await screen.findByRole('button', { name: 'Close the modale' }),
+    );
+
+    await waitForElementToBeRemoved(screen.queryByText('Some awesome content'));
+    expect(onClose).toHaveBeenCalled();
+  });
+});
