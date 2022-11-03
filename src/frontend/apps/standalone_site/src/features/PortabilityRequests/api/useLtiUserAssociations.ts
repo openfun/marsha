3058815c246078b1b2dc8eb4cbae0a467b50bc5a
Quentin BEY <quentin.bey@polyconseil.fr>
2022-11-03 16:57:06 +0100

    ✨(frontend) use JWT for LTI user association
    When a token is provided in the URL, use it to create
    the LTI/site user association.

---
+++ src/frontend/apps/standalone_site/src/features/PortabilityRequests/api/useLtiUserAssociations.ts
@@ -0,0 +1,44 @@
+import { createOne } from 'lib-components';
+import { useMutation, UseMutationOptions, useQueryClient } from 'react-query';
+
+type UseCreateLtiUserAssociationData = {
+  association_jwt: string;
+};
+type UseCreateLtiUserAssociationError =
+  | { code: 'exception' }
+  | {
+      code: 'invalid';
+      errors: { [key in keyof UseCreateLtiUserAssociationData]?: string[] }[];
+    };
+type UseCreateLtiUserAssociationOptions = UseMutationOptions<
+  null, // TData, the mutation result : nothing
+  UseCreateLtiUserAssociationError,
+  UseCreateLtiUserAssociationData
+>;
+export const useCreateLtiUserAssociation = (
+  options?: UseCreateLtiUserAssociationOptions,
+) => {
+  const queryClient = useQueryClient();
+  return useMutation<
+    null, // TData, the mutation result : nothing
+    UseCreateLtiUserAssociationError,
+    UseCreateLtiUserAssociationData
+  >(
+    (newLtiUserAssociation) =>
+      createOne({
+        name: 'lti-user-associations',
+        object: newLtiUserAssociation,
+      }),
+    {
+      ...options,
+      onSuccess: (data, variables, context) => {
+        queryClient.invalidateQueries('lti-user-associations');
+        // Also refresh portability requests because requesting user may have been updated
+        queryClient.invalidateQueries('portability-requests');
+        if (options?.onSuccess) {
+          options.onSuccess(data, variables, context);
+        }
+      },
+    },
+  );
+};
