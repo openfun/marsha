285d9c4c08f7ca5f7cb0f00b92b6895118b5c447
Quentin BEY <quentin.bey@polyconseil.fr>
2022-11-17 16:36:25 +0100

    ✅(backend) test for duplicate in organization API
    Even we don't have two `OUTER JOIN` on the organization API list
    endpoint, we add a test to assert there won't be duplicated data
    if we later add a filter on consumer site administrators.

--- src/backend/marsha/core/tests/api/organizations/test_list.py
+++ src/backend/marsha/core/tests/api/organizations/test_list.py
@@ -9,6 +9,8 @@ from marsha.core.simple_jwt.factories import UserAccessTokenFactory
 class OrganizationListAPITest(TestCase):
     """Test the list API for organization objects."""
 
+    maxDiff = None
+
     def test_list_organizations_by_anonymous_user(self):
         """Anonymous users cannot list organizations."""
         factories.OrganizationFactory()
@@ -68,4 +70,72 @@ class OrganizationListAPITest(TestCase):
                     "users": [str(user.pk)],
                 }
             ],
+        )
+
+    def test_list_organizations_not_duplicated(self):
+        """
+        Test the list of organizations to not be duplicated.
+        This cannot be possible for now, as the base queryset uses
+        only one OUTER JOIN, but we expect to prevent this in the future.
+        """
+        user = factories.UserFactory()
+        created_on = timezone.now()
+        organization = factories.OrganizationFactory(created_on=created_on)
+        factories.OrganizationAccessFactory(
+            user=user, organization=organization, role=models.ADMINISTRATOR
+        )
+
+        # Other organization access not linked to the current user
+        other_accesses = factories.OrganizationAccessFactory.create_batch(
+            10, organization=organization
+        )
+
+        # Other organization not linked to the current user
+        factories.OrganizationAccessFactory.create_batch(3)
+
+        # Other organization linked to the current user but not as admin
+        factories.OrganizationAccessFactory(
+            user=user,
+            role=models.INSTRUCTOR,
+        )
+
+        # In case we also add a consumer site admin to access organization
+        consumer_site_organization = factories.ConsumerSiteOrganizationFactory(
+            organization=organization,
+        )
+        factories.ConsumerSiteAccessFactory(
+            user=user,
+            consumer_site=consumer_site_organization.consumer_site,
+            role=models.ADMINISTRATOR,
+        )
+
+        # other consumer site administrator
+        factories.ConsumerSiteAccessFactory(
+            consumer_site=consumer_site_organization.consumer_site,
+            role=models.ADMINISTRATOR,
+        )
+
+        factories.ConsumerSiteAccessFactory.create_batch(
+            10, consumer_site=consumer_site_organization.consumer_site
+        )
+
+        jwt_token = UserAccessTokenFactory(user=user)
+
+        response = self.client.get(
+            "/api/organizations/",
+            HTTP_AUTHORIZATION=f"Bearer {jwt_token}",
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["count"], 1)
+
+        self.assertEqual(response.json()["results"][0]["id"], str(organization.pk))
+        self.assertEqual(response.json()["results"][0]["name"], organization.name)
+        self.assertListEqual(
+            response.json()["results"][0]["consumer_sites"],
+            [str(consumer_site_organization.consumer_site.pk)],
+        )
+        self.assertListEqual(
+            sorted(response.json()["results"][0]["users"]),
+            sorted([str(user.pk)] + [str(access.user_id) for access in other_accesses]),
         )
