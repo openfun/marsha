9286e7d6e807c3027378406bd4001175597f986f
ALC Consulting <freelance.anthony@gmail.com>
2022-11-21 16:54:30 +0100

    🚨(frontend) mutualize ESLint
    The rules are more or less similar between the projects.
    We mutualized the rules in a eslintrc-root.
    We can still override the rules in the projects.

--- src/frontend/packages/lib_components/.eslintrc.js
+++ src/frontend/packages/lib_components/.eslintrc.js
@@ -1,32 +1,10 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: '../../.eslintrc-root.js',
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true,
-    },
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-  plugins: ['import'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/no-non-null-assertion': 'error',
-    'array-callback-return': 'error',
-    'block-scoped-var': 'error',
-    curly: 2,
-    'default-case': 'error',
-    eqeqeq: 2,
-    'import/no-duplicates': ['error', { considerQueryString: false }],
     'import/order': [
       'error',
       {
@@ -51,30 +29,5 @@ module.exports = {
         'newlines-between': 'always',
       },
     ],
-    'no-alert': 1,
-    'react/prop-types': 0,
   },
-  ignorePatterns: ['node_modules/'],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  overrides: [
-    {
-      files: ['*.spec.*'],
-
-      rules: {
-        '@typescript-eslint/ban-types': 0,
-        '@typescript-eslint/no-empty-function': 0,
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/no-non-null-assertion': 0,
-        '@typescript-eslint/no-unsafe-argument': 0,
-        '@typescript-eslint/no-unsafe-assignment': 0,
-        '@typescript-eslint/no-unsafe-call': 0,
-        '@typescript-eslint/no-unsafe-member-access': 0,
-        '@typescript-eslint/no-unsafe-return': 0,
-      },
-    },
-  ],
 };
