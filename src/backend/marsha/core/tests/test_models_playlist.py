3f7cc9f4986960f9965da05a014ce03c9cafaa93
Manuel Raynaud <manu@raynaud.io>
2022-11-21 16:27:32 +0100

    ♻️(back) set lti_id and consumer_site nullable on playlist model
    We must be able to create a playlist without defining a lti_id or a
    consumer_site. Both are only used in a LTI context and there is no
    reason on the stansalone site to ask for this to an instructor when he
    wants to create a new playlist. An integrity constrait is added to
    ensure that lti_id ans consumer_site are still defined at the same time.

--- src/backend/marsha/core/tests/test_models_playlist.py
+++ src/backend/marsha/core/tests/test_models_playlist.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 
 from safedelete.models import SOFT_DELETE_CASCADE
 
-from ..factories import PlaylistFactory
+from ..factories import ConsumerSiteFactory, OrganizationFactory, PlaylistFactory
 
 
 class PlaylistModelsTestCase(TestCase):
@@ -40,3 +40,60 @@ class PlaylistModelsTestCase(TestCase):
         # Soft deleted playlists should not count for unicity
         playlist.delete(force_policy=SOFT_DELETE_CASCADE)
         PlaylistFactory(lti_id=playlist.lti_id, consumer_site=playlist.consumer_site)
+
+    def test_modules_playlist_organization_consumer_site_lti_id_check_integrtity(self):
+        """Playlist consumer_site_or_organization_check_idx constraint."""
+        consumer_site = ConsumerSiteFactory()
+        organization = OrganizationFactory()
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                # All parameters set to None is not allowed
+                PlaylistFactory(consumer_site=None, organization=None, lti_id=None)
+
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                # Defining consumer_site without a lti_id is not allowed
+                PlaylistFactory(
+                    consumer_site=consumer_site, organization=None, lti_id=None
+                )
+
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                # Defining the lti_id without a consumer_site is not allowed
+                PlaylistFactory(consumer_site=None, organization=None, lti_id="foo")
+
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                # Defining an organization with a lti_id is not allowed
+                PlaylistFactory(
+                    consumer_site=None, organization=organization, lti_id="foo"
+                )
+
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                # Defining an organization with a consumer_site is not allowed
+                PlaylistFactory(
+                    consumer_site=consumer_site, organization=organization, lti_id=None
+                )
+
+        # Defining consumer_site, lti_id and organization is allowed
+        PlaylistFactory(
+            organization=organization,
+            consumer_site=consumer_site,
+            lti_id="an other lti id",
+        )
+        # Defining both consumer_site and lti_id is allowed
+        PlaylistFactory(
+            consumer_site=consumer_site,
+            lti_id="allowed with consumer site",
+            organization=None,
+        )
+
+        # Defining an organization without consumer_site and lti_id is allowed
+        PlaylistFactory(organization=organization, consumer_site=None, lti_id=None)
+
+        # Having multiple playlist without lti_id and consumer_site but with an
+        # organization is allowed
+        PlaylistFactory.create_batch(
+            4, organization=organization, consumer_site=None, lti_id=None
+        )
