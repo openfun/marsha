44f4a5fae9b413a0560f8eeccea1dddca483d6b6
Quentin BEY <quentin.bey@polyconseil.fr>
2022-11-29 11:58:43 +0100

    ⚡️(frontend) limit Authenticator dependencies
    This restrict the Authencator useEffect dependencies to
    `location.pathname` and `location.search` to prevent massive
    component reload.
    This also improve the related tests.
    Thanks to @AntoLC

--- src/frontend/apps/standalone_site/src/features/Authentication/components/Authenticator.tsx
+++ src/frontend/apps/standalone_site/src/features/Authentication/components/Authenticator.tsx
@@ -9,9 +9,9 @@ const TARGET_URL_STORAGE_KEY = 'redirect_uri';
 const QUERY_PARAMS_CHALLENGE_TOKEN_NAME = 'token';
 
 export const Authenticator = ({ children }: PropsWithChildren<unknown>) => {
-  const location = useLocation();
+  const { pathname, search } = useLocation();
   const history = useHistory();
-  const code = new URLSearchParams(location.search).get(
+  const code = new URLSearchParams(search).get(
     QUERY_PARAMS_CHALLENGE_TOKEN_NAME,
   );
   const { currentUser, setCurrentUser } = useCurrentUser((state) => ({
@@ -37,13 +37,10 @@ export const Authenticator = ({ children }: PropsWithChildren<unknown>) => {
     } else {
       const targetedAddress =
         process.env.REACT_APP_BACKEND_API_BASE_URL || window.location.origin;
-      localStorage.setItem(
-        TARGET_URL_STORAGE_KEY,
-        location.pathname + location.search,
-      );
+      localStorage.setItem(TARGET_URL_STORAGE_KEY, pathname + search);
       window.location.replace(targetedAddress + '/account/login/');
     }
-  }, [code, jwt, location, setJwt]);
+  }, [code, jwt, pathname, search, setJwt]);
 
   useEffect(() => {
     if (!jwt || currentUser) {
@@ -57,10 +54,10 @@ export const Authenticator = ({ children }: PropsWithChildren<unknown>) => {
       localStorage.removeItem(TARGET_URL_STORAGE_KEY);
       // redirect to the originally targeted URL (ie before the authentication loop)
       // or the root page if no target was set
-      history.replace(targetUri || location.pathname);
+      history.replace(targetUri || pathname);
     };
     fetchUserData();
-  }, [jwt, currentUser, location, setCurrentUser, history]);
+  }, [jwt, currentUser, pathname, setCurrentUser, history]);
 
   if (!jwt && !code) {
     return <Fragment />;
