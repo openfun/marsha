dde13158b94f82b00849a255668ac8f4d4cb6b4d
ALC Consulting <freelance.anthony@gmail.com>
2022-12-08 13:47:59 +0100

    💄(frontend) merge classroom theme with main theme
    Merge the classroom theme with the main theme
    to harmonize the styles between apps.

--- src/frontend/packages/lib_classroom/src/components/index.ts
+++ src/frontend/packages/lib_classroom/src/components/index.ts
@@ -1,4 +1,4 @@
 export * from './UploadDocuments';
 export * from './ClassroomForm';
-export { default as DashboardClassroom, bbbTheme } from './DashboardClassroom';
+export { default as DashboardClassroom } from './DashboardClassroom';
 export * from './DashboardClassroom/route';
