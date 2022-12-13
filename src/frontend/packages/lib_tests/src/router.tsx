665601fa4d5f3de5746507516f8d9c2a7e7e7210
Manuel Raynaud <manu@raynaud.io>
2022-12-13 14:55:53 +0100

    ✨(lib-test) allow to wrap routing elements
    In the wrapInRouter helper, we want to inject wrappers around what
    contains the MemoryRouter. This can be usefull to inject a context
    available for all routes declared in the MemoryRouter. A first usage
    will be to use the UploadManager in the tests who needs it.

--- src/frontend/packages/lib_tests/src/router.tsx
+++ src/frontend/packages/lib_tests/src/router.tsx
@@ -2,29 +2,39 @@ import { Maybe } from 'lib-common';
 import React from 'react';
 import { MemoryRouter, Route, RouteProps, Switch } from 'react-router-dom';
 
+const defaultWrapper = (routing: JSX.Element) => routing;
+
 export const wrapInRouter = (
   Component: JSX.Element,
   routes?: RouteProps[],
   componentPath = '/',
   history: Maybe<string[]> = undefined,
   header: React.ReactNode = undefined,
-) => (
-  <MemoryRouter initialEntries={history || [componentPath]} initialIndex={0}>
-    {header}
-    <Switch>
-      {routes &&
-        routes.map((routeProps) => (
-          <Route
-            exact
-            key={
-              Array.isArray(routeProps.path)
-                ? String(routeProps.path[0])
-                : String(routeProps.path || '')
-            }
-            {...routeProps}
-          />
-        ))}
-      <Route path={componentPath}>{Component}</Route>
-    </Switch>
-  </MemoryRouter>
-);
+  wrapper: (routing: JSX.Element) => JSX.Element = defaultWrapper,
+) => {
+  const switchElement = (
+    <React.Fragment>
+      {header}
+      <Switch>
+        {routes &&
+          routes.map((routeProps) => (
+            <Route
+              exact
+              key={
+                Array.isArray(routeProps.path)
+                  ? String(routeProps.path[0])
+                  : String(routeProps.path || '')
+              }
+              {...routeProps}
+            />
+          ))}
+        <Route path={componentPath}>{Component}</Route>
+      </Switch>
+    </React.Fragment>
+  );
+  return (
+    <MemoryRouter initialEntries={history || [componentPath]} initialIndex={0}>
+      {wrapper(switchElement)}
+    </MemoryRouter>
+  );
+};
