ddd764b8dd34c6254173071f0edd395c4d60fc32
ALC Consulting <freelance.anthony@gmail.com>
2022-12-14 14:06:00 +0100

    ✨(website) jwt persistency
    Add persistence on the jwt to not have to login again
    when the page is refreshed.
    We use the local storage to store the jwt.
    The jwt is removed in 2 cases:
     - when the time in minutes defined in the config file
       is reached after is creation.
     - when the back end returns a 401 error.

--- src/frontend/packages/lib_components/src/hooks/stores/useJwt/index.ts
+++ src/frontend/packages/lib_components/src/hooks/stores/useJwt/index.ts
@@ -1,27 +1,47 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 import { DecodedJwt } from '../../../types/jwt';
 import { decodeJwt } from '../../../utils/decodeJwt';
 
 interface JwtStoreInterface {
   jwt?: string;
+  jwtCreateTimestamp?: number;
   internalDecodedJwt?: DecodedJwt;
   setJwt: (jwt: string) => void;
   getDecodedJwt: () => DecodedJwt;
+  reset: () => void;
 }
 
-export const useJwt = create<JwtStoreInterface>((set, get) => ({
-  jwt: undefined,
-  internalDecodedJwt: undefined,
-  setJwt: (jwt) => set((state) => ({ ...state, jwt })),
-  getDecodedJwt: () => {
-    const currentValue = get().internalDecodedJwt;
-    if (currentValue) {
-      return currentValue;
-    }
+export const useJwt = create<JwtStoreInterface>()(
+  persist(
+    (set, get) => ({
+      jwt: undefined,
+      internalDecodedJwt: undefined,
+      jwtCreateTimestamp: undefined,
+      setJwt: (jwt) =>
+        set((state) => ({ ...state, jwt, jwtCreateTimestamp: Date.now() })),
+      getDecodedJwt: () => {
+        const currentValue = get().internalDecodedJwt;
+        if (currentValue) {
+          return currentValue;
+        }
 
-    const decoded = decodeJwt(get().jwt);
-    set((state) => ({ ...state, internalDecodedJwt: decoded }));
-    return decoded;
-  },
-}));
+        const decoded = decodeJwt(get().jwt);
+        set((state) => ({ ...state, internalDecodedJwt: decoded }));
+        return decoded;
+      },
+      reset: () => {
+        useJwt.persist.clearStorage();
+        set((state) => ({
+          ...state,
+          jwt: undefined,
+          jwtCreateTimestamp: undefined,
+        }));
+      },
+    }),
+    {
+      name: 'jwt-storage',
+    },
+  ),
+);
