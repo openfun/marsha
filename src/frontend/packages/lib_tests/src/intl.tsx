3dd3ba29d07d76446a3c4abfb4676ad3f4cad666
Vassili Rezvoy <vassili.rezvoy@polyconseil.fr>
2022-12-16 11:20:27 +0100

    ✅(frontend) disable react-intl error message in tests
    In some test, traduction could not be loded resulting in errors logs in the
    console.
    Prevent this by overriding the on error methods for tests only.

--- src/frontend/packages/lib_tests/src/intl.tsx
+++ src/frontend/packages/lib_tests/src/intl.tsx
@@ -2,5 +2,16 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 
 export const wrapInIntlProvider = (Component: JSX.Element, locale = 'en') => (
-  <IntlProvider locale={locale}>{Component}</IntlProvider>
+  <IntlProvider
+    locale={locale}
+    onError={(err) => {
+      // https://github.com/formatjs/formatjs/issues/465
+      if (err.code === 'MISSING_TRANSLATION') {
+        return;
+      }
+      throw err;
+    }}
+  >
+    {Component}
+  </IntlProvider>
 );
