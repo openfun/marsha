78df438e59e57106067699d829e44e76b8b4e90f
Anthony LC <anthony.le-courric@polyconseil.fr>
2022-12-19 17:19:32 +0100

    🚚(website) block login page to logged user
    Block the login page if the user is already logged in.
    If the user is logged in, he is redirected to the dashboard.

--- src/frontend/apps/standalone_site/src/features/Authentication/components/Login.spec.tsx
+++ src/frontend/apps/standalone_site/src/features/Authentication/components/Login.spec.tsx
@@ -1,5 +1,7 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
 import { ResponsiveContext } from 'grommet';
+import { useJwt } from 'lib-components';
 import { render } from 'lib-tests';
 
 import { getFullThemeExtend } from 'styles/theme.extend';
@@ -7,6 +9,16 @@ import { getFullThemeExtend } from 'styles/theme.extend';
 import { Login } from './Login';
 
 const fullTheme = getFullThemeExtend();
+
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+jest.spyOn(AbortController.prototype, 'abort');
 
 jest.mock('./LoginForm', () => ({
   LoginForm: () => <div>My LoginForm</div>,
@@ -23,6 +35,7 @@ jest.mock('features/Header', () => ({
 describe('<Login />', () => {
   afterEach(() => {
     jest.resetAllMocks();
+    fetchMock.restore();
   });
 
   it('checks render.', () => {
@@ -48,5 +61,40 @@ describe('<Login />', () => {
     expect(screen.queryByLabelText(/Marsha logo/i)).not.toBeInTheDocument();
     expect(screen.getByText(/My HeaderLight/i)).toBeInTheDocument();
     expect(screen.getByText(/My RenaterAuthenticator/i)).toBeInTheDocument();
+  });
+
+  it('checks redirect if logged', async () => {
+    useJwt.setState({
+      refreshJwt: 'my refresh Jwt',
+    });
+
+    fetchMock.post('/account/api/token/refresh/', {
+      access: 'my access',
+      refresh: 'my refresh Jwt2',
+    });
+
+    render(<Login />);
+
+    await waitFor(() => {
+      expect(useJwt.getState().jwt).toBe('my access');
+    });
+    expect(useJwt.getState().refreshJwt).toBe('my refresh Jwt2');
+    expect(mockHistoryPush).toHaveBeenCalledWith('/');
+  });
+
+  it('checks no redirect if logged', async () => {
+    useJwt.setState({
+      refreshJwt: 'my refresh Jwt',
+    });
+
+    fetchMock.post('/account/api/token/refresh/', 400);
+
+    render(<Login />);
+
+    await waitFor(() => {
+      expect(useJwt.getState().jwt).toBe(undefined);
+    });
+    expect(useJwt.getState().refreshJwt).toBe(undefined);
+    expect(screen.getByText(/My LoginForm/i)).toBeInTheDocument();
   });
 });
