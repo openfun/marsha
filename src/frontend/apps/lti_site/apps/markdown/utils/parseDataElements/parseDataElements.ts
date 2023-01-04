09e8a98187d0ef3df5b30894828b1249d65b7333
Manuel Raynaud <manu@raynaud.io>
2023-01-04 10:56:13 +0100

    ♻️(front) create resource_id in AppData
    In the AppData, the resource property was removed once a copy made in
    the model property. We need to know the resource id outside any react
    context in order to configure the useJwt zustand storage name. This
    storage name needs to be unique and we want to use the resource id to
    make it unique. We create a new property resource_id in appData in order
    to save the current resource id.

--- src/frontend/apps/lti_site/apps/markdown/utils/parseDataElements/parseDataElements.ts
+++ src/frontend/apps/lti_site/apps/markdown/utils/parseDataElements/parseDataElements.ts
@@ -4,6 +4,7 @@ import { MarkdownDocumentModelName as modelName } from 'lib-components';
 export const parseDataElements = (element: Element): MarkdownAppData => {
   const context = JSON.parse(element.getAttribute('data-context')!);
 
+  context.resource_id = context.resource?.id;
   if (context.modelName === modelName.MARKDOWN_DOCUMENTS) {
     context.markdownDocument = context.resource;
     delete context.resource;
