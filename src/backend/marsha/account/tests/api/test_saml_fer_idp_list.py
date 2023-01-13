c4844e2962097b2c3d485e9febc419ae14dedc87
Quentin BEY <quentin.bey@polyconseil.fr>
2023-01-13 10:08:12 +0100

    ⬆️(backend) update to social-edu-federation 2.1.0
    This is required to allow the logo data retrieved
    from the Renater's metadata.

--- src/backend/marsha/account/tests/api/test_saml_fer_idp_list.py
+++ src/backend/marsha/account/tests/api/test_saml_fer_idp_list.py
@@ -14,6 +14,32 @@ from social_edu_federation.testing.saml_tools import (
 )
 
 
+# social-edu-federation uses a default logo in metadata
+DEFAULT_LOGO = (
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz3"
+    "4AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29m"
+    "dHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubX"
+    "dtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJC"
+    "KE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00"
+    "YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHC"
+    "hahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9k"
+    "Lp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+q"
+    "XkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMs"
+    "OF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0"
+    "eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8Qyq"
+    "wZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjli"
+    "thJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ek"
+    "okRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6E"
+    "hOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFw"
+    "g59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+g"
+    "qjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDF"
+    "Nx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVO"
+    "J3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HU"
+    "tlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASU"
+    "VORK5CYII="
+)
+
+
 class MetadataUrlOpenMock:
     """Mocking object for the urlopen to fetch custom metadata."""
 
@@ -84,7 +110,7 @@ class SamlFerIdpListAPIViewTest(TestCase):
                     "display_name": "Before in list",
                     "organization_name": "Before in list organization name",
                     "organization_display_name": "Before in list organization display name",
-                    "logo": None,
+                    "logo": DEFAULT_LOGO,
                     "login_url": "http://testserver/account/login/saml_fer/?idp=before-in-list",
                 },
                 {
@@ -92,7 +118,7 @@ class SamlFerIdpListAPIViewTest(TestCase):
                     "display_name": "Local accepting IdP",
                     "organization_name": "Local organization name",
                     "organization_display_name": "Local organization display name",
-                    "logo": None,
+                    "logo": DEFAULT_LOGO,
                     "login_url": (
                         "http://testserver/account/login/saml_fer/?idp=local-accepting-idp"
                     ),
@@ -102,7 +128,7 @@ class SamlFerIdpListAPIViewTest(TestCase):
                     "display_name": "Ze last in list",
                     "organization_name": "Ze last in list organization name",
                     "organization_display_name": "Ze last in list organization display name",
-                    "logo": None,
+                    "logo": DEFAULT_LOGO,
                     "login_url": "http://testserver/account/login/saml_fer/?idp=ze-last-in-list",
                 },
             ],
@@ -126,7 +152,7 @@ class SamlFerIdpListAPIViewTest(TestCase):
                     "display_name": "Local accepting IdP",
                     "organization_name": "Local organization name",
                     "organization_display_name": "Local organization display name",
-                    "logo": None,
+                    "logo": DEFAULT_LOGO,
                     "login_url": (
                         "http://testserver/account/login/saml_fer/?idp=local-accepting-idp"
                     ),
@@ -150,7 +176,7 @@ class SamlFerIdpListAPIViewTest(TestCase):
                     "display_name": "Before in list",
                     "organization_name": "Before in list organization name",
                     "organization_display_name": "Before in list organization display name",
-                    "logo": None,
+                    "logo": DEFAULT_LOGO,
                     "login_url": "http://testserver/account/login/saml_fer/?idp=before-in-list",
                 },
                 {
@@ -158,7 +184,7 @@ class SamlFerIdpListAPIViewTest(TestCase):
                     "display_name": "Ze last in list",
                     "organization_name": "Ze last in list organization name",
                     "organization_display_name": "Ze last in list organization display name",
-                    "logo": None,
+                    "logo": DEFAULT_LOGO,
                     "login_url": "http://testserver/account/login/saml_fer/?idp=ze-last-in-list",
                 },
             ],
