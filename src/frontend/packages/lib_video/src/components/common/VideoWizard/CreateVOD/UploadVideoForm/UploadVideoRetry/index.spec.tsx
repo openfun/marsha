66b14ffccb627bd2ca37c685237cbee8bcb2a96f
alfredpichard <alfred.pichard@polyconseil.fr>
2023-01-17 17:11:43 +0100

    ✨(frontend) throw max size error on video upload
    if the max size is reached when uploading a video, there is now a
    specific message displayed in the component for the user with the max
    size authorized

--- src/frontend/packages/lib_video/src/components/common/VideoWizard/CreateVOD/UploadVideoForm/UploadVideoRetry/index.spec.tsx
+++ src/frontend/packages/lib_video/src/components/common/VideoWizard/CreateVOD/UploadVideoForm/UploadVideoRetry/index.spec.tsx
@@ -8,11 +8,30 @@ import { UploadVideoRetry } from '.';
 const mockedOnClick = jest.fn();
 
 describe('<UploadVideoRetry />', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('renders UploadVideoRetry and clicks on it', () => {
     render(<UploadVideoRetry onClickRetry={mockedOnClick} />);
     screen.getByText(
       'An error occured when uploading your video. Please retry.',
     );
+    const retryButton = screen.getByRole('button');
+
+    userEvent.click(retryButton);
+
+    expect(mockedOnClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders UploadVideoRetry with sizeError and clicks on it', () => {
+    render(
+      <UploadVideoRetry
+        onClickRetry={mockedOnClick}
+        maxSize={Math.pow(10, 9)}
+      />,
+    );
+    screen.getByText('Error : File too large. Max size authorized is 1 GB.');
     const retryButton = screen.getByRole('button');
 
     userEvent.click(retryButton);
