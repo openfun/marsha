14ba515cee9058de0cf39ae85d58211d37dc996f
alfredpichard <alfred.pichard@polyconseil.fr>
2023-01-18 15:53:31 +0100

    ✨(backend) expand size metadata to video resource
    add a max_upload_size info to video views

--- src/frontend/packages/lib_video/src/types/metadata.ts
+++ src/frontend/packages/lib_video/src/types/metadata.ts
@@ -14,6 +14,9 @@ export interface VideoMetadata extends ResourceMetadata<Video> {
   live: {
     segment_duration_seconds: number;
   };
+  vod: {
+    upload_max_size_bytes: number;
+  };
 }
 
 export type TimedTextMetadata = ResourceMetadata<TimedText>;
