f8dd3b568d86bf40532d5cc0ecffa16b63dc3b5b
alfredpichard <alfred.pichard@polyconseil.fr>
2023-01-18 15:55:57 +0100

    ✨(frontend) throw max size error on video upload
    if the max size is reached when uploading a video, there is now a
    specific message displayed in the component for the user with the max
    size authorized

--- src/frontend/packages/lib_video/src/components/common/VideoWizard/CreateVOD/UploadVideoForm/UploadVideoRetry/index.tsx
+++ src/frontend/packages/lib_video/src/components/common/VideoWizard/CreateVOD/UploadVideoForm/UploadVideoRetry/index.tsx
@@ -1,5 +1,5 @@
 import { Text } from 'grommet';
-import { RetryUploadButton } from 'lib-components';
+import { formatSizeErrorScale, RetryUploadButton } from 'lib-components';
 import React from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 
@@ -12,19 +12,38 @@ const messages = defineMessages({
       "Button's label offering the user to go validate is form and create a new video.",
     id: 'components.UploadVideoRetry.errorOccuredVideoUpload',
   },
+  errorFileTooLarge: {
+    defaultMessage: 'Error : File too large. Max size authorized is {maxSize}.',
+    description:
+      "Button's label explaining the 400 error that happens on large files upload.",
+    id: 'components.UploadVideoRetry.errorFileTooLarge',
+  },
 });
 
 interface UploadVideoRetryProps {
   onClickRetry: () => void;
+  maxSize?: number;
 }
 
-export const UploadVideoRetry = ({ onClickRetry }: UploadVideoRetryProps) => {
+export const UploadVideoRetry = ({
+  onClickRetry,
+  maxSize,
+}: UploadVideoRetryProps) => {
   const intl = useIntl();
+  let outputMessage: string;
+
+  if (maxSize) {
+    outputMessage = intl.formatMessage(messages.errorFileTooLarge, {
+      maxSize: formatSizeErrorScale(maxSize),
+    });
+  } else {
+    outputMessage = intl.formatMessage(messages.errorOccuredVideoUpload);
+  }
 
   return (
     <BigDashedBox direction="row">
       <Text color="red-active" size="1rem">
-        {intl.formatMessage(messages.errorOccuredVideoUpload)}
+        {outputMessage}
       </Text>
       <RetryUploadButton
         color="red-active"
