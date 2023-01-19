65c13744780528eff1a596ee5ce677bc152a9756
Vassili Rezvoy <vassili.rezvoy@polyconseil.fr>
2023-01-20 00:12:00 +0100

    💚(frontend) fix tests in LocalizedTimedTextTrackUpload/LanguageSelect
    Fix tests in LocalizedTimedTextTrackUpload/LanguageSelect in lib-video.

--- src/frontend/packages/lib_tests/src/intl.tsx
+++ src/frontend/packages/lib_tests/src/intl.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { IntlProvider } from 'react-intl';
+import { IntlProvider, ReactIntlErrorCode } from 'react-intl';
 
 export const wrapInIntlProvider = (Component: JSX.Element, locale = 'en') => (
   <IntlProvider
     locale={locale}
     onError={(err) => {
       // https://github.com/formatjs/formatjs/issues/465
-      if (err.code === 'MISSING_TRANSLATION') {
+      if (
+        err.code === ReactIntlErrorCode.MISSING_TRANSLATION ||
+        err.code === ReactIntlErrorCode.MISSING_DATA
+      ) {
         return;
       }
       throw err;
