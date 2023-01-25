9497a94eca4c51d5ea1ca753164253d58339d833
Nicolas Clerc <kernicpanel@nclerc.fr>
2023-01-25 13:03:54 +0100

    🦺(backend) add max size upload to classroom document metadata
    As we want to inform the user about the maximum uploadable file size,
    this value has been added to classroom document metadata.

---
+++ src/backend/marsha/bbb/metadata.py
@@ -0,0 +1,15 @@
+"""This module holds custom metadata class dedicated to Marsha."""
+from django.conf import settings
+
+from rest_framework.metadata import SimpleMetadata
+
+
+class ClassroomDocumentMetadata(SimpleMetadata):
+    """Metadata class dedicated to classroom document model."""
+
+    def determine_metadata(self, request, view):
+        """Adds classroom document info in metadata."""
+        metadata = super().determine_metadata(request, view)
+        metadata["upload_max_size_bytes"] = settings.CLASSROOM_DOCUMENT_SOURCE_MAX_SIZE
+
+        return metadata
