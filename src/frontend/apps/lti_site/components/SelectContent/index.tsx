d097938695902dc7e38379ab9f533ba05e64c286
Nicolas Clerc <kernicpanel@nclerc.fr>
2023-01-26 18:38:24 +0100

    🐛(frontend) use an id to attach ClipboardJS
    When we use several CopyClipboard on the same page, cliking on one
    triggers all toasts.
    Using an unique id instead of a class fixes this behavior.

--- src/frontend/apps/lti_site/components/SelectContent/index.tsx
+++ src/frontend/apps/lti_site/components/SelectContent/index.tsx
@@ -69,6 +69,7 @@ export const SelectContent = ({
     <Box pad="medium">
       <Box justify="center" align="center" direction="row">
         <CopyClipboard
+          copyId={`key-${playlist?.id}`}
           text={
             <FormattedMessage
               {...messages.playlistTitle}
