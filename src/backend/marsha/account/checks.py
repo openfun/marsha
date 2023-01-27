9c89fd390f8f76b72132c5742b9c859a69410628
Quentin BEY <quentin.bey@polyconseil.fr>
2023-01-27 11:55:39 +0100

    🔧(backend) editable SAML FER teacher role lookup
    This allow to customize the organization teachers' roles
    to look for.
    The default value is still `teacher` but we can now add
    more roles to the list in production, such as:
    
    - `faculty`
    - `employee`
    - `staff`

---
+++ src/backend/marsha/account/checks.py
@@ -0,0 +1,35 @@
+"""Django checks for the account application of project marsha."""
+from django.conf import settings
+from django.core.checks import Warning as ChecksWarning
+
+from social_edu_federation.defaults import EduPersonAffiliationEnum
+
+
+def teacher_role_setting_check(*args, **kwargs):  # pylint: disable=unused-argument
+    """
+    Check that the SOCIAL_AUTH_SAML_FER_TEACHER_ROLES setting defines
+    a list with expected values.
+    """
+    existing_roles = sorted(
+        [role.value for role in EduPersonAffiliationEnum.__members__.values()]
+    )
+
+    if not all(
+        role in existing_roles for role in settings.SOCIAL_AUTH_SAML_FER_TEACHER_ROLES
+    ):
+        missing_roles = set(settings.SOCIAL_AUTH_SAML_FER_TEACHER_ROLES) - set(
+            existing_roles
+        )
+        return [
+            ChecksWarning(
+                f"{missing_roles} not available to detect teacher through SAML FER.",
+                hint=(
+                    "You should consider fixing the `SOCIAL_AUTH_SAML_FER_TEACHER_ROLES`"
+                    f" setting to use one of `{existing_roles}` value."
+                ),
+                obj="marsha.account.social_pipeline.organization.create_organization_from_saml",
+                id="marsha.account.social_pipeline.W001",
+            )
+        ]
+
+    return []
--- src/backend/marsha/settings.py
+++ src/backend/marsha/settings.py
@@ -573,6 +573,7 @@ class Base(Configuration):
         "social_edu_federation.django.metadata_store.CachedMetadataStore"
     )
     SOCIAL_AUTH_SAML_FER_REDIRECT_IS_HTTPS = values.BooleanValue(False)
+    SOCIAL_AUTH_SAML_FER_TEACHER_ROLES = values.ListValue(["teacher"])
 
     SOCIAL_AUTH_SAML_FER_SECURITY_CONFIG = {
         "authnRequestsSigned": values.BooleanValue(
