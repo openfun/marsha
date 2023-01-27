663507e1b7fedd717115c0b8a09f0f597d1043a8
Quentin BEY <quentin.bey@polyconseil.fr>
2023-01-27 11:55:39 +0100

    🔧(backend) editable SAML FER teacher role lookup
    This allow to customize the organization teachers' roles
    to look for.
    The default value is still `teacher` but we can now add
    more roles to the list in production, such as:
    
    - `faculty`
    - `employee`
    - `staff`

---
+++ src/backend/marsha/account/tests/test_checks.py
@@ -0,0 +1,37 @@
+"""Tests the `account` application checks."""
+from django.core.checks import Warning as ChecksWarning
+from django.test import TestCase, override_settings
+
+from ..checks import teacher_role_setting_check
+
+
+class CheckTestCase(TestCase):
+    """Test case for the `account` application checks."""
+
+    @override_settings(SOCIAL_AUTH_SAML_FER_TEACHER_ROLES=["teacher"])
+    def test_teacher_role_setting_properly_defined(self):
+        """Asserts the check returns an empty list when the setting is properly defined."""
+        self.assertListEqual(teacher_role_setting_check(), [])
+
+    @override_settings(SOCIAL_AUTH_SAML_FER_TEACHER_ROLES=["teacher", "invalid"])
+    def test_teacher_role_setting_with_invalid_value(self):
+        """Asserts the check returns a warning when the setting is not properly defined."""
+        self.assertListEqual(
+            teacher_role_setting_check(),
+            [
+                ChecksWarning(
+                    "{'invalid'} not available to detect teacher through SAML FER.",
+                    hint=(
+                        "You should consider fixing the `SOCIAL_AUTH_SAML_FER_TEACHER_ROLES`"
+                        " setting to use one of `['affiliate', 'alum', 'emeritus', 'employee',"
+                        " 'faculty', 'library-walk-in', 'member', 'registered-reader',"
+                        " 'researcher', 'retired', 'staff', 'student', 'teacher']` value."
+                    ),
+                    obj=(
+                        "marsha.account.social_pipeline.organization"
+                        ".create_organization_from_saml"
+                    ),
+                    id="marsha.account.social_pipeline.W001",
+                )
+            ],
+        )
