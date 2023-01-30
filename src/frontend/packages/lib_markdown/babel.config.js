db93ba83cf1d26052a9322b3bd2e9fc3f7f4f749
Nicolas Clerc <kernicpanel@nclerc.fr>
2023-01-30 10:54:22 +0100

    📦️(lib-markdown) create package lib_markdown
    Create a new package called lib-markdown based on lib-classroom.
    This package will contain everything related to the markdown editor.

---
+++ src/frontend/packages/lib_markdown/babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: [
+    [
+      'formatjs',
+      {
+        extractFromFormatMessageCall: true,
+        idInterpolationPattern: '[sha512:contenthash:base64:6]',
+        ast: true,
+      },
+    ],
+    '@babel/proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-runtime',
+  ],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        corejs: 3,
+        forceAllTransforms: true,
+        targets: 'last 1 version, >0.2%, not IE <= 11',
+        useBuiltIns: 'entry',
+      },
+    ],
+    '@babel/preset-typescript',
+    'react',
+  ],
+};
