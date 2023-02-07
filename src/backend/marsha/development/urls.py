ab19ae0d49fecbf17330528479f0d87f131ec2e4
Manuel Raynaud <manu@raynaud.io>
2023-02-07 21:10:29 +0100

    🔥(back) remove service-worker view in development
    We missed to remove the service worker view in development. We must
    remove it.

--- src/backend/marsha/development/urls.py
+++ src/backend/marsha/development/urls.py
@@ -2,12 +2,11 @@
 
 from django.urls import path
 
-from .views import DevelopmentLTIView, service_worker_view
+from .views import DevelopmentLTIView
 
 
 app_name = "development"
 
 urlpatterns = [
     path("development/", DevelopmentLTIView.as_view(), name="lti-development-view"),
-    path("service-worker.js", service_worker_view, name="service-worker-view"),
 ]
