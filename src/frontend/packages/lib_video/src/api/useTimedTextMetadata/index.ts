c5b3645ab26ca18235b7b485da366fe2ee89ecf2
alfredpichard <alfred.pichard@polyconseil.fr>
2023-02-09 15:30:50 +0100

    ✨(frontend) handle timedtext metadata
    timedtext metadata to retrieve max upload size from the settings

---
+++ src/frontend/packages/lib_video/src/api/useTimedTextMetadata/index.ts
@@ -0,0 +1,29 @@
+import { metadata } from 'lib-components';
+import { useQuery, UseQueryOptions } from 'react-query';
+
+import { TimedTextMetadata } from 'types/metadata';
+
+export const useTimedTextMetadata = (
+  locale: string,
+  queryConfig?: UseQueryOptions<
+    TimedTextMetadata,
+    'timedtexttracks',
+    TimedTextMetadata,
+    string[]
+  >,
+) => {
+  const key = ['timedtexttracks', locale];
+  return useQuery<
+    TimedTextMetadata,
+    'timedtexttracks',
+    TimedTextMetadata,
+    string[]
+  >(key, metadata, {
+    refetchInterval: false,
+    refetchIntervalInBackground: false,
+    refetchOnWindowFocus: false,
+    cacheTime: Infinity,
+    staleTime: Infinity,
+    ...queryConfig,
+  });
+};
