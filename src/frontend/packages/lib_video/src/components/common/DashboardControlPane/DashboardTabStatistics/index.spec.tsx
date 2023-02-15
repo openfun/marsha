d37b9c460b257597e84bb2c42e8a0be307354097
Manuel Raynaud <manu@raynaud.io>
2023-02-15 14:23:40 +0100

    ♻️(front) change DashboardTabStatistics label to display viewers
    In the DashboardTabStatistics we are displaying the number of viewers
    for a video, not the number of people present on the whole webinar. Also
    the component is a bit refactor and test is added.

---
+++ src/frontend/packages/lib_video/src/components/common/DashboardControlPane/DashboardTabStatistics/index.spec.tsx
@@ -0,0 +1,74 @@
+import { screen } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+import { videoMockFactory } from 'lib-components';
+import { Deferred, render } from 'lib-tests';
+import React from 'react';
+
+import { wrapInVideo } from 'utils/wrapInVideo';
+
+import { DashboardTabStatistics } from '.';
+
+describe('DashboardTabStatistics', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+  it('displays the number of viewers', async () => {
+    const video = videoMockFactory();
+
+    const deferred = new Deferred();
+
+    fetchMock.mock(`/api/videos/${video.id}/stats/`, deferred.promise);
+
+    render(wrapInVideo(<DashboardTabStatistics />, video));
+
+    // Loader
+    expect(screen.getByRole('status')).toBeInTheDocument();
+
+    deferred.resolve({
+      nb_views: 3,
+    });
+
+    expect(await screen.findByText('Viewers')).toBeInTheDocument();
+    expect(screen.getByText(3)).toBeInTheDocument();
+  });
+
+  it('displays one viewer', async () => {
+    const video = videoMockFactory();
+
+    const deferred = new Deferred();
+
+    fetchMock.mock(`/api/videos/${video.id}/stats/`, deferred.promise);
+
+    render(wrapInVideo(<DashboardTabStatistics />, video));
+
+    // Loader
+    expect(screen.getByRole('status')).toBeInTheDocument();
+
+    deferred.resolve({
+      nb_views: 1,
+    });
+
+    expect(await screen.findByText('Viewer')).toBeInTheDocument();
+    expect(screen.getByText(1)).toBeInTheDocument();
+  });
+
+  it('displays no viewer', async () => {
+    const video = videoMockFactory();
+
+    const deferred = new Deferred();
+
+    fetchMock.mock(`/api/videos/${video.id}/stats/`, deferred.promise);
+
+    render(wrapInVideo(<DashboardTabStatistics />, video));
+
+    // Loader
+    expect(screen.getByRole('status')).toBeInTheDocument();
+
+    deferred.resolve({
+      nb_views: 0,
+    });
+
+    expect(await screen.findByText('Viewers')).toBeInTheDocument();
+    expect(screen.getByText(0)).toBeInTheDocument();
+  });
+});
