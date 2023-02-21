aec8d3f8778bc63ee71d245fd9b67d610445463e
Nicolas Clerc <kernicpanel@nclerc.fr>
2023-02-21 09:10:29 +0100

    ♻️(frontend) extract roles options to component
    As role options will be used in several components, it has been
    extracted.

---
+++ src/frontend/apps/standalone_site/src/features/Playlist/features/UpdatePlaylist/components/UserRoleOptions.tsx
@@ -0,0 +1,36 @@
+import { defineMessages, IntlShape } from 'react-intl';
+
+import { PlaylistRole } from '../types/playlistAccess';
+
+const messages = defineMessages({
+  adminLabel: {
+    defaultMessage: 'Administrator',
+    description: 'Administrator role label in playlist access select.',
+    id: 'features.Playlist.features.UpdatePlaylist.components.UserRoleOptions.adminLabel',
+  },
+  instructorLabel: {
+    defaultMessage: 'Instructor',
+    description: 'Instructor role label in playlist access select.',
+    id: 'features.Playlist.features.UpdatePlaylist.components.UserRoleOptions.instructorLabel',
+  },
+  studentLabel: {
+    defaultMessage: 'Student',
+    description: 'Student role label in playlist access select.',
+    id: 'features.Playlist.features.UpdatePlaylist.components.UserRoleOptions.studentLabel',
+  },
+});
+
+export const userRoleOptions = (intl: IntlShape) => [
+  {
+    label: intl.formatMessage(messages.adminLabel),
+    key: PlaylistRole.ADMINISTRATOR,
+  },
+  {
+    label: intl.formatMessage(messages.instructorLabel),
+    key: PlaylistRole.TEACHER,
+  },
+  {
+    label: intl.formatMessage(messages.studentLabel),
+    key: PlaylistRole.STUDENT,
+  },
+];
