9da1bf0862a219bb182c453dd3c54f33f0027d9d
ALC Consulting <freelance.anthony@gmail.com>
2023-02-22 11:48:03 +0100

    ♻️(website) refacto isDesktop property from useResponsive hook
    - Change the way we detect if the user is on a desktop or not.
    - Add tests

--- src/frontend/apps/standalone_site/src/hooks/useResponsive.tsx
+++ src/frontend/apps/standalone_site/src/hooks/useResponsive.tsx
@@ -2,16 +2,15 @@ import { ResponsiveContext, ThemeContext, ThemeType } from 'grommet';
 import { useCallback, useContext } from 'react';
 
 export const useResponsive = () => {
-  const listSmallerScreen = ['small', 'xsmall', 'xxsmall'];
   const breakpoint = useContext(ResponsiveContext);
   const theme = useContext(ThemeContext) as ThemeType;
 
   const isSmallerBreakpoint = useCallback(
     (breakpointSmaller: string, breakpointBigger: string) => {
       const breakpointSmallerValue =
-        theme.global?.breakpoints?.[breakpointSmaller]?.value;
+        theme?.global?.breakpoints?.[breakpointSmaller]?.value;
       const breakpointBiggerValue =
-        theme.global?.breakpoints?.[breakpointBigger]?.value;
+        theme?.global?.breakpoints?.[breakpointBigger]?.value;
 
       if (!breakpointSmallerValue || !breakpointBiggerValue) {
         return false;
@@ -23,7 +22,7 @@ export const useResponsive = () => {
   );
 
   return {
-    isDesktop: !listSmallerScreen.includes(breakpoint),
+    isDesktop: !isSmallerBreakpoint(breakpoint, 'xsmedium'),
     breakpoint,
     isSmallerBreakpoint,
   };
