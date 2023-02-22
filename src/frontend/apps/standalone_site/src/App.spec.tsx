54a54e3158c4c506ce579a4f2f464577b483da75
Anthony LC <anthony.le-courric@polyconseil.fr>
2023-02-22 11:51:55 +0100

    ✅(website) add missing api mock on App test
    A mock to the classroom api call was missing in the App test.
    This commit adds it.

--- src/frontend/apps/standalone_site/src/App.spec.tsx
+++ src/frontend/apps/standalone_site/src/App.spec.tsx
@@ -15,11 +15,30 @@ const consoleWarn = jest
 window.scrollTo = jest.fn();
 window.isCDNLoaded = true;
 
+const someResponse = {
+  count: 1,
+  next: null,
+  previous: null,
+  results: [
+    {
+      id: '1234',
+      title: 'some title',
+      description: 'some description',
+      welcome_text: 'some welcome classroom',
+      ended: true,
+      starting_at: '2022-10-18T11:00:00Z',
+      estimated_duration: '01:23:00',
+    },
+  ],
+};
+
 describe('<App />', () => {
   beforeEach(() => {
     useCurrentUser.setState({
       currentUser: undefined,
     });
+
+    fetchMock.get('/api/classrooms/?limit=5&offset=0', someResponse);
   });
 
   afterEach(() => {
@@ -41,6 +60,9 @@ describe('<App />', () => {
       screen.getByRole(/menuitem/i, { name: /My playlists/i }),
     ).toBeInTheDocument();
     expect(consoleWarn).toHaveBeenCalled();
+    expect(
+      await screen.findByText(/some welcome classroom/i),
+    ).toBeInTheDocument();
   });
 
   test('renders with another language', async () => {
@@ -65,6 +87,9 @@ describe('<App />', () => {
     expect(await screen.findByText(/John Doe/i)).toBeInTheDocument();
     expect(
       screen.getByRole(/menuitem/i, { name: /Mon Tableau de bord/i }),
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText(/some welcome classroom/i),
     ).toBeInTheDocument();
   });
 });
