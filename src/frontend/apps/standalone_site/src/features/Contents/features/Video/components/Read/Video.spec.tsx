81cb30e3a3a7214f9553e28cd74657f3fcc6e644
ALC Consulting <freelance.anthony@gmail.com>
2023-03-01 14:22:05 +0100

    💄(website) prioritize manual thumbnail over automated one
    When a video is uploaded on our website, we automatically generate a
    thumbnail for it. We can also manually select a thumbnail for a video.
    If a video has both an automated and a manual thumbnail, we want to
    prioritize the manual one and display it in the related video component.

--- src/frontend/apps/standalone_site/src/features/Contents/features/Video/components/Read/Video.spec.tsx
+++ src/frontend/apps/standalone_site/src/features/Contents/features/Video/components/Read/Video.spec.tsx
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/react';
-import { videoMockFactory } from 'lib-components';
+import { thumbnailMockFactory, videoMockFactory } from 'lib-components';
 import { render } from 'lib-tests';
 
 import Video from './Video';
@@ -8,21 +8,56 @@ describe('<Video />', () => {
   test('renders Video', () => {
     const video = videoMockFactory({
       id: '4321',
-      title: 'Nouvelle video title',
-      description: 'Nouvelle video description',
+      title: 'New video title',
+      description: 'New video description',
       playlist: {
         ...videoMockFactory().playlist,
-        title: 'Nouvelle playlist title',
+        title: 'New playlist title',
       },
     });
     render(<Video video={video} />);
 
-    expect(screen.getByText('Nouvelle video title')).toBeInTheDocument();
-    expect(screen.getByText('Nouvelle video description')).toBeInTheDocument();
-    expect(screen.getByText('Nouvelle playlist title')).toBeInTheDocument();
+    expect(
+      screen.getByRole('img', {
+        name: 'thumbnail',
+      }),
+    ).toHaveStyle(
+      `background: url(https://example.com/default_thumbnail/240) no-repeat center / cover`,
+    );
+    expect(screen.getByText('New video title')).toBeInTheDocument();
+    expect(screen.getByText('New video description')).toBeInTheDocument();
+    expect(screen.getByText('New playlist title')).toBeInTheDocument();
     expect(screen.getByRole('link')).toHaveAttribute(
       'href',
       '/my-contents/videos/4321',
+    );
+  });
+
+  test('renders thumbnail', () => {
+    const video = videoMockFactory({
+      id: '4321',
+      title: 'New video title',
+      description: 'New video description',
+      playlist: {
+        ...videoMockFactory().playlist,
+        title: 'New playlist title',
+      },
+      thumbnail: {
+        ...thumbnailMockFactory(),
+        urls: {
+          ...thumbnailMockFactory().urls,
+          240: 'https://example.com/240',
+        },
+      },
+    });
+    render(<Video video={video} />);
+
+    expect(
+      screen.getByRole('img', {
+        name: 'thumbnail',
+      }),
+    ).toHaveStyle(
+      `background: url(https://example.com/240) no-repeat center / cover`,
     );
   });
 });
