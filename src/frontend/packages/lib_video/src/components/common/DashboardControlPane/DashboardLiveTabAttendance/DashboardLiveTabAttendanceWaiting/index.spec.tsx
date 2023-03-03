3237c056cdd4d04b60b7fa25fc6cd4ba735918be
Quentin BEY <quentin.bey@polyconseil.fr>
2023-03-03 18:26:43 +0100

    ⚰️(frontend) remove unused list_attendances mock
    The test does not requires to mocke this call...

--- src/frontend/packages/lib_video/src/components/common/DashboardControlPane/DashboardLiveTabAttendance/DashboardLiveTabAttendanceWaiting/index.spec.tsx
+++ src/frontend/packages/lib_video/src/components/common/DashboardControlPane/DashboardLiveTabAttendance/DashboardLiveTabAttendanceWaiting/index.spec.tsx
@@ -1,7 +1,5 @@
 import { screen } from '@testing-library/react';
-import fetchMock from 'fetch-mock';
-import { useJwt } from 'lib-components';
-import { render, Deferred } from 'lib-tests';
+import { render } from 'lib-tests';
 import React from 'react';
 
 import { DashboardLiveTabAttendanceWaiting } from '.';
@@ -19,26 +17,8 @@ jest.mock('lib-components', () => ({
 }));
 
 describe('<DashboardLiveTabAttendanceWaiting />', () => {
-  jest.spyOn(console, 'error').mockImplementation(() => jest.fn());
-
-  beforeEach(() => {
-    useJwt.setState({
-      jwt: 'json web token',
-    });
-  });
-
-  afterEach(() => fetchMock.restore());
-
   it('displays the default message when there is no attendance', async () => {
-    const deferred = new Deferred();
-    fetchMock.mock(
-      '/api/livesessions/list_attendances/?limit=999',
-      deferred.promise,
-    );
-
     render(<DashboardLiveTabAttendanceWaiting />);
-
-    deferred.resolve({ count: 0, next: null, previous: null, results: [] });
 
     expect(
       await screen.findByText('The live has no participant yet'),
