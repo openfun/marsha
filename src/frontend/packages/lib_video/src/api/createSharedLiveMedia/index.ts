dac0beddc81b15e2275969f583c25c86937e36b4
ALC Consulting <freelance.anthony@gmail.com>
2023-03-07 13:44:47 +0100

    ♻️(lib-video) add videoId to the create shared live media API
    Before that the video id was determined from the token, but in the website
    the token doesn't have this information. So we need to pass it
    explicitly to the API.

--- src/frontend/packages/lib_video/src/api/createSharedLiveMedia/index.ts
+++ src/frontend/packages/lib_video/src/api/createSharedLiveMedia/index.ts
@@ -3,10 +3,18 @@ import {
   useJwt,
   API_ENDPOINT,
   SharedLiveMedia,
+  fetchResponseHandler,
 } from 'lib-components';
 
-export const createSharedLiveMedia = async () => {
+interface createSharedLiveMediaBody {
+  video: string;
+}
+
+export const createSharedLiveMedia = async (
+  body: createSharedLiveMediaBody,
+): Promise<SharedLiveMedia> => {
   const response = await fetchWrapper(`${API_ENDPOINT}/sharedlivemedias/`, {
+    body: JSON.stringify(body),
     headers: {
       Authorization: `Bearer ${useJwt.getState().jwt ?? ''}`,
       'Content-Type': 'application/json',
@@ -14,11 +22,7 @@ export const createSharedLiveMedia = async () => {
     method: 'POST',
   });
 
-  if (!response.ok) {
-    throw new Error('Failed to create a new shared live media.');
-  }
-
-  const sharedLiveMedia = (await response.json()) as SharedLiveMedia;
-
-  return sharedLiveMedia;
+  return await fetchResponseHandler(response, {
+    errorMessage: `Failed to create a new shared live media.`,
+  });
 };
