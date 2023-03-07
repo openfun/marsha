d11b8c2803e2de5af80f36255acaebb783c00cb9
Anthony LC <anthony.le-courric@polyconseil.fr>
2023-03-07 13:46:46 +0100

    ♻️(lib-video) add videoId to the createTimedTextTrack API
    Before that the video id was determined from the token, but in the website
    the token doesn't have this information. So we need to pass it
    explicitly to the API.

--- src/frontend/packages/lib_video/src/components/common/VideoWidgetProvider/widgets/UploadClosedCaptions/index.spec.tsx
+++ src/frontend/packages/lib_video/src/components/common/VideoWidgetProvider/widgets/UploadClosedCaptions/index.spec.tsx
@@ -1,11 +1,12 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import fetchMock from 'fetch-mock';
-import { useJwt } from 'lib-components';
+import { useJwt, videoMockFactory } from 'lib-components';
 import { render } from 'lib-tests';
 import React from 'react';
 
 import { useInfoWidgetModal } from 'hooks/useInfoWidgetModal';
+import { wrapInVideo } from 'utils/wrapInVideo';
 
 import { UploadClosedCaptions } from '.';
 
@@ -47,7 +48,7 @@ describe('<UploadClosedCaptions />', () => {
       mockSetInfoWidgetModalProvider,
     ]);
 
-    render(<UploadClosedCaptions />);
+    render(wrapInVideo(<UploadClosedCaptions />, videoMockFactory()));
 
     screen.getByText('Closed captions');
 
