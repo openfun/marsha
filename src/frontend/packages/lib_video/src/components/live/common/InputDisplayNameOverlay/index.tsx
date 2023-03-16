54585020e21f171e6ca59e30585ba75ee63a55de
ALC Consulting <freelance.anthony@gmail.com>
2023-03-16 11:48:51 +0100

    ⚰️(frontend) remove unused messages
    Some messages were not used inside components. We removed them.

--- src/frontend/packages/lib_video/src/components/live/common/InputDisplayNameOverlay/index.tsx
+++ src/frontend/packages/lib_video/src/components/live/common/InputDisplayNameOverlay/index.tsx
@@ -13,59 +13,6 @@ const messages = defineMessages({
     description: 'A title describing the close button action',
     id: 'components.InputDiplayNameOverlay.closeButtonTitle',
   },
-  inputAnonymousKeywordForbiddenAlertMessage: {
-    defaultMessage: 'Keyword "{forbiddenPrefix}" is not allowed.',
-    description:
-      'An alert message explaining why the entered display name is invalid.',
-    id: 'components.InputDisplayNameOverlay.inputAnonymousKeywordForbiddenAlertMessage',
-  },
-  inputDisplayNameInformative: {
-    defaultMessage:
-      "The display name is the pseudonym you will be authenticated with on this live. You won't be able to change it during the live. Other participants will see you with this name. The instructor will however be able to see your genuine identity if you have previously identified yourself with the LTI.",
-    description:
-      'An informative text about the display name which is asked to enter.',
-    id: 'components.InputDisplayNameOverlay.inputDisplayNameInformative',
-  },
-  inputDisplayNameLabel: {
-    defaultMessage: 'Display name',
-    description: 'An label describing the input below.',
-    id: 'components.InputDisplayNameOverlay.inputDisplayNameLabel',
-  },
-  inputDisplayNamePlaceholder: {
-    defaultMessage: 'Enter your desired display name...',
-    description: 'The input bar to fill your display name.',
-    id: 'components.InputBar.inputDisplayNamePlaceholder',
-  },
-  inputTooShortAlertMessage: {
-    defaultMessage: 'Min length is {minLength} characters.',
-    description:
-      'An alert message explaining why the entered display name is invalid.',
-    id: 'components.InputDisplayNameOverlay.inputTooShortAlertMessage',
-  },
-  inputTooLongAlertMessage: {
-    defaultMessage: 'Max length is {maxLength} characters.',
-    description:
-      'An alert message explaining why the entered display name is invalid.',
-    id: 'components.InputDisplayNameOverlay.inputTooLongAlertMessage',
-  },
-  inputXmppError: {
-    defaultMessage: 'Impossible to connect you to the chat. Please retry.',
-    description: 'An alert message saying that the user cannot be connected.',
-    id: 'components.InputDisplayNameOverlay.inputXmppError',
-  },
-  inputXmppTimeout: {
-    defaultMessage: 'The server took too long to respond. Please retry.',
-    description:
-      'An alert message saying that the servers answer to the nickname change request took too much time.',
-    id: 'components.InputDisplayNameOverlay.inputXmppTimeout',
-  },
-  inputNicknameAlreadyExists: {
-    defaultMessage:
-      'Your nickname is already used in the chat. Please choose another one.',
-    description:
-      "An alert message saying that the user can't select the entered nickname because someone is already using it in the chat.",
-    id: 'components.InputDisplayNameOverlay.inputNicknameAlreadyExists',
-  },
 });
 
 export const InputDisplayNameOverlay = () => {
