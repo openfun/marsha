b57e3edb8ffe49433842e42092404bc286cac20d
alfredpichard <alfred.pichard@polyconseil.fr>
2023-03-22 16:08:16 +0100

    ✨(frontend) update the classroom dashboard form
    use the new classroom widget provider in the classroom creation form

--- src/frontend/packages/lib_classroom/src/components/DashboardClassroomForm/index.tsx
+++ src/frontend/packages/lib_classroom/src/components/DashboardClassroomForm/index.tsx
@@ -1,18 +1,33 @@
-import { Button } from 'grommet';
+import { Box, Button, Tab, Tabs, ThemeContext } from 'grommet';
+import { normalizeColor } from 'grommet/utils';
+import { theme } from 'lib-common';
 import { Classroom } from 'lib-components';
 import React from 'react';
-import { toast } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 import { defineMessages, useIntl } from 'react-intl';
+import styled from 'styled-components';
 
-import { ClassroomForm } from '@lib-classroom/components/ClassroomForm';
-import { DashboardClassroomLayout } from '@lib-classroom/components/DashboardClassroomLayout';
-import { useCreateClassroomAction } from '@lib-classroom/data/queries';
+import { ClassroomInfoBar } from 'components/ClassroomInfoBar';
+import { ClassroomWidgetProvider } from 'components/ClassroomWidgetProvider';
+import { useCreateClassroomAction } from 'data/queries';
+import { CurrentClassroomProvider } from 'hooks/useCurrentClassroom';
+
+const StyledClassroomInformationBarWrapper = styled(Box)`
+  -webkit-box-shadow: 0px 0px 7px 5px ${normalizeColor('shadow-1', theme)};
+  box-shadow: 0px 0px 7px 5px ${normalizeColor('shadow-1', theme)};
+`;
 
 const messages = defineMessages({
   createClassroomFail: {
     defaultMessage: 'Classroom not created!',
     description: 'Message when classroom creation failed.',
     id: 'component.DashboardClassroomForm.createClassroomFail',
+  },
+  titleConfiguration: {
+    defaultMessage: 'Configuration',
+    description:
+      'Title of the tab used to configure the live in capital letters',
+    id: 'components.DashboardClassroomForm.titleConfiguration',
   },
   startClassroomLabel: {
     defaultMessage: 'Launch the classroom now in BBB',
@@ -27,6 +42,33 @@ interface DashboardClassroomFormProps {
 
 const DashboardClassroomForm = ({ classroom }: DashboardClassroomFormProps) => {
   const intl = useIntl();
+  const extendedTheme = {
+    tabs: {
+      header: {
+        extend: `button * { \
+          font-size: 16px; \
+        }`,
+      },
+    },
+    tab: {
+      extend: ` color:${normalizeColor('blue-active', theme)};\
+        font-family: 'Roboto-Bold';\
+        height: 21px;\
+        letter-spacing: -0.36px;\
+        padding-bottom:35px;\
+        padding-left:50px;\
+        padding-right:50px;\
+        padding-top:15px;\
+        text-align: center;\
+        text-transform: uppercase; \
+        `,
+      border: {
+        color: 'inherit',
+        size: 'medium',
+      },
+    },
+  };
+
   const createClassroomMutation = useCreateClassroomAction(classroom.id, {
     onSuccess: (data) => {
       toast.success(data.message);
@@ -36,25 +78,43 @@ const DashboardClassroomForm = ({ classroom }: DashboardClassroomFormProps) => {
     },
   });
 
-  const right = (
-    <Button
-      type="submit"
-      label={intl.formatMessage(messages.startClassroomLabel)}
-      disabled={!classroom.title}
-      primary
-      size="large"
-      fill="horizontal"
-      onClick={() => {
-        createClassroomMutation.mutate(classroom);
-      }}
-    />
-  );
-
   return (
-    <DashboardClassroomLayout
-      left={<ClassroomForm classroom={classroom} />}
-      right={right}
-    />
+    <CurrentClassroomProvider value={classroom}>
+      <Box background={{ color: 'bg-marsha' }} fill>
+        <StyledClassroomInformationBarWrapper
+          align="center"
+          background="white"
+          direction="row-responsive"
+          height="80px"
+          justify="between"
+          margin="small"
+          pad={{
+            vertical: 'small',
+            horizontal: 'medium',
+          }}
+          round="xsmall"
+        >
+          <ClassroomInfoBar flex startDate={classroom.starting_at} />
+          <Button
+            type="submit"
+            label={intl.formatMessage(messages.startClassroomLabel)}
+            disabled={!classroom.title}
+            primary
+            size="small"
+            onClick={() => {
+              createClassroomMutation.mutate(classroom);
+            }}
+          />
+        </StyledClassroomInformationBarWrapper>
+        <ThemeContext.Extend value={extendedTheme}>
+          <Tabs>
+            <Tab title={intl.formatMessage(messages.titleConfiguration)}>
+              <ClassroomWidgetProvider />
+            </Tab>
+          </Tabs>
+        </ThemeContext.Extend>
+      </Box>
+    </CurrentClassroomProvider>
   );
 };
 
