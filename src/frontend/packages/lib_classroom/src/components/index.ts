afca83ee9cb7e342ed3ca8486d4abe7368d23a75
alfredpichard <alfred.pichard@polyconseil.fr>
2023-03-22 16:08:16 +0100

    ✨(frontend) update the classroom dashboard form
    use the new classroom widget provider in the classroom creation form

--- src/frontend/packages/lib_classroom/src/components/index.ts
+++ src/frontend/packages/lib_classroom/src/components/index.ts
@@ -1,4 +1,3 @@
 export * from './UploadDocuments';
-export * from './ClassroomForm';
 export { default as DashboardClassroom } from './DashboardClassroom';
 export * from './DashboardClassroom/route';
