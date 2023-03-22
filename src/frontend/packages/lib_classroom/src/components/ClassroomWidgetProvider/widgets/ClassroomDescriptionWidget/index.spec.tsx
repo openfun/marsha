ddd0589b6f587cb5cbbc5ad0f8e22973d4179416
alfredpichard <alfred.pichard@polyconseil.fr>
2023-03-22 16:05:46 +0100

    ✨(frontend) add a classroom widget provider
    this widget provider is made with the same logic as videoWidgetProvider
    This commit also introduces a first widget designed to schedule a classroom

---
+++ src/frontend/packages/lib_classroom/src/components/ClassroomWidgetProvider/widgets/ClassroomDescriptionWidget/index.spec.tsx
@@ -0,0 +1,113 @@
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import fetchMock from 'fetch-mock';
+import { InfoWidgetModalProvider } from 'lib-components';
+import { render, Deferred } from 'lib-tests';
+import { Settings } from 'luxon';
+import React from 'react';
+
+import { classroomMockFactory } from 'utils/tests/factories';
+import { wrapInClassroom } from 'utils/wrapInClassroom';
+
+import { ClassroomDescriptionWidget } from '.';
+
+jest.mock('lib-components', () => ({
+  ...jest.requireActual('lib-components'),
+  useAppConfig: () => ({
+    modelName: 'classrooms',
+    resource: {
+      id: '1',
+    },
+  }),
+}));
+
+Settings.defaultLocale = 'en';
+Settings.defaultZone = 'Europe/Paris';
+
+describe('<ClassroomDescriptionWidget />', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+    fetchMock.restore();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it('renders the widget', () => {
+    const classroom = classroomMockFactory({ id: '1', started: false });
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/classrooms/1/create/', deferredPatch.promise);
+
+    render(
+      wrapInClassroom(
+        <InfoWidgetModalProvider value={null}>
+          <ClassroomDescriptionWidget />
+        </InfoWidgetModalProvider>,
+        classroom,
+      ),
+    );
+    expect(screen.getAllByText('Description')).toHaveLength(2);
+    expect(screen.getByText('Welcome text')).toBeInTheDocument();
+  });
+
+  it('schedules a classroom', async () => {
+    let classroom = classroomMockFactory({ id: '1', started: false });
+
+    const deferredPatch = new Deferred();
+    fetchMock.patch('/api/classrooms/1/', deferredPatch.promise);
+
+    const { rerender } = render(
+      wrapInClassroom(
+        <InfoWidgetModalProvider value={null}>
+          <ClassroomDescriptionWidget />
+        </InfoWidgetModalProvider>,
+        classroom,
+      ),
+    );
+
+    // simulate classroom update
+    const inputWelcomeText = screen.getByRole('textbox', {
+      name: /welcome text/i,
+    });
+    userEvent.type(inputWelcomeText, 'updated welcome text', {
+      initialSelectionStart: 0,
+      initialSelectionEnd: classroom.welcome_text.length,
+    });
+    fireEvent.blur(inputWelcomeText);
+
+    jest.runAllTimers();
+
+    deferredPatch.resolve({ message: 'Classroom scheduled.' });
+
+    await waitFor(() => expect(fetchMock.calls()).toHaveLength(2));
+
+    expect(fetchMock.calls()[1]![0]).toEqual('/api/classrooms/1/');
+    expect(fetchMock.calls()[1]![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PATCH',
+      body: JSON.stringify({
+        welcome_text: 'updated welcome text',
+      }),
+    });
+
+    // simulate classroom update
+    classroom = {
+      ...classroom,
+      title: 'updated title',
+      welcome_text: 'updated welcome text',
+    };
+    rerender(
+      wrapInClassroom(
+        <InfoWidgetModalProvider value={null}>
+          <ClassroomDescriptionWidget />
+        </InfoWidgetModalProvider>,
+        classroom,
+      ),
+    );
+  });
+});
---
+++ src/frontend/packages/lib_classroom/src/components/ClassroomWidgetProvider/widgets/ClassroomDescriptionWidget/index.tsx
@@ -0,0 +1,137 @@
+import { Form, FormField, TextArea } from 'grommet';
+import { Maybe } from 'lib-common';
+import { Classroom, debounce, FoldableItem } from 'lib-components';
+import React, { useEffect, useRef, useState } from 'react';
+import toast from 'react-hot-toast';
+import { defineMessages, useIntl } from 'react-intl';
+
+import { useUpdateClassroom } from 'data/queries';
+import { useCurrentClassroom } from 'hooks/useCurrentClassroom';
+
+const messages = defineMessages({
+  title: {
+    defaultMessage: 'Description',
+    description: 'A title for the widget.',
+    id: 'component.ClassroomDescription.title',
+  },
+  descriptionLabel: {
+    defaultMessage: 'Description',
+    description: 'Label for description in classroom creation form.',
+    id: 'component.ClassroomDescription.descriptionLabel',
+  },
+  info: {
+    defaultMessage:
+      'This widget can be used to set a description to the classroom.',
+    description: 'Helptext for the widget.',
+    id: 'component.ClassroomDescription.info',
+  },
+  welcomeTextLabel: {
+    defaultMessage: 'Welcome text',
+    description: 'Label for welcome text in classroom creation form.',
+    id: 'component.ClassroomDescription.welcomeTextLabel',
+  },
+  updateClassroomSuccess: {
+    defaultMessage: 'Classroom updated.',
+    description: 'Message when classroom update is successful.',
+    id: 'component.ClassroomDescription.updateClassroomSuccess',
+  },
+  updateClassroomFail: {
+    defaultMessage: 'Classroom not updated!',
+    description: 'Message when classroom update failed.',
+    id: 'component.ClassroomDescription.updateClassroomFail',
+  },
+});
+
+export const ClassroomDescriptionWidget = () => {
+  const classroom = useCurrentClassroom();
+  const intl = useIntl();
+  const updateClassroomMutation = useUpdateClassroom(classroom.id, {
+    onSuccess: () => {
+      toast.success(intl.formatMessage(messages.updateClassroomSuccess));
+    },
+    onError: () => {
+      toast.error(intl.formatMessage(messages.updateClassroomFail));
+      setUpdatedClassroomState(classroom);
+    },
+  });
+  const [updatedClassroomState, setUpdatedClassroomState] = useState<
+    Partial<Classroom>
+  >({});
+
+  const timeoutId = useRef<Maybe<number>>();
+
+  const debouncedUpdateClassroom = React.useRef(
+    debounce((updatedClassroom: Partial<Classroom>) => {
+      if (JSON.stringify(updatedClassroom) !== '{}') {
+        updateClassroomMutation.mutate(updatedClassroom);
+      }
+    }),
+  ).current;
+
+  const handleChange = (updatedClassroomAttribute: Partial<Classroom>) => {
+    const updatedClassroom = {
+      ...updatedClassroomState,
+      ...updatedClassroomAttribute,
+    };
+    setUpdatedClassroomState(updatedClassroom);
+    window.clearTimeout(timeoutId.current);
+    debouncedUpdateClassroom(updatedClassroom);
+  };
+
+  const handleBlur = () => {
+    if (JSON.stringify(updatedClassroomState) !== '{}') {
+      window.clearTimeout(timeoutId.current);
+      updateClassroomMutation.mutate(updatedClassroomState);
+    }
+  };
+
+  useEffect(() => {
+    setUpdatedClassroomState({});
+  }, [classroom]);
+
+  return (
+    <FoldableItem
+      infoText={intl.formatMessage(messages.info)}
+      initialOpenValue
+      title={intl.formatMessage(messages.title)}
+    >
+      <Form
+        value={{ ...classroom, ...updatedClassroomState }}
+        onBlur={handleBlur}
+      >
+        <FormField
+          label={intl.formatMessage(messages.descriptionLabel)}
+          htmlFor="description"
+          margin={{ bottom: 'medium' }}
+        >
+          <TextArea
+            name="description"
+            id="description"
+            value={{ ...classroom, ...updatedClassroomState }.description || ''}
+            onChange={(e) => {
+              handleChange({ description: e.target.value });
+            }}
+            resize="vertical"
+          />
+        </FormField>
+        <FormField
+          label={intl.formatMessage(messages.welcomeTextLabel)}
+          htmlFor="welcome_text"
+          margin={{ bottom: 'medium' }}
+        >
+          <TextArea
+            name="welcome_text"
+            id="welcome_text"
+            value={
+              { ...classroom, ...updatedClassroomState }.welcome_text || ''
+            }
+            onChange={(e) => {
+              handleChange({ welcome_text: e.target.value });
+            }}
+            resize="vertical"
+          />
+        </FormField>
+      </Form>
+    </FoldableItem>
+  );
+};
