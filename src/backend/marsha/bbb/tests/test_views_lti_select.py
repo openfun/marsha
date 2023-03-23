b7b84c5ac8b7f7733b0d75b77659e0cf8da9389f
Nicolas Clerc <kernicpanel@nclerc.fr>
2023-03-23 15:47:07 +0100

    ✨(backend) filter inactive resource for consumer site
    As we want each consumer site to deactivate any resource, they are
    filtered through inactive_resources attribute.

--- src/backend/marsha/bbb/tests/test_views_lti_select.py
+++ src/backend/marsha/bbb/tests/test_views_lti_select.py
@@ -8,7 +8,10 @@ from django.test import TestCase, override_settings
 
 from marsha.bbb.factories import ClassroomFactory
 from marsha.core.factories import PlaylistFactory
-from marsha.core.tests.testing_utils import generate_passport_and_signed_lti_parameters
+from marsha.core.tests.testing_utils import (
+    generate_passport_and_signed_lti_parameters,
+    reload_urlconf,
+)
 from marsha.core.utils.lti_select_utils import get_lti_select_resources
 
 
@@ -26,8 +29,16 @@ class SelectLTIViewTestCase(TestCase):
         get_lti_select_resources.cache_clear()
 
     @override_settings(BBB_ENABLED=True)
-    def test_views_lti_select_bbb_enabled(self):
-        """Frontend context flag should be enabled when flag is enabled."""
+    def test_views_lti_select_bbb_enabled_activated(self):
+        """
+        Frontend context flag should be enabled when flag is enabled
+        and resource is active.
+        """
+        # Force URLs reload to use BBB_ENABLED
+        # Some strange behavior if put in setUp
+        # Does not happen when running all tests.
+        reload_urlconf()
+
         lti_consumer_parameters = {
             "roles": random.choice(["instructor", "administrator"]),
             "content_item_return_url": "https://lti-consumer.site/lti",
@@ -37,6 +48,7 @@ class SelectLTIViewTestCase(TestCase):
             url="http://testserver/lti/select/",
             lti_parameters=lti_consumer_parameters,
         )
+
         playlist = PlaylistFactory(
             lti_id=lti_parameters.get("context_id"),
             consumer_site=passport.consumer_site,
@@ -58,13 +70,62 @@ class SelectLTIViewTestCase(TestCase):
         context = json.loads(unescape(match.group(1)))
 
         self.assertTrue(context.get("flags").get("BBB"))
+        self.assertTrue(context.get("flags").get("classroom"))
         self.assertEqual(
             context.get("classrooms")[0].get("lti_url"),
             f"http://testserver/lti/classrooms/{classroom.id}",
         )
 
+    @override_settings(BBB_ENABLED=True)
+    def test_views_lti_select_bbb_enabled_not_activated(self):
+        """
+        Frontend context flag should be disabled when flag is enabled
+        and resource is not activated.
+        """
+        # Force URLs reload to use BBB_ENABLED
+        # Some strange behavior if put in setUp
+        # Does not happen when running all tests.
+        reload_urlconf()
+
+        lti_consumer_parameters = {
+            "roles": random.choice(["instructor", "administrator"]),
+            "content_item_return_url": "https://lti-consumer.site/lti",
+            "context_id": "sent_lti_context_id",
+        }
+        lti_parameters, passport = generate_passport_and_signed_lti_parameters(
+            url="http://testserver/lti/select/",
+            lti_parameters=lti_consumer_parameters,
+        )
+
+        passport.consumer_site.inactive_resources = ["classroom"]
+        passport.consumer_site.save()
+
+        playlist = PlaylistFactory(
+            lti_id=lti_parameters.get("context_id"),
+            consumer_site=passport.consumer_site,
+        )
+        ClassroomFactory(playlist=playlist)
+
+        response = self.client.post(
+            "/lti/select/",
+            lti_parameters,
+            HTTP_REFERER="http://testserver",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<html>")
+
+        match = re.search(
+            '<div id="marsha-frontend-data" data-context="(.*)">',
+            response.content.decode("utf-8"),
+        )
+        context = json.loads(unescape(match.group(1)))
+
+        self.assertFalse(context.get("flags").get("BBB"))
+        self.assertFalse(context.get("flags").get("classroom"))
+        self.assertIsNone(context.get("classrooms"))
+
     @override_settings(BBB_ENABLED=False)
-    def test_views_lti_select_bbb_disabled(self):
+    def test_views_lti_select_bbb_disabled_activated(self):
         """Frontend context flag should be disabled when flag is disabled."""
         lti_consumer_parameters = {
             "roles": random.choice(["instructor", "administrator"]),
@@ -91,4 +152,5 @@ class SelectLTIViewTestCase(TestCase):
         context = json.loads(unescape(match.group(1)))
 
         self.assertFalse(context.get("flags").get("BBB"))
+        self.assertFalse(context.get("flags").get("classroom"))
         self.assertIsNone(context.get("classrooms"))
--- src/backend/marsha/markdown/tests/test_views_lti_select.py
+++ src/backend/marsha/markdown/tests/test_views_lti_select.py
@@ -7,7 +7,10 @@ import re
 from django.test import TestCase, override_settings
 
 from marsha.core.factories import PlaylistFactory
-from marsha.core.tests.testing_utils import generate_passport_and_signed_lti_parameters
+from marsha.core.tests.testing_utils import (
+    generate_passport_and_signed_lti_parameters,
+    reload_urlconf,
+)
 from marsha.core.utils.lti_select_utils import get_lti_select_resources
 
 from ..factories import MarkdownDocumentFactory
@@ -22,9 +25,15 @@ class SelectLTIViewTestCase(TestCase):
         super().setUp()
         get_lti_select_resources.cache_clear()
 
+        # Force URLs reload to use DEPOSIT_ENABLED
+        reload_urlconf()
+
     @override_settings(MARKDOWN_ENABLED=True)
-    def test_views_lti_select_markdown_enabled(self):
-        """Frontend context flag should be enabled when flag is enabled."""
+    def test_views_lti_select_markdown_enabled_activated(self):
+        """
+        Frontend context flag should be enabled when flag is enabled
+        and resource is active.
+        """
         lti_consumer_parameters = {
             "roles": random.choice(["instructor", "administrator"]),
             "content_item_return_url": "https://lti-consumer.site/lti",
@@ -34,6 +43,7 @@ class SelectLTIViewTestCase(TestCase):
             url="http://testserver/lti/select/",
             lti_parameters=lti_consumer_parameters,
         )
+
         playlist = PlaylistFactory(
             lti_id=lti_parameters.get("context_id"),
             consumer_site=passport.consumer_site,
@@ -60,8 +70,50 @@ class SelectLTIViewTestCase(TestCase):
             f"http://testserver/lti/markdown-documents/{markdown_document.id}",
         )
 
+    @override_settings(MARKDOWN_ENABLED=True)
+    def test_views_lti_select_markdown_enabled_not_activated(self):
+        """
+        Frontend context flag should be disabled when flag is enabled
+        and resource is not activated.
+        """
+        lti_consumer_parameters = {
+            "roles": random.choice(["instructor", "administrator"]),
+            "content_item_return_url": "https://lti-consumer.site/lti",
+            "context_id": "sent_lti_context_id",
+        }
+        lti_parameters, passport = generate_passport_and_signed_lti_parameters(
+            url="http://testserver/lti/select/",
+            lti_parameters=lti_consumer_parameters,
+        )
+
+        passport.consumer_site.inactive_resources = ["markdown"]
+        passport.consumer_site.save()
+
+        playlist = PlaylistFactory(
+            lti_id=lti_parameters.get("context_id"),
+            consumer_site=passport.consumer_site,
+        )
+        MarkdownDocumentFactory(playlist=playlist)
+
+        response = self.client.post(
+            "/lti/select/",
+            lti_parameters,
+            HTTP_REFERER="http://testserver",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<html>")
+
+        match = re.search(
+            '<div id="marsha-frontend-data" data-context="(.*)">',
+            response.content.decode("utf-8"),
+        )
+        context = json.loads(unescape(match.group(1)))
+
+        self.assertFalse(context.get("flags").get("markdown"))
+        self.assertIsNone(context.get("markdowns"))
+
     @override_settings(MARKDOWN_ENABLED=False)
-    def test_views_lti_select_markdown_disabled(self):
+    def test_views_lti_select_markdown_disabled_activated(self):
         """Frontend context flag should be disabled when flag is disabled."""
         lti_consumer_parameters = {
             "roles": random.choice(["instructor", "administrator"]),
