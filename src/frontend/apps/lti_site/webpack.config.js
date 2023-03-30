9be2e2857cccbbdd3935cf57ce7cfc5ad1172714
ALC Consulting <freelance.anthony@gmail.com>
2023-03-30 10:27:20 +0200

    🔧(marsha-config) marsha packages per apps
    Some apps does not need marsha packages in their config.
    This commit gives the possibility to choose which marsha packages
    will be included in the app config.

--- src/frontend/apps/lti_site/webpack.config.js
+++ src/frontend/apps/lti_site/webpack.config.js
@@ -41,7 +41,7 @@ const config = {
       buffer: require.resolve('buffer/'),
       stream: require.resolve('stream-browserify'),
     },
-    alias,
+    alias: alias['lti'],
   },
 
   module: {
--- src/frontend/packages/marsha-config/index.js
+++ src/frontend/packages/marsha-config/index.js
@@ -2,26 +2,34 @@ const path = require('path');
 
 const { packages: packagesConfig } = require('./config.json');
 
-const appPackages = [];
+const appPackages = {};
 const alias = {};
 const moduleNameMapper = {};
 
 for (const packageConfig of packagesConfig) {
   // This is needed for webpack to transpile the packages on the fly
-  appPackages.push(
-    path.join(__dirname, `../../packages/${packageConfig.folder}`),
-  );
+  for (const app of packageConfig.apps) {
+    if (!appPackages[app]) {
+      appPackages[app] = [];
+      alias[app] = {};
+      moduleNameMapper[app] = {};
+    }
 
-  // This is needed for webpack to work with the alias
-  alias[packageConfig.alias] = path.resolve(
-    __dirname,
-    `../../packages/${packageConfig.folder}/src/`,
-  );
+    appPackages[app].push(
+      path.join(__dirname, `../../packages/${packageConfig.folder}`),
+    );
 
-  // This is needed for jest to work with the alias
-  moduleNameMapper[
-    `${packageConfig.alias}/(.*)`
-  ] = `<rootDir>../../packages/${packageConfig.folder}/src/$1`;
+    // This is needed for webpack to work with the alias
+    alias[app][packageConfig.alias] = path.resolve(
+      __dirname,
+      `../../packages/${packageConfig.folder}/src/`,
+    );
+
+    // This is needed for jest to work with the alias
+    moduleNameMapper[app][
+      `${packageConfig.alias}/(.*)`
+    ] = `<rootDir>../../packages/${packageConfig.folder}/src/$1`;
+  }
 }
 
 module.exports = {
