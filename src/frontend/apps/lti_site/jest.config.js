a6d10bb2d80d88ac8be2168426f08ea7f9b08a66
ALC Consulting <freelance.anthony@gmail.com>
2023-04-03 10:56:28 +0200

    🔥(lib-common) remove rollup from lib_common
    We don't need to build lib_common anymore, it is transpile during
    the apps build.
    - review dependencies packages about lib_common
    - 🔥 all build relative dependencies from lib_common
    - 🔥 rollup.config.mjs from lib_common
    - adapt CI config and frontend package

--- src/frontend/apps/lti_site/jest.config.js
+++ src/frontend/apps/lti_site/jest.config.js
@@ -5,8 +5,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'css'],
   moduleNameMapper: {
     '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
-    'is-reference':
-      '<rootDir>/../../node_modules/is-reference/dist/is-reference.js',
+    'is-reference': '<rootDir>/../../node_modules/is-reference/src/index.js',
     'estree-walker': '<rootDir>/../../node_modules/estree-walker/src/index.js',
     ...appModuleNameMapper['lti'],
   },
