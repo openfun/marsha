3b917ca27ea6ebd102a7f607a8cdc67540b10bb9
Manuel Raynaud <manu@raynaud.io>
2023-04-25 14:38:14 +0200

    ♻️(libs) move edition icon on title input at the begining of the input
    The edition icon was at the end of the input. This input can be very
    large and we don't understand the icon anymore. By removing the reverse
    props, the icon is now at the beginning and we better understand what it
    means.

--- src/frontend/packages/lib_video/src/components/common/VideoInfoBar/index.tsx
+++ src/frontend/packages/lib_video/src/components/common/VideoInfoBar/index.tsx
@@ -161,7 +161,6 @@ export const VideoInfoBar = ({ isTeacher, startDate }: VideoInfoBarProps) => {
             title={intl.formatMessage(messages.placeholderTitleInput)}
             value={title}
             plain
-            reverse
             width="100%"
           />
         </StyledBoxTextInput>
