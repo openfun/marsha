61c331bde2cb25261bdfcf74c998f70ca7f4ec51
alfredpichard <alfred.pichard@polyconseil.fr>
2023-05-03 14:52:45 +0200

    ✨(frontend) add DeleteVideo widget
    this widget allows a playlist or organization admin to delete a video.
    It is only enabled on the standalone site because it does not make sense
    to do such an operation in LTI.

---
+++ src/frontend/packages/lib_video/src/components/common/VideoWidgetProvider/widgets/DeleteVideo/index.spec.tsx
@@ -0,0 +1,187 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import fetchMock from 'fetch-mock';
+import {
+  InfoWidgetModalProvider,
+  useCurrentResourceContext,
+  useJwt,
+  videoMockFactory,
+} from 'lib-components';
+import { render } from 'lib-tests';
+import React from 'react';
+
+import { wrapInVideo } from '@lib-video/utils/wrapInVideo';
+
+import { DeleteVideo } from '.';
+
+jest.mock('lib-components', () => ({
+  ...jest.requireActual('lib-components'),
+  report: jest.fn(),
+  useCurrentResourceContext: jest.fn(),
+}));
+
+const mockedUseCurrentResourceContext =
+  useCurrentResourceContext as jest.MockedFunction<
+    typeof useCurrentResourceContext
+  >;
+
+describe('<DeleteVideo />', () => {
+  beforeEach(() => {
+    useJwt.setState({
+      jwt: 'json web token',
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('does not render the component on LTI', () => {
+    mockedUseCurrentResourceContext.mockReturnValue([
+      {
+        isFromWebsite: false,
+        permissions: {
+          can_access_dashboard: true,
+          can_update: true,
+        },
+      },
+    ] as any);
+    const mockedVideo = videoMockFactory();
+    render(
+      wrapInVideo(
+        <InfoWidgetModalProvider value={null}>
+          <DeleteVideo />
+        </InfoWidgetModalProvider>,
+        mockedVideo,
+      ),
+    );
+
+    expect(screen.queryByText('DANGER ZONE')).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole('button', { name: 'Delete' }),
+    ).not.toBeInTheDocument();
+  });
+
+  it('renders the component on standalone site', () => {
+    mockedUseCurrentResourceContext.mockReturnValue([
+      {
+        isFromWebsite: true,
+        permissions: {
+          can_access_dashboard: true,
+          can_update: true,
+        },
+      },
+    ] as any);
+    const mockedVideo = videoMockFactory();
+    render(
+      wrapInVideo(
+        <InfoWidgetModalProvider value={null}>
+          <DeleteVideo />
+        </InfoWidgetModalProvider>,
+        mockedVideo,
+      ),
+    );
+
+    expect(screen.getByText('DANGER ZONE')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Delete' })).toBeInTheDocument();
+  });
+
+  it('successfully opens the confirmation modal', () => {
+    mockedUseCurrentResourceContext.mockReturnValue([
+      {
+        isFromWebsite: true,
+        permissions: {
+          can_access_dashboard: true,
+          can_update: true,
+        },
+      },
+    ] as any);
+    const mockedVideo = videoMockFactory();
+    render(
+      wrapInVideo(
+        <InfoWidgetModalProvider value={null}>
+          <DeleteVideo />
+        </InfoWidgetModalProvider>,
+        mockedVideo,
+      ),
+    );
+
+    const deleteButton = screen.getByRole('button', { name: 'Delete' });
+    userEvent.click(deleteButton);
+    expect(
+      screen.getByText(
+        'Are you sure you want to delete this resource ? This action is irreversible.',
+      ),
+    ).toBeInTheDocument();
+  });
+
+  it('successfully deletes the video', async () => {
+    mockedUseCurrentResourceContext.mockReturnValue([
+      {
+        isFromWebsite: true,
+        permissions: {
+          can_access_dashboard: true,
+          can_update: true,
+        },
+      },
+    ] as any);
+    const mockedVideo = videoMockFactory();
+    fetchMock.delete(`/api/videos/${mockedVideo.id}/`, 204);
+    render(
+      wrapInVideo(
+        <InfoWidgetModalProvider value={null}>
+          <DeleteVideo />
+        </InfoWidgetModalProvider>,
+        mockedVideo,
+      ),
+    );
+
+    const deleteButton = screen.getByRole('button', { name: 'Delete' });
+    userEvent.click(deleteButton);
+
+    const confirmDeleteButton = screen.getByRole('button', {
+      name: 'Confirm',
+    });
+    userEvent.click(confirmDeleteButton);
+
+    const successMessage = await screen.findByText(
+      'Resource successfully deleted',
+    );
+    expect(successMessage).toBeInTheDocument();
+  });
+
+  it('fails to delete the video', async () => {
+    mockedUseCurrentResourceContext.mockReturnValue([
+      {
+        isFromWebsite: true,
+        permissions: {
+          can_access_dashboard: true,
+          can_update: true,
+        },
+      },
+    ] as any);
+    const mockedVideo = videoMockFactory();
+    fetchMock.delete(`/api/videos/${mockedVideo.id}/`, 403);
+    render(
+      wrapInVideo(
+        <InfoWidgetModalProvider value={null}>
+          <DeleteVideo />
+        </InfoWidgetModalProvider>,
+        mockedVideo,
+      ),
+    );
+
+    const deleteButton = screen.getByRole('button', { name: 'Delete' });
+    userEvent.click(deleteButton);
+
+    const confirmDeleteButton = screen.getByRole('button', {
+      name: 'Confirm',
+    });
+    userEvent.click(confirmDeleteButton);
+
+    const errorMessage = await screen.findByText(
+      'Failed to delete the resource',
+    );
+    expect(errorMessage).toBeInTheDocument();
+  });
+});
