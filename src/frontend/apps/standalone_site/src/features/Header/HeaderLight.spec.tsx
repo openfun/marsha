44cac585bfabc061c299bad8c7cc9db9787abaa3
ALC Consulting <freelance.anthony@gmail.com>
2023-05-04 17:53:49 +0200

    ✨(website) prop withLogoLink on the HeaderLight
    We have some cases where we want to have a link to the login page
    on the HeaderLight component.
    This commit adds a prop to the HeaderLight to optionally
    have a link to the logo.
    Because of the implementation in MainLayout, we created the
    wrapper component HeaderLightLink that will be prefill the
    prop withLogoLink.

--- src/frontend/apps/standalone_site/src/features/Header/HeaderLight.spec.tsx
+++ src/frontend/apps/standalone_site/src/features/Header/HeaderLight.spec.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { render } from 'lib-tests';
 import React from 'react';
 
-import HeaderLight from './HeaderLight';
+import { HeaderLight, HeaderLightLink } from './HeaderLight';
 
 jest.mock('assets/svg/logo_marsha.svg', () => ({
   ReactComponent: () => <div>My LogoIcon</div>,
@@ -12,5 +13,20 @@ describe('<HeaderLight />', () => {
     render(<HeaderLight />);
     expect(screen.getByRole(/menubar/i)).toBeInTheDocument();
     expect(screen.getByText(/My LogoIcon/i)).toBeInTheDocument();
+    expect(
+      screen.queryByRole('link', {
+        name: /My LogoIcon/i,
+      }),
+    ).not.toBeInTheDocument();
+  });
+
+  test('renders HeaderLightLink', () => {
+    render(<HeaderLightLink />);
+    expect(screen.getByRole(/menubar/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', {
+        name: /My LogoIcon/i,
+      }),
+    ).toBeInTheDocument();
   });
 });
--- src/frontend/apps/standalone_site/src/features/Header/HeaderLight.tsx
+++ src/frontend/apps/standalone_site/src/features/Header/HeaderLight.tsx
@@ -1,28 +1,47 @@
 import { Box } from 'grommet';
-import { Nullable } from 'lib-common';
+import { normalizeColor } from 'grommet/utils';
+import { Nullable, theme } from 'lib-common';
 import { forwardRef } from 'react';
+import { Link } from 'react-router-dom';
 
 import { ReactComponent as LogoIcon } from 'assets/svg/logo_marsha.svg';
+import { routes } from 'routes';
 
 interface HeaderLightProps {
   bgcolor?: string;
   color?: string;
+  withLogoLink?: boolean;
 }
 
-const HeaderLight = forwardRef<Nullable<HTMLDivElement>, HeaderLightProps>(
-  ({ bgcolor, color }, ref) => {
-    return (
-      <Box
-        ref={ref}
-        role="menubar"
-        pad="small"
-        background={{ color: bgcolor || 'bg-marsha' }}
-      >
-        <LogoIcon width={117} height={80} color={color} />
-      </Box>
-    );
-  },
-);
+export const HeaderLightLink = forwardRef<
+  Nullable<HTMLDivElement>,
+  Omit<HeaderLightProps, 'withLogoLink'>
+>((props, ref) => {
+  return <HeaderLight withLogoLink ref={ref} {...props} />;
+});
+HeaderLightLink.displayName = 'HeaderLightLink';
 
+export const HeaderLight = forwardRef<
+  Nullable<HTMLDivElement>,
+  HeaderLightProps
+>(({ bgcolor, color, withLogoLink = false }, ref) => {
+  const colorLink = normalizeColor(color || 'blue-active', theme);
+
+  return (
+    <Box
+      ref={ref}
+      role="menubar"
+      pad="small"
+      background={{ color: bgcolor || 'bg-marsha' }}
+    >
+      {withLogoLink ? (
+        <Link to={routes.LOGIN.path} style={{ color: colorLink }}>
+          <LogoIcon width={117} height={80} />
+        </Link>
+      ) : (
+        <LogoIcon width={117} height={80} color={color} />
+      )}
+    </Box>
+  );
+});
 HeaderLight.displayName = 'HeaderLight';
-export default HeaderLight;
