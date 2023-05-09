562f15e70607cc80cb572483f6e4bf9e34c3b5fe
ALC Consulting <freelance.anthony@gmail.com>
2023-05-09 12:13:36 +0200

    🚚(lib-video) createThumbnail more restful url
    - Change from `/api/thumbnails` to `/api/videos/[videoId]/thumbnails`.
    - Remove `videoId` from body

--- src/frontend/packages/lib_video/src/api/createThumbnail/index.ts
+++ src/frontend/packages/lib_video/src/api/createThumbnail/index.ts
@@ -15,11 +15,12 @@ interface createThumbnailBody {
 /**
  * Create a new thumbnail record for a language-mode combination.
  */
-export const createThumbnail = async (
-  body: createThumbnailBody,
-): Promise<Thumbnail> => {
+export const createThumbnail = async ({
+  video,
+  ...body
+}: createThumbnailBody): Promise<Thumbnail> => {
   const response = await fetchWrapper(
-    `${API_ENDPOINT}/${modelName.THUMBNAILS}/`,
+    `${API_ENDPOINT}/videos/${video}/${modelName.THUMBNAILS}/`,
     {
       body: JSON.stringify(body),
       headers: {
