439ab71f28a3c2d0145457696ff6afdab4955236
ALC Consulting <freelance.anthony@gmail.com>
2023-05-12 14:14:41 +0200

    ✨(lib-components) create ClosingCard component
    ClosingCard is a Card with a closing button.
    Useful for displaying a status message that can be dismissed.

---
+++ src/frontend/packages/lib_components/src/common/Card/ClosingCard/index.tsx
@@ -0,0 +1,62 @@
+import { Box, Card as GrommetCard, CardBody, BoxTypes, Button } from 'grommet';
+import { FormClose } from 'grommet-icons';
+import { PropsWithChildren, ReactNode, useState } from 'react';
+import styled from 'styled-components';
+
+const CardLayout = styled(GrommetCard)`
+  transition: all 0.3s;
+`;
+
+interface ClosingCardProps extends BoxTypes {
+  message: ReactNode;
+}
+
+export const ClosingCard = ({
+  message,
+  children,
+  ...cardLayoutProps
+}: PropsWithChildren<ClosingCardProps>) => {
+  const [display, setDisplay] = useState(true);
+
+  const hide = {
+    padding: 0,
+    margin: 0,
+    height: 0,
+    opacity: 0,
+  };
+
+  return (
+    <CardLayout
+      height="inherit"
+      width="xsmedium"
+      background="white"
+      round="xsmall"
+      elevation="small"
+      {...cardLayoutProps}
+      style={
+        display ? cardLayoutProps.style : { ...cardLayoutProps.style, ...hide }
+      }
+      role="alert"
+      aria-hidden={!display}
+    >
+      <CardBody pad={{ left: 'medium', vertical: 'xsmall', right: 'small' }}>
+        <Box direction="row" justify="between" align="center">
+          <Box>{message}</Box>
+          <Button onClick={() => setDisplay(false)} style={{ display: 'flex' }}>
+            <FormClose aria-label="close" />
+          </Button>
+        </Box>
+        {children && (
+          <Box
+            pad={{ vertical: 'small' }}
+            direction="column"
+            align="left"
+            gap="xsmall"
+          >
+            {children}
+          </Box>
+        )}
+      </CardBody>
+    </CardLayout>
+  );
+};
