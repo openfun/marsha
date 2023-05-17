a1addab1e246f5106cfb61e5ecefa180450fe1ff
alfredpichard <alfred.pichard@polyconseil.fr>
2023-05-17 15:00:37 +0200

    ✨(frontend) bulk delete lives
    this commit introduces the bulk deletion of lives.

--- src/frontend/apps/standalone_site/src/features/Contents/features/Live/components/Create/LiveCreate.spec.tsx
+++ src/frontend/apps/standalone_site/src/features/Contents/features/Live/components/Create/LiveCreate.spec.tsx
@@ -1,6 +1,11 @@
+import { useJwt } from '@lib-components/hooks';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import fetchMock from 'fetch-mock';
 import { render } from 'lib-tests';
+import { act } from 'react-dom/test-utils';
+
+import { useSelectFeatures } from 'features/Contents/store/selectionStore';
 
 import LiveCreate from './LiveCreate';
 
@@ -10,20 +15,184 @@ jest.mock('./LiveCreateForm', () => ({
 }));
 
 describe('<LiveCreate />', () => {
-  test('renders LiveCreate', () => {
+  beforeEach(() => {
+    useJwt.setState({
+      jwt: 'json web token',
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    fetchMock.restore();
+  });
+
+  it('renders LiveCreate', () => {
     render(<LiveCreate />);
 
-    const button = screen.getByRole('button', { name: /Create Webinar/i });
-    expect(button).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Create Webinar/i }),
+    ).toBeInTheDocument();
     expect(
       screen.queryByRole('heading', { name: /Create Webinar/i }),
     ).not.toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Select' })).toBeInTheDocument();
+  });
 
-    userEvent.click(button);
+  it('shows video creation Modal', () => {
+    render(<LiveCreate />);
+    const createButton = screen.getByRole('button', {
+      name: /Create Webinar/i,
+    });
+    userEvent.click(createButton);
 
     expect(
       screen.getByRole('heading', { name: /Create Webinar/i }),
     ).toBeInTheDocument();
     expect(screen.getByText('My WebinarCreate Form')).toBeInTheDocument();
+  });
+
+  it('switches into selection mode on select button click', () => {
+    render(<LiveCreate />);
+    const selectButton = screen.getByRole('button', { name: 'Select' });
+    userEvent.click(selectButton);
+    expect(
+      screen.queryByRole('button', { name: 'Select' }),
+    ).not.toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Delete 0 webinar' }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Delete 0 webinar' }),
+    ).toBeDisabled();
+  });
+
+  it('counts the right amount of selected items (1)', () => {
+    render(<LiveCreate />);
+    act(() =>
+      useSelectFeatures.setState({
+        isSelectionEnabled: true,
+        selectedItems: ['id1'],
+      }),
+    );
+    expect(
+      screen.queryByRole('button', { name: 'Select' }),
+    ).not.toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Delete 1 webinar' }),
+    ).toBeInTheDocument();
+  });
+
+  it('counts the right amount of selected items (3)', () => {
+    render(<LiveCreate />);
+    act(() =>
+      useSelectFeatures.setState({
+        isSelectionEnabled: true,
+        selectedItems: ['id1', 'id2', 'id3'],
+      }),
+    );
+    expect(
+      screen.queryByRole('button', { name: 'Select' }),
+    ).not.toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Delete 3 webinars' }),
+    ).toBeInTheDocument();
+  });
+
+  it('opens the delete confirmation modal', () => {
+    render(<LiveCreate />);
+    act(() =>
+      useSelectFeatures.setState({
+        isSelectionEnabled: true,
+        selectedItems: ['id1', 'id2', 'id3'],
+      }),
+    );
+    const deleteButon = screen.getByRole('button', {
+      name: 'Delete 3 webinars',
+    });
+    userEvent.click(deleteButon);
+
+    expect(screen.getByText('Confirm delete 3 webinars')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        'Are you sure you want to delete 3 webinars ? This action is irreversible.',
+      ),
+    ).toBeInTheDocument();
+  });
+
+  it('successfully deletes lives', async () => {
+    fetchMock.delete('/api/videos/', {
+      status: 204,
+      body: { ids: ['id1', 'id2'] },
+    });
+
+    render(<LiveCreate />);
+    act(() =>
+      useSelectFeatures.setState({
+        isSelectionEnabled: true,
+        selectedItems: ['id1', 'id2'],
+      }),
+    );
+    const deleteButon = screen.getByRole('button', {
+      name: 'Delete 2 webinars',
+    });
+    userEvent.click(deleteButon);
+
+    const confirmDeleteButton = screen.getByRole('button', {
+      name: 'Confirm delete 2 webinars',
+    });
+    userEvent.click(confirmDeleteButton);
+
+    expect(
+      await screen.findByText('2 webinars successfully deleted'),
+    ).toBeInTheDocument();
+
+    expect(fetchMock.lastCall()![0]).toEqual(`/api/videos/`);
+    expect(fetchMock.lastCall()![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: `{"ids":["id1","id2"]}`,
+      method: 'DELETE',
+    });
+  });
+
+  it('fails to delete lives', async () => {
+    fetchMock.delete('/api/videos/', {
+      status: 400,
+      body: { ids: ['id1', 'id2'] },
+    });
+
+    render(<LiveCreate />);
+    act(() =>
+      useSelectFeatures.setState({
+        isSelectionEnabled: true,
+        selectedItems: ['id1', 'id2'],
+      }),
+    );
+    const deleteButon = screen.getByRole('button', {
+      name: 'Delete 2 webinars',
+    });
+    userEvent.click(deleteButon);
+
+    const confirmDeleteButton = screen.getByRole('button', {
+      name: 'Confirm delete 2 webinars',
+    });
+    userEvent.click(confirmDeleteButton);
+
+    expect(
+      await screen.findByText('Failed to delete 2 webinars'),
+    ).toBeInTheDocument();
+
+    expect(fetchMock.lastCall()![0]).toEqual(`/api/videos/`);
+    expect(fetchMock.lastCall()![1]).toEqual({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: `{"ids":["id1","id2"]}`,
+      method: 'DELETE',
+    });
   });
 });
