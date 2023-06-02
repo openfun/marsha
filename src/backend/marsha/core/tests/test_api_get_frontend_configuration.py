3790a5b46e3aeee789d1450abfcc1dd549004983
Anthony Le Courric <freelance.anthony@gmail.com>
2023-06-02 15:00:35 +0200

    👔(backend) add inactive_resources to api/config route
    `api/config` route will give us the `inactive_resources`
    based from the request host domain.

--- src/backend/marsha/core/tests/test_api_get_frontend_configuration.py
+++ src/backend/marsha/core/tests/test_api_get_frontend_configuration.py
@@ -4,6 +4,7 @@ from django.test import TestCase, override_settings
 from waffle.testutils import override_switch
 
 from marsha.core.defaults import SENTRY
+from marsha.core.factories import SiteConfigFactory
 
 
 @override_settings(
@@ -32,6 +33,7 @@ class TestGetFrontendConfiguration(TestCase):
                 "environment": "development",
                 "release": "1.2.3",
                 "sentry_dsn": "https://sentry.dsn",
+                "inactive_resources": [],
             },
         )
 
@@ -51,5 +53,45 @@ class TestGetFrontendConfiguration(TestCase):
                 "environment": "development",
                 "release": "1.2.3",
                 "sentry_dsn": None,
+                "inactive_resources": [],
+            },
+        )
+
+    @override_switch(SENTRY, active=True)
+    @override_settings(ALLOWED_HOSTS=["not.existing"])
+    def test_api_get_frontend_configuration_domain_param_site_not_existing(self):
+        """
+        Check the response when the domain param is set and the site does not exist.
+        """
+        response = self.client.get("/api/config/", HTTP_HOST="not.existing")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            response.json(),
+            {
+                "environment": "development",
+                "release": "1.2.3",
+                "sentry_dsn": "https://sentry.dsn",
+                "inactive_resources": [],
+            },
+        )
+
+    @override_switch(SENTRY, active=True)
+    @override_settings(ALLOWED_HOSTS=["marsha.education"])
+    def test_api_get_frontend_configuration_domain_param_site_existing(self):
+        """
+        Check the response when the domain param is set and the site does not exist.
+        """
+        SiteConfigFactory(inactive_resources=["video"], site__domain="marsha.education")
+        response = self.client.get("/api/config/", HTTP_HOST="marsha.education")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            response.json(),
+            {
+                "environment": "development",
+                "release": "1.2.3",
+                "sentry_dsn": "https://sentry.dsn",
+                "inactive_resources": ["video"],
             },
         )
