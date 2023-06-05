8357704f89c00503583b942b5c944d26584d7841
Anthony Le Courric <freelance.anthony@gmail.com>
2023-06-05 17:52:12 +0200

    ⚡️(website) improve app config loading
    The translation had to be finished to be configured before
    the app could start to call the config api. We can now call the config
    api in parallel with the translation configuration. It improve the app
    loading time.

--- src/frontend/apps/standalone_site/src/features/App/App.tsx
+++ src/frontend/apps/standalone_site/src/features/App/App.tsx
@@ -1,113 +1,46 @@
 import { Grommet } from 'grommet';
 import { colors } from 'lib-common';
-import { useEffect, useMemo, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
-import { IntlProvider } from 'react-intl';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ContentSpinner } from 'components/Spinner';
-import { DEFAULT_LANGUAGE } from 'conf/global';
 import { getFullThemeExtend } from 'styles/theme.extend';
-import { getCurrentTranslation, getLanguage, getLocaleCode } from 'utils/lang';
 
 import AppConfig from './AppConfig';
 import AppRoutes from './AppRoutes';
 
 const themeExtended = getFullThemeExtend();
+const queryClient = new QueryClient();
 
 const App = () => {
-  const [isAppInitialized, setIsAppInitialized] = useState(false);
-  const [currentTranslation, setCurrentTranslation] =
-    useState<Record<string, string>>();
-  const queryClient = useMemo(() => new QueryClient(), []);
-  const [language, setLanguage] = useState<string>();
-  const [localCode, setLocalCode] = useState<string>();
-  const [isDomReady, setIsDomReady] = useState(false);
-
-  useEffect(() => {
-    const language = getLanguage();
-    setLanguage(language);
-    setLocalCode(getLocaleCode(language));
-  }, []);
-
-  /**
-   * Load the current language and translation
-   */
-  useEffect(() => {
-    if (language) {
-      getCurrentTranslation(language).then((translation) => {
-        setCurrentTranslation(translation);
-      });
-    }
-  }, [language]);
-
-  //  call this effect last to configure all stores first
-  useEffect(() => {
-    setIsAppInitialized(true);
-  }, [setIsAppInitialized]);
-
-  useEffect(() => {
-    const handleCDNLoaded = () => {
-      setIsDomReady(window.isCDNLoaded || false);
-    };
-
-    document.addEventListener('CDNLoaded', handleCDNLoaded);
-    handleCDNLoaded();
-    return () => {
-      document.removeEventListener('CDNLoaded', handleCDNLoaded);
-    };
-  }, []);
-
-  if (!isAppInitialized) {
-    return null;
-  }
-
   return (
-    <IntlProvider
-      messages={currentTranslation}
-      locale={localCode || ''}
-      defaultLocale={getLocaleCode(DEFAULT_LANGUAGE)}
-      onError={(err) => {
-        // https://github.com/formatjs/formatjs/issues/465
-        if (err.code === 'MISSING_TRANSLATION') {
-          return;
-        }
-        throw err;
-      }}
-    >
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
-        <Grommet theme={themeExtended}>
-          <Toaster
-            toastOptions={{
-              duration: 5000,
-              success: {
-                style: {
-                  background: colors['status-ok'],
-                },
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <Grommet theme={themeExtended}>
+        <Toaster
+          toastOptions={{
+            duration: 5000,
+            success: {
+              style: {
+                background: colors['status-ok'],
               },
-              error: {
-                style: {
-                  color: colors['white'],
-                  background: colors['accent-2'],
-                },
+            },
+            error: {
+              style: {
+                color: colors['white'],
+                background: colors['accent-2'],
               },
-            }}
-          />
-          <AppConfig>
-            <BrowserRouter>
-              {isDomReady ? (
-                <AppRoutes />
-              ) : (
-                <ContentSpinner boxProps={{ height: '100vh' }} />
-              )}
-            </BrowserRouter>
-          </AppConfig>
-        </Grommet>
-      </QueryClientProvider>
-    </IntlProvider>
+            },
+          }}
+        />
+        <AppConfig>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </AppConfig>
+      </Grommet>
+    </QueryClientProvider>
   );
 };
 
--- src/frontend/apps/standalone_site/src/features/App/AppConfig.tsx
+++ src/frontend/apps/standalone_site/src/features/App/AppConfig.tsx
@@ -1,17 +1,55 @@
 import { useSentry } from 'lib-components';
-import { Fragment, PropsWithChildren, useEffect } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
+import { IntlProvider } from 'react-intl';
 
 import { useConfig } from 'api/useConfig';
 import { ContentSpinner } from 'components/Spinner';
+import { DEFAULT_LANGUAGE } from 'conf/global';
 import { featureContentLoader, useContentFeatures } from 'features/Contents';
+import { getCurrentTranslation, getLanguage, getLocaleCode } from 'utils/lang';
 
 const AppConfig = ({ children }: PropsWithChildren<unknown>) => {
+  const [currentTranslation, setCurrentTranslation] =
+    useState<Record<string, string>>();
+  const [language, setLanguage] = useState<string>();
+  const [localCode, setLocalCode] = useState<string>();
+  const [isDomReady, setIsDomReady] = useState(false);
   const setSentry = useSentry((state) => state.setSentry);
   const { data: config } = useConfig({
     keepPreviousData: true,
     staleTime: Infinity,
   });
   const isFeatureLoaded = useContentFeatures((state) => state.isFeatureLoaded);
+  const isConfigReady = isFeatureLoaded && isDomReady;
+
+  useEffect(() => {
+    const language = getLanguage();
+    setLanguage(language);
+    setLocalCode(getLocaleCode(language));
+  }, []);
+
+  /**
+   * Load the current language and translation
+   */
+  useEffect(() => {
+    if (language) {
+      getCurrentTranslation(language).then((translation) => {
+        setCurrentTranslation(translation);
+      });
+    }
+  }, [language]);
+
+  useEffect(() => {
+    const handleCDNLoaded = () => {
+      setIsDomReady(window.isCDNLoaded || false);
+    };
+
+    document.addEventListener('CDNLoaded', handleCDNLoaded);
+    handleCDNLoaded();
+    return () => {
+      document.removeEventListener('CDNLoaded', handleCDNLoaded);
+    };
+  }, []);
 
   useEffect(() => {
     if (!config) {
@@ -30,11 +68,26 @@ const AppConfig = ({ children }: PropsWithChildren<unknown>) => {
     featureContentLoader(config.inactive_resources);
   }, [setSentry, config]);
 
-  if (!isFeatureLoaded) {
+  if (!isConfigReady) {
     return <ContentSpinner boxProps={{ height: '100vh' }} />;
   }
 
-  return <Fragment>{children}</Fragment>;
+  return (
+    <IntlProvider
+      messages={currentTranslation}
+      locale={localCode || ''}
+      defaultLocale={getLocaleCode(DEFAULT_LANGUAGE)}
+      onError={(err) => {
+        // https://github.com/formatjs/formatjs/issues/465
+        if (err.code === 'MISSING_TRANSLATION') {
+          return;
+        }
+        throw err;
+      }}
+    >
+      {children}
+    </IntlProvider>
+  );
 };
 
 export default AppConfig;
