e858e0938a7c6ede8de8159e1ebee074a5af8d89
ALC Consulting <freelance.anthony@gmail.com>
2023-07-07 11:06:22 +0200

    ✨(frontend) integrate Cunningham DataGrid into playlist page
    - Replace the current playlist DataGrid with the Cunningham
    DataGrid
    - Remove from codebase the old DataGrid

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -39,6 +39,7 @@ Versioning](https://semver.org/spec/v2.0.0.html).
 - refactor videojs id3 tags handling as videojs plugin
 - refactor videojs xapi handling as videojs plugin
 - upgrade react-query to @tanstack/react-query (v4) (#2340)
+- Replace component SortableTable by Cunningham DataGrid (#2311)
 
 ### Fixed
 
--- src/frontend/apps/standalone_site/src/features/Playlist/components/PlaylistPage.spec.tsx
+++ src/frontend/apps/standalone_site/src/features/Playlist/components/PlaylistPage.spec.tsx
@@ -56,26 +56,101 @@ describe('<PlaylistPage />', () => {
             title: 'playlist 1',
             lti_id: 'id-1',
             consumer_site: 'some.consumer',
+            created_on: '2023-05-22T14:50:50.400986Z',
+            organization: {
+              id: 'id-org',
+              name: 'org',
+            },
           },
           {
             title: 'playlist 2',
             lti_id: 'id-2',
             consumer_site: 'some.consumer',
+            created_on: '2023-05-24T14:50:50.400986Z',
+            organization: {
+              id: 'id-org2',
+              name: 'org 2',
+            },
           },
           {
             title: 'playlist 3',
             lti_id: 'id-3',
             consumer_site: 'an.other.consumer',
+            created_on: '2023-05-26T14:50:50.400986Z',
+            organization: undefined,
           },
         ],
       }),
     );
 
-    await screen.findByText('playlist 1');
-    screen.getByText('playlist 2');
-    screen.getByText('playlist 3');
+    expect(await screen.findByText('playlist 1')).toBeInTheDocument();
+    expect(screen.getByText('playlist 2')).toBeInTheDocument();
+    expect(screen.getByText('playlist 3')).toBeInTheDocument();
+    expect(screen.getByText('5/22/2023 2:50:50 PM')).toBeInTheDocument();
+    expect(screen.getByText('5/24/2023 2:50:50 PM')).toBeInTheDocument();
+    expect(screen.getByText('5/26/2023 2:50:50 PM')).toBeInTheDocument();
+    expect(screen.getByText('org')).toBeInTheDocument();
+    expect(screen.getByText('org 2')).toBeInTheDocument();
+    expect(screen.getByText(/Organization/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Create playlist' }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Title/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Created On/i }),
+    ).toBeInTheDocument();
+  });
 
-    screen.getByRole('button', { name: 'Create playlist' });
+  it('display the date according to the navigator language', async () => {
+    const deferred = new Deferred();
+    fetchMock.get(
+      '/api/playlists/?limit=20&offset=0&ordering=-created_on',
+      deferred.promise,
+    );
+
+    const languageGetter = jest.spyOn(window.navigator, 'language', 'get');
+    languageGetter.mockReturnValue('fr');
+
+    render(<PlaylistPage />);
+
+    act(() =>
+      deferred.resolve({
+        count: 3,
+        results: [
+          {
+            title: 'playlist 1',
+            lti_id: 'id-1',
+            consumer_site: 'some.consumer',
+            created_on: '2023-05-22T14:50:50.400986Z',
+            organization: {
+              id: 'id-org',
+              name: 'org',
+            },
+          },
+          {
+            title: 'playlist 2',
+            lti_id: 'id-2',
+            consumer_site: 'some.consumer',
+            created_on: '2023-05-24T14:50:50.400986Z',
+            organization: {
+              id: 'id-org2',
+              name: 'org 2',
+            },
+          },
+          {
+            title: 'playlist 3',
+            lti_id: 'id-3',
+            consumer_site: 'an.other.consumer',
+            created_on: '2023-05-26T14:50:50.400986Z',
+            organization: undefined,
+          },
+        ],
+      }),
+    );
+
+    expect(await screen.findByText('22/05/2023 14:50:50')).toBeInTheDocument();
+    expect(screen.getByText('24/05/2023 14:50:50')).toBeInTheDocument();
+    expect(screen.getByText('26/05/2023 14:50:50')).toBeInTheDocument();
   });
 
   it('displays an error', async () => {
@@ -119,48 +194,114 @@ describe('<PlaylistPage />', () => {
       },
       { overwriteRoutes: true },
     );
-    userEvent.click(screen.getByRole('button', { name: 'Retry' }));
+    await userEvent.click(screen.getByRole('button', { name: 'Retry' }));
 
-    await screen.findByText('playlist 1');
+    screen.getByText('playlist 1');
     screen.getByText('playlist 2');
     screen.getByText('playlist 3');
 
     screen.getByRole('button', { name: 'Create playlist' });
   });
 
-  it('allows sorting by create_on and by title', async () => {
+  it('checks sorting interactions', async () => {
+    const results = [
+      {
+        title: 'playlist 1',
+        lti_id: 'id-1',
+        consumer_site: 'some.consumer',
+        created_on: '2023-05-26T14:50:50.400986Z',
+      },
+      {
+        title: 'playlist 2',
+        lti_id: 'id-2',
+        consumer_site: 'some.consumer',
+        created_on: '2023-05-25T14:50:50.400986Z',
+      },
+      {
+        title: 'playlist 3',
+        lti_id: 'id-3',
+        consumer_site: 'an.other.consumer',
+        created_on: '2023-05-24T14:50:50.400986Z',
+      },
+    ];
+
     fetchMock.get('/api/playlists/?limit=20&offset=0&ordering=-created_on', {
       count: 3,
-      results: [
-        {
-          title: 'playlist 1',
-          lti_id: 'id-1',
-          consumer_site: 'some.consumer',
-        },
-        {
-          title: 'playlist 2',
-          lti_id: 'id-2',
-          consumer_site: 'some.consumer',
-        },
-        {
-          title: 'playlist 3',
-          lti_id: 'id-3',
-          consumer_site: 'an.other.consumer',
-        },
-      ],
+      results,
+    });
+
+    fetchMock.get('/api/playlists/?limit=20&offset=0', {
+      count: 3,
+      results: [...results].reverse(),
+    });
+
+    fetchMock.get('/api/playlists/?limit=20&offset=0&ordering=created_on', {
+      count: 3,
+      results,
+    });
+
+    fetchMock.get('/api/playlists/?limit=20&offset=0&ordering=title', {
+      count: 3,
+      results: [...results].reverse(),
+    });
+
+    fetchMock.get('/api/playlists/?limit=20&offset=0&ordering=-title', {
+      count: 3,
+      results,
     });
 
     render(<PlaylistPage />);
 
-    await screen.findByText('playlist 1');
-    userEvent.click(
-      screen.getByRole('button', { name: 'Sort item in the table' }),
-    );
+    expect(await screen.findByText('playlist 1')).toBeInTheDocument();
 
-    await screen.findByText('Title');
-    expect(screen.getByText('Creation date')).toBeInTheDocument();
-    expect(screen.getAllByText('Creation date (reversed)').length).toEqual(2);
-    expect(screen.getByText('Title (reversed)')).toBeInTheDocument();
+    expect(
+      fetchMock.called(
+        '/api/playlists/?limit=20&offset=0&ordering=-created_on',
+      ),
+    ).toBeTruthy();
+
+    const rows__created_on = screen.getAllByRole('row');
+    expect(rows__created_on[1]).toHaveTextContent('playlist 1');
+    expect(rows__created_on[2]).toHaveTextContent('playlist 2');
+    expect(rows__created_on[3]).toHaveTextContent('playlist 3');
+
+    await userEvent.click(screen.getByRole('button', { name: /Created On/i }));
+    expect(fetchMock.called('/api/playlists/?limit=20&offset=0')).toBeTruthy();
+
+    const rows = screen.getAllByRole('row');
+    expect(rows[1]).toHaveTextContent('playlist 3');
+    expect(rows[2]).toHaveTextContent('playlist 2');
+    expect(rows[3]).toHaveTextContent('playlist 1');
+
+    await userEvent.click(screen.getByRole('button', { name: /Created On/i }));
+    expect(
+      fetchMock.called('/api/playlists/?limit=20&offset=0&ordering=created_on'),
+    ).toBeTruthy();
+
+    const rows_created_on = screen.getAllByRole('row');
+    expect(rows_created_on[1]).toHaveTextContent('playlist 1');
+    expect(rows_created_on[2]).toHaveTextContent('playlist 2');
+    expect(rows_created_on[3]).toHaveTextContent('playlist 3');
+
+    await userEvent.click(screen.getByRole('button', { name: /Title/i }));
+    expect(
+      fetchMock.called('/api/playlists/?limit=20&offset=0&ordering=title'),
+    ).toBeTruthy();
+
+    const rows_title = screen.getAllByRole('row');
+    expect(rows_title[1]).toHaveTextContent('playlist 3');
+    expect(rows_title[2]).toHaveTextContent('playlist 2');
+    expect(rows_title[3]).toHaveTextContent('playlist 1');
+
+    await userEvent.click(screen.getByRole('button', { name: /Title/i }));
+    expect(
+      fetchMock.called('/api/playlists/?limit=20&offset=0&ordering=-title'),
+    ).toBeTruthy();
+
+    const rows__title = screen.getAllByRole('row');
+    expect(rows__title[1]).toHaveTextContent('playlist 1');
+    expect(rows__title[2]).toHaveTextContent('playlist 2');
+    expect(rows__title[3]).toHaveTextContent('playlist 3');
   });
 
   it('opens the create playlist form', async () => {
