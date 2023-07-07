9952709dc854d5861d87f9faf905732d4e9de997
Nicolas Clerc <kernicpanel@nclerc.fr>
2023-07-07 12:26:35 +0200

    ✨(frontend) use site configuration in components
    As we want to display custom data for custom frontends, we replace
    default elements with those set in useSiteConfig in several components.

--- src/frontend/apps/standalone_site/src/features/Header/Header.spec.tsx
+++ src/frontend/apps/standalone_site/src/features/Header/Header.spec.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { useCurrentUser, userMockFactory } from 'lib-components';
+import { useCurrentUser, userMockFactory, useSiteConfig } from 'lib-components';
 import { render } from 'lib-tests';
 
 import Header from './Header';
@@ -34,6 +34,71 @@ describe('<Header />', () => {
   });
 
   test('clic on Logo routes to Homepage', async () => {
+    render(<div>My Homepage</div>, {
+      routerOptions: {
+        routes: [
+          {
+            path: '/videos',
+            element: <div>My Videos</div>,
+          },
+        ],
+        header: <Header />,
+        history: ['/videos'],
+      },
+    });
+
+    const logo = screen.getByText(/logo_marsha.svg/i);
+    expect(logo).toBeInTheDocument();
+    expect(screen.getByText(/My Videos/i)).toBeInTheDocument();
+    expect(screen.queryByText(/My Homepage/i)).not.toBeInTheDocument();
+    await userEvent.click(logo);
+    expect(screen.getByText(/My Homepage/i)).toBeInTheDocument();
+    expect(screen.queryByText(/My Videos/i)).not.toBeInTheDocument();
+  });
+
+  test('clic on custom Logo routes to Homepage', async () => {
+    useSiteConfig.setState({
+      siteConfig: {
+        is_default_site: false,
+        footer_copyright: 'custom copyright',
+        logo_url: 'https://example.com/logo.svg',
+        login_html: 'custom login markdown',
+      },
+    });
+
+    render(<div>My Homepage</div>, {
+      routerOptions: {
+        routes: [
+          {
+            path: '/videos',
+            element: <div>My Videos</div>,
+          },
+        ],
+        header: <Header />,
+        history: ['/videos'],
+      },
+    });
+
+    const logo = screen.getByRole('img', { name: 'Home' });
+    expect(logo).toHaveAttribute('src', 'https://example.com/logo.svg');
+    expect(logo).toBeInTheDocument();
+    expect(screen.getByText(/My Videos/i)).toBeInTheDocument();
+    expect(screen.queryByText(/My Homepage/i)).not.toBeInTheDocument();
+    await userEvent.click(logo);
+    expect(screen.getByText(/My Homepage/i)).toBeInTheDocument();
+    expect(screen.queryByText(/My Videos/i)).not.toBeInTheDocument();
+  });
+
+  test('clic on custom Logo routes to Homepage with default logo', async () => {
+    useSiteConfig.setState({
+      siteConfig: {
+        is_default_site: false,
+        footer_copyright: 'custom copyright',
+        logo_url: undefined,
+        login_html: 'custom login markdown',
+      },
+    });
+
     render(<div>My Homepage</div>, {
       routerOptions: {
         routes: [
--- src/frontend/apps/standalone_site/src/features/Header/HeaderLight.spec.tsx
+++ src/frontend/apps/standalone_site/src/features/Header/HeaderLight.spec.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import { useSiteConfig } from 'lib-components';
 import { render } from 'lib-tests';
 import React from 'react';
 
@@ -9,6 +10,17 @@ jest.mock('assets/svg/logo_marsha.svg', () => ({
 }));
 
 describe('<HeaderLight />', () => {
+  beforeEach(() => {
+    useSiteConfig.setState({
+      siteConfig: {
+        is_default_site: true,
+        footer_copyright: undefined,
+        logo_url: undefined,
+        login_html: undefined,
+      },
+    });
+  });
+
   test('renders HeaderLight', () => {
     render(<HeaderLight />);
     expect(screen.getByRole('menubar')).toBeInTheDocument();
@@ -20,7 +32,71 @@ describe('<HeaderLight />', () => {
     ).not.toBeInTheDocument();
   });
 
+  test('renders HeaderLight custom site logo', async () => {
+    useSiteConfig.setState({
+      siteConfig: {
+        is_default_site: false,
+        footer_copyright: 'custom copyright',
+        logo_url: 'https://example.com/logo.svg',
+        login_html: 'custom login markdown',
+      },
+    });
+    render(<HeaderLight />);
+    const logo = await screen.findByRole('img', { name: 'Home' });
+    expect(logo).toHaveAttribute('src', 'https://example.com/logo.svg');
+    expect(logo).toBeInTheDocument();
+  });
+
+  test('renders HeaderLight custom site no logo', () => {
+    useSiteConfig.setState({
+      siteConfig: {
+        is_default_site: false,
+        footer_copyright: 'custom copyright',
+        logo_url: undefined,
+        login_html: 'custom login markdown',
+      },
+    });
+    render(<HeaderLight />);
+    expect(screen.getByText(/My LogoIcon/i)).toBeInTheDocument();
+  });
+
   test('renders HeaderLightLink', () => {
+    render(<HeaderLightLink />);
+    expect(screen.getByRole('menubar')).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', {
+        name: /My LogoIcon/i,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  test('renders HeaderLightLink custom site logo', () => {
+    useSiteConfig.setState({
+      siteConfig: {
+        is_default_site: false,
+        footer_copyright: 'custom copyright',
+        logo_url: 'https://example.com/logo.svg',
+        login_html: 'custom login markdown',
+      },
+    });
+    render(<HeaderLightLink />);
+    expect(screen.getByRole('menubar')).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', {
+        name: /Home/i,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  test('renders HeaderLightLink custom site no logo', () => {
+    useSiteConfig.setState({
+      siteConfig: {
+        is_default_site: false,
+        footer_copyright: 'custom copyright',
+        logo_url: undefined,
+        login_html: 'custom login markdown',
+      },
+    });
     render(<HeaderLightLink />);
     expect(screen.getByRole('menubar')).toBeInTheDocument();
     expect(
