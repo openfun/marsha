7fa4651f0ca93a406cfd1b9e1692c8f3e8b5c093
Anthony LC <freelance.anthony@gmail.com>
2023-07-10 15:50:58 +0200

    ➕(lti) replace tslint with eslint
    - add eslint package
    - add eslint config
    - remove tslint

---
+++ src/frontend/apps/lti_site/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  extends: ['marsha'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'sort-imports': [
+          'error',
+          {
+            ignoreDeclarationSort: true,
+          },
+        ],
+        'import/order': [
+          'error',
+          {
+            alphabetize: {
+              order: 'asc',
+            },
+            groups: [
+              'builtin',
+              'external',
+              'internal',
+              'parent',
+              'sibling',
+              'index',
+            ],
+            pathGroups: [
+              {
+                pattern:
+                  '+(__mock__|apps|components|data|i18n|scss|translations|types|utils)/**',
+                group: 'internal',
+              },
+            ],
+            pathGroupsExcludedImportTypes: ['builtin'],
+            'newlines-between': 'always',
+          },
+        ],
+      },
+    },
+  ],
+  ignorePatterns: ['node_modules/', '.eslintrc.js'],
+};
