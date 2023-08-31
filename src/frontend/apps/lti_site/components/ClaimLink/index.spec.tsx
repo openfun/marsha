0ac20f767ab178b04c37919e1512fe0f9faa3a31
Manuel Raynaud <manu@raynaud.io>
2023-08-31 10:53:28 +0200

    🐛(front) display claim link only when is-claimed request succeeded
    The claim link was display by default by setting the state based on
    appDatata content. This can lead to displaying the link few second and
    the state is changed only if the resource is-claimed. This introduce a
    bug because this endpoint can not be fetch by a student, so student have
    the link display. This fix does the opposite, when mounted, the state is
    set to false and then the link is display only if the request succeed
    and is claimed is strictly false.

--- src/frontend/apps/lti_site/components/ClaimLink/index.spec.tsx
+++ src/frontend/apps/lti_site/components/ClaimLink/index.spec.tsx
@@ -77,6 +77,7 @@ describe('<ClaimLink />', () => {
     );
 
     expect(fetchMock.calls()).toHaveLength(1);
+    expect(fetchMock.called('/api/playlists/488db2d0/is-claimed/')).toBe(true);
 
     const link = await screen.findByText(
       'Please login to manage this resource on http://localhost:8000/',
@@ -146,6 +147,68 @@ describe('<ClaimLink />', () => {
     );
 
     expect(fetchMock.calls()).toHaveLength(1);
+    expect(fetchMock.called('/api/playlists/488db2d0/is-claimed/')).toBe(true);
+    expect(
+      screen.queryByText(
+        'Please login to manage this resource on marsha.education.',
+      ),
+    ).not.toBeInTheDocument();
+  });
+
+  it('does not show claim link if is-claimed request fails', () => {
+    const playlist = playlistMockFactory({ id: '488db2d0' });
+    const video = videoMockFactory({ playlist });
+
+    fetchMock.get('/api/playlists/488db2d0/is-claimed/', 403);
+
+    render(
+      <AppConfigProvider
+        value={{
+          appName: appNames.CLASSROOM,
+          attendanceDelay: 10,
+          state: appState.SUCCESS,
+          modelName: modelName.VIDEOS,
+          resource: video,
+          sentry_dsn: 'test.dns.com',
+          environment: 'tests',
+          frontend: 'test-frontend',
+          frontend_home_url: 'http://localhost:8000/',
+          release: 'debug',
+          static: {
+            svg: {
+              icons: '',
+            },
+            img: {
+              liveBackground: '',
+              liveErrorBackground: '',
+              marshaWhiteLogo: '',
+              videoWizardBackground: '',
+              errorMain: '',
+            },
+          },
+          uploadPollInterval: 10,
+          p2p: {
+            isEnabled: false,
+            webTorrentTrackerUrls: [],
+            stunServerUrls: [],
+          },
+        }}
+      >
+        <ClaimLink
+          decodedJwt={
+            {
+              port_to_playlist_id: '488db2d0',
+              playlist_id: 'e8c0b8d0',
+              consumer_site: '32a1c2d0',
+              user: { id: '6b45a4d6' },
+            } as DecodedJwtLTI
+          }
+        />
+      </AppConfigProvider>,
+    );
+
+    expect(fetchMock.calls()).toHaveLength(1);
+    expect(fetchMock.called('/api/playlists/488db2d0/is-claimed/')).toBe(true);
     expect(
       screen.queryByText(
         'Please login to manage this resource on marsha.education.',
