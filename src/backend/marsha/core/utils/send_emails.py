b86615d3f1c15c30167dcf993007d1597a37c1bd
Nicolas Clerc <kernicpanel@nclerc.fr>
2023-09-20 14:18:42 +0200

    ♻️(backend) refactor send_email
    Three functions were very similar. They have been refactored.

--- src/backend/marsha/core/utils/send_emails.py
+++ src/backend/marsha/core/utils/send_emails.py
@@ -10,8 +10,8 @@ from django.utils.translation import gettext as _
 from marsha.core.utils.time_utils import to_datetime
 
 
-def send_ready_to_convert_notification(video):
-    """Send a ready to convert email notification."""
+def _send_email(video, template_name, subject):
+    """Email the user."""
     if email := video.live_info.get("live_stopped_with_email"):
         template_vars = {
             "email": email,
@@ -25,14 +25,13 @@ def send_ready_to_convert_notification(video):
         }
 
         msg_plain = render_to_string(
-            "core/mail/text/vod_conversion_ready.txt", template_vars
+            f"core/mail/text/{template_name}.txt", template_vars
         )
         msg_html = render_to_string(
-            "core/mail/html/vod_conversion_ready.html", template_vars
+            f"core/mail/html/{template_name}.html", template_vars
         )
         return send_mail(
-            subject=_("Live Stream is ready to convert - %(video.title)s")
-            % {"video.title": video.title},
+            subject=subject % {"video.title": video.title},
             message=msg_plain,
             from_email=None,
             recipient_list=[email],
@@ -41,61 +40,30 @@ def send_ready_to_convert_notification(video):
         )
 
     return None
+
+
+def send_ready_to_convert_notification(video):
+    """Send a ready to convert email notification."""
+    return _send_email(
+        video,
+        "vod_conversion_ready",
+        _("Live Stream is ready to convert - %(video.title)s"),
+    )
 
 
 def send_vod_ready_notification(video):
     """Send a convert to vod ready email notification."""
-    if email := video.live_stopped_by_email:
-        template_vars = {
-            "email": email,
-            "time_zone": settings.TIME_ZONE,
-            "video": video,
-            "video_access_url": video.last_lti_url,
-        }
-
-        msg_plain = render_to_string("core/mail/text/vod_ready.txt", template_vars)
-        msg_html = render_to_string("core/mail/html/vod_ready.html", template_vars)
-        return send_mail(
-            subject=_("Live Stream is converted to VOD - %(video.title)s")
-            % {"video.title": video.title},
-            message=msg_plain,
-            from_email=None,
-            recipient_list=[email],
-            html_message=msg_html,
-            fail_silently=False,
-        )
-
-    return None
+    return _send_email(
+        video,
+        "vod_ready",
+        _("Live Stream is converted to VOD - %(video.title)s"),
+    )
 
 
 def send_convert_reminder_notification(video):
     """Send a reminder to convert email notification."""
-    if email := video.live_stopped_by_email:
-        template_vars = {
-            "email": email,
-            "time_zone": settings.TIME_ZONE,
-            "video": video,
-            "video_access_url": video.last_lti_url,
-            "expiration_date": (
-                video.starting_at or to_datetime(video.live_info.get("started_at"))
-            )
-            + timedelta(days=settings.NB_DAYS_BEFORE_DELETING_AWS_ELEMENTAL_STACK),
-        }
-
-        msg_plain = render_to_string(
-            "core/mail/text/vod_conversion_reminder.txt", template_vars
-        )
-        msg_html = render_to_string(
-            "core/mail/html/vod_conversion_reminder.html", template_vars
-        )
-        return send_mail(
-            subject=_("Live Stream will be deleted soon - %(video.title)s")
-            % {"video.title": video.title},
-            message=msg_plain,
-            from_email=None,
-            recipient_list=[email],
-            html_message=msg_html,
-            fail_silently=False,
-        )
-
-    return None
+    return _send_email(
+        video,
+        "vod_conversion_reminder",
+        _("Live Stream will be deleted soon - %(video.title)s"),
+    )
