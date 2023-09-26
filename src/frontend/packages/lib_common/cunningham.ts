4005f494f07fd5059016df56ebb111b1dfb38f59
Anthony Le Courric <freelance.anthony@gmail.com>
2023-09-26 09:56:43 +0200

    🔧(lib-common) override cunningham tokens in the cunningham way
    The way to override the cunningham tokens was not the recommended way,
    we changed it to a more recommended way, it will help to get
    inner functionnalities bring by the cunningham design system like
    css class generation.

---
+++ src/frontend/packages/lib_common/cunningham.ts
@@ -0,0 +1,31 @@
+//import { DefaultTokens } from '@openfun/cunningham-react';
+
+const config = {
+  theme: {
+    font: {
+      sizes: {
+        ml: '0.938rem',
+        xl: '1.50rem',
+        t: '0.6875rem',
+        s: '0.75rem',
+        h1: '2.2rem',
+        h2: '1.7rem',
+        h3: '1.37rem',
+        h4: '1.15rem',
+        h5: '1rem',
+        h6: '0.87rem',
+      },
+      weights: {
+        thin: 100,
+        extrabold: 800,
+        black: 900,
+      },
+    },
+    spacings: {
+      '0': '0',
+      bx: '2.2rem',
+    },
+  },
+};
+
+export default config;
