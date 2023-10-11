95304ccccd29b93b5d5b6ed025b869ed8e038cc8
Anthony Le Courric <freelance.anthony@gmail.com>
2023-10-11 14:19:12 +0200

    🥅(lib-classroom) display invite link error message
    If the link provided is invalid, display an error message.

--- src/frontend/packages/lib_components/src/common/ErrorComponents/BoxError/index.tsx
+++ src/frontend/packages/lib_components/src/common/ErrorComponents/BoxError/index.tsx
@@ -1,10 +1,14 @@
-import { Box } from 'grommet';
+import { Box, BoxExtendedProps } from 'grommet';
 import { Alert } from 'grommet-icons';
 import React from 'react';
 
 import { Text } from '@lib-components/common/Text';
 
-export const BoxError = ({ message }: { message: string }) => {
+interface BoxErrorProps extends BoxExtendedProps {
+  message: string;
+}
+
+export const BoxError = ({ message, ...props }: BoxErrorProps) => {
   return (
     <Box
       direction="row"
@@ -12,6 +16,7 @@ export const BoxError = ({ message }: { message: string }) => {
       justify="center"
       margin={{ bottom: 'medium' }}
       gap="small"
+      {...props}
     >
       <Alert size="42rem" color="#df8c00" />
       <Text weight="bold">{message}</Text>
