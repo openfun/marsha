95f0a6db58a6638b5a41be0e8430486aa6fada2c
Anthony Le Courric <freelance.anthony@gmail.com>
2023-12-13 10:45:53 +0100

    🐛(lib_components) fix circular dependency in lib_components
    Vite was complaining about a circular dependency in lib_components,
    this commit fixes it.

--- src/frontend/packages/lib_components/src/common/Modal/index.ts
+++ src/frontend/packages/lib_components/src/common/Modal/index.ts
@@ -1,3 +1,3 @@
 export { default as Modal } from './Modal';
 export type { ModalControlMethods } from './Modal';
-export { default as ModalButton } from './ModalButton';
+export * from './ModalButton';
