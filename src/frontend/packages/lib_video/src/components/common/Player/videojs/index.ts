85d30ce2020428b7b64fea211ad3a99fd276e75f
Anthony Le Courric <freelance.anthony@gmail.com>
2023-12-22 12:21:44 +0100

    ⬆️(lib_video) upgrade video.js to 8.7.0
    A new version of video.js was available, we upgraded to it.

---
+++ src/frontend/packages/lib_video/src/components/common/Player/videojs/index.ts
@@ -0,0 +1,6 @@
+export type * from './downloadVideoPlugin/types';
+export type * from './p2pHlsPlugin/types';
+export type * from './qualitySelectorPlugin/types';
+export type * from './sharedMediaPlugin/types';
+export type * from './transcriptPlugin/types';
+export type * from './xapiPlugin/types';
--- src/frontend/packages/lib_video/src/types/libs/video.js/extend.d.ts
+++ src/frontend/packages/lib_video/src/types/libs/video.js/extend.d.ts
@@ -1,52 +1,107 @@
-import 'video.js';
 import { Engine } from 'p2p-media-loader-hlsjs';
+import 'video.js';
+import PlayerInit from 'video.js/dist/types/player';
+import PluginInit from 'video.js/dist/types/plugin';
+import Tech from 'video.js/dist/types/tech/tech';
 
-import { DownloadVideoPluginOptions } from '../../../components/common/Player/videojs/downloadVideoPlugin/types';
-import { SharedLiveMediaOptions } from '../../../components/common/Player/videojs/sharedMediaPlugin/types';
-import { TranscriptPluginOptions } from '../../../components/common/Player/videojs/transcriptPlugin/types';
-import { XapiPluginOptions } from '../../../components/common/Player/videojs/xapiPlugin/types';
+import {
+  DownloadVideoPluginOptions,
+  SharedLiveMediaOptions,
+  TranscriptButtonOptions,
+  XapiPluginOptions,
+} from '../../../components/common/Player/videojs/';
 
 declare module 'video.js' {
-  interface VideoJsPlayerOptions {
+  export function MiddlewareUse(
+    type: string,
+    middleware: (Player) => {
+      setSource: (
+        playerSource: Source,
+        next: (err: Error | null, source: Source) => void,
+      ) => void;
+    },
+  ): void;
+
+  type PlayerOptionInit = typeof PlayerInit.prototype.options_;
+  interface PlayerOption extends PlayerOptionInit {
+    autoplay?: boolean;
+    controls?: boolean;
     debug?: boolean;
-    responsive?: boolean;
+    fluid?: boolean;
+    sources: Source[];
+    plugins?: Plugins;
+    html5: {
+      vhs?: {
+        limitRenditionByPlayerDimensions?: boolean;
+        overrideNative?: boolean;
+        useDevicePixelRatio?: boolean;
+      };
+      nativeAudioTracks?: boolean;
+      nativeVideoTracks?: boolean;
+      hlsjsConfig?: {
+        liveSyncDurationCount?: number;
+        loader: Engine;
+      };
+    };
   }
 
-  interface VideoJsPlayerPluginOptions {
-    httpSourceSelector?: VideoJsHttpSourceSelectorPluginOptions;
-    qualitySelector?: QualitySelectorOptions;
-  }
-
-  interface VideoJsPlayer {
-    // videojs-quality-selector-plugin
-    videojs_quality_selector_plugin_initialized: boolean;
+  export class Player extends PlayerInit {
+    qualityLevels: () => QualityLevels;
+    currentSources(): Tech & Source[];
+    currentSource(): Tech & Source;
+    videojs_quality_selector_plugin_default?: string;
+    videojs_quality_selector_plugin_initialized?: boolean;
     videojs_quality_selector_plugin_is_paused?: boolean;
     videojs_quality_selector_plugin_currentime?: number;
-    videojs_quality_selector_plugin_default?: string;
-    cache_: {
-      initTime: number;
+    options_: PlayerOption;
+    lastSource_: {
+      player: string;
+      tech: string;
     };
-    // p2p-media-loader-hlsjs
+    controlBar: {
+      el(): Element;
+      addChild(
+        name: string,
+        option?:
+          | DownloadVideoPluginOptions
+          | SharedLiveMediaOptions
+          | TranscriptButtonOptions,
+      ): {
+        el(): Element;
+      };
+      getChild(name: string): {
+        el(): Element;
+      };
+      getDescendant(name: string): {
+        el(): Element;
+      };
+    };
+    p2pHlsPlugin: () => void;
     config: { loader: { getEngine: () => Engine } };
     media: { currentTime: number };
     downloadVideoPlugin: (options: DownloadVideoPluginOptions) => void;
     sharedMediaPlugin: (options: SharedLiveMediaOptions) => void;
     transcriptPlugin: (option: TranscriptPluginOptions) => void;
-    p2pHlsPlugin: () => void;
-    id3Plugin: () => void;
-    xapiPlugin: (options: XapiPluginOptions) => void;
-    // videojs-http-source-selector
     httpSourceSelector: () => void;
-    qualitySelector: () => {
-      on(
-        type?: string | string[],
-        listener?: (...args: unknown[]) => void,
-      ): void;
-    };
-    qualityLevels: () => QualityLevels;
-    currentSources(): VideoJsExtendedSourceObject[];
-    currentSource(): VideoJsExtendedSourceObject;
+    id3Plugin: () => void;
+    textTracks: () => TextTrackList;
+    xapiPlugin: (options: XapiPluginOptions) => void;
+    remoteTextTracks: () => TextTrackList;
+    seeking: () => boolean;
   }
+
+  export interface Source {
+    src: string;
+    type?: string;
+    size?: string;
+    selected?: boolean;
+  }
+
+  export type Plugins = typeof PluginInit & {
+    qualitySelector?: {
+      default: string;
+    };
+  };
 }
 
 interface QualityLevels {
@@ -58,17 +113,6 @@ interface QualityLevels {
     action: () => void,
   ) => void;
   trigger: (event: string) => void;
-}
-
-interface VideoJsExtendedSourceObject {
-  src: string;
-  type?: string;
-  size?: string;
-  selected?: boolean;
-}
-
-interface VideoJsHttpSourceSelectorPluginOptions {
-  default: 'low' | 'high' | 'auto';
 }
 
 interface VideoJsQualityLevelsPluginRepresentation {
