36716cf027832722f585f4526d6802cef95778fb
Nicolas Clerc <kernicpanel@nclerc.fr>
2024-08-22 17:21:31 +0200

    ✨(backend) add transcript task
    Adds a celery task to launch a peertube transcription.

--- src/backend/marsha/core/tasks/video.py
+++ src/backend/marsha/core/tasks/video.py
@@ -3,6 +3,7 @@
 import logging
 
 from django_peertube_runner_connector.transcode import transcode_video
+from django_peertube_runner_connector.transcript import transcript_video
 from sentry_sdk import capture_exception
 
 from marsha.celery_app import app
@@ -15,11 +16,12 @@ logger = logging.getLogger(__name__)
 
 @app.task
 def launch_video_transcoding(video_pk: str, stamp: str, domain: str):
-    """Resize a thumbnail using video_storage.
+    """Transcodes a video using video_storage.
     Args:
         video_pk (UUID): The video to transcode.
         stamp (str): The stamp at which the thumbnail was uploaded
         which will be used to find the key.
+        domain (str): The domain name used to construct the download URL for peerTube runners.
     """
     video = Video.objects.get(pk=video_pk)
     try:
@@ -36,4 +38,22 @@ def launch_video_transcoding(video_pk: str, stamp: str, domain: str):
     except Exception as exception:  # pylint: disable=broad-except+
         capture_exception(exception)
         video.update_upload_state(ERROR, None)
+        logger.exception(exception)
+
+
+@app.task
+def launch_video_transcript(video_pk: str, stamp: str, domain: str):
+    """Transcripts a video using video_storage.
+    Args:
+        video_pk (UUID): The video to transcript.
+        stamp (str): The stamp at which the thumbnail was uploaded
+        which will be used to find the key.
+        domain (str): The domain name used to construct the download URL for peerTube runners.
+    """
+    video = Video.objects.get(pk=video_pk)
+    try:
+        prefix_destination = video.get_videos_storage_prefix(stamp)
+        transcript_video(destination=prefix_destination, domain=domain)
+    except Exception as exception:  # pylint: disable=broad-except+
+        capture_exception(exception)
         logger.exception(exception)
