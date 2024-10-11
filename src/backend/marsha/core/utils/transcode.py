d27457e518d9308791e63baaa8a033c7dc4c7924
Nicolas Clerc <kernicpanel@nclerc.fr>
2024-10-11 14:04:42 +0200

    ✨(back) cleanup transcoding temp files
    Once a video transcoded we want to delete the first transcoded video
    that is using a temporary file. Also a management command cleaning old
    temporary transcoded video is added

--- src/backend/marsha/core/utils/transcode.py
+++ src/backend/marsha/core/utils/transcode.py
@@ -1,15 +1,22 @@
 """ Utils related to transcoding """
 
 from django_peertube_runner_connector.models import Video as TranscodedVideo, VideoState
+from django_peertube_runner_connector.utils.files import delete_temp_file
 
-from marsha.core.defaults import ERROR, PEERTUBE_PIPELINE, READY
+from marsha.core.defaults import (
+    ERROR,
+    PEERTUBE_PIPELINE,
+    READY,
+    TMP_VIDEOS_STORAGE_BASE_DIRECTORY,
+    VOD_VIDEOS_STORAGE_BASE_DIRECTORY,
+)
 from marsha.core.models.video import Video
 from marsha.core.utils.time_utils import to_datetime
 
 
 def transcoding_ended_callback(transcoded_video: TranscodedVideo):
     """
-    Callback used when the a Peertube runnner has finished
+    Callback used when a Peertube runner has finished
     to transcode a video.
 
     Parameters
@@ -23,6 +30,11 @@ def transcoding_ended_callback(transcoded_video: TranscodedVideo):
     uploaded_on = directory[-1]
     video_id = directory[-3]
     video = Video.objects.get(pk=video_id)
+    tmp_filename = transcoded_video.directory.replace(
+        VOD_VIDEOS_STORAGE_BASE_DIRECTORY, TMP_VIDEOS_STORAGE_BASE_DIRECTORY
+    )
+
+    delete_temp_file(transcoded_video, tmp_filename)
 
     if transcoded_video.state == VideoState.TRANSCODING_FAILED:
         video.update_upload_state(ERROR, None)
@@ -39,3 +51,12 @@ def transcoding_ended_callback(transcoded_video: TranscodedVideo):
         to_datetime(uploaded_on),
         **{"resolutions": resolutions},
     )
+
+
+def delete_transcoding_temp_files():
+    """Delete all transcoding temp files."""
+    for transcoded_video in TranscodedVideo.objects.all():
+        tmp_filename = transcoded_video.directory.replace(
+            VOD_VIDEOS_STORAGE_BASE_DIRECTORY, TMP_VIDEOS_STORAGE_BASE_DIRECTORY
+        )
+        delete_temp_file(transcoded_video, tmp_filename)
