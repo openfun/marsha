a94e37d0a695ca665a802ec75c7b14cfee48442e
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2025-05-16 15:35:30 +0200

    🔧(docker) fix prosody table creation on startup
    Starting with PostreSQL 15, the CREATE privilege on public schema is revoked
    from all users except the database owner.
    Adding a connection to the Prosody database at container startup to manually
    grant all privileges on the `public` schema, ensuring Prosody can create its
    tables properly.

--- docker/files/docker-entrypoint-initdb.d/init_user_db.sh
+++ docker/files/docker-entrypoint-initdb.d/init_user_db.sh
@@ -6,3 +6,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE prosody;
     GRANT ALL PRIVILEGES ON DATABASE prosody TO prosody;
 EOSQL
+
+# Connect to the prosody database to grant all privileges on schema public
+# Required since PostgreSQL > 15
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "prosody" <<-EOSQL
+  GRANT ALL ON SCHEMA public TO prosody;
+EOSQL
