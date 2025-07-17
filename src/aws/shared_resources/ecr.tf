5053f7dcf1c555bad9dbf2abb7d315852d5d0a02
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2025-07-17 10:11:38 +0200

    🔥(terraform) remove unused AWS provisioning
    As everything have now been migrated from AWS to Scaleway, removing all
    AWS provisioning and configuration.

--- src/aws/shared_resources/ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "marsha_lambda" {
-  name                 = "marsha/lambda"
-  image_tag_mutability = "MUTABLE"
-}
--- src/aws/shared_resources/output.tf
+++
@@ -1,7 +0,0 @@
-output "ecr_lambda_arn" {
-  value = aws_ecr_repository.marsha_lambda.arn
-}
-
-output "ecr_lambda_url" {
-  value = aws_ecr_repository.marsha_lambda.repository_url
-}
